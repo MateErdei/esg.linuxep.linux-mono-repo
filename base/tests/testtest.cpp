582a9e0e1d394e656ab348b381a7a81d3ee26630
Douglas Leeder <douglas.leeder@sophos.com>
2018-06-06 15:13:38 +0100

    [base] Add a test test to verify we can use cmake and googletest to run a test.

---
+++ base/tests/testtest.cpp
@@ -0,0 +1,15 @@
+//
+// Created by pair on 06/06/18.
+//
+
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+namespace
+{
+    TEST(TestTest, TestZeroEqualsZero) // NOLINT
+    {
+        ASSERT_EQ(0,0);
+    }
+}
