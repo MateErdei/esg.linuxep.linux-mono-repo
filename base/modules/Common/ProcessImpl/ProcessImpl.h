1b473535b93271a26c79846c9dcb6dacd2dd6e9a
Gesner Passos <gesner.passos@sophos.com>
2018-06-13 11:02:27 +0100

    [base] Added Sophos Copyright to files created for process [+TM]

--- base/modules/Common/ProcessImpl/ProcessImpl.h
+++ base/modules/Common/ProcessImpl/ProcessImpl.h
@@ -1,6 +1,8 @@
-//
-// Created by pair on 12/06/18.
-//
+/******************************************************************************************************
+
+Copyright 2018, Sophos Limited.  All rights reserved.
+
+******************************************************************************************************/
 
 #ifndef EVEREST_BASE_COMMON_PROCESS_IPROCESSIMPL_H
 #define EVEREST_BASE_COMMON_PROCESS_IPROCESSIMPL_H
