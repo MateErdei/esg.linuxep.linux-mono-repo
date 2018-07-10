dcbc8a6a443e84b17f15fa890a26dc8c0809a0f1
Gesner Passos <gesner.passos@sophos.com>
2018-07-10 15:38:45 +0100

    [base] [LINUXEP-5847] Move timeout under mainly for tests.

--- base/modules/Common/PluginApiImpl/PluginResourceManagement.h
+++ base/modules/Common/PluginApiImpl/PluginResourceManagement.h
@@ -19,14 +19,14 @@ namespace Common
             PluginResourceManagement();
             PluginResourceManagement(Common::ZeroMQWrapper::IContext*);
 
-            void setDefaultTimeout(int timeoutMs) ;
-            void setDefaultConnectTimeout(int timeoutMs) ;
             std::unique_ptr<Common::PluginApi::IBaseServiceApi> createPluginAPI( const std::string & pluginName, std::shared_ptr<Common::PluginApi::IPluginCallbackApi> pluginCallback)  override ;
             std::unique_ptr<Common::PluginApi::ISensorDataPublisher> createSensorDataPublisher(const std::string & pluginName) override ;
             std::unique_ptr<Common::PluginApi::ISensorDataSubscriber> createSensorDataSubscriber(const std::string & sensorDataCategorySubscription,
                                                                                                  std::shared_ptr<Common::PluginApi::ISensorDataCallback> sensorDataCallback) override ;
 
             /* mainly for tests */
+            void setDefaultTimeout(int timeoutMs) ;
+            void setDefaultConnectTimeout(int timeoutMs) ;
             Common::ZeroMQWrapper::IContext & getSocketContext();
         private:
             void setTimeouts( Common::ZeroMQWrapper::ISocketSetup & socket);
