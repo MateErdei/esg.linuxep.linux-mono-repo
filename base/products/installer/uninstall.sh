065ac7a611f7c7038c375c461ac608fe91ea7dff
Douglas Leeder <douglas.leeder@sophos.com>
2018-07-16 10:29:47 +0100

    [base] [LINUXEP-6066] Create a low-privilege user ("sophos-spl-user")

--- base/products/installer/uninstall.sh
+++ base/products/installer/uninstall.sh
@@ -10,4 +10,4 @@ fi
 
 rm -rf "$SCRIPTDIR"
 [[ -n $NO_REMOVE_GROUP ]] || delgroup sophos-spl-group 2>/dev/null >/dev/null
-
+[[ -n $NO_REMOVE_USER ]] || deluser sophos-spl-user 2>/dev/null >/dev/null
