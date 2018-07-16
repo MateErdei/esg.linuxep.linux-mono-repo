b827d601c4a462e11d01205f15f43d160d1868dd
Douglas Leeder <douglas.leeder@sophos.com>
2018-07-16 13:52:33 +0100

    [base] [LINUXEP-6020] Switch to error codes defined at the top of the installer.
    Not exported to environment so won't blow the environment limit.
    
    [+AC]

--- base/products/installer/install.sh
+++ base/products/installer/install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+EXIT_FAIL_CREATE_DIRECTORY=10
+EXIT_FAIL_FIND_GROUPADD=11
+EXIT_FAIL_ADD_GROUP=12
+EXIT_FAIL_FIND_USERADD=13
+EXIT_FAIL_ADDUSER=14
+EXIT_FAIL_VERSIONEDCOPY=20
+
 STARTINGDIR=$(pwd)
 SCRIPTDIR=${0%/*}
 if [[ "$SCRIPTDIR" == "$0" ]]
@@ -27,21 +34,22 @@ export SOPHOS_INSTALL
 GROUP_NAME=sophos-spl-group
 GROUPADD="$(which groupadd)"
 [[ -x "${GROUPADD}" ]] || GROUPADD=/usr/sbin/groupadd
-[[ -x "${GROUPADD}" ]] || failure 11 "Failed to find groupadd to add low-privilege group"
-/usr/bin/getent group "${GROUP_NAME}" 2>&1 > /dev/null || "${GROUPADD}" -r "${GROUP_NAME}" || failure 12 "Failed to add group $GROUP_NAME"
+[[ -x "${GROUPADD}" ]] || failure ${EXIT_FAIL_FIND_GROUPADD} "Failed to find groupadd to add low-privilege group"
+/usr/bin/getent group "${GROUP_NAME}" 2>&1 > /dev/null || "${GROUPADD}" -r "${GROUP_NAME}" || failure ${EXIT_FAIL_ADD_GROUP} "Failed to add group $GROUP_NAME"
 
-mkdir -p $SOPHOS_INSTALL || failure 10 "Failed to create installation directory: $SOPHOS_INSTALL"
+mkdir -p $SOPHOS_INSTALL || failure ${EXIT_FAIL_CREATE_DIRECTORY} "Failed to create installation directory: $SOPHOS_INSTALL"
 chmod 711 "$SOPHOS_INSTALL"
 chown root:${GROUP_NAME} "$SOPHOS_INSTALL"
 
 USER_NAME=sophos-spl-user
 USERADD="$(which useradd)"
 [[ -x "${USERADD}" ]] || USERADD=/usr/sbin/useradd
-[[ -x "${USERADD}" ]] || failure 13 "Failed to find useradd to add low-privilege user"
-/usr/bin/getent shadow "${USER_NAME}" || "${USERADD}" -d "${SOPHOS_INSTALL}" -g "${GROUP_NAME}" -M -N -r -s /bin/false "${USER_NAME}" || failure 14 "Failed to add user $USER_NAME"
+[[ -x "${USERADD}" ]] || failure ${EXIT_FAIL_FIND_USERADD} "Failed to find useradd to add low-privilege user"
+/usr/bin/getent shadow "${USER_NAME}" || "${USERADD}" -d "${SOPHOS_INSTALL}" -g "${GROUP_NAME}" -M -N -r -s /bin/false "${USER_NAME}" \
+    || failure ${EXIT_FAIL_ADDUSER} "Failed to add user $USER_NAME"
 
 for F in $(find $DIST/files -type f)
 do
-    $DIST/installer/versionedcopy $F
+    $DIST/installer/versionedcopy $F || failure ${EXIT_FAIL_VERSIONEDCOPY} "Failed to copy $F to installation"
 done
 chmod 700 "$SOPHOS_INSTALL/uninstall.sh"
