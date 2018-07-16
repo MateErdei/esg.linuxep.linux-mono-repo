6f8824e5dfe0be0e94b88707a4692ff00a038251
Douglas Leeder <douglas.leeder@sophos.com>
2018-07-16 13:59:42 +0100

    [base] [LINUXEP-6020] Search got getent properly.
    [+AC]

--- base/products/installer/install.sh
+++ base/products/installer/install.sh
@@ -5,6 +5,7 @@ EXIT_FAIL_FIND_GROUPADD=11
 EXIT_FAIL_ADD_GROUP=12
 EXIT_FAIL_FIND_USERADD=13
 EXIT_FAIL_ADDUSER=14
+EXIT_FAIL_FIND_GETENT=15
 EXIT_FAIL_VERSIONEDCOPY=20
 
 STARTINGDIR=$(pwd)
@@ -32,10 +33,15 @@ export SOPHOS_INSTALL
 
 ## Add a low-privilege group
 GROUP_NAME=sophos-spl-group
+
+GETENT=/usr/bin/getent
+[[ -x "${GETENT}" ]] || GETENT=$(which getent)
+[[ -x "${GETENT}" ]] || failure ${EXIT_FAIL_FIND_GETENT} "Failed to find getent"
+
 GROUPADD="$(which groupadd)"
 [[ -x "${GROUPADD}" ]] || GROUPADD=/usr/sbin/groupadd
 [[ -x "${GROUPADD}" ]] || failure ${EXIT_FAIL_FIND_GROUPADD} "Failed to find groupadd to add low-privilege group"
-/usr/bin/getent group "${GROUP_NAME}" 2>&1 > /dev/null || "${GROUPADD}" -r "${GROUP_NAME}" || failure ${EXIT_FAIL_ADD_GROUP} "Failed to add group $GROUP_NAME"
+"${GETENT}" group "${GROUP_NAME}" 2>&1 > /dev/null || "${GROUPADD}" -r "${GROUP_NAME}" || failure ${EXIT_FAIL_ADD_GROUP} "Failed to add group $GROUP_NAME"
 
 mkdir -p $SOPHOS_INSTALL || failure ${EXIT_FAIL_CREATE_DIRECTORY} "Failed to create installation directory: $SOPHOS_INSTALL"
 chmod 711 "$SOPHOS_INSTALL"
@@ -45,7 +51,7 @@ USER_NAME=sophos-spl-user
 USERADD="$(which useradd)"
 [[ -x "${USERADD}" ]] || USERADD=/usr/sbin/useradd
 [[ -x "${USERADD}" ]] || failure ${EXIT_FAIL_FIND_USERADD} "Failed to find useradd to add low-privilege user"
-/usr/bin/getent shadow "${USER_NAME}" || "${USERADD}" -d "${SOPHOS_INSTALL}" -g "${GROUP_NAME}" -M -N -r -s /bin/false "${USER_NAME}" \
+"${GETENT}" shadow "${USER_NAME}" || "${USERADD}" -d "${SOPHOS_INSTALL}" -g "${GROUP_NAME}" -M -N -r -s /bin/false "${USER_NAME}" \
     || failure ${EXIT_FAIL_ADDUSER} "Failed to add user $USER_NAME"
 
 for F in $(find $DIST/files -type f)
