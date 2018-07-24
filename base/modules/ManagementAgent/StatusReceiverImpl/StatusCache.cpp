2310cd1fb4156af5a1b99388bc2fca6ec93875da
Douglas Leeder <douglas.leeder@sophos.com>
2018-07-24 10:30:25 +0100

    [base] [LINUXEP-5987] Add using namespace line, to reduce length of method declarations.
    [+TM]

--- base/modules/ManagementAgent/StatusReceiverImpl/StatusCache.cpp
+++ base/modules/ManagementAgent/StatusReceiverImpl/StatusCache.cpp
@@ -6,8 +6,10 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 
 #include "StatusCache.h"
 
-bool ManagementAgent::StatusReceiverImpl::StatusCache::statusChanged(const std::string& appid,
-                                                                          const std::string& statusForComparison)
+using namespace ManagementAgent::StatusReceiverImpl;
+
+bool StatusCache::statusChanged(const std::string& appid,
+                                const std::string& statusForComparison)
 {
     auto search = m_statusCache.find(appid);
     if (search == m_statusCache.end())
@@ -21,8 +23,8 @@ bool ManagementAgent::StatusReceiverImpl::StatusCache::statusChanged(const std::
     return updateStatus(appid, statusForComparison);
 }
 
-bool ManagementAgent::StatusReceiverImpl::StatusCache::updateStatus(const std::string& appid,
-                                                                         const std::string& statusForComparison)
+bool StatusCache::updateStatus(const std::string& appid,
+                               const std::string& statusForComparison)
 {
     m_statusCache[appid] = statusForComparison;
     return true;
