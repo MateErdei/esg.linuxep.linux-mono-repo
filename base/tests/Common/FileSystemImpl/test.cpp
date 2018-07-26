06d3d263a14741f9b91cdbd36d678d6b52563d22
RichardLycett <richard.lycett@sophos.com>
2018-07-26 15:07:38 +0100

    [base] Don't need filesystemimpltests as a library. Remove old useless test.cpp file. [AC] [GP]

--- base/tests/Common/FileSystemImpl/test.cpp
+++
@@ -1,9 +0,0 @@
-/******************************************************************************************************
-
-Copyright 2018, Sophos Limited.  All rights reserved.
-
-******************************************************************************************************/
-
-
-//File used to make CMake happy when Lib only contains .h file.
-// Can delete when the lib contains a cpp file.  Should not be an issue as we only use for tests.
