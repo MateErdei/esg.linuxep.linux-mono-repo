6a807c7edfc1329e02986b936a585af86c2f39c2
Ethan Vince-Urwin <ethan.vince-urwin@sophos.com>
2018-07-26 16:40:34 +0100

    [base] [LINUXEP-5927] -Added test to provide management agent can be initialised without throwing.

--- base/tests/ManagementAgentImpl/ManagementAgentImplTests.cpp
+++ base/tests/ManagementAgentImpl/ManagementAgentImplTests.cpp
@@ -11,20 +11,21 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 ******************************************************************************************************/
 
 
+#include <ManagementAgent/ManagementAgentImpl/ManagementAgentMain.h>
 #include <ManagementAgent/McsRouterPluginCommunicationImpl/PolicyTask.h>
 #include <ManagementAgent/PolicyReceiverImpl/PolicyReceiverImpl.h>
 
 #include <Common/FileSystemImpl/FileSystemImpl.h>
 #include <Common/TaskQueueImpl/TaskQueueImpl.h>
 
+#include <tests/ManagementAgent/McsRouterPluginCommunicationImpl/MockPluginManager.h>
 #include <tests/Common/TaskQueueImpl/FakeQueue.h>
 #include <tests/Common/FileSystemImpl/MockFileSystem.h>
-#include <tests/ManagementAgent/McsRouterPluginCommunicationImpl/MockPluginManager.h>
+#include <tests/Common/ApplicationConfiguration/MockedApplicationPathManager.h>
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <gmock/gmock-matchers.h>
-#include <modules/ManagementAgent/ManagementAgentImpl/ManagementAgentMain.h>
 
 namespace
 {
@@ -36,6 +37,11 @@ namespace
         {
             initialise(pluginManager);
         }
+
+        void runWrapper()
+        {
+            run();
+        }
     };
 
     class ManagementAgentImplTests : public ::testing::Test
@@ -44,6 +50,14 @@ namespace
 
         void SetUp() override
         {
+            const std::string frontend="inproc://frontend";
+            const std::string backend="inproc://backend";
+
+            m_mockApplicationManager = new NiceMock<MockedApplicationPathManager>();
+            MockedApplicationPathManager &mockPathManager(*m_mockApplicationManager);
+            ON_CALL(mockPathManager, getManagementAgentSocketAddress()).WillByDefault(Return(frontend));
+            Common::ApplicationConfiguration::replaceApplicationPathManager(
+                    std::unique_ptr<Common::ApplicationConfiguration::IApplicationPathManager>(m_mockApplicationManager));
 
         }
 
@@ -52,7 +66,33 @@ namespace
 
         }
 
+        std::string createJsonString()
+        {
+            std::string jsonString = R"({
+                                    "policyAppIds": [
+                                    "app1"
+                                     ],
+                                     "statusAppIds": [
+                                      "app2"
+                                     ],
+                                     "pluginName": "PluginName",
+                                     "xmlTranslatorPath": "path1",
+                                     "executableFullPath": "path2",
+                                     "executableArguments": [
+                                      "arg1"
+                                     ],
+                                     "environmentVariables": [
+                                      {
+                                       "name": "hello",
+                                       "value": "world"
+                                      }
+                                     ]
+                                    })";
+            return jsonString;
+        }
+
         StrictMock<MockPluginManager> m_mockPluginManager;
+        NiceMock<MockedApplicationPathManager>* m_mockApplicationManager;
     };
 
     TEST_F(ManagementAgentImplTests, ManagementAgentMainConstructorWithValidDataDoesNotThrow) // NOLINT
@@ -60,12 +100,29 @@ namespace
         EXPECT_NO_THROW(ManagementAgent::ManagementAgentImpl::ManagementAgentMain managementAgent());
     }
 
-    TEST_F(ManagementAgentImplTests, initialiseWillNotThrow)
+    TEST_F(ManagementAgentImplTests, initialiseWillSetupManagementAgentAndNotThrow) // NOLINT
     {
+        auto filesystemMock = new NiceMock<MockFileSystem>();
+        Common::FileSystem::replaceFileSystem(std::unique_ptr<Common::FileSystem::IFileSystem>(filesystemMock));
 
-        //TestManagementAgent managementAgent;
+        std::vector<std::string> pluginFiles = {"plugin.json"};
 
-        //EXPECT_NO_THROW(managementAgent.initialiseWrapper(m_mockPluginManager));
+        std::string jsonContent = createJsonString();
+
+        EXPECT_CALL(*filesystemMock, listFiles(_)).WillOnce(Return(pluginFiles));
+        EXPECT_CALL(*filesystemMock, readFile(_)).WillOnce(Return(jsonContent));
+        EXPECT_CALL(*m_mockApplicationManager, getMcsPolicyFilePath()).WillRepeatedly(Return("/tmp"));
+        EXPECT_CALL(*m_mockApplicationManager, getMcsActionFilePath()).WillRepeatedly(Return("/tmp"));
+
+        EXPECT_CALL(m_mockPluginManager, registerPlugin("PluginName")).Times(1);
+        EXPECT_CALL(m_mockPluginManager, setPolicyReceiver(_)).Times(1);
+        EXPECT_CALL(m_mockPluginManager, setStatusReceiver(_)).Times(1);
+        EXPECT_CALL(m_mockPluginManager, setEventReceiver(_)).Times(1);
+
+        TestManagementAgent managementAgent;
+
+        EXPECT_NO_THROW(managementAgent.initialiseWrapper(m_mockPluginManager));
+
     }
 }
 
