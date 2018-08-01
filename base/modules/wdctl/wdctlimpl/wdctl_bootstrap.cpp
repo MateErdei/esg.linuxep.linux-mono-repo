2a7d2c93e44055d1da71a50f21319716f9ffe7a5
Douglas Leeder <douglas.leeder@sophos.com>
2018-08-01 16:49:24 +0100

    [base] [LINUXEP-6321] Initialise logging in wdctl.

--- base/modules/wdctl/wdctlimpl/wdctl_bootstrap.cpp
+++ base/modules/wdctl/wdctlimpl/wdctl_bootstrap.cpp
@@ -6,6 +6,10 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 #include "wdctl_bootstrap.h"
 #include "Logger.h"
 
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
+#include <log4cplus/configurator.h>
+
 using namespace wdctl::wdctlimpl;
 
 int wdctl_bootstrap::main(int argc, char **argv)
@@ -27,7 +31,15 @@ StringVector wdctl_bootstrap::convertArgv(unsigned int argc, char **argv)
 
 int wdctl_bootstrap::main(const StringVector& args)
 {
+    log4cplus::initialize();
+
+    log4cplus::BasicConfigurator config;
+    config.configure();
+
     m_args.parseArguments(args);
     LOGINFO(m_args.m_command<<" "<<m_args.m_argument);
+
+    log4cplus::Logger::shutdown();
+
     return 0;
 }
