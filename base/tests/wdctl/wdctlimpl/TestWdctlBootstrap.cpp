512c35d06cab4ab5b19ec5e96fbcb0caaa26cee1
Douglas Leeder <douglas.leeder@sophos.com>
2018-08-01 12:02:32 +0100

    [base] [LINUXEP-5919] Test converting arguments works.

---
+++ base/tests/wdctl/wdctlimpl/TestWdctlBootstrap.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************************************
+
+Copyright 2018, Sophos Limited.  All rights reserved.
+
+******************************************************************************************************/
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <modules/wdctl/wdctlimpl/wdctl_bootstrap.h>
+
+namespace
+{
+    class StringHolder
+    {
+    public:
+        explicit StringHolder(const char *orig)
+        {
+            m_value = strdup(orig);
+        }
+
+        ~StringHolder()
+        {
+            free(m_value);
+        }
+
+        char *get()
+        {
+            return m_value;
+        }
+
+        char *m_value;
+    };
+}
+
+TEST(TestWdctlBootstrap, ConvertArgs) //NOLINT
+{
+    StringHolder name(strdup("wdctl"));
+    ASSERT_NE(name.get(),nullptr);
+    char *argv[] = {name.get(), nullptr};
+    int argc = sizeof(argv) / sizeof(char*) - 1;
+
+    auto res = wdctl::wdctlimpl::wdctl_bootstrap::convertArgv(argc, argv);
+    ASSERT_EQ(res.size(),1);
+    EXPECT_EQ(res.at(0),"wdctl");
+}
