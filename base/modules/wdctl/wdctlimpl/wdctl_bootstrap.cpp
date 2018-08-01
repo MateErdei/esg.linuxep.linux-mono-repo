913bf3c74cf7796be01b8ac01e1cdd109de34329
Douglas Leeder <douglas.leeder@sophos.com>
2018-08-01 11:26:26 +0100

    [base] [LINUXEP-5919] Process command-line arguments into a vector.

--- base/modules/wdctl/wdctlimpl/wdctl_bootstrap.cpp
+++ base/modules/wdctl/wdctlimpl/wdctl_bootstrap.cpp
@@ -9,7 +9,23 @@ using namespace wdctl::wdctlimpl;
 
 int wdctl_bootstrap::main(int argc, char **argv)
 {
-    static_cast<void>(argc);
-    static_cast<void>(argv);
+    StringVector args = convertArgv(static_cast<unsigned int>(argc), argv);
+    return main(args);
+}
+
+wdctl_bootstrap::StringVector wdctl_bootstrap::convertArgv(unsigned int argc, char **argv)
+{
+    StringVector result;
+    result.reserve(argc);
+    for (unsigned int i=0; i<argc; ++i)
+    {
+        result.emplace_back(argv[i]);
+    }
+    return result;
+}
+
+int wdctl_bootstrap::main(const wdctl_bootstrap::StringVector& args)
+{
+    static_cast<void>(args);
     return 0;
 }
--- base/modules/wdctl/wdctlimpl/wdctl_bootstrap.h
+++ base/modules/wdctl/wdctlimpl/wdctl_bootstrap.h
@@ -5,6 +5,9 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 ******************************************************************************************************/
 #pragma once
 
+#include <string>
+#include <vector>
+
 namespace wdctl
 {
     namespace wdctlimpl
@@ -12,7 +15,10 @@ namespace wdctl
         class wdctl_bootstrap
         {
         public:
+            using StringVector = std::vector<std::string>;
             int main(int argc, char* argv[]);
+            int main(const StringVector& args);
+            static StringVector convertArgv(unsigned int argc, char* argv[]);
         };
     }
 }
