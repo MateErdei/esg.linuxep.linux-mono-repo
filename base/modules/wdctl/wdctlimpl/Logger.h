da821bce4da4df3afc2faf929f9b0cc220b90923
Douglas Leeder <douglas.leeder@sophos.com>
2018-08-02 09:41:36 +0100

    [base] [LINUXEP-6321] Switch to Log4Cplus logging.

--- base/modules/wdctl/wdctlimpl/Logger.h
+++ base/modules/wdctl/wdctlimpl/Logger.h
@@ -6,15 +6,13 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 
 #pragma once
 
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
 
-#include <iostream>
+extern log4cplus::Logger GL_WDCTL_LOGGER;
 
-//TODO: To incorporate the log facility after LINUXEP-5909 is completed.
-
-#define LOGDEBUG(x) std::cout << x << std::endl // NOLINT
-#define LOGINFO(x) std::cout << x << std::endl // NOLINT
-#define LOGSUPPORT(x) std::cout << x << std::endl // NOLINT
-#define LOGWARN(x) std::cerr << x << std::endl // NOLINT
-#define LOGERROR(x) std::cerr << x << std::endl // NOLINT
-
-
+#define LOGDEBUG(x) LOG4CPLUS_DEBUG(GL_WDCTL_LOGGER, x) // NOLINT
+#define LOGINFO(x) LOG4CPLUS_INFO(GL_WDCTL_LOGGER, x) // NOLINT
+#define LOGSUPPORT(x) LOG4CPLUS_INFO(GL_WDCTL_LOGGER, x) // NOLINT
+#define LOGWARN(x) LOG4CPLUS_WARN(GL_WDCTL_LOGGER, x) // NOLINT
+#define LOGERROR(x) LOG4CPLUG_ERROR(GL_WDCTL_LOGGER, x) // NOLINT
--- base/modules/wdctl/wdctlimpl/wdctl_bootstrap.cpp
+++ base/modules/wdctl/wdctlimpl/wdctl_bootstrap.cpp
@@ -7,10 +7,21 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 #include "Logger.h"
 
 #include <log4cplus/logger.h>
-#include <log4cplus/loggingmacros.h>
 #include <log4cplus/configurator.h>
 
+#include <cstdlib>
+#include <csignal>
+
+#include <unistd.h>
+#include <sys/select.h>
+
+#ifndef PATH_MAX
+# define PATH_MAX 2048
+#endif
+
 using namespace wdctl::wdctlimpl;
+
+log4cplus::Logger GL_WDCTL_LOGGER; //NOLINT
 
 int wdctl_bootstrap::main(int argc, char **argv)
 {
@@ -28,15 +39,43 @@ StringVector wdctl_bootstrap::convertArgv(unsigned int argc, char **argv)
     }
     return result;
 }
+namespace
+{
+    std::string work_out_install_directory()
+    {
+        // Check if we have an environment variable telling us the installation location
+        char* SOPHOS_INSTALL = secure_getenv("SOPHOS_INSTALL");
+        if (SOPHOS_INSTALL != nullptr)
+        {
+            return SOPHOS_INSTALL;
+        }
+        // If we don't have environment variable, assume we were started in the installation directory
+        char path[PATH_MAX];
+
+//        ssize_t ret = ::readlink("/proc/self/exe", path, PATH_MAX);
+
+        char* cwd = getcwd(path,PATH_MAX);
+        if (cwd != nullptr)
+        {
+            return cwd;
+        }
+        // If we can't get the cwd then use a fixed string.
+        return "/opt/sophos-spl";
+    }
+}
 
 int wdctl_bootstrap::main(const StringVector& args)
 {
     log4cplus::initialize();
 
+
+
     log4cplus::BasicConfigurator config;
     config.configure();
 
     m_args.parseArguments(args);
+
+    GL_WDCTL_LOGGER = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("wdctl"));
     LOGINFO(m_args.m_command<<" "<<m_args.m_argument);
 
     log4cplus::Logger::shutdown();
