c23546100cf20f5dcc718fdf69975ed09d6d7548
Douglas Leeder <douglas.leeder@sophos.com>
2018-08-03 16:19:53 +0100

    [base] [LINUXEP-5920][LINUXEP-6320] Send stop action to watchdog.

--- base/modules/wdctl/wdctlactions/StopAction.cpp
+++ base/modules/wdctl/wdctlactions/StopAction.cpp
@@ -6,6 +6,7 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 #include "StopAction.h"
 #include "Logger.h"
 
+#include <Common/ApplicationConfiguration/IApplicationPathManager.h>
 #include <Common/ZeroMQWrapper/IContext.h>
 #include <Common/ZeroMQWrapper/ISocketRequester.h>
 #include <Common/ZeroMQWrapper/ISocketRequesterPtr.h>
@@ -19,9 +20,10 @@ StopAction::StopAction(const wdctl::wdctlimpl::Arguments& args)
 
 int StopAction::run()
 {
-    Common::ZeroMQWrapper::ISocketRequesterPtr socket = m_context->getRequester();
-
     LOGINFO("Attempting to stop "<<m_args.m_argument);
+
+    Common::ZeroMQWrapper::ISocketRequesterPtr socket = m_context->getRequester();
+    socket->connect(Common::ApplicationConfiguration::applicationPathManager().getWatchdogSocketAddress());
 
     socket->write({"STOP",m_args.m_argument});
 
