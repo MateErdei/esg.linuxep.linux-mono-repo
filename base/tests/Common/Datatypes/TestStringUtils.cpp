e4e5748d27c32a8a9e21f316e595bad4aaa373d8
Douglas Leeder <douglas.leeder@sophos.com>
2018-08-07 15:46:57 +0100

    [base] [LINUXEP-5920][LINUXEP-6320] Shorten lines with a using statement.
    [+TM]

--- base/tests/Common/Datatypes/TestStringUtils.cpp
+++ base/tests/Common/Datatypes/TestStringUtils.cpp
@@ -8,16 +8,18 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 
 #include "gtest/gtest.h"
 
+using namespace Common::Datatypes;
+
 TEST(TestStringUtils, endswith) // NOLINT
 {
-    EXPECT_TRUE(Common::Datatypes::StringUtils::endswith("FOOBAR","BAR"));
-    EXPECT_TRUE(Common::Datatypes::StringUtils::endswith("BAR","BAR"));
-    EXPECT_FALSE(Common::Datatypes::StringUtils::endswith("FOOBAS","BAR"));
-    EXPECT_FALSE(Common::Datatypes::StringUtils::endswith("","BAR"));
-    EXPECT_FALSE(Common::Datatypes::StringUtils::endswith("F","BAR"));
-    EXPECT_TRUE(Common::Datatypes::StringUtils::endswith("FOOBAR",""));
-    EXPECT_TRUE(Common::Datatypes::StringUtils::endswith("BARFOOBAR","BAR"));
-    EXPECT_TRUE(Common::Datatypes::StringUtils::endswith("stuff.json",".json"));
-    EXPECT_TRUE(Common::Datatypes::StringUtils::endswith("stuff.json.json",".json"));
-    EXPECT_FALSE(Common::Datatypes::StringUtils::endswith("stuff.json.other",".json"));
+    EXPECT_TRUE(StringUtils::endswith("FOOBAR","BAR"));
+    EXPECT_TRUE(StringUtils::endswith("BAR","BAR"));
+    EXPECT_FALSE(StringUtils::endswith("FOOBAS","BAR"));
+    EXPECT_FALSE(StringUtils::endswith("","BAR"));
+    EXPECT_FALSE(StringUtils::endswith("F","BAR"));
+    EXPECT_TRUE(StringUtils::endswith("FOOBAR",""));
+    EXPECT_TRUE(StringUtils::endswith("BARFOOBAR","BAR"));
+    EXPECT_TRUE(StringUtils::endswith("stuff.json",".json"));
+    EXPECT_TRUE(StringUtils::endswith("stuff.json.json",".json"));
+    EXPECT_FALSE(StringUtils::endswith("stuff.json.other",".json"));
 }
