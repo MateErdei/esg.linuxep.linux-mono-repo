008085f2fc157561246e1bb5d7ee293e64eba4c0
Douglas Leeder <douglas.leeder@sophos.com>
2018-08-24 10:30:26 +0100

    [base] [LINUXEP-6511] Add main() wrapper, to catch exceptions.
    Use in UpdateScheduler to fix CID 31700 Uncaught exception
    
    If the exception is ever thrown, the program will crash.
    
    In main: A C++ exception is thrown but never caught (CWE-248)

--- base/products/UpdateScheduler/main.cpp
+++ base/products/UpdateScheduler/main.cpp
@@ -5,7 +5,11 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 ******************************************************************************************************/
 #include "UpdateSchedulerBootstrap.h"
 
-int main()
+#include <Common/UtilityImpl/Main.h>
+
+static int update_scheduler_main()
 {
     return UpdateSchedulerImpl::main_entry();
 }
+
+MAIN(update_scheduler_main());
