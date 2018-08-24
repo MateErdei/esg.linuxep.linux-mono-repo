4a78d5a35610878b197440bb93ea046dd3630930
Douglas Leeder <douglas.leeder@sophos.com>
2018-08-24 11:09:54 +0100

    [base] [LINUXEP-6511] Catch exceptions in main for management_agent
    Fixes:
    31505 Uncaught exception
    
    If the exception is ever thrown, the program will crash.
    
    In main: A C++ exception is thrown but never caught (CWE-248)

--- base/products/ManagementAgent/main.cpp
+++ base/products/ManagementAgent/main.cpp
@@ -6,7 +6,12 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 
 #include <ManagementAgent/ManagementAgentImpl/ManagementAgentMain.h>
 
-int main(int argc, char* argv[])
+#include <Common/UtilityImpl/Main.h>
+
+
+static int management_agent_main(int argc, char* argv[])
 {
     return ManagementAgent::ManagementAgentImpl::ManagementAgentMain::main(argc,argv);
 }
+
+MAIN(management_agent_main(argc,argv))
