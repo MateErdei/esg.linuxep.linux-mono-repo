b68cb343157cd0d2d8b760736a03c13f762d8778
Douglas Leeder <douglas.leeder@sophos.com>
2018-08-30 15:53:46 +0100

    [base] [LINUXEP-6507] Use hard-links rather than sym-links because we try to copy the build to filer6.

--- base/products/distribution/copy_libs.py
+++ base/products/distribution/copy_libs.py
@@ -5,6 +5,7 @@ import sys
 import shutil
 import filecmp
 import re
+import errno
 
 DIGIT_EXT = re.compile(r"^(.*?)\.\d+$")
 
@@ -18,6 +19,20 @@ def safe_symlink(source, target):
         print(e)
 
 
+def safe_delete(p):
+    try:
+        os.unlink(p)
+    except EnvironmentError as e:
+        if e.errno == errno.ENOENT:
+            return
+        raise
+
+
+def safe_hardlink(source, target):
+    safe_delete(target)
+    os.link(source, target)
+
+
 def main(argv):
     dest = argv[1]
     targets = argv[2:]
@@ -29,22 +44,22 @@ def main(argv):
         base = os.path.basename(t)
         target_dest = os.path.join(dest, base)
         touchedFiles.append(base)
-        ## Copy if changed
+        # Copy if changed
         if not (os.path.isfile(target_dest) and filecmp.cmp(t, target_dest)):
             shutil.copy2(t, target_dest)
-        ## Create symlinks
+        # Create symlinks
         target = base
         while True:
             mo = DIGIT_EXT.match(target)
             if not mo:
                 break
             # print(target,mo.group(1))
-            safe_symlink(target, os.path.join(dest,mo.group(1)))
+            safe_hardlink(target_dest, os.path.join(dest, mo.group(1)))
             target = mo.group(1)
 
-    ## Delete dead symlinks
+    # Delete dead symlinks
     for f in os.listdir(dest):
-        full = os.path.join(dest,f)
+        full = os.path.join(dest, f)
         if os.path.islink(full) and not os.path.isfile(full):
             os.unlink(full)
 
