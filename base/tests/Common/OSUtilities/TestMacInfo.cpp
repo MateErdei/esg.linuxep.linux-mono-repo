461b48a5d664a6e5f4537040ea3d96217b07119e
Gesner Passos <gesner.passos@sophos.com>
2018-09-18 11:03:22 +0100

    [base] [LINUXEP-6474] Handle not having /sbin/ifconfig

--- base/tests/Common/OSUtilities/TestMacInfo.cpp
+++ base/tests/Common/OSUtilities/TestMacInfo.cpp
@@ -6,9 +6,11 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 #include <Common/OSUtilities/MACinfo.h>
 #include <Common/ProcessImpl/ProcessImpl.h>
 #include <Common/Process/IProcess.h>
+#include <Common/FileSystem/IFileSystem.h>
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <iostream>
 
 using namespace Common::OSUtilities;
 using PairResult = std::pair<std::string , std::string >;
@@ -17,6 +19,12 @@ using ListInputOutput = std::vector<PairResult >;
 
 TEST(TestMacInfo, MacsShouldBeAvailableInIfconfig) // NOLINT
 {
+    auto fSystem = Common::FileSystem::fileSystem();
+    if( !fSystem->isExecutable("/sbin/ifconfig") )
+    {
+        std::cout << "[  SKIPPED ] /sbin/ifconfig not present " << std::endl;
+        return;
+    }
     auto process = Common::Process::createProcess();
     process->exec("/sbin/ifconfig", std::vector<std::string>());
     std::string ifconfigOutput = process->output();
