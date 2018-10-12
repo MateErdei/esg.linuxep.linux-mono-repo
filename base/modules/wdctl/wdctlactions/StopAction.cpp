cf2b7092bc8a43aaa6f8d746f5e0440d2820e82f
Richard Lycett <Richard.Lycett@sophos.com>
2018-10-12 09:24:40 +0100

    [base] [LINUXEP-7035] Added check against registry when trying to stop plugin with wdctl and only raise an error if the plugin is registered and fails to stop.

--- base/modules/wdctl/wdctlactions/StopAction.cpp
+++ base/modules/wdctl/wdctlactions/StopAction.cpp
@@ -6,6 +6,8 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 #include "StopAction.h"
 #include "Logger.h"
 
+#include <Common/FileSystem/IFileSystem.h>
+
 #include <Common/ApplicationConfiguration/IApplicationPathManager.h>
 #include <Common/ZeroMQWrapper/IContext.h>
 #include <Common/ZeroMQWrapper/ISocketRequester.h>
@@ -20,6 +22,7 @@ StopAction::StopAction(const wdctl::wdctlarguments::Arguments& args)
 
 int StopAction::run()
 {
+
     LOGINFO("Attempting to stop "<<m_args.m_argument);
 
     auto response = doOperationToWatchdog({"STOP",m_args.m_argument});
@@ -29,6 +32,26 @@ int StopAction::run()
         return 0;
     }
 
-    LOGERROR("Failed to stop "<< m_args.m_argument<<": "<<response.at(0));
-    return 1;
+    std::string pluginRegistry = Common::ApplicationConfiguration::applicationPathManager().getPluginRegistryPath();
+    std::string registryFile = m_args.m_argument + ".json";
+
+    Path destination = Common::FileSystem::join(
+            pluginRegistry,
+            Common::FileSystem::basename(registryFile)
+    );
+
+    if (Common::FileSystem::fileSystem()->isFile(m_args.m_argument))
+    {
+        LOGERROR("Failed to stop "<< m_args.m_argument<<": "<<response.at(0));
+        return 1;
+    }
+    else
+    {
+        LOGINFO("Plugin "<<m_args.m_argument << " not in registry");
+        return 0;
+    }
+
+
+
+
 }
