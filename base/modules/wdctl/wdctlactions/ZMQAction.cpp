11cf0877dc9404f033310dc3794e7f2df0eee453
Richard Lycett <Richard.Lycett@sophos.com>
2018-10-16 17:40:20 +0100

    [base] [BUGFIX-7042] Fix systemctl command

--- base/modules/wdctl/wdctlactions/ZMQAction.cpp
+++ base/modules/wdctl/wdctlactions/ZMQAction.cpp
@@ -34,7 +34,7 @@ Common::ZeroMQWrapper::IReadable::data_t wdctl::wdctlactions::ZMQAction::doOpera
     {
         if( Common::FileSystem::fileSystem()->isFile(systemctlPath))
         {
-            std::string systemCommand = systemctlPath +  " sophos-spl status";
+            std::string systemCommand = systemctlPath +  " status sophos-spl";
             if( system(systemCommand.c_str()) != 0)
             {
                 return {std::string("Watchdog is not running")};
