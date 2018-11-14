06886b4bf1eec4fa0f21a608c82875e819664630
Gesner <gesner.passos@sophos.com>
2018-11-14 15:29:24 +0000

    [base] [LINXUEP-6170] Demonstrate that pub/sub may loose data but subscribers keep receiving them.

--- base/tests/Common/ZeroMQWrapperImpl/TestReqRepReliability.cpp
+++ base/tests/Common/ZeroMQWrapperImpl/TestReqRepReliability.cpp
@@ -8,7 +8,9 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 #include <Common/ZeroMQWrapper/IContext.h>
 #include <Common/ZeroMQWrapper/ISocketRequesterPtr.h>
 #include <Common/ZeroMQWrapper/ISocketReplierPtr.h>
-
+#include <Common/ZeroMQWrapper/ISocketPublisher.h>
+#include <Common/ZeroMQWrapper/ISocketSubscriber.h>
+#include <Common/ZeroMQWrapperImpl/SocketSubscriberImpl.h>
 #include <Common/ZeroMQWrapperImpl/SocketRequesterImpl.h>
 #include <Common/ZeroMQWrapperImpl/SocketReplierImpl.h>
 #include <Common/ZeroMQWrapper/IIPCTimeoutException.h>
@@ -20,6 +22,8 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 #include <thread>
 #include <future>
 #include <zmq.h>
+
+
 #ifndef ARTISANBUILD
 
 namespace
@@ -459,6 +463,66 @@ namespace
         EXPECT_NO_THROW(futureReplier.get());
 
 
+    }
+
+    TEST_F(ReqRepReliabilityTests, SlowSubscribersMayLooseMessagesButWillStillReceiveMessagesFromThePublisher)  // NOLINT
+    {
+
+        std::string serveraddress = m_testContext.serverAddress();
+        Tests::TestExecutionSynchronizer synchronizer;
+        std::atomic<bool> stopPublisher(false);
+        auto fasterPublisher = std::async(std::launch::async, [serveraddress, &synchronizer, &stopPublisher]() {
+            std::unique_ptr<Common::ZeroMQWrapper::IContext> m_context = Common::ZeroMQWrapper::createContext();
+            auto publisher = m_context->getPublisher();
+            publisher->listen(serveraddress);
+
+            for(int i = 0; i< 100000000; i++)
+            {
+                if (stopPublisher) break;
+                std::string value = std::to_string(i);
+                publisher->write(data_t{"news", value});
+            }
+        });
+
+        // simulate a replier that answers after the requester timeout.
+        auto slowSubscriber = std::async(std::launch::async, [serveraddress, &synchronizer]() {
+            std::unique_ptr<Common::ZeroMQWrapper::IContext> m_context = Common::ZeroMQWrapper::createContext();
+            auto subscriber = m_context->getSubscriber();
+            auto subscriberimpl = dynamic_cast<Common::ZeroMQWrapperImpl::SocketSubscriberImpl*>(subscriber.get());
+            assert(subscriberimpl);
+            int hwm=10;
+            int rc = zmq_setsockopt(subscriberimpl->skt(), ZMQ_RCVHWM, &hwm, sizeof(hwm));
+            assert(rc==0);
+
+            subscriber->connect(serveraddress);
+            subscriber->subscribeTo("news");
+
+            int count_jumps = 0;
+            int previous = 0;
+            while( true )
+            {
+                data_t newdata = subscriber->read();
+                int value = std::stoi( newdata.back());
+                if( value != previous + 1)
+                {
+                    count_jumps++;
+                    previous = value;
+                }
+                // demonstrate that it will loose data but keeping up receiving new data as possible.
+                if( value > 1000  && count_jumps > 2)
+                {
+                    synchronizer.notify();
+                    break;
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            }
+        });
+
+
+        EXPECT_TRUE(synchronizer.waitfor(3000));
+        stopPublisher = true;
+        slowSubscriber.wait();
+        fasterPublisher.wait();
     }
 
 
