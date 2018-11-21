1d1985de39518d8fdaf4a8d73f508f7cb39255bb
Douglas Leeder <douglas.leeder@sophos.com>
2018-11-21 15:13:22 +0000

    [base] [LINUXEP-5910] Make PluginRegistryImpl use log4cplus.

--- base/modules/Common/PluginRegistryImpl/Logger.h
+++ base/modules/Common/PluginRegistryImpl/Logger.h
@@ -3,14 +3,15 @@
 Copyright 2018, Sophos Limited.  All rights reserved.
 
 ******************************************************************************************************/
+#pragma once
 
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
 
-#ifndef COMMON_PLUGINREGISTRYIMPL_LOGGER_H
-#define COMMON_PLUGINREGISTRYIMPL_LOGGER_H
+log4cplus::Logger& getLogger();
 
-#define LOGERROR(x) std::cerr << x << std::endl //NOLINT
-#define LOGWARN(x) std::cerr << x << std::endl //NOLINT
-#define LOGDEBUG(x) std::cerr << x << std::endl //NOLINT
-#define LOGSUPPORT(x) std::cerr << x << std::endl //NOLINT
-
-#endif //COMMON_PLUGINREGISTRYIMPL_LOGGER_H
+#define LOGDEBUG(x) LOG4CPLUS_DEBUG(getLogger(), x) // NOLINT
+#define LOGINFO(x) LOG4CPLUS_INFO(getLogger(), x) // NOLINT
+#define LOGSUPPORT(x) LOG4CPLUS_INFO(getLogger(), x) // NOLINT
+#define LOGWARN(x) LOG4CPLUS_WARN(getLogger(), x) // NOLINT
+#define LOGERROR(x) LOG4CPLUS_ERROR(getLogger(), x) // NOLINT
