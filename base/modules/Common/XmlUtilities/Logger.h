7feafa3aa34bccca4eb6b6ffdc80d74bd2d7525e
Douglas Leeder <douglas.leeder@sophos.com>
2018-11-23 12:53:01 +0000

    [base] [LINUXEP-5910] Make XmlUtilities use log4cplus.

--- base/modules/Common/XmlUtilities/Logger.h
+++ base/modules/Common/XmlUtilities/Logger.h
@@ -5,8 +5,13 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 ******************************************************************************************************/
 #pragma once
 
-#include <iostream>
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
 
-#define LOGERROR(x) std::cerr << x << std::endl // NOLINT
-#define LOGDEBUG(x) std::cerr << x << std::endl // NOLINT
-#define LOGSUPPORT(x) std::cerr << x << std::endl // NOLINT
+log4cplus::Logger& getLogger();
+
+#define LOGDEBUG(x) LOG4CPLUS_DEBUG(getLogger(), x) // NOLINT
+#define LOGINFO(x) LOG4CPLUS_INFO(getLogger(), x) // NOLINT
+#define LOGSUPPORT(x) LOG4CPLUS_INFO(getLogger(), x) // NOLINT
+#define LOGWARN(x) LOG4CPLUS_WARN(getLogger(), x) // NOLINT
+#define LOGERROR(x) LOG4CPLUS_ERROR(getLogger(), x) // NOLINT
