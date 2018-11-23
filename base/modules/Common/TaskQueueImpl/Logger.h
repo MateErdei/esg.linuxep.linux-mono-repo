77395bcef97ce3cc10f0a7272b0088069d5dd7cc
Douglas Leeder <douglas.leeder@sophos.com>
2018-11-23 11:48:49 +0000

    [base] [LINUXEP-5910] Make TaskQueueImpl use log4cplus.

--- base/modules/Common/TaskQueueImpl/Logger.h
+++ base/modules/Common/TaskQueueImpl/Logger.h
@@ -4,13 +4,14 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 
 ******************************************************************************************************/
 #pragma once
-#include <iostream>
 
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
 
-#define LOGINFO(x) std::cerr << x << '\n'
-#define LOGWARN(x) std::cerr << x << '\n'
-#define LOGERROR(x) std::cerr << x << '\n'
-#define LOGDEBUG(x) std::cerr << x << '\n'
-#define LOGSUPPORT(x) std::cerr << x << '\n'
+log4cplus::Logger& getLogger();
 
-
+#define LOGDEBUG(x) LOG4CPLUS_DEBUG(getLogger(), x) // NOLINT
+#define LOGINFO(x) LOG4CPLUS_INFO(getLogger(), x) // NOLINT
+#define LOGSUPPORT(x) LOG4CPLUS_INFO(getLogger(), x) // NOLINT
+#define LOGWARN(x) LOG4CPLUS_WARN(getLogger(), x) // NOLINT
+#define LOGERROR(x) LOG4CPLUS_ERROR(getLogger(), x) // NOLINT
