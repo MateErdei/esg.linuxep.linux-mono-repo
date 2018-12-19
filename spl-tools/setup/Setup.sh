c0b768cfbf52165fddadeb8f40f7f9ba87d273c1
MichaelSlade <michael.slade@sophos.com>
2018-12-19 16:07:36 +0000

    [spl-tools] [LINUXEP-7371] Refactor share mounting

--- spl-tools/setup/Setup.sh
+++ spl-tools/setup/Setup.sh
@@ -47,77 +47,59 @@ function cloneSSPLRepos()
 	done
 }
 
+function mountShare()
+{
+    localMountDir="$1"
+    fstabString="$2"
+
+    if grep -iq "$localMountDir" /etc/fstab
+    then
+        warning "$localMountDir already in /etc/fstab"
+        return
+    fi
+
+    if echo $fstabString | grep -q USERNAME
+    then
+        read -p "Enter username for $localMountDir: " username
+        read -sp "Enter password for $localMountDir (This will be written in plaintext to /etc/fstab): " password
+        echo ""
+
+        fstabString="${fstabString/USERNAME/$username}"
+        fstabString="${fstabString/PASSWORD/$password}"
+    fi
+
+    sudo cp /etc/fstab /etc/fstab.bk
+    echo "$fstabString" | sudo tee --append /etc/fstab &> /dev/null
+
+    sudo mkdir -p $localMountDir
+    sudo umount $localMountDir &> /dev/null
+    if ! sudo mount $localMountDir
+    then
+        sudo mv /etc/fstab.bk /etc/fstab
+        error "Failed to mount $localMountDir"
+    fi
+}
+
 function mountFiler5()
 {
     filer5Loc="/uk-filer5/prodro"
-    if grep -iq $filer5Loc /etc/fstab 
-    then
-        warning "$filer5Loc already in /etc/fstab"
-        return
-    fi
-    read -p "Enter username for filer5: " filer5username
-    read -sp "Enter password for filer5 (This will be written in plaintext to /etc/fstab): " filer5password
-    echo ""
-
-    filer5mount="//UK-FILER5.PROD.SOPHOS/PRODRO $filer5Loc    cifs   users,noauto,user=${filer5username},domain=PRODUCTION,vers=2.0,password=${filer5password} 0 0"
-    sudo cp /etc/fstab /etc/fstab.bk
-    echo "$filer5mount" | sudo tee --append /etc/fstab &> /dev/null
-    
-    sudo mkdir -p $filer5Loc
-    sudo umount $filer5Loc &> /dev/null
-    if ! sudo mount $filer5Loc
-    then
-        sudo mv /etc/fstab.bk /etc/fstab
-        error "Failed to mount filer5"
-    fi
+    filer5mount="//UK-FILER5.PROD.SOPHOS/PRODRO $filer5Loc    cifs   users,noauto,user=USERNAME,domain=PRODUCTION,vers=2.0,password=PASSWORD 0 0"
+    mountShare "${filer5Loc}" "${filer5mount}"
 }
 
 function mountFiler6()
 {
-    filer6Loc="/mnt/filer6/bfr"
-    if grep -iq "$filer6Loc" /etc/fstab 
-    then
-        warning "$filer6Loc already in /etc/fstab"
-        return
-    fi
-    read -p "Enter username for filer6: " filer6username
-    read -sp "Enter password for filer6 (This will be written in plaintext to /etc/fstab): " filer6password
-    echo ""
 
-    filer6mount="//UK-FILER6.ENG.SOPHOS/BFR  $filer6Loc cifs   noauto,users,rw,domain=GREEN,username=${filer6username},password=${filer6password},vers=2.0   0   0"
-    sudo cp /etc/fstab /etc/fstab.bk
-    echo "$filer6mount" | sudo tee --append /etc/fstab &> /dev/null
-    
-    sudo mkdir -p $filer6Loc 
-    sudo umount $filer6Loc &> /dev/null
-    if ! sudo mount $filer6Loc
-    then
-        sudo mv /etc/fstab.bk /etc/fstab
-        error "Failed to mount filer6"
-    fi
+    filer6Loc="/mnt/filer6/bfr"
+    filer6mount="//UK-FILER6.ENG.SOPHOS/BFR  $filer6Loc cifs   noauto,users,rw,domain=GREEN,username=USERNAME,password=PASSWORD,vers=2.0   0   0"
+    mountShare "${filer6Loc}" "${filer6mount}"
 }
 
 function mountAllegro()
 {
     allegroLoc="/redist"
-    if grep -iq "$allegroLoc" /etc/fstab 
-    then
-        warning "$allegroLoc already in /etc/fstab"
-        return
-    fi
-
     allegromount="allegro:/redist $allegroLoc nfs ro,vers=3,tcp,exec 0 0"
-    sudo cp /etc/fstab /etc/fstab.bk
-    echo "$allegromount" | sudo tee --append /etc/fstab &> /dev/null
-    
-    sudo mkdir -p $allegroLoc
-    sudo umount $allegroLoc &> /dev/null
-    if ! sudo mount $allegroLoc
-    then
-        sudo mv /etc/fstab.bk /etc/fstab
-        error "Failed to mount allegro"
-    fi
-    
+    mountShare "${allegroLoc}" "${allegromount}"
 }
 
 function setupMounts()
