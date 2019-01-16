6995e89b44b857a3c1a694c365c890db872f917f
EthanVinceUrwin <ethan.vince-urwin@sophos.com>
2019-01-16 15:53:18 +0000

    [base] [LINUXEP-7422] Added defaults to TextOffsetLength member variables

--- base/modules/Common/EventTypes/CommonEventData.h
+++ base/modules/Common/EventTypes/CommonEventData.h
@@ -51,8 +51,8 @@ namespace Common
         struct TextOffsetLength
         {
         public:
-            std::uint32_t length;
-            std::uint32_t offset;
+            std::uint32_t length = 0;
+            std::uint32_t offset = 0;
         };
 
         struct Pathname
