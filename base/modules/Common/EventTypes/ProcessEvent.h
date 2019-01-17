f9d734e1bf5c8b68b7bbd4c1011520e0c4fb14a1
EthanVinceUrwin <ethan.vince-urwin@sophos.com>
2019-01-17 09:39:03 +0000

    [base] [LINUXEP-7422] More comments

--- base/modules/Common/EventTypes/ProcessEvent.h
+++ base/modules/Common/EventTypes/ProcessEvent.h
@@ -28,16 +28,32 @@ namespace Common
             ~ProcessEvent() = default;
 
             const EventType getEventType() const;
+
+            // Gets the sophosPid which is the PID concatenated with timestamp of when the process started.
             const Common::EventTypes::SophosPid getSophosPid() const;
+
+            // A SophosPid is the PID concatenated with timestamp of when the process started.
+            // This is the parent process SophosPID
             const Common::EventTypes::SophosPid getParentSophosPid() const;
+
+            // Gets the sophosPid which is the TID (thread ID) concatenated with timestamp of when the process started.
             const Common::EventTypes::SophosTid getParentSophosTid() const;
+
             const std::string getEventTypeId() const override;
             const unsigned long long getEndTime() const;
             const Common::EventTypes::OptionalUInt64 getFileSize() const ;
+
+            // Process flags, we don't know what these will be on Linux, will likely just be 0.
             const std::uint32_t getFlags() const;
+
+            // On linux this is the process namespace.
             const std::uint32_t getSessionId() const;
+
+            // On linux this is the UID, on windows SID (Security Identifier) has a specific meaning.
             const std::string getSid() const ;
             const Common::EventTypes::Pathname getPathname() const;
+
+            // Command line of running process - e.g. what you get when you run a ps on Linux.
             const std::string getCmdLine() const;
             const std::string getSha256() const;
             const std::string getSha1() const;
@@ -67,11 +83,20 @@ namespace Common
 
         private:
             Common::EventTypes::ProcessEvent::EventType m_eventType;
+
+            // A SophosPid is the PID concatenated with timestamp of when the process started.
             Common::EventTypes::SophosPid m_sophosPid;
+
+            // A SophosPid is the PID concatenated with timestamp of when the process started.
+            // This is the parent process SophosPID
             Common::EventTypes::SophosPid m_parentSophosPid;
+
+            // A sophosTid is the TID (thread ID) concatenated with timestamp of when the process started.
             Common::EventTypes::SophosTid m_parentSophosTid;
             unsigned long long m_endTime;
             Common::EventTypes::OptionalUInt64 m_fileSize;
+
+            // Process flags, we don't know what these will be on Linux, will likely just be 0.
             std::uint32_t m_flags;
 
             // On linux this is the process namespace.
@@ -80,6 +105,8 @@ namespace Common
             // On linux this is the UID, on windows SID (Security Identifier) has a specific meaning.
             std::string m_sid;
             Common::EventTypes::Pathname m_pathname;
+
+            // Command line of running process - e.g. what you get when you run a ps on Linux.
             std::string m_cmdLine;
             std::string m_sha256;
             std::string m_sha1;
