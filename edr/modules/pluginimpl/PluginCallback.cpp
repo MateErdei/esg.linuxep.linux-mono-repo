118a1f081096c34336b098930162b39d71cabd71
Douglas Leeder <douglas.leeder@sophos.com>
2019-02-08 16:51:54 +0000

    [edr] [LINUXEP-7519] Format files using clang-format.
    Commands to redo formatting:
    
    git fetch origin master:master
    git rebase -Xours master
    find modules products tests -name '*.cpp' -o -name '*.h' -a ! -name 'googletest*' -print0 | xargs -0 clang-format -i -verbose

--- edr/modules/pluginimpl/PluginCallback.cpp
+++ edr/modules/pluginimpl/PluginCallback.cpp
@@ -5,35 +5,32 @@ Copyright 2018 Sophos Limited.  All rights reserved.
 ******************************************************************************************************/
 
 #include "PluginCallback.h"
+
 #include "Logger.h"
 #include "Telemetry.h"
 
 namespace Plugin
 {
-
-    PluginCallback::PluginCallback(std::shared_ptr<QueueTask> task) :
-        m_task(std::move(task))
+    PluginCallback::PluginCallback(std::shared_ptr<QueueTask> task) : m_task(std::move(task))
     {
         std::string noPolicySetStatus{
-                R"sophos(<?xml version="1.0" encoding="utf-8" ?>
+            R"sophos(<?xml version="1.0" encoding="utf-8" ?>
                     <status xmlns="http://www.sophos.com/EE/EESavStatus">
                         <CompRes xmlns="com.sophos\msys\csc" Res="NoRef" RevID="" policyType="2" />
-                    </status>)sophos"};
-        Common::PluginApi::StatusInfo noPolicyStatusInfo = {noPolicySetStatus, noPolicySetStatus, "SAV"};
+                    </status>)sophos"
+        };
+        Common::PluginApi::StatusInfo noPolicyStatusInfo = { noPolicySetStatus, noPolicySetStatus, "SAV" };
         m_statusInfo = noPolicyStatusInfo;
         LOGDEBUG("Plugin Callback Started");
     }
 
-    void PluginCallback::applyNewPolicy(const std::string &policyXml)
+    void PluginCallback::applyNewPolicy(const std::string& policyXml)
     {
         LOGSUPPORT("Applying new policy");
-        m_task->push(Task{Task::TaskType::Policy, policyXml});
+        m_task->push(Task{ Task::TaskType::Policy, policyXml });
     }
 
-    void PluginCallback::queueAction(const std::string &actionXml)
-    {
-        LOGSUPPORT("Queueing action");
-    }
+    void PluginCallback::queueAction(const std::string& actionXml) { LOGSUPPORT("Queueing action"); }
 
     void PluginCallback::onShutdown()
     {
@@ -41,7 +38,7 @@ namespace Plugin
         m_task->pushStop();
     }
 
-    Common::PluginApi::StatusInfo PluginCallback::getStatus(const std::string &appId)
+    Common::PluginApi::StatusInfo PluginCallback::getStatus(const std::string& appId)
     {
         LOGSUPPORT("Received get status request");
         return m_statusInfo;
@@ -56,9 +53,9 @@ namespace Plugin
     std::string PluginCallback::getTelemetry()
     {
         LOGSUPPORT("Received get telemetry request");
-        auto & telemetry = Telemetry::instance();
+        auto& telemetry = Telemetry::instance();
         std::string telemetryJson = telemetry.getJson();
         telemetry.clear();
         return telemetryJson;
     }
-}
+} // namespace Plugin
--- edr/tests/plugin/TestPlugin.cpp
+++ edr/tests/plugin/TestPlugin.cpp
@@ -4,20 +4,18 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 
 ******************************************************************************************************/
 
-
 #include <Common/FileSystem/IFileSystem.h>
-#include <modules/pluginimpl/Logger.h>
 #include <Common/Logging/ConsoleLoggingSetup.h>
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
+#include <modules/pluginimpl/Logger.h>
 
 using namespace Common::FileSystem;
 
 TEST(TestFileSystemExample, binBashShouldExist) // NOLINT
 {
-    auto * ifileSystem = Common::FileSystem::fileSystem();
+    auto* ifileSystem = Common::FileSystem::fileSystem();
     EXPECT_TRUE(ifileSystem->isExecutable("/bin/bash"));
 }
-
 
 TEST(TestLinkerWorks, WorksWithTheLogging) // NOLINT
 {
