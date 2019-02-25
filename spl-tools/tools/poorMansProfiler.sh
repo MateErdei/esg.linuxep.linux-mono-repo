0773019bed7cb2b99a318a6c21d845d6b9e7faaf
Douglas Leeder <douglas.leeder@sophos.com>
2019-02-25 13:53:17 +0000

    [spl-tools] Better handling of PID, and measuring progress.

--- spl-tools/tools/poorMansProfiler.sh
+++ spl-tools/tools/poorMansProfiler.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 pid=$1
-[[ -d /proc/$pid ]] || pid=$(pgrep $pid)
+[[ -d /proc/${pid} ]] || pid=$(pgrep -f ${pid})
+
+[[ -n ${pid} ]] || {
+    echo "FAILED: $pid \$pid is empty"
+    exit 1
+}
 
 for x in $(seq 1 500); do
-   gdb -ex "set pagination 0" -ex "thread apply all bt" -batch -p $pid 2> /dev/null
+   gdb -ex "set pagination 0" -ex "thread apply all bt" -batch -p "${pid}" 2> /dev/null
   sleep 0.01
+  echo ${x} >&2
 done
