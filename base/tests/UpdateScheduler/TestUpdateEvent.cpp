8da8bd3475284428b87045aafca5ce5132b348b9
Douglas Leeder <douglas.leeder@sophos.com>
2019-02-28 10:02:19 +0000

    [base] [LINUXEP-7642] Refactor TestUpdateEvent to move testing into runTest method.
    Move static strings into test methods.

--- base/tests/UpdateScheduler/TestUpdateEvent.cpp
+++ base/tests/UpdateScheduler/TestUpdateEvent.cpp
@@ -13,109 +13,6 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 #include <gtest/gtest.h>
 #include <tests/Common/UtilityImpl/MockFormattedTime.h>
 
-static std::string successEventXML{ R"sophos(<?xml version="1.0"?>
-<event xmlns="http://www.sophos.com/EE/AUEvent" type="sophos.mgt.entityAppEvent">
-  <timestamp>20180816 083654</timestamp>
-  <appInfo>
-    <number>0</number>
-  <updateSource>Sophos</updateSource>
-  </appInfo>
-  <entityInfo xmlns="http://www.sophos.com/EntityInfo">AGENT:WIN:1.0.0</entityInfo>
-</event>)sophos" };
-
-static std::string installFailedEventXML{ R"sophos(<?xml version="1.0"?>
-<event xmlns="http://www.sophos.com/EE/AUEvent" type="sophos.mgt.entityAppEvent">
-  <timestamp>20180816 083654</timestamp>
-  <appInfo>
-    <number>103</number>
-    <message>
-      <message_inserts>
-        <insert>BaseName</insert>
-        <insert>PluginName</insert>
-      </message_inserts>
-    </message>
-  <updateSource>Sophos</updateSource>
-  </appInfo>
-  <entityInfo xmlns="http://www.sophos.com/EntityInfo">AGENT:WIN:1.0.0</entityInfo>
-</event>)sophos" };
-
-static std::string installCaughtErrorEventXML{ R"sophos(<?xml version="1.0"?>
-<event xmlns="http://www.sophos.com/EE/AUEvent" type="sophos.mgt.entityAppEvent">
-  <timestamp>20180816 083654</timestamp>
-  <appInfo>
-    <number>106</number>
-    <message>
-      <message_inserts>
-
-      </message_inserts>
-    </message>
-  <updateSource>Sophos</updateSource>
-  </appInfo>
-  <entityInfo xmlns="http://www.sophos.com/EntityInfo">AGENT:WIN:1.0.0</entityInfo>
-</event>)sophos" };
-
-static std::string downloadFailedEventXML{ R"sophos(<?xml version="1.0"?>
-<event xmlns="http://www.sophos.com/EE/AUEvent" type="sophos.mgt.entityAppEvent">
-  <timestamp>20180816 083654</timestamp>
-  <appInfo>
-    <number>107</number>
-    <message>
-      <message_inserts>
-        <insert>BaseName</insert>
-        <insert>PluginName</insert>
-      </message_inserts>
-    </message>
-  <updateSource>Sophos</updateSource>
-  </appInfo>
-  <entityInfo xmlns="http://www.sophos.com/EntityInfo">AGENT:WIN:1.0.0</entityInfo>
-</event>)sophos" };
-
-static std::string packageSourceMissingEventXML{ R"sophos(<?xml version="1.0"?>
-<event xmlns="http://www.sophos.com/EE/AUEvent" type="sophos.mgt.entityAppEvent">
-  <timestamp>20180816 083654</timestamp>
-  <appInfo>
-    <number>111</number>
-    <message>
-      <message_inserts>
-        <insert>BaseName</insert>
-      </message_inserts>
-    </message>
-  <updateSource>Sophos</updateSource>
-  </appInfo>
-  <entityInfo xmlns="http://www.sophos.com/EntityInfo">AGENT:WIN:1.0.0</entityInfo>
-</event>)sophos" };
-
-static std::string connectionErrorEventXML{ R"sophos(<?xml version="1.0"?>
-<event xmlns="http://www.sophos.com/EE/AUEvent" type="sophos.mgt.entityAppEvent">
-  <timestamp>20180816 083654</timestamp>
-  <appInfo>
-    <number>112</number>
-    <message>
-      <message_inserts>
-
-      </message_inserts>
-    </message>
-  <updateSource>Sophos</updateSource>
-  </appInfo>
-  <entityInfo xmlns="http://www.sophos.com/EntityInfo">AGENT:WIN:1.0.0</entityInfo>
-</event>)sophos" };
-
-static std::string packagesSourceMissingEventXML{ R"sophos(<?xml version="1.0"?>
-<event xmlns="http://www.sophos.com/EE/AUEvent" type="sophos.mgt.entityAppEvent">
-  <timestamp>20180816 083654</timestamp>
-  <appInfo>
-    <number>113</number>
-    <message>
-      <message_inserts>
-        <insert>BaseName</insert>
-        <insert>PluginName</insert>
-      </message_inserts>
-    </message>
-  <updateSource>Sophos</updateSource>
-  </appInfo>
-  <entityInfo xmlns="http://www.sophos.com/EntityInfo">AGENT:WIN:1.0.0</entityInfo>
-</event>)sophos" };
-
 using namespace UpdateSchedulerImpl::configModule;
 using namespace SulDownloader;
 using namespace SulDownloader::suldownloaderdata;
@@ -143,58 +40,168 @@ public:
     {
         std::unique_ptr<MockMapHostCacheId> mockRevId(new ::testing::StrictMock<MockMapHostCacheId>());
         EXPECT_CALL(*mockRevId, cacheID(SophosURL)).WillOnce(Return("Sophos"));
-        return std::move(mockRevId);
+        return mockRevId;
     }
 
     std::unique_ptr<MockFormattedTime> formattedTime()
     {
         std::unique_ptr<MockFormattedTime> mockFormattedTime(new ::testing::StrictMock<MockFormattedTime>());
         EXPECT_CALL(*mockFormattedTime, currentTime()).WillOnce(Return("20180816 083654"));
-        return std::move(mockFormattedTime);
+        return mockFormattedTime;
     }
+
+    void runTest(const UpdateEvent& event, const std::string& expectedXML);
 
     std::unique_ptr<MockMapHostCacheId> m_hostCacheId;
     std::unique_ptr<MockFormattedTime> m_formattedTime;
 };
 
+void TestSerializeEvent::runTest(const UpdateEvent& event, const std::string& expectedXML)
+{
+    EXPECT_EQ(serializeUpdateEvent(event, *m_hostCacheId, *m_formattedTime), expectedXML);
+}
+
 TEST_F(TestSerializeEvent, SuccessEvent) // NOLINT
 {
+    static const std::string successEventXML{ R"sophos(<?xml version="1.0"?>
+<event xmlns="http://www.sophos.com/EE/AUEvent" type="sophos.mgt.entityAppEvent">
+  <timestamp>20180816 083654</timestamp>
+  <appInfo>
+    <number>0</number>
+  <updateSource>Sophos</updateSource>
+  </appInfo>
+  <entityInfo xmlns="http://www.sophos.com/EntityInfo">AGENT:WIN:1.0.0</entityInfo>
+</event>)sophos" };
+
     UpdateEvent event = getEvent(DownloadReportTestBuilder::goodReport());
-    EXPECT_EQ(serializeUpdateEvent(event, *m_hostCacheId, *m_formattedTime), successEventXML);
+    runTest(event, successEventXML);
 }
 
 TEST_F(TestSerializeEvent, installFAiledTwoProducts) // NOLINT
 {
+    static const std::string installFailedEventXML{ R"sophos(<?xml version="1.0"?>
+<event xmlns="http://www.sophos.com/EE/AUEvent" type="sophos.mgt.entityAppEvent">
+  <timestamp>20180816 083654</timestamp>
+  <appInfo>
+    <number>103</number>
+    <message>
+      <message_inserts>
+        <insert>BaseName</insert>
+        <insert>PluginName</insert>
+      </message_inserts>
+    </message>
+  <updateSource>Sophos</updateSource>
+  </appInfo>
+  <entityInfo xmlns="http://www.sophos.com/EntityInfo">AGENT:WIN:1.0.0</entityInfo>
+</event>)sophos" };
+
     UpdateEvent event = getEvent(DownloadReportTestBuilder::installFailedTwoProducts());
-    EXPECT_EQ(serializeUpdateEvent(event, *m_hostCacheId, *m_formattedTime), installFailedEventXML);
+    runTest(event, installFailedEventXML);
 }
 
 TEST_F(TestSerializeEvent, installCaughtErrorEvent) // NOLINT
 {
+    static const std::string installCaughtErrorEventXML{ R"sophos(<?xml version="1.0"?>
+<event xmlns="http://www.sophos.com/EE/AUEvent" type="sophos.mgt.entityAppEvent">
+  <timestamp>20180816 083654</timestamp>
+  <appInfo>
+    <number>106</number>
+    <message>
+      <message_inserts>
+
+      </message_inserts>
+    </message>
+  <updateSource>Sophos</updateSource>
+  </appInfo>
+  <entityInfo xmlns="http://www.sophos.com/EntityInfo">AGENT:WIN:1.0.0</entityInfo>
+</event>)sophos" };
+
     UpdateEvent event = getEvent(DownloadReportTestBuilder::installCaughtError());
-    EXPECT_EQ(serializeUpdateEvent(event, *m_hostCacheId, *m_formattedTime), installCaughtErrorEventXML);
+    runTest(event, installCaughtErrorEventXML);
 }
 
 TEST_F(TestSerializeEvent, downloadFailedEvent) // NOLINT
 {
+    static const std::string downloadFailedEventXML{ R"sophos(<?xml version="1.0"?>
+<event xmlns="http://www.sophos.com/EE/AUEvent" type="sophos.mgt.entityAppEvent">
+  <timestamp>20180816 083654</timestamp>
+  <appInfo>
+    <number>107</number>
+    <message>
+      <message_inserts>
+        <insert>BaseName</insert>
+        <insert>PluginName</insert>
+      </message_inserts>
+    </message>
+  <updateSource>Sophos</updateSource>
+  </appInfo>
+  <entityInfo xmlns="http://www.sophos.com/EntityInfo">AGENT:WIN:1.0.0</entityInfo>
+</event>)sophos" };
+
     UpdateEvent event = getEvent(DownloadReportTestBuilder::downloadFailedError());
-    EXPECT_EQ(serializeUpdateEvent(event, *m_hostCacheId, *m_formattedTime), downloadFailedEventXML);
+    runTest(event, downloadFailedEventXML);
 }
 
 TEST_F(TestSerializeEvent, productMissing) // NOLINT
 {
+    static const std::string packageSourceMissingEventXML{ R"sophos(<?xml version="1.0"?>
+<event xmlns="http://www.sophos.com/EE/AUEvent" type="sophos.mgt.entityAppEvent">
+  <timestamp>20180816 083654</timestamp>
+  <appInfo>
+    <number>111</number>
+    <message>
+      <message_inserts>
+        <insert>BaseName</insert>
+      </message_inserts>
+    </message>
+  <updateSource>Sophos</updateSource>
+  </appInfo>
+  <entityInfo xmlns="http://www.sophos.com/EntityInfo">AGENT:WIN:1.0.0</entityInfo>
+</event>)sophos" };
+
     UpdateEvent event = getEvent(DownloadReportTestBuilder::baseProductMissing());
-    EXPECT_EQ(serializeUpdateEvent(event, *m_hostCacheId, *m_formattedTime), packageSourceMissingEventXML);
+    runTest(event, packageSourceMissingEventXML);
 }
 
 TEST_F(TestSerializeEvent, productsMissing) // NOLINT
 {
+    static const std::string packagesSourceMissingEventXML{ R"sophos(<?xml version="1.0"?>
+<event xmlns="http://www.sophos.com/EE/AUEvent" type="sophos.mgt.entityAppEvent">
+  <timestamp>20180816 083654</timestamp>
+  <appInfo>
+    <number>113</number>
+    <message>
+      <message_inserts>
+        <insert>BaseName</insert>
+        <insert>PluginName</insert>
+      </message_inserts>
+    </message>
+  <updateSource>Sophos</updateSource>
+  </appInfo>
+  <entityInfo xmlns="http://www.sophos.com/EntityInfo">AGENT:WIN:1.0.0</entityInfo>
+</event>)sophos" };
+
     UpdateEvent event = getEvent(DownloadReportTestBuilder::bothProductsMissing());
-    EXPECT_EQ(serializeUpdateEvent(event, *m_hostCacheId, *m_formattedTime), packagesSourceMissingEventXML);
+    runTest(event, packagesSourceMissingEventXML);
 }
 
 TEST_F(TestSerializeEvent, connectionError) // NOLINT
 {
+    static const std::string connectionErrorEventXML{ R"sophos(<?xml version="1.0"?>
+<event xmlns="http://www.sophos.com/EE/AUEvent" type="sophos.mgt.entityAppEvent">
+  <timestamp>20180816 083654</timestamp>
+  <appInfo>
+    <number>112</number>
+    <message>
+      <message_inserts>
+
+      </message_inserts>
+    </message>
+  <updateSource>Sophos</updateSource>
+  </appInfo>
+  <entityInfo xmlns="http://www.sophos.com/EntityInfo">AGENT:WIN:1.0.0</entityInfo>
+</event>)sophos" };
+
     UpdateEvent event = getEvent(DownloadReportTestBuilder::connectionError());
-    EXPECT_EQ(serializeUpdateEvent(event, *m_hostCacheId, *m_formattedTime), connectionErrorEventXML);
+    runTest(event, connectionErrorEventXML);
 }
