1180d1c492e079b6cec271be8f04a1aaa50adce8
Douglas Leeder <douglas.leeder@sophos.com>
2019-03-15 14:34:55 +0000

    [base] [LINUXEP-7498] Force copy_libs to be run before rest of build.

--- base/build.sh
+++ base/build.sh
@@ -320,6 +320,7 @@ function build()
         -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
         .. \
         || exitFailure 14 "Failed to configure $PRODUCT"
+    make -j${NPROC} copy_libs || exitFailure 15 "Failed to build $PRODUCT"
     make -j${NPROC} || exitFailure 15 "Failed to build $PRODUCT"
 
     if (( ${VALGRIND} == 1 ))
