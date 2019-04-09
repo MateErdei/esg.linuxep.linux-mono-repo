f280049a9a21d0747875dae2bd4d278d2f4d00f3
Gesner <gesner.passos@sophos.com>
2019-04-09 15:33:21 +0100

    [base] Introduce suldownloaderconfigtests

---
+++ base/tests/FuzzTests/suldownloaderconfigtests.cpp
@@ -0,0 +1,60 @@
+/******************************************************************************************************
+
+Copyright 2018-2019, Sophos Limited.  All rights reserved.
+
+******************************************************************************************************/
+#include <SulDownloader/suldownloaderdata/ConfigurationData.h>
+#include <SulDownloader/suldownloaderdata/SulDownloaderException.h>
+#include <Common/FileSystem/IFileSystem.h>
+#include <fstream>
+#include <iostream>
+#include <unistd.h>
+#include <chrono>
+#include <thread>
+
+
+struct DevNullRedirect
+{
+    DevNullRedirect(): file("/dev/null")
+    {
+        strm_buffer = std::cout.rdbuf();
+        strm_err_buffer = std::cerr.rdbuf();
+        std::cout.rdbuf(file.rdbuf());
+        std::cerr.rdbuf(file.rdbuf());
+    }
+    ~DevNullRedirect()
+    {
+        std::cout.rdbuf(strm_buffer);
+        std::cerr.rdbuf(strm_err_buffer);
+    }
+
+    std::ofstream file;
+    std::streambuf * strm_buffer;
+    std::streambuf * strm_err_buffer;
+
+};
+
+
+int main()
+{
+    std::array<uint8_t, 9000> buffer;
+    ssize_t read_bytes = ::read(STDIN_FILENO, buffer.data(), buffer.size());
+    // failure to read is not to be considered in the parser. Just skip.
+    if (read_bytes == -1)
+    {
+        perror("Failed to read");
+        return 1;
+    }
+    DevNullRedirect devNullRedirect;
+    std::string content(buffer.data(),buffer.data()+read_bytes);
+
+
+    try
+    {
+        (void) SulDownloader::suldownloaderdata::ConfigurationData::fromJsonSettings(content);
+    }catch ( SulDownloader::suldownloaderdata::SulDownloaderException & )
+    {
+        return 2;
+    }
+    return 0;
+}
