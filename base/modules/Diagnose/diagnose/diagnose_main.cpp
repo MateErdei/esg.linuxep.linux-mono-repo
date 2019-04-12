67687214782ba2baa612d6b3b97593caf7c7c283
Douglas Leeder <douglas.leeder@sophos.com>
2019-04-12 13:56:49 +0100

    [base] [LINUXEP-8015] Change no-tar exit code to not match existing codes.

--- base/modules/Diagnose/diagnose/diagnose_main.cpp
+++ base/modules/Diagnose/diagnose/diagnose_main.cpp
@@ -66,7 +66,7 @@ namespace diagnose
         if (!diagnose::CheckForTar::isTarAvailable())
         {
             std::cerr << "tar command not available: unable to generate diagnose output" << std::endl;
-            return 2;
+            return 4;
         }
 
         try
