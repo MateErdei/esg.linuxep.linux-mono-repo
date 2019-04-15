9a777101749961829784a052d128b6dd88db5924
Mel Inkersole <mel.inkersole@sophos.com>
2019-04-15 16:05:30 +0100

    [base] [LINUXEP-6637] Initial commit using curl to make a HTTP request to a server running locally. [+MI] [+DS]

--- base/modules/Telemetry/TelemetryImpl/Telemetry.cpp
+++ base/modules/Telemetry/TelemetryImpl/Telemetry.cpp
@@ -7,6 +7,7 @@ Copyright 2019, Sophos Limited.  All rights reserved.
 #include "Logger.h"
 
 #include <Common/Logging/FileLoggingSetup.h>
+#include "curl.h"
 
 #include <sstream>
 
@@ -24,6 +25,29 @@ namespace Telemetry
             msg << argv[i] << " ";
         }
         LOGINFO(msg.str());
+
+
+        CURL *curl;
+        CURLcode res;
+
+        curl_global_init(CURL_GLOBAL_DEFAULT);
+
+        curl = curl_easy_init();
+        if(curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:4443");
+
+            /* Perform the request, res will get the return code */
+            res = curl_easy_perform(curl);
+            /* Check for errors */
+            if(res != CURLE_OK)
+                fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                        curl_easy_strerror(res));
+
+            /* always cleanup */
+            curl_easy_cleanup(curl);
+        }
+
+        curl_global_cleanup();
 
         return 0;
     }
