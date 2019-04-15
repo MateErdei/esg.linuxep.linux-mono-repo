faab884367ca35b8c9518319149abea148f54ccc
Mel Inkersole <mel.inkersole@sophos.com>
2019-04-15 11:52:13 +0100

    [base] [LINUXEP-7970] Changed the for loop to use const and changed the static cast to a const cast. Removed the size decrement of 1 as this is no longer required from a previous commit. [+MI] [+DS]

--- base/tests/Telemetry/TelemetryTests.cpp
+++ base/tests/Telemetry/TelemetryTests.cpp
@@ -31,12 +31,12 @@ TEST_F(TelemetryTest, main_entry_ReturnsSuccess) // NOLINT
     std::vector<std::string> arguments = {"arg1", "arg2"};
 
     std::vector<char*> argv;
-    for (auto& arg : arguments)
+    for (const auto& arg : arguments)
     {
-        argv.emplace_back(static_cast<char*>(arg.data()));
+        argv.emplace_back(const_cast<char*>(arg.data()));
     }
 
     int expectedErrorCode = 0;
 
-    EXPECT_EQ(Telemetry::main_entry(argv.size() - 1, argv.data()), expectedErrorCode);
+    EXPECT_EQ(Telemetry::main_entry(argv.size(), argv.data()), expectedErrorCode);
 }
