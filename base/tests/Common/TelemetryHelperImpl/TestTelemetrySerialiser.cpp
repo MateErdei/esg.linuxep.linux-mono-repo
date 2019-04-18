fdc663727452fbfd77e22d5afd009d620b676baa
Timothy Cobley <timothy.cobley@sophos.com>
2019-04-18 16:49:29 +0100

    [base] [LINUXEP-6638] Reinstate TelemetryValue unit tests [AC] [TC]

--- base/tests/Common/TelemetryHelperImpl/TestTelemetrySerialiser.cpp
+++ base/tests/Common/TelemetryHelperImpl/TestTelemetrySerialiser.cpp
@@ -25,75 +25,74 @@ const std::string TEST_STRING = "Test String";  // NOLINT
 //    std::cout << j.dump() << std::endl;
 //
 //}
-namespace Common::Telemetry
+using namespace Common::Telemetry;
+
+TEST(TestTelemetrySerialiserImpl, SerialiseDictionaryNoKey) // NOLINT
 {
-    TEST(TestTelemetrySerialiserImpl, SerialiseDictionaryNoKey) // NOLINT
-    {
-        TelemetryObject root;
-        TelemetryValue val1;
-        val1.set(1);
-        TelemetryValue val2;
-        val2.set(2);
-        TelemetryValue val3;
-        val3.set(3);
-        TelemetryValue val4;
-        val4.set(4);
-        TelemetryValue val5;
-        val5.set(5);
-        root.set("key1", val1);
-        TelemetryObject nested1;
-        nested1.set("nested1", val2);
-        nested1.set("nested2", val3);
-        nested1.set("nested3", val4);
-        root.set("key2", nested1);
+    TelemetryObject root;
+    TelemetryValue val1;
+    val1.set(1);
+    TelemetryValue val2;
+    val2.set(2);
+    TelemetryValue val3;
+    val3.set(3);
+    TelemetryValue val4;
+    val4.set(4);
+    TelemetryValue val5;
+    val5.set(5);
+    root.set("key1", val1);
+    TelemetryObject nested1;
+    nested1.set("nested1", val2);
+    nested1.set("nested2", val3);
+    nested1.set("nested3", val4);
+    root.set("key2", nested1);
 
-        TelemetryObject stringObj;
-        TelemetryValue stringVal("string");
-        stringObj.set(stringVal);
+    TelemetryObject stringObj;
+    TelemetryValue stringVal("string");
+    stringObj.set(stringVal);
 
-        TelemetryObject boolObj;
-        TelemetryValue boolVal(true);
-        boolObj.set(boolVal);
+    TelemetryObject boolObj;
+    TelemetryValue boolVal(true);
+    boolObj.set(boolVal);
 
-        std::list<TelemetryObject> array{stringObj, boolObj, nested1};
-        root.set("my array", array);
+    std::list<TelemetryObject> array{stringObj, boolObj, nested1};
+    root.set("my array", array);
 
-        nlohmann::json j = root;
+    nlohmann::json j = root;
 
-        ASSERT_EQ(1, j["key1"].get<int>());
-        ASSERT_EQ(2, j["key2"]["nested1"].get<int>());
-        ASSERT_EQ(3, j["key2"]["nested2"].get<int>());
-        ASSERT_EQ(4, j["key2"]["nested3"].get<int>());
+    ASSERT_EQ(1, j["key1"].get<int>());
+    ASSERT_EQ(2, j["key2"]["nested1"].get<int>());
+    ASSERT_EQ(3, j["key2"]["nested2"].get<int>());
+    ASSERT_EQ(4, j["key2"]["nested3"].get<int>());
 
-        ASSERT_EQ("string", j["my array"][0].get<std::string>());
-        ASSERT_EQ(true, j["my array"][1].get<bool>());
-        ASSERT_EQ(2, j["my array"][2]["nested1"].get<int>());
-        ASSERT_EQ(3, j["my array"][2]["nested2"].get<int>());
-        ASSERT_EQ(4, j["my array"][2]["nested3"].get<int>());
+    ASSERT_EQ("string", j["my array"][0].get<std::string>());
+    ASSERT_EQ(true, j["my array"][1].get<bool>());
+    ASSERT_EQ(2, j["my array"][2]["nested1"].get<int>());
+    ASSERT_EQ(3, j["my array"][2]["nested2"].get<int>());
+    ASSERT_EQ(4, j["my array"][2]["nested3"].get<int>());
 
-        auto newRoot = j.get<TelemetryObject>();
-        ASSERT_EQ(root, newRoot);
-    }
+    auto newRoot = j.get<TelemetryObject>();
+    ASSERT_EQ(root, newRoot);
+}
 
-    TEST(TestTelemetryValueImpl, SerialiseToString) // NOLINT
-    {
-        TelemetryObject obj;
-        TelemetryValue value;
-        value.set("test");
-        obj.set("key", value);
-        TelemetrySerialiser serialiser;
-        ASSERT_EQ(R"({"key":"test"})", serialiser.serialise(obj));
-    }
+TEST(TestTelemetryValueImpl, SerialiseToString) // NOLINT
+{
+    TelemetryObject obj;
+    TelemetryValue value;
+    value.set("test");
+    obj.set("key", value);
+    TelemetrySerialiser serialiser;
+    ASSERT_EQ(R"({"key":"test"})", serialiser.serialise(obj));
+}
 
-    TEST(TestTelemetryValueImpl, DeserialiseToTelemetryObject) // NOLINT
-    {
-        TelemetryObject obj;
-        TelemetryValue value;
-        value.set("test");
-        obj.set("key", value);
-        std::string jsonString = R"({"key":"test"})";
-        TelemetrySerialiser serialiser;
-        auto deserialised = serialiser.deserialise(jsonString);
-        ASSERT_EQ(obj, deserialised);
-    }
+TEST(TestTelemetryValueImpl, DeserialiseToTelemetryObject) // NOLINT
+{
+    TelemetryObject obj;
+    TelemetryValue value;
+    value.set("test");
+    obj.set("key", value);
+    std::string jsonString = R"({"key":"test"})";
+    TelemetrySerialiser serialiser;
+    auto deserialised = serialiser.deserialise(jsonString);
+    ASSERT_EQ(obj, deserialised);
 }
--- base/tests/Common/TelemetryHelperImpl/TestTelemetryValue.cpp
+++ base/tests/Common/TelemetryHelperImpl/TestTelemetryValue.cpp
@@ -4,109 +4,131 @@
 ///
 ///******************************************************************************************************/
 
-//#include <Common/TelemetryImpl/TelemetryValue.h>
-//
-//#include <include/gtest/gtest.h>
-//
-//const int TEST_INTEGER = 10;
-//const bool TEST_BOOL = true;
-//const std::string TEST_STRING = "Test String";  // NOLINT
-//
-//// Construction
-//TEST(TestTelemetryValueImpl, Construction) // NOLINT
-//{
-//    Common::Telemetry::TelemetryValue telemetryValue;
-//    ASSERT_EQ(NodeType::value, telemetryValue.getType());
-//}
-//
-//TEST(TestTelemetryValueImpl, ConstructionWithString) // NOLINT
-//{
-//    Common::Telemetry::TelemetryValue telemetryValue("key", TEST_STRING);
-//    ASSERT_EQ(NodeType::value, telemetryValue.getType());
-//    ASSERT_EQ(Common::Telemetry::ValueType::string_type, telemetryValue.getValueType());
-//    ASSERT_EQ(TEST_STRING, telemetryValue.getString());
-//}
-//
-//TEST(TestTelemetryValueImpl, ConstructionWithInt) // NOLINT
-//{
-//    Common::Telemetry::TelemetryValue telemetryValue("key", TEST_INTEGER);
-//    ASSERT_EQ(NodeType::value, telemetryValue.getType());
-//    ASSERT_EQ(Common::Telemetry::ValueType::integer_type, telemetryValue.getValueType());
-//    ASSERT_EQ(TEST_INTEGER, telemetryValue.getInteger());
-//}
-//
-//TEST(TestTelemetryValueImpl, ConstructionWithBool) // NOLINT
-//{
-//    Common::Telemetry::TelemetryValue telemetryValue("key", TEST_BOOL);
-//    ASSERT_EQ(NodeType::value, telemetryValue.getType());
-//    ASSERT_EQ(Common::Telemetry::ValueType::boolean_type, telemetryValue.getValueType());
-//    ASSERT_EQ(TEST_BOOL, telemetryValue.getBoolean());
-//}
-//
-//// Integers
-//TEST(TestTelemetryValueImpl, IntegerValue) // NOLINT
-//{
-//    Common::Telemetry::TelemetryValue telemetryValue;
-//    telemetryValue.set(TEST_INTEGER);
-//
-//    ASSERT_EQ(Common::Telemetry::ValueType::integer_type, telemetryValue.getValueType());
-//    ASSERT_EQ(TEST_INTEGER, telemetryValue.getInteger());
-//}
-//
-//TEST(TestTelemetryValueImpl, GetIntegerThrowsWhenSetToString) // NOLINT
-//{
-//    Common::Telemetry::TelemetryValue telemetryValue;
-//    telemetryValue.set(TEST_STRING);
-//    ASSERT_EQ(Common::Telemetry::ValueType::string_type, telemetryValue.getValueType());
-//    EXPECT_THROW(telemetryValue.getInteger(), std::bad_variant_access);  // NOLINT
-//}
-//
-//// Strings
-//TEST(TestTelemetryValueImpl, StringValue) // NOLINT
-//{
-//    Common::Telemetry::TelemetryValue telemetryValue;
-//    telemetryValue.set(TEST_STRING);
-//
-//    ASSERT_EQ(Common::Telemetry::ValueType::string_type, telemetryValue.getValueType());
-//    ASSERT_EQ(TEST_STRING, telemetryValue.getString());
-//}
-//
-//TEST(TestTelemetryValueImpl, CStringValue) // NOLINT
-//{
-//    Common::Telemetry::TelemetryValue telemetryValue;
-//    telemetryValue.set("astring");
-//
-//    ASSERT_EQ(Common::Telemetry::ValueType::string_type, telemetryValue.getValueType());
-//    ASSERT_EQ("astring", telemetryValue.getString());
-//}
-//
-//TEST(TestTelemetryValueImpl, GetStringThrowsWhenSetToInt) // NOLINT
-//{
-//    Common::Telemetry::TelemetryValue telemetryValue;
-//    telemetryValue.set(TEST_INTEGER);
-//    ASSERT_EQ(Common::Telemetry::ValueType::integer_type, telemetryValue.getValueType());
-//    EXPECT_THROW(telemetryValue.getString(), std::bad_variant_access);  // NOLINT
-//}
-//
-//// Bools
-//TEST(TestTelemetryValueImpl, BoolValue) // NOLINT
-//{
-//    Common::Telemetry::TelemetryValue telemetryValue;
-//    telemetryValue.set(TEST_BOOL);
-//    ASSERT_EQ(Common::Telemetry::ValueType::boolean_type, telemetryValue.getValueType());
-//    ASSERT_EQ(TEST_BOOL, telemetryValue.getBoolean());
-//}
-//
-//TEST(TestTelemetryValueImpl, GetBoolThrowsWhenSetToString) // NOLINT
-//{
-//    Common::Telemetry::TelemetryValue telemetryValue;
-//    telemetryValue.set(TEST_STRING);
-//    ASSERT_EQ(Common::Telemetry::ValueType::string_type, telemetryValue.getValueType());
-//    EXPECT_THROW(telemetryValue.getBoolean(), std::bad_variant_access);  // NOLINT
-//}
-//
-//TEST(TestTelemetryValueImpl, GetBoolThrowsWhenNothingSet) // NOLINT
-//{
-//    Common::Telemetry::TelemetryValue telemetryValue;
-//    EXPECT_THROW(telemetryValue.getBoolean(), std::bad_variant_access);  // NOLINT
-//}
+#include <Common/TelemetryHelperImpl/TelemetryValue.h>
+
+#include <include/gtest/gtest.h>
+
+const int TEST_INTEGER = 10;
+const bool TEST_BOOL = true;
+const char* TEST_CSTRING = "Test String";
+const std::string TEST_STRING = TEST_CSTRING;  // NOLINT
+
+using namespace Common::Telemetry;
+
+// Construction
+TEST(TestTelemetryValueImpl, Construction) // NOLINT
+{
+    TelemetryValue telemetryValue;
+    ASSERT_EQ(TelemetryValue::ValueType::unset, telemetryValue.getValueType());
+}
+
+TEST(TestTelemetryValueImpl, ConstructionWithString) // NOLINT
+{
+    TelemetryValue telemetryValue(TEST_STRING);
+    ASSERT_EQ(TelemetryValue::ValueType::string_type, telemetryValue.getValueType());
+    ASSERT_EQ(TEST_STRING, telemetryValue.getString());
+}
+
+TEST(TestTelemetryValueImpl, ConstructionWithCString) // NOLINT
+{
+    TelemetryValue telemetryValue(TEST_CSTRING);
+    ASSERT_EQ(TelemetryValue::ValueType::string_type, telemetryValue.getValueType());
+    ASSERT_EQ(TEST_STRING, telemetryValue.getString());
+}
+
+TEST(TestTelemetryValueImpl, ConstructionWithInt) // NOLINT
+{
+    TelemetryValue telemetryValue(TEST_INTEGER);
+    ASSERT_EQ(TelemetryValue::ValueType::integer_type, telemetryValue.getValueType());
+    ASSERT_EQ(TEST_INTEGER, telemetryValue.getInteger());
+}
+
+TEST(TestTelemetryValueImpl, ConstructionWithBool) // NOLINT
+{
+    TelemetryValue telemetryValue(TEST_BOOL);
+    ASSERT_EQ(TelemetryValue::ValueType::boolean_type, telemetryValue.getValueType());
+    ASSERT_EQ(TEST_BOOL, telemetryValue.getBoolean());
+}
+
+// Integers
+TEST(TestTelemetryValueImpl, IntegerValue) // NOLINT
+{
+    TelemetryValue telemetryValue;
+    telemetryValue.set(TEST_INTEGER);
+
+    ASSERT_EQ(TelemetryValue::ValueType::integer_type, telemetryValue.getValueType());
+    ASSERT_EQ(TEST_INTEGER, telemetryValue.getInteger());
+}
+
+TEST(TestTelemetryValueImpl, GetIntegerThrowsWhenSetToString) // NOLINT
+{
+    TelemetryValue telemetryValue;
+    telemetryValue.set(TEST_STRING);
+    ASSERT_EQ(TelemetryValue::ValueType::string_type, telemetryValue.getValueType());
+    EXPECT_THROW(telemetryValue.getInteger(), std::invalid_argument);  // NOLINT
+}
+
+// Strings
+TEST(TestTelemetryValueImpl, StringValue) // NOLINT
+{
+    TelemetryValue telemetryValue;
+    telemetryValue.set(TEST_STRING);
+
+    ASSERT_EQ(TelemetryValue::ValueType::string_type, telemetryValue.getValueType());
+    ASSERT_EQ(TEST_STRING, telemetryValue.getString());
+}
+
+TEST(TestTelemetryValueImpl, CStringValue) // NOLINT
+{
+    TelemetryValue telemetryValue;
+    telemetryValue.set("astring");
+
+    ASSERT_EQ(TelemetryValue::ValueType::string_type, telemetryValue.getValueType());
+    ASSERT_EQ("astring", telemetryValue.getString());
+}
+
+TEST(TestTelemetryValueImpl, GetStringThrowsWhenSetToInt) // NOLINT
+{
+    TelemetryValue telemetryValue;
+    telemetryValue.set(TEST_INTEGER);
+    ASSERT_EQ(TelemetryValue::ValueType::integer_type, telemetryValue.getValueType());
+    EXPECT_THROW(telemetryValue.getString(), std::invalid_argument);  // NOLINT
+}
+
+// Bools
+TEST(TestTelemetryValueImpl, BoolValue) // NOLINT
+{
+    TelemetryValue telemetryValue;
+    telemetryValue.set(TEST_BOOL);
+    ASSERT_EQ(TelemetryValue::ValueType::boolean_type, telemetryValue.getValueType());
+    ASSERT_EQ(TEST_BOOL, telemetryValue.getBoolean());
+}
+
+TEST(TestTelemetryValueImpl, GetBoolThrowsWhenSetToString) // NOLINT
+{
+    TelemetryValue telemetryValue;
+    telemetryValue.set(TEST_STRING);
+    ASSERT_EQ(TelemetryValue::ValueType::string_type, telemetryValue.getValueType());
+    EXPECT_THROW(telemetryValue.getBoolean(), std::invalid_argument);  // NOLINT
+}
+
+TEST(TestTelemetryValueImpl, GetBoolThrowsWhenNothingSet) // NOLINT
+{
+    TelemetryValue telemetryValue;
+    EXPECT_THROW(telemetryValue.getBoolean(), std::invalid_argument);  // NOLINT
+}
+// Operators
+TEST(TestTelemetryValueImpl, EqualityMatchingItems) // NOLINT
+{
+    TelemetryValue telemetryValue1(TEST_INTEGER);
+    TelemetryValue telemetryValue2(TEST_INTEGER);
+    ASSERT_TRUE(telemetryValue1 == telemetryValue2);
+}
+
+TEST(TestTelemetryValueImpl, EqualityDifferingItems) // NOLINT
+{
+    TelemetryValue telemetryValue1(TEST_INTEGER);
+    TelemetryValue telemetryValue2(999);
+    ASSERT_TRUE(telemetryValue1 != telemetryValue2);
+}
+
