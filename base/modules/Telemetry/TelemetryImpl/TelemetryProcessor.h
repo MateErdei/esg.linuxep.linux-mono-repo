650a5acf4b11ad427f36b84c426c8b83361f5f8c
Alex Cooper <alex.cooper@sophos.com>
2019-04-30 12:04:24 +0100

    [base] [LINUXEP-7973] Update header to refelect removal of functions.

--- base/modules/Telemetry/TelemetryImpl/TelemetryProcessor.h
+++ base/modules/Telemetry/TelemetryImpl/TelemetryProcessor.h
@@ -10,20 +10,10 @@ Copyright 2019, Sophos Limited.  All rights reserved.
 #include "../../../thirdparty/nlohmann-json/json.hpp"
 #include <Common/FileSystem/IFileSystem.h>
 
-class TelemetryProcessor {
-public:
-    explicit TelemetryProcessor(std::string  jsonOutputPath);
-
-    void run();
-
-private:
-    Path m_telemetryJsonOutput;
-
+namespace Telemetry::TelemetryProcessor
+{
     void addTelemetry(const std::string& sourceName, const std::string& json);
     void gatherTelemetry();
-    void sendTelemetry();
     void saveTelemetryToDisk(const std::string& jsonOutputFile);
     std::string getSerialisedTelemetry();
-
-};
-
+}
