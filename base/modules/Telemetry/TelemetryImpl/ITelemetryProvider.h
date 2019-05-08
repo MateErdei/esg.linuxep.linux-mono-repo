08127fdb0a4cec9ffad7b835d80a5b9525482e12
Alex Cooper <alex.cooper@sophos.com>
2019-05-08 15:37:11 +0100

    [base] [LINUXEP-7973] clangformat / optimise imports broke an include

--- base/modules/Telemetry/TelemetryImpl/ITelemetryProvider.h
+++ base/modules/Telemetry/TelemetryImpl/ITelemetryProvider.h
@@ -5,6 +5,7 @@ Copyright 2019, Sophos Limited.  All rights reserved.
 ******************************************************************************************************/
 
 #pragma once
+#include <string>
 
 namespace Telemetry
 {
