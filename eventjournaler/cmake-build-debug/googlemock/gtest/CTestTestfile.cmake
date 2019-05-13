c65ceded828068e79372e5073d2c062979c3e423
EthanVinceUrwin <ethan.vince-urwin@sophos.com>
2019-05-13 17:32:48 +0100

    [eventjournaler] [LINUXEP-8100] Updated sspl-plugin-template to take the googletest repo as a submodule rather than hard-coding it.

---
+++ eventjournaler/cmake-build-debug/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pair/CLionProjects/sspl-tools/3rdparty-googletest/googletest
+# Build directory: /home/pair/CLionProjects/sspl-tools/3rdparty-googletest/cmake-build-debug/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
