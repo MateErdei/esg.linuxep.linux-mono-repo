6360f4f906373a2d7763b27d19f0c91124e871d0
EthanVinceUrwin <ethan.vince-urwin@sophos.com>
2019-05-14 09:51:31 +0100

    [edr] [LINUXEP-8100] Deleting actual googletest files from this repo

--- edr/cmake-build-debug/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pair/CLionProjects/sspl-tools/3rdparty-googletest/googlemock
-# Build directory: /home/pair/CLionProjects/sspl-tools/3rdparty-googletest/cmake-build-debug/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
