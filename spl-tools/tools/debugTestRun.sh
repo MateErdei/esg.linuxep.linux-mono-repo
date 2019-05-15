7f3c274fe329bce15e82307c6a74c29c666d1b96
Douglas Leeder <douglas.leeder@sophos.com>
2019-05-15 16:11:37 +0100

    [spl-tools] [LINUXEP-7815] Attempt to verify git checkout works.

--- spl-tools/tools/debugTestRun.sh
+++ spl-tools/tools/debugTestRun.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+set -ex
+
+function failure()
+{
+    local EXIT=$1
+    shift
+    echo "$@" >&2
+    exit $EXIT
+}
+
 function update()
 {
     if [[ -d $2/.git ]]
@@ -14,6 +24,7 @@ function update()
 ## Sort out SSH key
 
 update ssh://git@stash.sophos.net:7999/linuxep/everest-base.git sspl-base
+[[ -d sspl-base ]] || failure 1 "Failed to checkout Base"
 update ssh://git@stash.sophos.net:7999/linuxep/thininstaller.git thininstaller
 update ssh://git@stash.sophos.net:7999/linuxep/everest-systemproducttests.git sspl-systemtests
 update ssh://git@stash.sophos.net:7999/linuxep/sspl-plugin-mdr-component.git sspl-plugin-mdr-component
@@ -22,6 +33,7 @@ update ssh://git@stash.sophos.net:7999/linuxep/sspl-plugin-mdr-componentsuite.gi
 
 mkdir -p /redist
 [[ -d /redist/binaries ]] || mount -t nfs allegro.eng.sophos:/redist  /redist
+
 
 cd sspl-base
 ./build.sh --debug
