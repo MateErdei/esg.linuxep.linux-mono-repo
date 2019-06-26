d471e3870373048b810e9b19357b391204e791d9
jakemclanaghan <jake.mclanaghan@sophos.com>
2019-06-26 16:50:02 +0100

    [av] [LINUXEP-8359] Fixed script to take path to release package as argument

--- av/products/distribution/getReleasePackageVersion.py
+++ av/products/distribution/getReleasePackageVersion.py
@@ -3,20 +3,22 @@ import os
 import sys
 
 def main(argv):
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
-    path_to_release_package = os.path.join("..", "..", "build-files", "release-package.xml")
+    path_to_release_package = argv[1]
+    if not os.path.exists(path_to_release_package):
+        sys.stderr.write("ERROR - path: {} \tdoes not exist\n".format(path_to_release_package))
+        return 1
     try:
         with open(path_to_release_package) as release_package:
             document = xml.dom.minidom.parse(release_package)
             top_level_node = document.childNodes[0]
             version = top_level_node.getAttribute("version")
     except Exception as e:
-        print("failed to get version from release-package.xml with error: {}".format(e))
-        version = None
+        sys.stderr.write("failed to get version from release-package.xml with error: {}\n".format(e))
+        return 2
 
-    return version
+    sys.stdout.write(version)
+    return 0
 
 
 if __name__ == '__main__':
-    sys.exit(sys.stdout.write(main(sys.argv)))
+    sys.exit(main(sys.argv))
