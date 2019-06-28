393c4f5d167f4102fa1862fd7607539a95e0f6aa
AlexCooper <--replace-all>
2019-06-28 14:23:43 +0100

    [spl-tools] [LINUXEP-8239] remove comments

--- spl-tools/dogfood/get_logs_from_s3.sh
+++ spl-tools/dogfood/get_logs_from_s3.sh
@@ -5,12 +5,3 @@ set -e
 [[ -n $DEST ]] || DEST=/mnt/uk-filer6/linux/SSPLDogfood/
 
 aws s3 mv s3://sspl-dogfood-feedback/ "$DEST" --recursive --region eu-west-1
-
-# Copy logs from S3 into dated and versioned folders on filer6
-#python "${0%/*}/processFeedback.py" $INCOMING $DEST
-
-# Update Dogfood LogMonitor UI
-#python "${0%/*}/logMonitor.py" "${0%/*}/cloud_machines.txt" "${0%/*}/opm_machines.txt"
-
-# Remove logs older than 4 weeks
-#find $DEST -mindepth 1 -path "${DEST}OLD LOGS" -prune -o -type d -ctime +28 -print -exec rm -rf {} \;
