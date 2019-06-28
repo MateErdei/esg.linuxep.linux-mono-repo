76a0b3f0808ac10743cf3aef23ad1fa0403e6c9d
TrevorMcGing <trevor.mcging@sophos.com>
2019-06-28 16:37:26 +0100

    [bugfix]remove googletest submodule [+MK]

--- versig/build.sh
+++ versig/build.sh
@@ -34,18 +34,24 @@ function build()
     cd $BASE
     ## Need to do this before we set LD_LIBRARY_PATH, since it uses ssh
     ## which doesn't like our openssl
-    git submodule sync --recursive || exitFailure 34 "Failed to sync submodule configuration"
-    GIT_SSL_NO_VERIFY=true  \
-        git -c http.sslVerify=false submodule update --init --recursive || {
-        sleep 1
-        echo ".gitmodules:"
-        cat .gitmodules
-        echo ".git/config:"
-        cat .git/config
-        exitFailure 33 "Failed to get googletest via git"
-    }
+#    git submodule sync --recursive || exitFailure 34 "Failed to sync submodule configuration"
+#    GIT_SSL_NO_VERIFY=true  \
+#        git -c http.sslVerify=false submodule update --init --recursive || {
+#        sleep 1
+#        echo ".gitmodules:"
+#        cat .gitmodules
+#        echo ".git/config:"
+#        cat .git/config
+#        exitFailure 33 "Failed to get googletest via git"
+#    }
 
     unpack_scaffold_gcc_make $BASE/input
+
+    GOOGLETESTTAR=$BASE/input/googletest-release-1.8.1.tar.gz
+    pushd $BASE/tests
+    tar xzf $GOOGLETESTTAR
+    mv googletest-release-1.8.1 googletest
+    popd
 
     OPENSSL_TAR=$BASE/input/openssl.tar
     [[ -f $OPENSSL_TAR ]] || exitFailure 12 "Failed to find openssl"
