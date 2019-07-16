4a13d8fba6a8e1360a3161dccccdaa7c7bef4879
Jake McLanaghan <jake.mclanaghan@sophos.com>
2019-07-16 15:43:46 +0100

    [spl-tools] [tLINUXDAR-135] test

--- spl-tools/jenkins/violagang.sh
+++ spl-tools/jenkins/violagang.sh
@@ -8,6 +8,17 @@ THIS_DIR=$(readlink -f ${0%/*})
 
 touch /tmp/thisJenkinsJobMadeByViolaGang
 
-echo "=> Updating existing packages..."
-apt-get update
-apt-get dist-upgrade -qy
+function update_apt()
+{
+    echo "=> Updating existing packages with apt..."
+    apt-get update
+    apt-get dist-upgrade -qy
+}
+
+function update_yum()
+{
+    echo "=> Updating existing packages with yum..."
+    yum -y update
+}
+
+which apt-get &>/dev/null && update_apt || echo update_yum
