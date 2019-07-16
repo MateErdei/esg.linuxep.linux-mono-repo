44d7ec88c3c5ea11db91ad9eabd54c6388d0d49d
Jake McLanaghan <jake.mclanaghan@sophos.com>
2019-07-16 14:55:58 +0100

    [spl-tools] [tLINUXDAR-135] test

--- spl-tools/jenkins/violagang.sh
+++ spl-tools/jenkins/violagang.sh
@@ -4,4 +4,10 @@
 # Script to automatically configure or update a Jenkins ubuntu node
 #
 
+THIS_DIR=$(readlink -f ${0%/*})
+
 touch /tmp/thisJenkinsJobMadeByViolaGang
+
+log_info "Updating existing packages..."
+apt-get update
+apt-get dist-upgrade -qy
