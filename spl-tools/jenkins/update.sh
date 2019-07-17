258c709934a9ee01343060a6bbcb4f41f4d667c9
Jake McLanaghan <jake.mclanaghan@sophos.com>
2019-07-17 13:46:22 +0100

    [spl-tools] [LINUXDAR-135] error handling

--- spl-tools/jenkins/update.sh
+++ spl-tools/jenkins/update.sh
@@ -13,22 +13,23 @@ touch /tmp/thisJenkinsJobMadeByViolaGang
 function update_apt()
 {
     echo "=> Updating existing packages with apt..."
-    apt-get update || exit 1
-    apt-get upgrade -qy || exit 1
+    apt-get update || return 1
+    apt-get upgrade -qy || return 1
 }
 
 function update_yum()
 {
     echo "=> Updating existing packages with yum..."
-    yum -y update || exit 1
+    yum -y update || return 1
 }
 
 if [ -n "$(which apt-get)" ]
 then
-    update_yum || rm -rf ./${THIS_DIR} && exit 1
+    update_apt || rm -rf ./${THIS_DIR} && exit 1
 elif [ -n "$(which yum)" ]
 then
     update_yum || rm -rf ./${THIS_DIR} && exit 1
 else
+    echo System has neither apt-get or yum
     exit 1
 fi
