b6063f17fd25c48368644de97bae43acd27b664d
Jake McLanaghan <jake.mclanaghan@sophos.com>
2019-07-17 17:17:55 +0100

    [spl-tools] [LINUXDAR-135] diagnostic

--- spl-tools/jenkins/update.sh
+++ spl-tools/jenkins/update.sh
@@ -16,6 +16,7 @@ function update_apt()
     apt-get update || return 1
     apt-get upgrade -qy || return 1
     rm /home/jenkins/differentMachineId || return 1
+    [[ ! -f /home/jenkins/differentMachineId ]] || return 1
 }
 
 function update_yum()
