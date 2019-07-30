0bec3fa438cd3002b6340528fe358011332c0064
Wellington Machingauta <wellington.machingauta@sophos.com>
2019-07-30 15:00:45 +0000

    [base] [LINUXDAR-549]  diagnose-error-for-journal-since...review comments

--- base/tests/diagnose/SystemCommandsTests.cpp
+++ base/tests/diagnose/SystemCommandsTests.cpp
@@ -35,16 +35,16 @@ public:
     void setupMocks(size_t numberOfMockProcesses)
     {
         ASSERT_TRUE(numberOfMockProcesses > 0);
-        mockProcessIndex_ = 0;
+        m_mockProcessIndex = 0;
 
         for (size_t i = 0; i < numberOfMockProcesses; ++i)
         {
-            mockProcesses_.emplace_back(new MockProcess());
+            m_mockProcesses.emplace_back(new MockProcess());
         }
 
         Common::ProcessImpl::ProcessFactory::instance().replaceCreator([this]() {
-            return (mockProcessIndex_ == mockProcesses_.size()) ? nullptr
-                                                                : std::move(mockProcesses_[mockProcessIndex_++]);
+            return (m_mockProcessIndex == m_mockProcesses.size()) ? nullptr
+                                                                  : std::move(m_mockProcesses[m_mockProcessIndex++]);
         });
 
         m_mockFileSystem = new StrictMock<MockFileSystem>();
@@ -53,12 +53,12 @@ public:
     }
 
     Common::Logging::ConsoleLoggingSetup m_loggingSetup;
-    std::vector<std::unique_ptr<MockProcess>> mockProcesses_;
+    std::vector<std::unique_ptr<MockProcess>> m_mockProcesses;
 
     StrictMock<MockFileSystem>* m_mockFileSystem;
 
 private:
-    size_t mockProcessIndex_ = 0;
+    size_t m_mockProcessIndex = 0;
 };
 
 // These tests assume that keys in SystemTelemetryConfig objects are iterated over in dictionary order.
@@ -66,17 +66,17 @@ private:
 TEST_F(DiagnoseSystemCommandsTests, RunCommandRunAndWritesToFileOk) // NOLINT
 {
     setupMocks(1);
-    auto& mockProcess_ = mockProcesses_[0];
+    auto& mockProcess = m_mockProcesses[0];
 
-    std::string systemDirPath("/Nerver/Createdir/");
+    std::string systemDirPath("/Never/Createdir/");
     diagnose::SystemCommands systemCommands(systemDirPath);
     EXPECT_CALL(*m_mockFileSystem, isExecutable(_)).WillRepeatedly(Return(true));
 
-    EXPECT_CALL(*mockProcess_, exec(_, _));
-    EXPECT_CALL(*mockProcess_, setOutputLimit(_));
-    EXPECT_CALL(*mockProcess_, wait(_, _)).WillOnce(Return(Common::Process::ProcessStatus::FINISHED));
-    EXPECT_CALL(*mockProcess_, output()).WillOnce(Return(L_dfTLocalLines));
-    EXPECT_CALL(*mockProcess_, exitCode()).WillRepeatedly(Return(EXIT_SUCCESS));
+    EXPECT_CALL(*mockProcess, exec(_, _));
+    EXPECT_CALL(*mockProcess, setOutputLimit(_));
+    EXPECT_CALL(*mockProcess, wait(_, _)).WillOnce(Return(Common::Process::ProcessStatus::FINISHED));
+    EXPECT_CALL(*mockProcess, output()).WillOnce(Return(L_dfTLocalLines));
+    EXPECT_CALL(*mockProcess, exitCode()).WillRepeatedly(Return(EXIT_SUCCESS));
     // write to file called with output
     EXPECT_CALL(*m_mockFileSystem, writeFile((systemDirPath + "df"), L_dfTLocalLines));
 
@@ -88,17 +88,17 @@ TEST_F(DiagnoseSystemCommandsTests, RunCommandRunAndWritesToFileOk) // NOLINT
 TEST_F(DiagnoseSystemCommandsTests, RunCommandHandlesProcessExitCodeIsFailure) // NOLINT
 {
     setupMocks(1);
-    auto& mockProcess_ = mockProcesses_[0];
+    auto& mockProcess = m_mockProcesses[0];
 
-    std::string systemDirPath("/Nerver/Createdir/");
+    std::string systemDirPath("/Never/Createdir/");
     diagnose::SystemCommands systemCommands(systemDirPath);
     EXPECT_CALL(*m_mockFileSystem, isExecutable(_)).WillRepeatedly(Return(true));
 
-    EXPECT_CALL(*mockProcess_, exec(_, _));
-    EXPECT_CALL(*mockProcess_, setOutputLimit(_));
-    EXPECT_CALL(*mockProcess_, wait(_, _)).WillOnce(Return(Common::Process::ProcessStatus::FINISHED));
-    EXPECT_CALL(*mockProcess_, output()).WillOnce(Return("Command failed with unexpected blabhla"));
-    EXPECT_CALL(*mockProcess_, exitCode()).WillOnce(Return(2));
+    EXPECT_CALL(*mockProcess, exec(_, _));
+    EXPECT_CALL(*mockProcess, setOutputLimit(_));
+    EXPECT_CALL(*mockProcess, wait(_, _)).WillOnce(Return(Common::Process::ProcessStatus::FINISHED));
+    EXPECT_CALL(*mockProcess, output()).WillOnce(Return("Command failed with unexpected blabhla"));
+    EXPECT_CALL(*mockProcess, exitCode()).WillOnce(Return(2));
 
     // write to file called with error message
     EXPECT_CALL(*m_mockFileSystem, writeFile((systemDirPath + "df"), _));
@@ -111,21 +111,21 @@ TEST_F(DiagnoseSystemCommandsTests, RunCommandHandlesProcessExitCodeIsFailure) /
 TEST_F(DiagnoseSystemCommandsTests, RunCommandMultipleTimesWithTimeout) // NOLINT
 {
     setupMocks(2);
-    std::string systemDirPath("/Nerver/Createdir/");
+    std::string systemDirPath("/Never/Createdir/");
     diagnose::SystemCommands systemCommands(systemDirPath);
 
     InSequence s;
     { // mockProcesses_[0]
         EXPECT_CALL(*m_mockFileSystem, isExecutable(_)).WillRepeatedly(Return(true));
 
-        EXPECT_CALL(*mockProcesses_[0], setOutputLimit(_));
-        EXPECT_CALL(*mockProcesses_[0], exec(_, _));
-        EXPECT_CALL(*mockProcesses_[0], wait(_, _)).WillOnce(Return(Common::Process::ProcessStatus::TIMEOUT));
-        EXPECT_CALL(*mockProcesses_[0], kill());
+        EXPECT_CALL(*m_mockProcesses[0], setOutputLimit(_));
+        EXPECT_CALL(*m_mockProcesses[0], exec(_, _));
+        EXPECT_CALL(*m_mockProcesses[0], wait(_, _)).WillOnce(Return(Common::Process::ProcessStatus::TIMEOUT));
+        EXPECT_CALL(*m_mockProcesses[0], kill());
 
         // write to file called with timeout as reason for failure
         std::string timeoutError("Process execution timed out after 10s running: '/usr/bin/df -h'");
-        EXPECT_CALL(*m_mockFileSystem, writeFile("/Nerver/Createdir/df-timeout", timeoutError));
+        EXPECT_CALL(*m_mockFileSystem, writeFile(systemDirPath + "df-timeout", timeoutError));
     }
 
     auto retCodeFail = systemCommands.runCommand("df", { "-h" }, "df-timeout");
@@ -134,11 +134,11 @@ TEST_F(DiagnoseSystemCommandsTests, RunCommandMultipleTimesWithTimeout) // NOLIN
     { // mockProcesses_[1]
         EXPECT_CALL(*m_mockFileSystem, isExecutable(_)).WillRepeatedly(Return(true));
 
-        EXPECT_CALL(*mockProcesses_[1], setOutputLimit(_));
-        EXPECT_CALL(*mockProcesses_[1], exec(_, _));
-        EXPECT_CALL(*mockProcesses_[1], wait(_, _)).WillOnce(Return(Common::Process::ProcessStatus::FINISHED));
-        EXPECT_CALL(*mockProcesses_[1], output()).WillOnce(Return(L_dfTLocalLines));
-        EXPECT_CALL(*mockProcesses_[1], exitCode()).WillRepeatedly(Return(EXIT_SUCCESS));
+        EXPECT_CALL(*m_mockProcesses[1], setOutputLimit(_));
+        EXPECT_CALL(*m_mockProcesses[1], exec(_, _));
+        EXPECT_CALL(*m_mockProcesses[1], wait(_, _)).WillOnce(Return(Common::Process::ProcessStatus::FINISHED));
+        EXPECT_CALL(*m_mockProcesses[1], output()).WillOnce(Return(L_dfTLocalLines));
+        EXPECT_CALL(*m_mockProcesses[1], exitCode()).WillRepeatedly(Return(EXIT_SUCCESS));
         // write to file called with output
         EXPECT_CALL(*m_mockFileSystem, writeFile((systemDirPath + "df"), L_dfTLocalLines));
     }
