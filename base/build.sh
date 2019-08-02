4ef41deb798bb3ae6c9c02f82ab3c81c362eb4f3
TrevorMcGing <trevor.mcging@sophos.com>
2019-08-02 12:35:52 +0100

    [base] [LINUXDAR-546] When python unit tests are executed the pyc files are may be regenerated Added lines in build.sh script to remove any created .pyc and .pyo files that we do not want to distribute.

--- base/build.sh
+++ base/build.sh
@@ -355,6 +355,11 @@ function build()
                 exitFailure 16 "Unit tests failed for $PRODUCT: $EXITCODE"
             }
         fi
+
+        # python would have been executed in unit tests creating pyc and or pyo files
+        # need to make sure these are removed before creating distribution files.
+        find ../build${BITS} -name "*.pyc" -type f | xargs -r rm -f
+        find ../build${BITS} -name "*.pyo" -type f | xargs -r rm -f
     fi
     make -j${NPROC} install || exitFailure 17 "Failed to install $PRODUCT"
     make dist || exitFailure 18 "Failed to create distribution"
