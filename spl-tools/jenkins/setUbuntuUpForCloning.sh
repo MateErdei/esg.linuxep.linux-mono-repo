d183139b065943addbf62d3fba506c430d5d8816
Jake McLanaghan <jake.mclanaghan@sophos.com>
2019-08-12 10:06:45 +0100

    [spl-tools] [LINUXDAR-135] refactored

---
+++ spl-tools/jenkins/setUbuntuUpForCloning.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xe
+
+if [[ -z "${VSPHERE_IP}" ]]
+
+then
+	echo "Didn't get IP address for ${VM_NAME}" >2
+	exit 1
+fi
+/bin/bash jenkins/runScriptOnRemoteMachine.sh deleteDifferentMachineId.sh root@${VSPHERE_IP}
+sleep 120
