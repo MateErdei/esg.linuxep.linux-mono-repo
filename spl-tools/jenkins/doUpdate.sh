3f9a0d45cf6b642fa02dc66c6218270bc370df8d
Jake McLanaghan <jake.mclanaghan@sophos.com>
2019-08-12 10:51:32 +0100

    [spl-tools] [LINUXDAR-135] addressing review comments

--- spl-tools/jenkins/doUpdate.sh
+++ spl-tools/jenkins/doUpdate.sh
@@ -10,5 +10,5 @@ then
 	echo "Didn't get IP address for ${VM_NAME}" >2
 	exit 1
 fi
-/bin/bash jenkins/runScriptOnRemoteMachine.sh update.sh root@${VSPHERE_IP}
+jenkins/runScriptOnRemoteMachine.sh update.sh root@${VSPHERE_IP}
 sleep 120
