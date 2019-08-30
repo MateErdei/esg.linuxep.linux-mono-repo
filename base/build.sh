87bc664d77326b19c088a506ea3b71482be0ae0b
Mel Inkersole <mel.inkersole@sophos.com>
2019-08-30 12:24:51 +0100

    [base] [LINUXDAR-150] Added GCoverage for CLion builds [+RL]

--- base/build.sh
+++ base/build.sh
@@ -321,6 +321,7 @@ function build()
         -DREDIST="${REDIST}" \
         -DINPUT="${REDIST}" \
         -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+        -DCMAKE_NO_GCOV="true" \
         .. \
         || exitFailure 14 "Failed to configure $PRODUCT"
     make -j${NPROC} copy_libs || exitFailure 15 "Failed to build $PRODUCT"
