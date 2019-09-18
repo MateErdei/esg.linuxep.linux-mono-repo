d167601c32ab02035508905f747682387ae5da14
Wellington Machingauta <wellington.machingauta@sophos.com>
2019-09-18 15:08:47 +0100

    [spl-tools] [LINUXDAR-676]remove warning and send email

--- spl-tools/bootstrap_ubuntu.sh
+++ spl-tools/bootstrap_ubuntu.sh
@@ -2,12 +2,6 @@
 
 echo "Bootstrapping Ubuntu"
 
-grep filer6 /etc/fstab > /dev/null
-if [[ "x$?" != 'x0' ]]
-then
-   echo "Please clear your cached vagrant box, and get latest from filer6 by running 'vagrant destroy <box-name>' and 'vagrant box remove <box - name>'"
-fi
-
 apt-get update --fix-missing
 apt-get install build-essential cmake python-pip auditd libc6-i386 --assume-yes
 pip2 install robotframework pyzmq protobuf paramiko pycapnp watchdog psutil kittyfuzzer katnip
