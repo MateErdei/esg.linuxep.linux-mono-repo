a398133c36c1c132380add0320b501a0b2b480b4
TrevorMcGing <trevor.mcging@sophos.com>
2019-09-19 16:30:14 +0100

    [base] [LINUXDAR_680] Added unit test for processing sophos_alias.txt file

--- base/tests/UpdateScheduler/TestUpdatePolicyTranslator.cpp
+++ base/tests/UpdateScheduler/TestUpdatePolicyTranslator.cpp
@@ -756,6 +756,65 @@ TEST_F(TestUpdatePolicyTranslator, ParseMDRPolicy) // NOLINT
     EXPECT_EQ(scheduledUpdateTime.minute, 0);
 }
 
+TEST_F(TestUpdatePolicyTranslator, ParseMDRPolicyWithSophosAliasOverrideSet) // NOLINT
+{
+    auto mockFileSystem = new StrictMock<MockFileSystem>();
+    std::unique_ptr<MockFileSystem> mockIFileSystemPtr = std::unique_ptr<MockFileSystem>(mockFileSystem);
+    Tests::replaceFileSystem(std::move(mockIFileSystemPtr));
+
+    UpdatePolicyTranslator translator;
+
+
+    EXPECT_CALL(*mockFileSystem, isFile("/installroot/base/update/var/sophos_alias.txt")).WillOnce(Return(true));
+    std::string customerFileUrl("http://ostia.eng.sophos/customer");
+    EXPECT_CALL(*mockFileSystem, readFile(_)).WillOnce(Return(customerFileUrl));
+
+    auto settingsHolder = translator.translatePolicy(mdrSSPLBasePolicy);
+    auto config = settingsHolder.configurationData;
+
+    EXPECT_TRUE(settingsHolder.updateCacheCertificatesContent.empty());
+
+    EXPECT_EQ(config.getCredentials().getUsername(), "ff705287d6b62738f8e672865cff1b05");
+    EXPECT_EQ(config.getCredentials().getPassword(), "ff705287d6b62738f8e672865cff1b05");
+    EXPECT_EQ(config.getCertificatePath(), "/opt/sophos-spl/base/update/certs");
+    EXPECT_EQ(config.getInstallArguments()[0], "--instdir");
+    EXPECT_EQ(config.getInstallArguments()[1], "/opt/sophos-spl");
+    EXPECT_EQ(config.getSystemSslCertificatePath(), ":system:");
+    EXPECT_EQ(config.getUpdateCacheSslCertificatePath(), "");
+
+    auto urls = config.getSophosUpdateUrls();
+    ASSERT_EQ(urls.size(), 1);
+    EXPECT_EQ(urls[0], customerFileUrl);
+
+    EXPECT_TRUE(config.getLocalUpdateCacheUrls().empty());
+
+    const auto& primarySubscription = config.getPrimarySubscription();
+    EXPECT_EQ(primarySubscription.baseVersion(), "");
+    EXPECT_EQ(primarySubscription.rigidName(), "ServerProtectionLinux-Base");
+    EXPECT_EQ(primarySubscription.tag(), "RECOMMENDED");
+    EXPECT_EQ(primarySubscription.fixVersion(), "");
+
+    const auto& productsSubscription = config.getProductsSubscription();
+    ASSERT_EQ(productsSubscription.size(), 1);
+    EXPECT_EQ(productsSubscription[0].baseVersion(), "");
+    EXPECT_EQ(productsSubscription[0].rigidName(), "ServerProtectionLinux-Plugin-MDR");
+    EXPECT_EQ(productsSubscription[0].tag(), "RECOMMENDED");
+    EXPECT_EQ(productsSubscription[0].fixVersion(), "");
+
+    const auto& features = config.getFeatures();
+    std::vector<std::string> expectedFeatures = { "CORE", "SDU", "MDR" };
+    EXPECT_EQ(features, expectedFeatures);
+
+    EXPECT_EQ(config.getPolicyProxy(), SulDownloader::suldownloaderdata::Proxy());
+    EXPECT_EQ(settingsHolder.schedulerPeriod, std::chrono::minutes(60));
+    EXPECT_EQ(settingsHolder.scheduledUpdate.getEnabled(), true);
+    auto scheduledUpdateTime = settingsHolder.scheduledUpdate.getScheduledTime();
+    EXPECT_EQ(scheduledUpdateTime.weekDay, 3);
+    EXPECT_EQ(scheduledUpdateTime.hour, 11);
+    EXPECT_EQ(scheduledUpdateTime.minute, 0);
+}
+
+
 TEST_F(TestUpdatePolicyTranslator, ParseMDRPolicyWithNoFeaturesReportsErrorInLog) // NOLINT
 {
     auto mockFileSystem = new StrictMock<MockFileSystem>();
