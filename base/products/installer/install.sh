c9d0a62e5a70c3a39f52892ce6f02818e7132abf
TrevorMcGing <trevor.mcging@sophos.com>
2019-10-02 11:38:13 +0100

    [base] [LINUXDAR-688] Moved location of when the telemetry config file permissions are set to later on. Found the settings were being changed back to root after.

--- base/products/installer/install.sh
+++ base/products/installer/install.sh
@@ -320,6 +320,9 @@ done
 
 ln -snf "liblog4cplus-2.0.so" "${SOPHOS_INSTALL}/base/lib64/liblog4cplus.so"
 
+chown -h "root:${GROUP_NAME}" ${SOPHOS_INSTALL}/base/etc/telemetry-config.json
+chmod 440 ${SOPHOS_INSTALL}/base/etc/telemetry-config.json
+
 chown root:${GROUP_NAME} "${SOPHOS_INSTALL}/base"
 chown root:${GROUP_NAME} "${SOPHOS_INSTALL}/base/bin"
 chmod u+x "${SOPHOS_INSTALL}/base/bin"/*
