651199e904d08296dca2032875426c5479189f32
Gesner <gesner.passos@sophos.com>
2019-10-08 13:30:22 +0100

    [base] [LINUXDAR-148] Check Jenkins usage of xml report.

--- base/buildRunTestsAndProduceCoverageReport.sh
+++ base/buildRunTestsAndProduceCoverageReport.sh
@@ -12,7 +12,12 @@ do
     esac
     shift
 done
-echo "Build with python coverage capability"
-python3 -m build_scripts.artisan_fetch build/release-package.xml
-./build.sh --python-coverage
 echo "build Run Tests and Produce Coverge Report.sh with systemtests: ${SYSTEM_TEST}"
+cp /mnt/filer6/linux/SSPL/users/Gesner/coverage.xml ${SYSTEM_TEST}/coverage.xml
+#python3 -m build_scripts.artisan_fetch build/release-package.xml
+#./build.sh --python-coverage
+#echo "Keep the coverage for unit tests"
+#cp modules/.coverage  unit_tests_coverage
+#pushd ${SYSTEM_TEST}
+#RERUNFAILED=true BASE_SOURCE="${BASE}" bash SupportFiles/jenkins/jenkinsBuildCommand.sh -i SMOKE
+#popd
