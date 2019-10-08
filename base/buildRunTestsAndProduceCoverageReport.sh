e6dc98551b4bb2bce7bb80c2a1249b62a1d42531
Gesner <gesner.passos@sophos.com>
2019-10-08 15:08:06 +0100

    [base] [LINUXDAR-148] path adjustments for the coverage report

--- base/buildRunTestsAndProduceCoverageReport.sh
+++ base/buildRunTestsAndProduceCoverageReport.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -x
 
 # assumes this is executed from everest-base/
 BASE=$(pwd)
@@ -15,7 +15,7 @@ do
     shift
 done
 echo 'remove previous coverage results'
-rm -rf modules/.coverage unit_tests_coverage system_tests_coverage /tmp/register_central* /tmp/mcs_router*
+sudo rm -rf modules/.coverage unit_tests_coverage system_tests_coverage /tmp/register_central* /tmp/mcs_router*
 echo "build Run Tests and Produce Coverge Report.sh with systemtests: ${SYSTEM_TEST}"
 python3 -m build_scripts.artisan_fetch build/release-package.xml
 ./build.sh --python-coverage
@@ -24,14 +24,17 @@ echo "Keep the coverage for unit tests"
 cp modules/.coverage  unit_tests_coverage
 pushd ${SYSTEM_TEST}
 echo 'run system tests'
-RERUNFAILED=true BASE_SOURCE="${SDDS_COMPONENT}" bash SupportFiles/jenkins/jenkinsBuildCommand.sh -i SMOKE -t 'MCS Status Sent When Message Relay Changed'
+RERUNFAILED=true BASE_SOURCE="${SDDS_COMPONENT}" bash SupportFiles/jenkins/jenkinsBuildCommand.sh -i SMOKE -t 'MCSStatusSentWhenMessageRelayChanged'
 echo 'combine system tests results'
-sudo chmod a+wr /tmp/register_central* /tmp/mcs_router*
+USER=$(whoami)
+sudo chown ${USER} /tmp/register_central* /tmp/mcs_router*
 ${SDDS_COMPONENT}/pyCoverage combine /tmp/register_central* /tmp/mcs_router*
 echo 'replace name to the original source'
-sed -i "s_/opt/sophos-spl/base/lib64_${BASE}/modules/mcs_router_g" .coverage
+sed -i "s#/opt/sophos-spl/base/lib64#${BASE}/modules/mcsrouter#g" .coverage
+sed -i "s/py.0/py/g" .coverage
+sed -i "s_/opt/sophos-spl/base/lib_${SDDS_COMPONENT}/files/base/lib_g" .coverage
 cp .coverage ${BASE}/system_tests_coverage
-popd$
+popd
 echo 'combine coverage results and publish it '
 ${SDDS_COMPONENT}/pyCoverage combine unit_tests_coverage system_tests_coverage
-${SDDS_COMPONENT}/pyCoverage xml
+${SDDS_COMPONENT}/pyCoverage xml -i
