331fbcca57ebb6693c1337b02f41b8e4af0f6ade
Gesner <gesner.passos@sophos.com>
2019-10-08 13:58:22 +0100

    [base] [LINUXDAR-148] handle build and production of coverage report.

--- base/buildRunTestsAndProduceCoverageReport.sh
+++ base/buildRunTestsAndProduceCoverageReport.sh
@@ -15,12 +15,15 @@ do
     shift
 done
 echo "build Run Tests and Produce Coverge Report.sh with systemtests: ${SYSTEM_TEST}"
-cp /mnt/filer6/linux/SSPL/users/Gesner/coverage.xml "${SYSTEM_TEST}/coverage.xml"
-echo "done"
-#python3 -m build_scripts.artisan_fetch build/release-package.xml
-#./build.sh --python-coverage
-#echo "Keep the coverage for unit tests"
-#cp modules/.coverage  unit_tests_coverage
-#pushd ${SYSTEM_TEST}
-#RERUNFAILED=true BASE_SOURCE="${BASE}" bash SupportFiles/jenkins/jenkinsBuildCommand.sh -i SMOKE
-#popd
+python3 -m build_scripts.artisan_fetch build/release-package.xml
+./build.sh --python-coverage
+echo "Keep the coverage for unit tests"
+cp modules/.coverage  unit_tests_coverage
+pushd ${SYSTEM_TEST}
+RERUNFAILED=true BASE_SOURCE="${BASE}/build64/output/SDDS_COMPONENT" bash SupportFiles/jenkins/jenkinsBuildCommand.sh -i SMOKE -t 'MCS Status Sent When Message Relay Changed'
+${BASE}/build64/output/SDDS_COMPONENT/pyCoverage combine /tmp/register_central* /tmp/mcs_router*
+sed -i "s_/opt/sophos-spl/base/lib64_${BASE}/modules/mcs_router_g" .coverage
+cp .coverage ${BASE}/system_tests_coverage
+popd$
+${BASE}/build64/output/SDDS_COMPONENT/pyCoverage combine unit_tests_coverage system_tests_coverage
+${BASE}/build64/output/SDDS_COMPONENT/pyCoverage xml
