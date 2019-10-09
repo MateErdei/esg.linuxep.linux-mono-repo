efd5407292fb794bd2bafcd3aaa39703288c5d3e
Gesner <gesner.passos@sophos.com>
2019-10-09 12:49:59 +0100

    [base] [LINUXDAR-148] Avoid explosion of files created in /tmp folder.

--- base/modules/mcsrouter/mcsrouter/cov_mcs_router.py
+++ base/modules/mcsrouter/mcsrouter/cov_mcs_router.py
@@ -4,7 +4,8 @@ import mcsrouter.mcs_router as mcs_router
 
 if __name__ == "__main__":
     import coverage
-    cov = coverage.Coverage(data_file="/tmp/mcs_router", data_suffix=True)
+    cov = coverage.Coverage(data_file="/tmp/mcs_router_coverage",
+                            data_suffix=False, cover_pylib=False, auto_data=True, branch=True)
     cov.start()
     code = 1
     try:
--- base/modules/mcsrouter/mcsrouter/cov_register_central.py
+++ base/modules/mcsrouter/mcsrouter/cov_register_central.py
@@ -4,7 +4,8 @@ import mcsrouter.register_central as register_central
 
 if __name__ == "__main__":
     import coverage
-    cov = coverage.Coverage(data_file="/tmp/register_central", data_suffix=True)
+    cov = coverage.Coverage(data_file="/tmp/register_central_coverage",
+                            data_suffix=False, cover_pylib=False, auto_data=True, branch=True)
     cov.start()
     code = 1
     try:
