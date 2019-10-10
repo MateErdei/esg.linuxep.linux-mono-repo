7e5f90bf1bede13764d7c6013ec95032a01af7c7
Gesner <gesner.passos@sophos.com>
2019-10-10 09:24:28 +0100

    [base] [LINUXDAR-148] Attempt to run nova tests in pythoncoverage.

--- base/buildRunTestsAndProduceCoverageReport.sh
+++ base/buildRunTestsAndProduceCoverageReport.sh
@@ -27,7 +27,7 @@ echo "Keep the coverage for unit tests"
 cp modules/.coverage  unit_tests_coverage
 pushd ${SYSTEM_TEST}
 echo 'run system tests'
-RERUNFAILED=true BASE_SOURCE="${SDDS_COMPONENT}" bash SupportFiles/jenkins/jenkinsBuildCommand.sh  -i CENTRAL -i FAKE_CLOUD -i MCS -i MCS_ROUTER -i MESSAGE_RELAY -i REGISTRATION -i THIN_INSTALLER -i UPDATE_CACHE || echo "Test failure does not prevent the coverage report. "
+RERUNFAILED=true BASE_SOURCE="${SDDS_COMPONENT}" bash SupportFiles/jenkins/jenkinsBuildCommand.sh  -i CENTRAL -i FAKE_CLOUD -i MCS -i MCS_ROUTER -i MESSAGE_RELAY -i REGISTRATION -i THIN_INSTALLER -i UPDATE_CACHE -s testnovaproxy || echo "Test failure does not prevent the coverage report. "
 echo 'combine system tests results'
 USER=$(whoami)
 sudo chown ${USER} /tmp/register_central* /tmp/mcs_router*
