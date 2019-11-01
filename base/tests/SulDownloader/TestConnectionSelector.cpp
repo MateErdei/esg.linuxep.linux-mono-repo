2343a77b398131bc1dd72f1a432d1916fa244710
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2019-11-01 15:29:16 +0000

    [base] [LINUXDAR-775]add unit test

--- base/tests/SulDownloader/TestConnectionSelector.cpp
+++ base/tests/SulDownloader/TestConnectionSelector.cpp
@@ -352,3 +352,59 @@ TEST_F( // NOLINT
         connectionCandidates[2].getUpdateLocationURL().c_str(), "https://sophosupdate.sophos.com/latest/warehouse");
     EXPECT_TRUE(connectionCandidates[2].getProxy().empty());
 }
+
+TEST_F( // NOLINT
+        ConnectionSelectorTest,
+        getConnectionCandidatesShouldReturnValidCandidatesWhenObfuscatedPassedHasBeenPadded)
+{
+    std::string oldString = R"("proxy": {
+                               "url": "noproxy:",
+                               "credential": {
+                               "username": "",
+                               "password": "",
+                               "proxyType": ""
+                                }
+                               },)";
+
+    std::string newString = R"("proxy": {
+                               "url": "http://testproxy.com",
+                               "credential": {
+                               "username": "testproxyusername",
+                               "password": "CCAcWWDAL1sCAV1YiHE20dTJIXMaTLuxrBppRLRbXgGOmQBrysz16sn7RuzXPaX6XHk================",
+                               "proxyType": "2"
+                                }
+                               },)";
+
+    suldownloaderdata::ConfigurationData configurationData =
+            suldownloaderdata::ConfigurationData::fromJsonSettings(createJsonString(oldString, newString));
+
+    ConnectionSelector selector;
+    auto connectionCandidates = selector.getConnectionCandidates(configurationData);
+
+    // connectionCandidates should be ordered. With cache updates first.
+    ASSERT_EQ(connectionCandidates.size(), 3);
+
+    EXPECT_TRUE(connectionCandidates[0].isCacheUpdate());
+    EXPECT_STREQ(connectionCandidates[0].getCredentials().getUsername().c_str(), "administrator");
+    EXPECT_STREQ(connectionCandidates[0].getCredentials().getPassword().c_str(), "password");
+    EXPECT_STREQ(connectionCandidates[0].getUpdateLocationURL().c_str(), "https://cache.sophos.com/latest/warehouse");
+    EXPECT_EQ(connectionCandidates[0].getProxy().getUrl(), "noproxy:"); // UpdateCache bypass proxy
+    EXPECT_STREQ(connectionCandidates[0].getProxy().getCredentials().getUsername().c_str(), "testproxyusername");
+    EXPECT_STREQ(connectionCandidates[0].getProxy().getCredentials().getDeobfuscatedPassword().c_str(), "password");
+
+    EXPECT_FALSE(connectionCandidates[1].isCacheUpdate());
+    EXPECT_STREQ(connectionCandidates[1].getCredentials().getUsername().c_str(), "administrator");
+    EXPECT_STREQ(connectionCandidates[1].getCredentials().getPassword().c_str(), "password");
+    EXPECT_STREQ(
+            connectionCandidates[1].getUpdateLocationURL().c_str(), "https://sophosupdate.sophos.com/latest/warehouse");
+    EXPECT_EQ(connectionCandidates[1].getProxy().getUrl(), "http://testproxy.com");
+    EXPECT_STREQ(connectionCandidates[1].getProxy().getCredentials().getUsername().c_str(), "testproxyusername");
+    EXPECT_STREQ(connectionCandidates[1].getProxy().getCredentials().getDeobfuscatedPassword().c_str(), "password");
+
+    EXPECT_FALSE(connectionCandidates[2].isCacheUpdate());
+    EXPECT_STREQ(connectionCandidates[2].getCredentials().getUsername().c_str(), "administrator");
+    EXPECT_STREQ(connectionCandidates[2].getCredentials().getPassword().c_str(), "password");
+    EXPECT_STREQ(
+            connectionCandidates[2].getUpdateLocationURL().c_str(), "https://sophosupdate.sophos.com/latest/warehouse");
+    EXPECT_TRUE(connectionCandidates[2].getProxy().empty());
+}
