7f03370df95a84a4b9a45d3ef006ef956f538481
Gesner <gesner.passos@sophos.com>
2019-11-05 11:45:36 +0000

    [base] [LINUXDAR-141] Initial setup of PluginApi Tests for the fuzzer.

--- base/tests/LibFuzzerTests/PluginApiTest.cpp
+++ base/tests/LibFuzzerTests/PluginApiTest.cpp
@@ -9,8 +9,8 @@ Copyright 2018-2019, Sophos Limited.  All rights reserved.
 #include "FuzzerUtils.h"
 
 #include "google/protobuf/text_format.h"
-#include <Common/Logging/ConsoleLoggingSetup.h>
-#include <modules/Common/ApplicationConfiguration/IApplicationConfiguration.h>
+
+//#include <modules/Common/ApplicationConfiguration/IApplicationConfiguration.h>
 #include <modules/Common/ApplicationConfiguration/IApplicationPathManager.h>
 #include <modules/Common/FileSystem/IFileSystem.h>
 #include <modules/Common/PluginApiImpl/PluginResourceManagement.h>
@@ -20,52 +20,23 @@ Copyright 2018-2019, Sophos Limited.  All rights reserved.
 #include <modules/Common/ZMQWrapperApi/IContextSharedPtr.h>
 #include <modules/Common/ZeroMQWrapper/IIPCException.h>
 #include <modules/Common/ZeroMQWrapper/ISocketRequester.h>
-
+//#include <modules/ManagementAgent/ManagementAgentImpl/ManagementAgentMain.h>
 #include <vector_strings.pb.h>
 #include <thread>
 #ifdef HasLibFuzzer
 #    include <libprotobuf-mutator/src/libfuzzer/libfuzzer_macro.h>
 #    include <libprotobuf-mutator/src/mutator.h>
 #endif
-
+#include <fcntl.h>
 #include <future>
-#include <Common/FileSystemImpl/FilePermissionsImpl.h>
-#include <tests/Common/Helpers/FilePermissionsReplaceAndRestore.h>
-#include <Common/FileSystem/IFilePermissions.h>
+#include <stddef.h>
+#include <stdint.h>
 
-/** this class is just to allow the tests to be executed without requiring root*/
-class NullFilePermission : public Common::FileSystem::FilePermissionsImpl
-{
-public:
-    void chown(const Path& , const std::string& , const std::string& ) const override
-    {
-    }
-    void chmod(const Path& , __mode_t ) const override
-    {
-    }
-    gid_t getGroupId(const std::string& groupString) const override
-    {
-        return 1;
-    }
-};
-
-class ScopedFilePermission
-{
-public:
-    ScopedFilePermission()
-    {
-        Tests::replaceFilePermissions( std::unique_ptr<Common::FileSystem::IFilePermissions>( new NullFilePermission()) );
-    }
-    ~ScopedFilePermission()
-    {
-        Tests::restoreFilePermissions();
-    }
-};
 
 ///**
-// * This class holds the Dummy PLugin instance running on its own thread and the zmq socket to communicate with
-// * the plugin.
-// * It is constructed as static object from the fuzzer test to allow the dummy plugin to be running for the whole
+// * This class holds the ManagementAgent instance running on its own thread and the zmq socket to communicate with
+// * the management agent.
+// * It is constructed as static object from the fuzzer test to allow the management agent to be running for the whole
 // * test phase.
 //example of file:
 //parts: "hello"
@@ -77,44 +48,33 @@ class DummyPlugin: public  Common::PluginApi::IPluginCallbackApi
 public:
     void applyNewPolicy(const std::string& ) override
     {
-        std::cout << "Apply policy" << std::endl;
+
     }
 
     void queueAction(const std::string& ) override
     {
-        std::cout << "queue action" << std::endl;
+
     }
 
     void onShutdown() override
     {
-        std::cout << "shutdown" << std::endl;
+
     }
     Common::PluginApi::StatusInfo getStatus(const std::string& ) override
     {
-        std::cout << "get status" << std::endl;
         return Common::PluginApi::StatusInfo{};
     }
     std::string getTelemetry() override
     {
-        std::cout << "send telemetry" << std::endl;
         return std::string{};
     }
 
 };
 
-void setup()
-{
-    auto fileS = Common::FileSystem::fileSystem();
-
-    std::string fullPath = Common::FileSystem::join(Common::ApplicationConfiguration::SOPHOS_INSTALL, "");
-    fileS->makedirs("/tmp/fuzz/var/ipc/plugins/");
-
-}
-
-class DummyPluginRunner : public Runner
+class ManagementRunner : public Runner
 {
 public:
-    ~DummyPluginRunner()
+    ~ManagementRunner()
     {
         try
         {
@@ -122,14 +82,8 @@ public:
         }catch (std::exception&)
         {}
     }
-    DummyPluginRunner() : Runner()
+    ManagementRunner() : Runner()
     {
-        setup();
-        Common::Logging::ConsoleLoggingSetup setup;
-        Common::ApplicationConfiguration::applicationConfiguration().setData(
-                Common::ApplicationConfiguration::SOPHOS_INSTALL, "/tmp/fuzz");
-        ScopedFilePermission scopedFilePermission;
-        Common::ApplicationConfiguration::applicationConfiguration().setData("Test", "inproc://test.ipc");
         m_contextPtr = Common::ZMQWrapperApi::createContext();
         Common::ZMQWrapperApi::IContextSharedPtr copyContext = m_contextPtr;
         setMainLoop([copyContext, this]() {
@@ -142,18 +96,18 @@ public:
         std::this_thread::sleep_for(std::chrono::seconds(2));
 
         m_requester = m_contextPtr->getRequester();
-        std::string dummy_address =
+        std::string mng_address =
                 Common::ApplicationConfiguration::applicationPathManager().getPluginSocketAddress("Test");
         m_requester->setTimeout(1000);
-        m_requester->connect(dummy_address);
+        m_requester->connect(mng_address);
     }
 
-    /** Helper method to allow sending requests to Dummy Plugin*/
+    /** Helper method to allow sending requests to Management Agent*/
     Common::PluginProtocol::DataMessage getReply(const std::vector<std::string>& content)
     {
         try
         {
-            m_requester->write(content);
+            m_requester->write(content );
             auto reply = m_requester->read();
             Common::PluginProtocol::Protocol protocol;
             return protocol.deserialize(reply);
@@ -177,7 +131,7 @@ public:
         }
     }
 
-    bool dummyPluginRunning() { return threadRunning(); }
+    bool managemementAgentRunning() { return threadRunning(); }
 
 private:
     std::promise<void> m_promise;
@@ -193,23 +147,23 @@ DEFINE_PROTO_FUZZER(const VectorStringsProto::Query & query)
 void mainTest(const VectorStringsProto::Query & query)
 {
 #endif
-//    // It is static to ensure that only one DummyPluginRunner will exist while the full fuzz test runs.
+//    // It is static to ensure that only one ManagementRunner will exist while the full fuzz test runs.
 //    // this also means that the 'state' is not dependent only on the input message which may become a problem
-//    // but allows for a very robust verification of the DummyPlugin as it will be test throughout the whole
+//    // but allows for a very robust verification of the ManagementAgent as it will be test througout the whole
 //    // fuzz test.
-    static DummyPluginRunner dummyRunner;
+    static ManagementRunner managementRunner;
     std::vector<std::string> data;
     for( auto & part: query.parts())
     {
         data.push_back(part);
     }
-    if( !dummyRunner.dummyPluginRunning())
+    if( !managementRunner.managemementAgentRunning())
     {
-            std::cout << "Dummy Plugin may have crashed"<< std::endl;
+            std::cout << "Management agent may have crashed"<< std::endl;
 
             abort();
     }
-    dummyRunner.getReply(data);
+    managementRunner.getReply(data);
 }
 
 /**
