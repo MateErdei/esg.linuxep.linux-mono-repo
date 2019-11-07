c0b2e951725e066c8784a3a437157c7af5b64d3c
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2019-11-07 11:05:16 +0000

    [spl-tools] [BUGFIX] add requirements for running fuzzer

--- spl-tools/bootstrap_ubuntu.sh
+++ spl-tools/bootstrap_ubuntu.sh
@@ -3,7 +3,7 @@
 echo "Bootstrapping Ubuntu"
 
 apt-get update --fix-missing
-apt-get install build-essential cmake python-pip auditd libc6-i386 --assume-yes
+apt-get install build-essential cmake python-pip python3-pip auditd libc6-i386 clang --assume-yes
 pip2 install robotframework pyzmq protobuf paramiko pycapnp watchdog psutil kittyfuzzer katnip
 
 cp /home/vagrant/auditdConfig.txt /vagrant
