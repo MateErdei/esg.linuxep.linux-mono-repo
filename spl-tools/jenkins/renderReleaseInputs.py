fab1caa79d88b0cd4bd4925e1641e85670b6cb89
AlexCooper <--replace-all>
2019-11-15 17:49:03 +0000

    [spl-tools] [LINUXDAR-650] make dependency graph clearer

--- spl-tools/jenkins/renderReleaseInputs.py
+++ spl-tools/jenkins/renderReleaseInputs.py
@@ -3,7 +3,7 @@
 import os
 import json
 from graphviz import Digraph
-dot = Digraph(comment='Dependencies', engine="neato", format='png')
+dot = Digraph(comment='Dependencies', engine="dot", format='png')
 
 json_file_source = "releaseInputs.json"
 json_string = ""
@@ -18,22 +18,29 @@ root_dictionary = json.loads(json_string)
 
 all_inputs = {}
 
+dot.attr(rankdir='LR')
+
+dot.node("KEY - SSPL Components", color='blue', shape='doublecircle')
+dot.node("KEY - Repos we build", color='blue')
+
 for component_name, info_dict in root_dictionary.items():
     component_id = "{} ({})".format(component_name, info_dict["version"])
-    dot.node(component_id)
+
+    if "sspl" in component_id.lower():
+        dot.node(component_id, color='blue', shape='doublecircle')
+    else:
+        dot.node(component_id, color='blue')
 
     for i in info_dict["inputs"]:
         input_id = "{} ({})".format(i["name"], i["version"])
-        dot.node(input_id)
         dot.edge(input_id, component_id)
-
 
 dot.attr('graph', overlap='false')
 
 # Save as png image
 dot.attr('graph', format='png')
-# If you run this lcoally you can change view to be true for it to open.
-dot.render("dependencies", view=False)
+# If you run this locally you can change view to be true for it to open.
+dot.render("dependencies", view=True)
 
 # Save as pdf
 dot.attr('graph', format='pdf')
