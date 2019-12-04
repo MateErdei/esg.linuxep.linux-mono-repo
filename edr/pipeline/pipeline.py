1259ddf5bbf3ea7fe71be06932f6832be5e74536
wellington machingauta <wellington.machingauta@sophos.com>
2019-12-04 16:39:58 +0000

    [edr] [LINUXDAR-802] - collect and publish robot results

--- edr/pipeline/pipeline.py
+++ edr/pipeline/pipeline.py
@@ -5,10 +5,9 @@ def robot_task(machine: tap.Machine):
     try:
         machine.run('python', machine.inputs.test_scripts / 'RobotFramework.py')
     finally:
-        machine.run('python', machine.inputs.test_scripts / 'debug_tap.py')
+        machine.run('python', machine.inputs.test_scripts / 'move_robot_results.py')
         machine.output_artifact('/opt/test/logs', 'logs')
         machine.output_artifact('/opt/test/results', 'results')
-
 
 @tap.pipeline(version=1, component='edr_plugin')
 def edr_plugin(stage: tap.Root, context: tap.PipelineContext):
@@ -17,5 +16,5 @@ def edr_plugin(stage: tap.Root, context: tap.PipelineContext):
     )
 
     with stage.group('test'):
-        stage.task('robot_framework', robot_task, machine=tap.Machine('ubuntu1804_x64_server_en_us', inputs=inputs, platform=tap.Platform.Linux))
+        stage.task('ubuntu1804_x64', robot_task, machine=tap.Machine('ubuntu1804_x64_server_en_us', inputs=inputs, platform=tap.Platform.Linux))
 
