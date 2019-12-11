866e22f9faedbb755fe330257650d08cdfa56f1f
Douglas Leeder <douglas.leeder@sophos.com>
2019-12-11 14:42:18 +0000

    [spl-tools] [LINUXDAR-885] Add better error detection in SetupCIBuildScripts.sh

--- spl-tools/setup/SetupCIBuildScripts.sh
+++ spl-tools/setup/SetupCIBuildScripts.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+function failure()
+{
+    echo "$@"
+    exit 1
+}
+
 if [[ $(id -u) != 0 ]]
 then
-    echo "Please run this script as root."
-    exit 0
+    failure "Please run this script as root."
 fi
 
 SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
@@ -14,12 +19,14 @@ echo [global] > pip.conf
 echo index-url = https://tap-artifactory1.eng.sophos/artifactory/api/pypi/pypi/simple >> pip.conf
 echo cert = $SCRIPTDIR/sophos_certs.pem >> pip.conf
 
-python3 -m pip install build_scripts
+python3 -m pip install build_scripts || failure "Unable to install build_scripts"
 popd
 rm -rf ~/.config/pip/
 
 #Update the hardcoded paths to filer 5 and filer 6 in the build scripts to work on dev machines
 BUILD_SCRIPT_INSTALL_DIR="$(python3 -m pip show build_scripts | grep "Location" | cut -d" " -f2)/build_scripts"
+
+[[ -d ${BUILD_SCRIPT_INSTALL_DIR} ]] || failure "Unable to find build_scripts install location"
 
 sed -i 's/\"\/mnt\/filer6\"/\"\/mnt\/filer6\/bfr\/\"/g' ${BUILD_SCRIPT_INSTALL_DIR}/build*
 sed -i 's/\/mnt\/filer\/bir/\/uk-filer5\/prodro\/bir/g' ${BUILD_SCRIPT_INSTALL_DIR}/build*
