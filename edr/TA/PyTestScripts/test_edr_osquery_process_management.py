799ad6de2f4754586e7d678b8e83c25d505c2eb7
Alex Cooper <Alex.Cooper@Sophos.com>
2020-01-02 17:15:24 +0000

    [edr] [LINUXDAR-811] Add test to check that the flags file gets regenerated even if it exists already.

--- edr/TA/PyTestScripts/test_edr_osquery_process_management.py
+++ edr/TA/PyTestScripts/test_edr_osquery_process_management.py
@@ -29,7 +29,6 @@ def _wait_for_osquery_to_stop(pid):
 
     raise AssertionError("osqueryd failed to stop")
 
-
 def test_edr_plugin_starts_osquery(sspl_mock, edr_plugin_instance):
     edr_plugin_instance.start_edr()
     _wait_for_osquery_to_run()
@@ -68,4 +67,34 @@ def test_edr_plugin_kills_existing_osquery_instances(sspl_mock, edr_plugin_insta
     _wait_for_osquery_to_stop(initial_osquery_pid)
     new_osquery_pid = _wait_for_osquery_to_run()
     assert (initial_osquery_pid != new_osquery_pid)
+
+
+def test_edr_plugin_regenerates_flags_file(sspl_mock, edr_plugin_instance):
+    edr_plugin_instance.start_edr()
+    initial_osquery_pid = _wait_for_osquery_to_run()
+    edr_plugin_instance.stop_edr()
+    _wait_for_osquery_to_stop(initial_osquery_pid)
+
+    flags_file_path = os.path.join(os.environ['SOPHOS_INSTALL'], 'plugins/edr/etc/osquery.flags')
+
+    # Quick sanity check to ensure the file has something we expect in it.
+    with open(flags_file_path, 'r') as flags_file:
+        # Check that something we expect is in the file which means the bad contents from above was overwritten
+        assert ("host_identifier" in flags_file.read())
+
+    # Replace flags file with rubbish text
+    os.remove(flags_file_path)
+    with open(flags_file_path, 'w') as flags_file:
+        flags_file.write("bad content")
+
+    # Start EDR again so it re-generates the flags file.
+    edr_plugin_instance.start_edr()
+    _wait_for_osquery_to_run()
+
+    with open(flags_file_path, 'r') as flags_file:
+        # Check that something we expect is in the file which means the bad contents from above was overwritten
+        assert ("host_identifier" in flags_file.read())
+
+
+
 
