f4193c253fcee169978429c1916af611d49ac5bc
Douglas Leeder <douglas.leeder@sophos.com>
2020-01-13 11:37:50 +0000

    [av] [LINUXDAR-939] Add ScanResponse object.

---
+++ av/modules/scan_messages/ScanResponse.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pair on 13/01/2020.
+//
+
+#include "ScanResponse.h"
---
+++ av/modules/scan_messages/ScanResponse.h
@@ -0,0 +1,16 @@
+//
+// Created by pair on 13/01/2020.
+//
+
+#ifndef SSPL_PLUGIN_MAV_SCANRESPONSE_H
+#define SSPL_PLUGIN_MAV_SCANRESPONSE_H
+
+namespace scan_messages
+{
+    class ScanResponse
+    {
+
+    };
+}
+
+#endif //SSPL_PLUGIN_MAV_SCANRESPONSE_H
