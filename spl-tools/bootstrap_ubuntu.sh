0b1a65b62bf129c06f3651b5a94e77adb1c3e44e
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2020-01-15 14:15:43 +0000

    [spl-tools] [LINUXDAR-923]install packages for local tap tests

--- spl-tools/bootstrap_ubuntu.sh
+++ spl-tools/bootstrap_ubuntu.sh
@@ -5,6 +5,6 @@ echo "Bootstrapping Ubuntu"
 apt-get update --fix-missing
 apt-get install build-essential cmake python-pip python3-pip auditd libc6-i386 clang --assume-yes
 pip2 install robotframework pyzmq protobuf paramiko pycapnp watchdog psutil kittyfuzzer katnip
-pip3 install pycapnp
+pip3 install pycapnp  pyzmq  pytest-html
 
 cp /home/vagrant/auditdConfig.txt /vagrant
