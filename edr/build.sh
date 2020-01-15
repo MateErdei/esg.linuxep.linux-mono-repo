a9045a54186053a146c9a2fd72692f51e2b170e3
Alex Cooper <Alex.Cooper@Sophos.com>
2020-01-15 14:49:35 +0000

    [edr] [LINUXDAR-821] Update rigidname and remove component suite reference from uninstaller. Remove duplicate cxx 17 standard cmake command.

--- edr/build.sh
+++ edr/build.sh
@@ -152,7 +152,7 @@ done
 [[ -n "${PLUGIN_NAME}" ]] || PLUGIN_NAME=${DEFAULT_PRODUCT}
 [[ -n "${PRODUCT}" ]] || PRODUCT=${PLUGIN_NAME}
 [[ -n "${PRODUCT_NAME}" ]] || PRODUCT_NAME="Sophos Endpoint Detection and Response plug-in"
-[[ -n "${PRODUCT_LINE_ID}" ]] || PRODUCT_LINE_ID="ServerProtectionLinux-EDR-Component"
+[[ -n "${PRODUCT_LINE_ID}" ]] || PRODUCT_LINE_ID="ServerProtectionLinux-Plugin-EDR"
 [[ -n "${DEFAULT_HOME_FOLDER}" ]] || DEFAULT_HOME_FOLDER="$PRODUCT"
 
 export NO_REMOVE_GCC=1
