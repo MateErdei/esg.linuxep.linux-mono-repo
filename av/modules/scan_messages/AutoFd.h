62c2afa7cfb65be8b12a0885603dce53d041cef4
Douglas Leeder <douglas.leeder@sophos.com>
2020-01-17 11:27:17 +0000

    [av] [LINUXDAR-946] Add fd() and conversion operator to AutoFd.
    Add [[nodiscard]] on the recommendation of clang-tidy. (Requires C++17)

--- av/modules/scan_messages/AutoFd.h
+++ av/modules/scan_messages/AutoFd.h
@@ -6,7 +6,6 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 
 #pragma once
 
-
 namespace scan_messages
 {
     class AutoFd
@@ -14,9 +13,16 @@ namespace scan_messages
     public:
         explicit AutoFd(int fd=-1);
         ~AutoFd();
+
+#ifdef AUTO_FD_IMPLICIT_INT
+        [[nodiscard]] operator int() const { return m_fd; }
+#endif /* AUTO_FD_IMPLICIT_INT */
+
         void reset(int fd=-1);
-        int get() { return m_fd; }
-        int release();
+        [[nodiscard]] int get() const { return m_fd; }
+        [[nodiscard]] int fd() const { return m_fd; }
+
+        [[nodiscard]] int release();
     private:
         int m_fd;
     };
