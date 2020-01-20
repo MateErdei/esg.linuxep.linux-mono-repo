bf7eb15e4191550ea3b7215753c0a9099c635764
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2020-01-20 10:16:28 +0000

    [edr] [LINUXDAR-812] add untit test for rotate file

--- edr/tests/plugin/TestPlugin.cpp
+++ edr/tests/plugin/TestPlugin.cpp
@@ -9,6 +9,9 @@ Copyright 2018, Sophos Limited.  All rights reserved.
 #include <gtest/gtest.h>
 #include <modules/pluginimpl/Logger.h>
 
+#include <modules/pluginimpl/OsqueryDataManager.h>
+#include <Common/Helpers/TempDir.h>
+
 using namespace Common::FileSystem;
 
 TEST(TestFileSystemExample, binBashShouldExist) // NOLINT
@@ -21,4 +24,23 @@ TEST(TestLinkerWorks, WorksWithTheLogging) // NOLINT
 {
     Common::Logging::ConsoleLoggingSetup consoleLoggingSetup;
     LOGINFO("Produce this logging");
+}
+
+TEST(OsqueryDataManager, rotateFileWorks)
+{
+    OsqueryDataManager m_DataManager;
+    auto* ifileSystem = Common::FileSystem::fileSystem();
+    Tests::TempDir tempdir{"/tmp"};
+
+    std::string basefile = tempdir.dirPath()+"/file";
+
+
+    ifileSystem->writeFileAtomically(basefile + ".1","blah1","/tmp");
+    ifileSystem->writeFileAtomically(basefile +".2","blah2","/tmp");
+    m_DataManager.rotateFiles(basefile,2);
+
+    ASSERT_TRUE(ifileSystem->isFile(basefile + ".2"));
+    ASSERT_EQ(ifileSystem->readFile(basefile + ".2"),"blah1");
+    ASSERT_TRUE(ifileSystem->isFile(basefile + ".3"));
+    ASSERT_EQ(ifileSystem->readFile(basefile + ".3"),"blah2");
 }
