b73a5fd89441e332958aadd11701e8c3175937b1
Gesner Passos <gesner.passos@sophos.com>
2020-01-22 11:40:26 +0000

    [edr] [LINUXDAR-813] clang format

--- edr/tests/manual/LiveQueryReport.cpp
+++ edr/tests/manual/LiveQueryReport.cpp
@@ -3,20 +3,18 @@
 Copyright 2020 Sophos Limited.  All rights reserved.
 
 ******************************************************************************************************/
-#include <string>
-#include <livequery/ResponseData.h>
-#include <livequery/ResponseStatus.h>
-#include <extensions/interface.h>
-#include <osquery/flags.h>
-#include <osquery/flagalias.h>
-#include <iostream>
+#include <Common/Logging/ConsoleLoggingSetup.h>
 #include <livequery/IQueryProcessor.h>
+#include <osquery/flagalias.h>
+#include <osquery/flags.h>
 #include <osqueryclient/OsqueryProcessor.h>
 #include <thirdparty/nlohmann-json/json.hpp>
-#include <Common/Logging/ConsoleLoggingSetup.h>
 
-namespace osquery{
+#include <iostream>
+#include <string>
 
+namespace osquery
+{
     FLAG(bool, decorations_top_level, false, "test");
 }
 
@@ -37,33 +35,32 @@ namespace livequery
         void sendResponse(const std::string& /*correlationId*/, const QueryResponse& response) override
         {
             auto content = serializeToJson(response);
-            nlohmann::json  jsoncontent = nlohmann::json::parse(content);
+            nlohmann::json jsoncontent = nlohmann::json::parse(content);
             std::cout << jsoncontent.dump(2) << std::endl;
         }
     };
 
-}
+} // namespace livequery
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
-    assert(argc==3);
-    if(argc != 3)
+    assert(argc == 3);
+    if (argc != 3)
     {
         std::cerr << "Usage: " << argv[0] << " [socketpath] [query]" << std::endl;
         return 2;
     }
     Common::Logging::ConsoleLoggingSetup loggingSetup;
-    std::string m_socketPath{argv[1]};
-    std::string query{argv[2]};
+    std::string m_socketPath { argv[1] };
+    std::string query { argv[2] };
     livequery::ShowInConsoleDispatcher showInConsoleDispatcher;
-    osqueryclient::OsqueryProcessor osqueryProcessor{m_socketPath};
+    osqueryclient::OsqueryProcessor osqueryProcessor { m_socketPath };
 
     nlohmann::json queryStructure;
     queryStructure["type"] = "sophos.mgt.action.RunLiveQuery";
     queryStructure["name"] = "";
     queryStructure["query"] = query;
 
-    livequery::processQuery(osqueryProcessor, showInConsoleDispatcher,
-            queryStructure.dump(), "anycorrelation" );
+    livequery::processQuery(osqueryProcessor, showInConsoleDispatcher, queryStructure.dump(), "anycorrelation");
     return 0;
 }
