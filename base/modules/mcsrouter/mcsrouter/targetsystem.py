503fd3f3899aac72d225d5865c8deeb9fe00cc57
Ethan Vince-Urwin <ethan.vince-urwin@sophos.com>
2020-01-23 15:11:30 +0000

    [base] [LINUXDAR-744] Made changes as per review comments. Also applied changes as suggested by pylint such as reducing the number of member variables and making some methods into functions

--- base/modules/mcsrouter/mcsrouter/targetsystem.py
+++ base/modules/mcsrouter/mcsrouter/targetsystem.py
@@ -5,7 +5,6 @@
 """
 TargetSystem Module
 """
-# pylint: disable=too-many-lines
 
 import json
 import platform
@@ -19,6 +18,7 @@ import urllib.request
 
 from .utils.byte2utf8 import to_utf8
 
+# Keys must never be a sub-set of another key
 DISTRIBUTION_NAME_MAP = {
     'redhat': 'redhat',
     'ubuntu': 'ubuntu',
@@ -35,8 +35,6 @@ class TargetSystem:
     """
     Represents the system into which we are trying to install.
     """
-    # pylint: disable=too-many-instance-attributes, no-self-use
-    # pylint: disable=too-many-public-methods, too-many-return-statements
 
     def __init__(self, install_dir="."):
         """
@@ -44,90 +42,14 @@ class TargetSystem:
         """
         self._save_uname()
         self.m_install_dir = install_dir
-        self.m_description, self.m_distributor_id, self.m_release = self._collect_lsb_release()
-        self.m_vendor = self.get_vendor()
-        self.m_product = self.m_description
-        self.m_os_version = self.get_os_version()
-
-    def _read_uname(self):
-        """
-        _read_uname
-        :return:
-        """
-        return [platform.node(), platform.release(), platform.machine()]
+        self.m_description, self.m_distributor_id, self.m_release = _collect_lsb_release()
+        self.m_vendor = self._get_vendor()
 
     def _save_uname(self):
-        """
-        _save_uname
-        :return:
-        """
-        self.uname = self._read_uname()
+        self.uname = _read_uname()
         self.last_uname_save = time.time()
 
-    def run_command(self, command, shell=False):
-        """
-        run_command
-        :return:
-        """
-        # Clean environment
-        env = os.environ.copy()
-        try:
-            proc = subprocess.Popen(command, env=env,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE,
-                                    shell=shell)
-            stdout_b, stderr_b = proc.communicate()  # pylint: disable=unused-variable
-            stdout = to_utf8(stdout_b) if stdout_b else ""
-            ret_code = proc.wait()
-            assert ret_code is not None
-        except OSError:
-            ret_code = -1
-            stdout = ""
-        return ret_code, stdout
-
-    def _collect_lsb_release(self):
-        """
-        Collect any lsb_release info
-        """
-        # pylint: disable=too-many-locals, too-many-branches, too-many-statements
-        description = None
-        distributor_id = None
-        release = None
-
-        path = os.environ.get("PATH", "")
-        lsb_release_exes = []
-        for split_path_element in path.split(":"):
-            lsb_release_bin = os.path.join(split_path_element, "lsb_release")
-            if os.path.isfile(lsb_release_bin):
-                lsb_release_exes.append(lsb_release_bin)
-
-        if "/usr/bin/lsb_release" not in lsb_release_exes \
-                and os.path.isfile("/usr/bin/lsb_release"):
-            lsb_release_exes.append("/usr/bin/lsb_release")
-
-        ret_code = -2
-        for lsb_release_exe in lsb_release_exes:
-            (ret_code, stdout) = self.run_command([lsb_release_exe, '-a'])
-            if ret_code == 0:
-                break
-
-        if ret_code == 0:
-            lines = stdout.split("\n")
-            # Each line consists of "Key:<whitespace>*<value>"
-            matcher = re.compile(r"([^:]+):\s*(.+)$")
-            for line in lines:
-                line_matched = matcher.match(line)
-                if line_matched:
-                    if "Description" in line:
-                        description = line_matched.group(2)
-                    elif "Distributor ID" in line:
-                        distributor_id = line_matched.group(2)
-                    elif "Release" in line:
-                        release = line_matched.group(2)
-
-        return description, distributor_id, release
-
-    def get_vendor(self):
+    def _get_vendor(self):
         if self.m_distributor_id:
             vendor = self.m_distributor_id.lower().replace(" ", "").replace("/", "_")
             for key in DISTRIBUTION_NAME_MAP:
@@ -137,7 +59,7 @@ class TargetSystem:
             return vendor
         return None
 
-    def get_os_version(self):
+    def _get_os_version(self):
         # Parse description for ubuntu version
         if self.m_vendor == "ubuntu":
             desc = self.m_description
@@ -152,105 +74,62 @@ class TargetSystem:
         # Attempt to parse the Release field into digit blocks
         release = self.m_release
         if release:
-            os_version = release.split(".")
-            if len(os_version) > 3:
-                os_version = os_version[:3]
-            else:
-                while len(os_version) < 3:
-                    os_version.append("")
-            return os_version
+            os_version = release.split(".") + [""]*3
+            return os_version[:3]
 
         number_block_re = re.compile(r"\d+")
-        if self.m_product:
-            blocks = number_block_re.findall(self.m_product)
+        if self.m_description:
+            blocks = number_block_re.findall(self.m_description)
             blocks_length = len(blocks)
             if blocks_length > 0:
-                return blocks
+                block_trio = blocks + [""]*3
+                return block_trio[:3]
 
         # Final fall-back - use kernel version
         kernel = self.kernel()
         if kernel:
-            return kernel.strip().split(".")
+            kernel_trio = kernel.strip().split(".") + [""]*3
+            return kernel_trio[:3]
 
         return "", "", ""
 
-    def detect_distro(self):
-        """
-        Detect the name of distro.
-        """
+    def _detect_distro(self):
         distro_check_files = ['/etc/lsb-release',
                               '/etc/issue',
                               '/etc/centos-release',
                               '/etc/redhat-release',
                               '/etc/system-release']
-
-        distro_identified = None
         for distro_file in distro_check_files:
+            distro_identified = self._check_distro_file(distro_file)
             if distro_identified:
-                break
-            distro_identified = self.check_distro_file(
-                distro_file)
+                return distro_identified
+        self.m_description = 'unknown'
+        return 'unknown'
 
-        if not distro_identified:
-            distro_identified = 'unknown'
-            self.m_product = 'unknown'
-
-        return distro_identified
-
-    def check_distro_file(self, distro_file):
-        """
-        Check a file to see if it describes the distro
-        """
+    def _check_distro_file(self, distro_file):
         if not os.path.exists(distro_file):
             return None
         with open(distro_file) as check_file:
             original_contents = check_file.readline()
-
         check_contents = original_contents.lower().replace(" ", "")
 
         # conversion mapping between string at start of file and distro name
         for distro_string in DISTRIBUTION_NAME_MAP:
             if check_contents.startswith(distro_string):
-                self.m_product = original_contents.strip()
+                self.m_description = original_contents.strip()
                 return DISTRIBUTION_NAME_MAP[distro_string]
         return None
 
-    def kernel(self):
-        """
-        Detect the kernel we are running on.
-        """
-        return self.uname[1]
-
-    def detect_is_ec2_instance(self):
-        """
-        detect_is_ec2_instance
-        :return:
-        """
+    def _detect_is_ec2_instance(self):
         if os.path.isfile(os.path.join(self.m_install_dir, "etc", "amazon")):
             return True
 
-        try:
-            with open('/sys/hypervisor/uuid') as uuid_file:
-                if "ec2" in uuid_file.read():
-                    return True
-        except EnvironmentError:
-            pass
-
-        try:
-            with open("/sys/devices/virtual/dmi/id/bios_version") as reader:
-                if "amazon" in reader.read():
-                    return True
-        except EnvironmentError:
-            pass
-
-        try:
-            with open("/sys/devices/virtual/dmi/id/product_uuid") as reader:
-                data = reader.read()
-
-            if data.startswith("EC2"):
+        ec2_dict = {'/sys/hypervisor/uuid': 'ec2',
+                    "/sys/devices/virtual/dmi/id/bios_version": "amazon",
+                    "/sys/devices/virtual/dmi/id/product_uuid": "EC2"}
+        for key in ec2_dict:
+            if _find_string_in_file(key, ec2_dict[key]):
                 return True
-        except EnvironmentError:
-            pass
 
         try:
             with open("/var/log/dmesg") as reader:
@@ -284,8 +163,7 @@ class TargetSystem:
         detect_instance_info
         :return:
         """
-
-        if self.detect_is_ec2_instance():
+        if self._detect_is_ec2_instance():
             try:
                 proxy_handler = urllib.request.ProxyHandler({})
                 opener = urllib.request.build_opener(proxy_handler)
@@ -296,9 +174,15 @@ class TargetSystem:
                 return {"region": aws_info["region"],
                         "accountId": aws_info["accountId"],
                         "instanceId": aws_info["instanceId"]}
-            except urllib.error.URLError:
+            except:  # pylint: disable=bare-except
                 pass
         return None
+
+    def kernel(self):
+        """
+        Detect the kernel we are running on.
+        """
+        return self.uname[1]
 
     def architecture(self):
         """
@@ -311,13 +195,10 @@ class TargetSystem:
         hostname
         :return:
         """
-        hostname = self.uname[0].split(".")[0]
         ten_mins = 600
-        if (abs(time.time() - self.last_uname_save)
-                > ten_mins or hostname == "localhost"):
+        if abs(time.time() - self.last_uname_save) > ten_mins or self.uname[0] == "localhost":
             self._save_uname()
-            hostname = self.uname[0].split(".")[0]
-        return hostname
+        return self.uname[0].split(".")[0]
 
     def vendor(self):
         """
@@ -326,18 +207,85 @@ class TargetSystem:
         """
         if self.m_vendor:
             return self.m_vendor
-        return self.detect_distro()
+        return self._detect_distro()
 
     def os_version(self):
         """
         os_version
         :return:
         """
-        return self.m_os_version
+        return self._get_os_version()
 
     def os_name(self):
         """
         os_name
         :return:
         """
-        return self.m_product
+        if self.m_description:
+            return self.m_description
+        return "unknown"
+
+def _read_uname():
+    return [platform.node(), platform.release(), platform.machine()]
+
+def _run_command(command, shell=False):
+    try:
+        proc = subprocess.Popen(command,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                shell=shell)
+        stdout_b, stderr_b = proc.communicate()  # pylint: disable=unused-variable
+        stdout = to_utf8(stdout_b) if stdout_b else ""
+        ret_code = proc.wait()
+        assert ret_code is not None
+    except OSError:
+        ret_code = -1
+        stdout = ""
+    return ret_code, stdout
+
+def _collect_lsb_release():
+    description = None
+    distributor_id = None
+    release = None
+
+    path = os.environ.get("PATH", "")
+    lsb_release_exes = []
+    for split_path_element in path.split(":"):
+        lsb_release_bin = os.path.join(split_path_element, "lsb_release")
+        if os.path.isfile(lsb_release_bin):
+            lsb_release_exes.append(lsb_release_bin)
+
+    if "/usr/bin/lsb_release" not in lsb_release_exes \
+            and os.path.isfile("/usr/bin/lsb_release"):
+        lsb_release_exes.append("/usr/bin/lsb_release")
+
+    ret_code = -2
+    for lsb_release_exe in lsb_release_exes:
+        (ret_code, stdout) = _run_command([lsb_release_exe, '-a'])
+        if ret_code == 0:
+            break
+
+    if ret_code == 0:
+        lines = stdout.split("\n")
+        # Each line consists of "Key:<whitespace>*<value>"
+        matcher = re.compile(r"([^:]+):\s*(.+)$")
+        for line in lines:
+            line_matched = matcher.match(line)
+            if line_matched:
+                if "Description" in line:
+                    description = line_matched.group(2)
+                elif "Distributor ID" in line:
+                    distributor_id = line_matched.group(2)
+                elif "Release" in line:
+                    release = line_matched.group(2)
+
+    return description, distributor_id, release
+
+def _find_string_in_file(file_to_read, string_to_find):
+    try:
+        with open(file_to_read) as file:
+            if string_to_find in file.read():
+                return True
+    except EnvironmentError:
+        pass
+    return False
--- base/tests/mcsrouter/TestTargetSystem.py
+++ base/tests/mcsrouter/TestTargetSystem.py
@@ -26,61 +26,61 @@ NO_LSB = [None, None, None]
 class TestTargetSystem(unittest.TestCase):
 
     # OS Version Tests
-    @mock.patch('mcsrouter.targetsystem.TargetSystem._collect_lsb_release', return_value=UBUNTU_3_VERSION)
+    @mock.patch('mcsrouter.targetsystem._collect_lsb_release', return_value=UBUNTU_3_VERSION)
     def test_os_version_retrieves_three_part_ubuntu_version(self, *mockargs):
         target_system = mcsrouter.targetsystem.TargetSystem('/tmp/sophos-spl')
         self.assertEqual(('18', '04', '2'), target_system.os_version())
 
-    @mock.patch('mcsrouter.targetsystem.TargetSystem._collect_lsb_release', return_value=UBUNTU_2_VERSION)
+    @mock.patch('mcsrouter.targetsystem._collect_lsb_release', return_value=UBUNTU_2_VERSION)
     def test_os_version_retrieves_two_part_ubuntu_version(self, *mockargs):
         target_system = mcsrouter.targetsystem.TargetSystem('/tmp/sophos-spl')
         self.assertEqual(('18', '04', ''), target_system.os_version())
 
-    @mock.patch('mcsrouter.targetsystem.TargetSystem._collect_lsb_release', return_value=NO_LSB)
+    @mock.patch('mcsrouter.targetsystem._collect_lsb_release', return_value=NO_LSB)
     def test_os_version_retrieves_two_part_ubuntu_version(self, *mockargs):
         target_system = mcsrouter.targetsystem.TargetSystem('/tmp/sophos-spl')
         self.assertEqual(['4', '15', '0-20-generic'], target_system.os_version())
 
-    @mock.patch('mcsrouter.targetsystem.TargetSystem._collect_lsb_release', return_value=NO_LSB)
+    @mock.patch('mcsrouter.targetsystem._collect_lsb_release', return_value=NO_LSB)
     @mock.patch('builtins.open', new_callable=mock_open, read_data="CentOS Linux release 7.5.1804 (Core)")
     def test_os_version_retrieves_centos_vendor(self, *mockargs):
         target_system = mcsrouter.targetsystem.TargetSystem('/tmp/sophos-spl')
         self.assertEqual('centos', target_system.vendor())
 
-    @mock.patch('mcsrouter.targetsystem.TargetSystem._collect_lsb_release', return_value=CENTOS_LSB_1)
+    @mock.patch('mcsrouter.targetsystem._collect_lsb_release', return_value=CENTOS_LSB_1)
     def test_os_version_retrieves_centos_major_version(self, *mockargs):
         target_system = mcsrouter.targetsystem.TargetSystem('/tmp/sophos-spl')
         self.assertEqual('centos', target_system.vendor())
         self.assertEqual(['7', '', ''], target_system.os_version())
 
-    @mock.patch('mcsrouter.targetsystem.TargetSystem._collect_lsb_release', return_value=CENTOS_LSB_2)
+    @mock.patch('mcsrouter.targetsystem._collect_lsb_release', return_value=CENTOS_LSB_2)
     def test_os_version_retrieves_centos_major_and_minor_version(self, *mockargs):
         target_system = mcsrouter.targetsystem.TargetSystem('/tmp/sophos-spl')
         self.assertEqual('centos', target_system.vendor())
         self.assertEqual(['7', '4', ''], target_system.os_version())
 
-    @mock.patch('mcsrouter.targetsystem.TargetSystem._collect_lsb_release', return_value=CENTOS_LSB_3)
+    @mock.patch('mcsrouter.targetsystem._collect_lsb_release', return_value=CENTOS_LSB_3)
     def test_os_version_does_not_retrieve_four_centos_versions(self, *mockargs):
         target_system = mcsrouter.targetsystem.TargetSystem('/tmp/sophos-spl')
         self.assertEqual('centos', target_system.vendor())
         self.assertEqual(['7', '4', '1'], target_system.os_version())
 
-    @mock.patch('mcsrouter.targetsystem.TargetSystem._collect_lsb_release', return_value=NO_LSB)
+    @mock.patch('mcsrouter.targetsystem._collect_lsb_release', return_value=NO_LSB)
     @mock.patch('builtins.open', new_callable=mock_open, read_data="Amazon Linux release")
     def test_os_version_retrieves_amazon_linux_vendor(self, *mockargs):
         target_system = mcsrouter.targetsystem.TargetSystem('/tmp/sophos-spl')
         self.assertEqual('amazon', target_system.vendor())
 
-    @mock.patch('mcsrouter.targetsystem.TargetSystem._collect_lsb_release', return_value=NO_LSB)
+    @mock.patch('mcsrouter.targetsystem._collect_lsb_release', return_value=NO_LSB)
     @mock.patch('builtins.open', new_callable=mock_open, read_data="Fedora release 27")
     def test_os_version_retrieves_unknown_vendor(self, *mockargs):
         target_system = mcsrouter.targetsystem.TargetSystem('/tmp/sophos-spl')
         self.assertEqual('unknown', target_system.vendor())
 
-    @mock.patch('mcsrouter.targetsystem.TargetSystem._collect_lsb_release', return_value=UBUNTU_3_VERSION)
+    @mock.patch('mcsrouter.targetsystem._collect_lsb_release', return_value=UBUNTU_3_VERSION)
     def test_read_uname_calls_os_uname(self, *mockargs):
         target_system = mcsrouter.targetsystem.TargetSystem('/tmp/sophos-spl')
-        uname = target_system._read_uname()
+        uname = mcsrouter.targetsystem._read_uname()
         self.assertEqual(uname[0], 'examplehostname')
         self.assertTrue(platform.node.call_count, 1)
         self.assertTrue(platform.release.call_count, 1)
