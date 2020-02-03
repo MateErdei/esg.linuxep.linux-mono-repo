eb425ebfab6e66a55e2f07d27aeab464a2ee7847
Douglas Leeder <douglas.leeder@sophos.com>
2020-02-03 13:42:44 +0000

    [av] [LINUXDAR-1377] Use prctl(PR_SET_PDEATHSIG, SIGTERM) to ensure the sub-process exits if the parent process exits.
    Ensure the Threat Detector or scheduled scan doesn't continue running if the av plugin process exits.

--- av/products/filewalker/scheduled_file_walker_launcher.c
+++ av/products/filewalker/scheduled_file_walker_launcher.c
@@ -7,6 +7,8 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 #include "../capability/PassOnCapability.h"
 
 #include <unistd.h>
+#include <signal.h>
+#include <sys/prctl.h>
 
 int main(int argc, char* argv[])
 {
@@ -17,6 +19,7 @@ int main(int argc, char* argv[])
         return ret;
     }
     set_no_new_privs();
+    prctl(PR_SET_PDEATHSIG, SIGTERM);
     argv[0] = "scheduled_file_walker";
     execv("/opt/sophos-spl/plugins/av/sbin/scheduled_file_walker", argv);
     return 70; // If the exec fails
--- av/products/sophos_threat_detector/sophos_threat_detector_launcher.c
+++ av/products/sophos_threat_detector/sophos_threat_detector_launcher.c
@@ -7,6 +7,8 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 #include "../capability/PassOnCapability.h"
 
 #include <unistd.h>
+#include <signal.h>
+#include <sys/prctl.h>
 
 static int pass_on_chroot_capability()
 {
@@ -21,6 +23,7 @@ int main(int argc, char* argv[])
         return ret;
     }
     set_no_new_privs();
+    prctl(PR_SET_PDEATHSIG, SIGTERM);
     argv[0] = "sophos_threat_detector";
     execv("/opt/sophos-spl/plugins/av/sbin/sophos_threat_detector", argv);
     return 70; // If the exec fails
