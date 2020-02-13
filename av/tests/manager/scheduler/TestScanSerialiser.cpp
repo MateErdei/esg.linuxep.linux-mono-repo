ac8e5e6fce7898815e4fef327293242dfc215ff5
Douglas Leeder <douglas.leeder@sophos.com>
2020-02-13 14:20:41 +0000

    [av] [LINUXDAR-1424] Extend unit test to ensure we serialise correctly.

--- av/tests/manager/scheduler/TestScanSerialiser.cpp
+++ av/tests/manager/scheduler/TestScanSerialiser.cpp
@@ -10,6 +10,10 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 #include "manager/scheduler/ScanSerialiser.h"
 #include "manager/scheduler/ScheduledScanConfiguration.h"
 
+#include <NamedScan.capnp.h>
+
+#include <capnp/serialize.h>
+
 using namespace manager::scheduler;
 
 TEST(ScanSerialiser, TestEmptyScan) // NOLINT
@@ -32,7 +36,20 @@ TEST(ScanSerialiser, TestEmptyScan) // NOLINT
     auto scans = m->scans();
     ASSERT_EQ(scans.size(), 1);
     const auto& scan = scans[0];
+    ASSERT_FALSE(scan.archiveScanning());
 
-    std::string contents = ScanSerialiser::serialiseScan(*m, scan);
-    ASSERT_FALSE(contents.empty());
+    std::string dataAsString = ScanSerialiser::serialiseScan(*m, scan);
+    ASSERT_FALSE(dataAsString.empty());
+
+
+    const kj::ArrayPtr<const capnp::word> view(
+            reinterpret_cast<const capnp::word*>(&(*std::begin(dataAsString))),
+            reinterpret_cast<const capnp::word*>(&(*std::end(dataAsString))));
+
+    capnp::FlatArrayMessageReader messageInput(view);
+    Sophos::ssplav::NamedScan::Reader requestReader =
+            messageInput.getRoot<Sophos::ssplav::NamedScan>();
+
+    EXPECT_FALSE(requestReader.getScanArchives());
+    EXPECT_EQ(requestReader.getName(), "Sophos Cloud Scheduled Scan");
 }
