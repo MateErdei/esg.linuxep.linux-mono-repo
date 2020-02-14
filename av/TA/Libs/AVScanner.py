5640b9a89a81b96dd7cc35d70cec7f7fd870c3e7
efthymios.makaras <efthymios.makaras@sophos.com>
2020-02-14 06:19:06 -0800

    [av] Removed logging as the library is not available in TAP atm

--- av/TA/Libs/AVScanner.py
+++ av/TA/Libs/AVScanner.py
@@ -1,23 +1,23 @@
 import sys
 import os
-from robot.api import logger
+#from robot.api import logger
 
 
 def create_long_path(dirname, depth, root='/', file="file", file_contents=""):
     os.chdir(root)
 
     try:
-        logger.console('Creating deep folder tree')
+ #       logger.console('Creating deep folder tree')
         for i in range(depth):
             if not os.path.exists(dirname):
                 os.mkdir(dirname)
             os.chdir(dirname)
     except Exception as e:
-        logger.console("Failed to create deep tree")
-        logger.console(e)
+  #      logger.console("Failed to create deep tree")
+   #     logger.console(e)
         sys.exit(2)
 
-    logger.console('Creating file')
+    #logger.console('Creating file')
     with open(file, 'w') as file:
         file.write(file_contents)
         file_path = file.name
