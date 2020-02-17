09a2797089fb9490808a73ca1d4cc904be001988
Hamza Zaman <hamza.zaman@sophos.com>
2020-02-17 11:32:13 +0000

    [av] LINUXDAR-1439: Sort out file path for integration robot scripts

--- av/TA/RobotFramework.py
+++ av/TA/RobotFramework.py
@@ -10,7 +10,7 @@ def main():
     log_files = ['log.html', 'output.xml', 'report.html']
 
     robot_args = {
-        'path':  r'/opt/test/inputs/test_scripts',
+        'path':  r'/opt/test/inputs/test_scripts/integration/',
         'name': 'integration',
         'loglevel': 'DEBUG',
         'consolecolors': 'ansi',
