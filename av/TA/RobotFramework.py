17515f6f5f7b66a9d54c524a0fe768a70219706b
Hamza Zaman <hamza.zaman@sophos.com>
2020-02-17 12:12:19 +0000

    [av] LINUXDAR-1439: Remove integration from filepath

--- av/TA/RobotFramework.py
+++ av/TA/RobotFramework.py
@@ -10,7 +10,7 @@ def main():
     log_files = ['log.html', 'output.xml', 'report.html']
 
     robot_args = {
-        'path':  r'/opt/test/inputs/test_scripts/integration',
+        'path':  r'/opt/test/inputs/test_scripts/',
         'name': 'integration',
         'loglevel': 'DEBUG',
         'consolecolors': 'ansi',
