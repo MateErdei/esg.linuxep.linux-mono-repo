a8769185bddfca6bef3e753cb20771a4638f101f
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2020-02-19 10:37:04 +0000

    [base] [BUGFIX] debug register command

--- base/testUtils/libs/MCSRouter.py
+++ base/testUtils/libs/MCSRouter.py
@@ -315,7 +315,10 @@ class MCSRouter(object):
             pass
 
         result = Result()
-        proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=self.devnull)
+        try:
+            proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=self.devnull)
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
         result.stdout, result.stderr = proc.communicate()
         result.stdout_path = None
         result.stderr_path = None
