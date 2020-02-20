c57cfac3a624efd80c33efe27b417251b8c64a93
Douglas Leeder <douglas.leeder@sophos.com>
2020-02-20 11:44:38 +0000

    [av] [LINUXDAR-1391] Add a unit test to verify that special mounts are excluded.

--- av/tests/avscanner/avscannerimpl/TestCommandLineScanRunner.cpp
+++ av/tests/avscanner/avscannerimpl/TestCommandLineScanRunner.cpp
@@ -97,3 +97,96 @@ TEST(CommandLineScanRunner, scanAbsolutePath) // NOLINT
     ASSERT_EQ(socket->m_paths.size(), 1);
     EXPECT_EQ(socket->m_paths.at(0), fs::absolute("sandbox/a/b/file1.txt").string());
 }
+
+
+TEST(CommandLineScanRunner, excludeSpecialMounts) // NOLINT
+{
+    fs::path startingpoint = fs::absolute("sandbox");
+
+    using namespace avscanner::avscannerimpl;
+    class MockMountPoint : public IMountPoint
+    {
+    public:
+        std::string m_mountPoint;
+
+        explicit MockMountPoint(const fs::path& startingpoint)
+            : m_mountPoint(startingpoint / "a/b")
+        {}
+
+        [[nodiscard]] std::string device() const override
+        {
+            return std::__cxx11::string();
+        }
+
+        [[nodiscard]] std::string filesystemType() const override
+        {
+            return std::__cxx11::string();
+        }
+
+        [[nodiscard]] bool isHardDisc() const override
+        {
+            return false;
+        }
+
+        [[nodiscard]] bool isNetwork() const override
+        {
+            return false;
+        }
+
+        [[nodiscard]] bool isOptical() const override
+        {
+            return false;
+        }
+
+        [[nodiscard]] bool isRemovable() const override
+        {
+            return false;
+        }
+
+        [[nodiscard]] bool isSpecial() const override
+        {
+            return true;
+        }
+
+        [[nodiscard]] std::string mountPoint() const override
+        {
+            return m_mountPoint;
+        }
+    };
+
+    class MockMountInfo : public IMountInfo
+    {
+    public:
+        std::vector<std::shared_ptr<IMountPoint>> m_mountPoints;
+        std::vector<std::shared_ptr<IMountPoint>> mountPoints() override
+        {
+            return m_mountPoints;
+        }
+    };
+
+    fs::create_directories("sandbox/a/b/d/e");
+    std::ofstream("sandbox/a/b/file1.txt");
+
+
+    std::vector<std::string> paths;
+    paths.emplace_back(startingpoint);
+    avscanner::avscannerimpl::CommandLineScanRunner runner(paths);
+
+    std::shared_ptr<RecordingMockSocket> socket;
+    socket.reset(new RecordingMockSocket());
+    runner.setSocket(socket);
+
+    std::shared_ptr<MockMountInfo> mountInfo;
+    mountInfo.reset(new MockMountInfo());
+    mountInfo->m_mountPoints.emplace_back(
+            std::make_shared<MockMountPoint>(startingpoint)
+            );
+    runner.setMountInfo(mountInfo);
+
+    runner.run();
+
+    fs::remove_all("sandbox");
+
+    EXPECT_EQ(socket->m_paths.size(), 0);
+}
+
