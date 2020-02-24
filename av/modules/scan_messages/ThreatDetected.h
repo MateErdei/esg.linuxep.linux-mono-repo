523fd61cd66980537233fc01b0ff15f31d817cf5
efthymios.makaras <efthymios.makaras@sophos.com>
2020-02-24 11:16:10 +0000

    [av] [LINUXDAR-1472] Default values initialised in the constructor

--- av/modules/scan_messages/ThreatDetected.h
+++ av/modules/scan_messages/ThreatDetected.h
@@ -14,7 +14,13 @@ namespace scan_messages
     class ThreatDetected
     {
     public:
-        ThreatDetected() = default;
+        ThreatDetected()
+        {
+            // SAV Linux always returns this value it means "virus"
+            m_threatType = "1";
+            // SAV Linux seems to be using this value all the time
+            m_idSource = "NameFilenameFilepathCIMD5";
+        };
 
         void setUserID(const std::string& userID);
         void setDetectionTime(const std::string& detectionTime);
@@ -32,13 +38,11 @@ namespace scan_messages
     protected:
         std::string m_userID;
         std::string m_detectionTime;
-        // SAV Linux always returns this value it means "virus"
-        std::string m_threatType = "1";
+        std::string m_threatType ;
         std::string m_scanType;
         std::string m_notificationStatus;
         std::string m_threatID;
-        // SAV Linux seems to be using this value all the time
-        std::string m_idSource = "NameFilenameFilepathCIMD5";
+        std::string m_idSource;
         std::string m_fileName;
         std::string m_filePath;
         std::string m_actionCode;
