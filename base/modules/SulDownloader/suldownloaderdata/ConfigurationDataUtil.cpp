fd53e009e22d3a70a70b135cc68aef4369a88b78
TrevorMcGing <trevor.mcging@sophos.com>
2020-02-27 13:41:57 +0000

    [base] [LINUXDAR-1416] Added more documentation to ConfigurationDataUtil Temp: added bugfix to build output list so that changes are be tested.

--- base/modules/SulDownloader/suldownloaderdata/ConfigurationDataUtil.cpp
+++ base/modules/SulDownloader/suldownloaderdata/ConfigurationDataUtil.cpp
@@ -23,10 +23,33 @@ namespace SulDownloader
             /*
              * This function will be called from UpdateScheduler and SulDownloadeder.
              * When called from UpdateScheduler the Subscriptions and Features will be compared.
-             * The UpdateScheduler can also to invoke the ConfigurationData verify process due to permissions,
+             * The UpdateScheduler cannot invoke the ConfigurationData verify process due to permissions,
              * therefore this check is also skipped.
-             *
              * When called from SulDownloader all checks are performed.
+             *
+             * These reason for both performing the checks is because update scheduler has no knowledge of the warehouse,
+             * so triggering an update may not actually trigger an update that results in the components install.sh being called.
+             *
+             *
+             * Scenarios this check is covering is base around the following problem
+             *
+             * A component has been downloaded previously and then been uninstalled, either manually (unsupported) or
+             * via policy change.  If this happens Update Scheduler will detect that the product (subscription) or feature set has changed, however
+             * if the files are in the primarywarehouse (local warehouse) and the remote warehouse are in sync the download
+             * will just report nothing has changed, and the uninstalled / missing feature or component install.sh script will not be called.
+             *
+             * As a component can have multiple features and features can be listed against multiple components,
+             * it is possible to have the set of features updated for component(s) that are already installed.  If the feature
+             * has been previously added and removed, it is likely that we hit the main problem described above, thus we need to force an install.
+             *
+             * If the subscriptions are different then we need to force an install, because sulDownloader at this point in time has no
+             * record of the past component installs / uninstalls, and does not know the state of the local warehouse, and if it is in sync with
+             * the remote warehouse.
+             *
+             * Finally as a catch all, the final check is to ensure that any components which are not currently installed will be (re)installed.
+             * However if we get here, then the subscription has not changed, therefore any missing component should be missing because
+             * it has been wrongly uninstalled.  This is a safety net to ensure all components that are required to be installed will be installed.
+             *
              */
 
             LOGDEBUG("Checking if should force install on all products");
