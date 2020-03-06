2b58c0514abcb92467c660488f9ee2068bc97fe8
Gesner Passos <gesner.passos@sophos.com>
2020-03-06 14:56:24 +0000

    [edr] [LINUXDAR-1505] Bump the allowed time for start up .

--- edr/TA/PyTestScripts/test_edr_osquery_process_management.py
+++ edr/TA/PyTestScripts/test_edr_osquery_process_management.py
@@ -128,7 +128,7 @@ def test_edr_plugin_regenerates_flags_file(sspl_mock, edr_plugin_instance):
     _wait_for_osquery_to_run()
 
     # Allow some time for the flags file to be overwritten
-    tries = 5
+    tries = 20
     flags_file_was_regenerated = False
     while tries > 0:
         tries -= 1
