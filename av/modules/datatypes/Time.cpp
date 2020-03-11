710517ba9b1e10eefb563333ca7adef9b5c74f48
DarrenSmith <darren.smith@sophos.com>
2020-03-11 17:15:28 +0000

    [av] [LINUXDAR-1515] Create .cpp for the Time class

---
+++ av/modules/datatypes/Time.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************************************
+
+Copyright 2020, Sophos Limited.  All rights reserved.
+
+******************************************************************************************************/
+
+#include "Time.h"
+
+#include <stdexcept>
+
+using namespace datatypes;
+
+std::string Time::epochToCentralTime(const std::time_t& rawtime)
+{
+    struct tm timeinfo{};
+    struct tm* result = localtime_r(&rawtime, &timeinfo);
+    if (result == nullptr)
+    {
+        throw std::runtime_error("Failed to get localtime");
+    }
+
+    char timebuffer[16];
+    int timesize = ::strftime(timebuffer, sizeof(timebuffer), "%Y%m%d %H%M%S", &timeinfo);
+    if (timesize == 0)
+    {
+        throw std::runtime_error("Failed to format timestamp");
+    }
+
+    return timebuffer;
+}
+
+std::string Time::currentToCentralTime()
+{
+    std::time_t currentTime = std::time(nullptr);
+    return epochToCentralTime(currentTime);
+}
--- av/modules/datatypes/Time.h
+++ av/modules/datatypes/Time.h
@@ -6,6 +6,9 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 
 #pragma once
 
+#include <ctime>
+#include <string>
+
 namespace datatypes
 {
     class Time
@@ -13,29 +16,8 @@ namespace datatypes
     public:
         Time() = delete;
 
-        static std::string epochToCentralTime(const std::time_t& rawtime)
-        {
-            struct tm timeinfo{};
-            struct tm* result = localtime_r(&rawtime, &timeinfo);
-            if (result == nullptr)
-            {
-                throw std::runtime_error("Failed to get localtime");
-            }
+        static std::string epochToCentralTime(const std::time_t& rawtime);
 
-            char timebuffer[128];
-            int timesize = ::strftime(timebuffer, sizeof(timebuffer), "%Y%m%d %H%M%S", &timeinfo);
-            if (timesize == 0)
-            {
-                throw std::runtime_error("Failed to format timestamp");
-            }
-
-            return timebuffer;
-        }
-
-        static std::string currentToCentralTime()
-        {
-            std::time_t currentTime = std::time(nullptr);
-            return epochToCentralTime(currentTime);
-        }
+        static std::string currentToCentralTime();
     };
 }
