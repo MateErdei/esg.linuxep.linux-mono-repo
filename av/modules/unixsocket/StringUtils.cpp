c405ad4caf611b5445bccf415410c7062371da78
DarrenSmith <darren.smith@sophos.com>
2020-03-17 16:45:44 +0000

    [av] [LINUXDAR-1532] Correct idSource to match the format used by SAVL

--- av/modules/unixsocket/StringUtils.cpp
+++ av/modules/unixsocket/StringUtils.cpp
@@ -15,6 +15,9 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 
 #include <Common/UtilityImpl/StringUtils.h>
 
+#include <openssl/sha.h>
+#include <iomanip>
+
 namespace fs = sophos_filesystem;
 
 void unixsocket::escapeControlCharacters(std::string& text)
@@ -44,6 +47,21 @@ void unixsocket::escapeControlCharacters(std::string& text)
     text.swap(buffer);
 }
 
+std::string sha256_hash(const std::string& str)
+{
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, str.c_str(), str.size());
+    SHA256_Final(hash, &sha256);
+    std::stringstream ss;
+    for(const auto& ch : hash)
+    {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)ch;
+    }
+    return ss.str();
+}
+
 //TO DO: maybe move it in a XMLUtils file?
 std::string unixsocket::generateThreatDetectedXml(const scan_messages::ServerThreatDetected& detection)
 {
@@ -56,6 +74,9 @@ std::string unixsocket::generateThreatDetectedXml(const scan_messages::ServerThr
     escapeControlCharacters(path);
     std::string fileName = fs::path(path).filename();
     std::string threatName =  detection.getThreatName();
+
+    std::string threatIDinput = path + threatName;
+    std::string threatID = "T" + sha256_hash(threatIDinput);
 
     std::locale loc("");
 
@@ -72,8 +93,8 @@ std::string unixsocket::generateThreatDetectedXml(const scan_messages::ServerThr
                     {"@@THREAT_PATH@@", path},
                     {"@@DETECTION_TIME@@", datatypes::Time::epochToCentralTime(detection.getDetectionTime())},
                     {"@@USER@@", detection.getUserID()},
-                    {"@@THREAT_ID@@", "1"},
-                    {"@@ID_SOURCE@@", "1"},
+                    {"@@THREAT_ID@@", threatID},
+                    {"@@ID_SOURCE@@", "Tsha256(path,name)"},
                     {"@@THREAT_NAME@@",threatName},
                     {"@@SMT_SCAN_TYPE@@",  std::to_string(detection.getScanType())},
                     {"@@NOTIFICATION_STATUS@@", std::to_string(detection.getNotificationStatus())},
