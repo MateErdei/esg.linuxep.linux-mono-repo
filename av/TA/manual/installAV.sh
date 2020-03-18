8c11c8bf2718bc4f7c49cf738d7f823239b85330
Douglas Leeder <douglas.leeder@sophos.com>
2020-03-18 14:50:20 +0000

    [av] [LINUXDAR-1529] Fail manual install if we don't have install.sh

--- av/TA/manual/installAV.sh
+++ av/TA/manual/installAV.sh
@@ -9,6 +9,14 @@ STARTINGDIR=$(pwd)
 cd ${0%/*}
 BASE=$(pwd)
 
+function failure()
+{
+    local EXIT=$1
+    shift
+    echo "$@" >&1
+    exit "$EXIT"
+}
+
 INPUTS_ROOT=$BASE/../..
 AV_ROOT=${INPUTS_ROOT}/av
 [[ ! -d $AV_ROOT ]] && AV_ROOT=${INPUTS_ROOT}/output
@@ -17,9 +25,10 @@ TEST_SUITE=${BASE}/..
 export MCS_CA=${MCS_CA:-${TEST_SUITE}/resources/certs/hmr-dev-sha1.pem}
 
 SDDS_BASE=${AV_ROOT}/base-sdds
-[[ -d $SDDS_BASE ]] || { echo "Can't find SDDS_BASE: $SDDS_BASE" ; exit 1 ; }
+[[ -d $SDDS_BASE ]] || failure 1 "Can't find SDDS_BASE: $SDDS_BASE"
+[[ -f $SDDS_BASE/install.sh ]] || failure 1 "Can't find SDDS_BASE/install.sh: $SDDS_BASE/install.sh"
 SDDS_AV=${AV_ROOT}/SDDS-COMPONENT
-[[ -d $SDDS_AV ]] || { echo "Can't find SDDS_AV: $SDDS_AV" ; exit 2 ; }
+[[ -d $SDDS_AV ]] || failure 2 "Can't find SDDS_AV: $SDDS_AV"
 
 SOPHOS_INSTALL=/opt/sophos-spl
 
@@ -35,4 +44,9 @@ touch "${OVERRIDE_FLAG_FILE}"
 chown -h "root:sophos-spl-group" "${OVERRIDE_FLAG_FILE}"
 chmod 640 "${OVERRIDE_FLAG_FILE}"
 
-exec ${SOPHOS_INSTALL}/base/bin/registerCentral "${MCS_TOKEN}" "${MCS_URL}"
+if [[ -n $MCS_URL ]]
+then
+    exec ${SOPHOS_INSTALL}/base/bin/registerCentral "${MCS_TOKEN}" "${MCS_URL}"
+else
+    echo "Not registering with Central as no token/url specified"
+fi
