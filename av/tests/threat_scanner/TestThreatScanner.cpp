72cde813bedd05541f9ee9819c23b015b417791b
edwardhathaway <edward.hathaway@sophos.com>
2020-03-26 12:22:54 +0000

    [av] [LINUXDAR-1562] Test construction of scanner.
    [+DLCL]

--- av/tests/threat_scanner/TestThreatScanner.cpp
+++ av/tests/threat_scanner/TestThreatScanner.cpp
@@ -3,3 +3,14 @@
 Copyright 2020, Sophos Limited.  All rights reserved.
 
 ******************************************************************************************************/
+
+#include <gtest/gtest.h>
+
+#include "threat_scanner/SusiScannerFactory.h"
+
+TEST(TestThreatScanner, test_construction) //NOLINT
+{
+    threat_scanner::SusiScannerFactory factory;
+    auto scanner = factory.createScanner();
+    scanner.reset();
+}
