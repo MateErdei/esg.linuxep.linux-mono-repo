c8bb1885ef4c580b0f260d1f4e7bacfdd8c4770d
efthymios.makaras <efthymios.makaras@sophos.com>
2020-04-02 15:40:35 +0100

    [av] [LINUXDAR-1625] Update xmlEscape method to escape xml special character instead of just control characters + unit test

--- av/modules/unixsocket/StringUtils.cpp
+++ av/modules/unixsocket/StringUtils.cpp
@@ -18,9 +18,103 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 
 #include <openssl/sha.h>
 #include <iomanip>
-#include <common/StringUtils.h>
 
 namespace fs = sophos_filesystem;
+
+void unixsocket::escapeControlCharacters(std::string& text)
+{
+    std::string buffer;
+    buffer.reserve(text.size());
+    for(size_t pos = 0; pos != text.size(); ++pos) {
+        switch(text[pos]) {
+            // control characters
+            case '\1': buffer.append("\\1");           break;
+            case '\2': buffer.append("\\2");           break;
+            case '\3': buffer.append("\\3");           break;
+            case '\4': buffer.append("\\4");           break;
+            case '\5': buffer.append("\\5");           break;
+            case '\6': buffer.append("\\6");           break;
+            case '\a': buffer.append("\\a");           break;
+            case '\b': buffer.append("\\b");           break;
+            case '\t': buffer.append("\\t");           break;
+            case '\n': buffer.append("\\n");           break;
+            case '\v': buffer.append("\\v");           break;
+            case '\f': buffer.append("\\f");           break;
+            case '\r': buffer.append("\\r");           break;
+            case '\\': buffer.append("\\\\");          break;
+            // xml special characters
+            case '&':  buffer.append("&amp;");         break;
+            case '\"': buffer.append("&quot;");        break;
+            case '\'': buffer.append("&apos;");        break;
+            case '<':  buffer.append("&lt;");          break;
+            case '>':  buffer.append("&gt;");          break;
+            default:
+                auto character = static_cast<unsigned>(text[pos]);
+                if (character <= 31 || character == 127)
+                {
+                    std::ostringstream escaped;
+                    escaped << '\\'
+                            << std::oct
+                            << std::setfill('0')
+                            << std::setw(3)
+                            << character
+                            ;
+                    buffer.append(escaped.str());
+                }
+                else
+                {
+                    buffer.push_back(text[pos]);
+                }
+                break;
+        }
+    }
+
+    text.swap(buffer);
+}
+
+std::string sha256_hash(const std::string& str)
+{
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, str.c_str(), str.size());
+    SHA256_Final(hash, &sha256);
+    std::stringstream ss;
+    for(const auto& ch : hash)
+    {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)ch;
+    }
+    return ss.str();
+}
+
+std::string unixsocket::toUtf8(const std::string& str)
+{
+    try
+    {
+        return boost::locale::conv::to_utf<char>(str, "UTF-8", boost::locale::conv::stop);
+    }
+    catch(const boost::locale::conv::conversion_error& e)
+    {
+        LOGERROR("Could not convert from: " << "UTF-8");
+    }
+
+    std::vector<std::string> encodings{"EUC-JP", "SJIS", "Latin1"};
+    for (const auto& encoding : encodings)
+    {
+        try
+        {
+            std::string encoding_info = " (" + encoding + ")";
+            return boost::locale::conv::to_utf<char>(str, encoding, boost::locale::conv::stop).append(encoding_info);
+        }
+        catch(const boost::locale::conv::conversion_error& e)
+        {
+            LOGERROR("Could not convert from: " << encoding);
+            continue;
+        }
+    }
+
+    throw std::runtime_error(std::string("Failed to convert string to utf8: ") + str);
+}
 
 //TO DO: maybe move it in a XMLUtils file?
 std::string unixsocket::generateThreatDetectedXml(const scan_messages::ServerThreatDetected& detection)
@@ -31,14 +125,14 @@ std::string unixsocket::generateThreatDetectedXml(const scan_messages::ServerThr
         LOGERROR("Received threat report with empty path!");
     }
 
-    std::string utf8Path = common::toUtf8(path);
+    std::string utf8Path = toUtf8(path);
 
-    common::escapeControlCharacters(utf8Path);
+    escapeControlCharacters(utf8Path);
     std::string fileName = fs::path(utf8Path).filename();
     std::string threatName =  detection.getThreatName();
 
     std::string threatIDinput = utf8Path + threatName;
-    std::string threatID = "T" + common::sha256_hash(threatIDinput);
+    std::string threatID = "T" + sha256_hash(threatIDinput);
 
     std::string result = Common::UtilityImpl::StringUtils::orderedStringReplace(
             R"sophos(<?xml version="1.0" encoding="utf-8"?>
--- av/tests/unixsocket/TestStringUtils.cpp
+++ av/tests/unixsocket/TestStringUtils.cpp
@@ -10,11 +10,45 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 #include <scan_messages/ThreatDetected.h>
 #include <capnp/serialize.h>
 #include "unixsocket/StringUtils.h"
-#include "common/StringUtils.h"
 
 
 using namespace unixsocket;
 using namespace scan_messages;
+
+TEST(TestStringUtils, TestXMLEscapeEscapesControlCharacters) // NOLINT
+{
+    std::string threatPath = "abc \1 \2 \3 \4 \5 \6 \016 \017 \020 \021 \022 \023 \024 \025 \026 \027 \030 \031 \032 \033 \034 \035 \036 \037 \177 \\ abc \a \b \t \n \v \f \r abc";
+    escapeControlCharacters(threatPath);
+    EXPECT_EQ(threatPath, "abc \\1 \\2 \\3 \\4 \\5 \\6 \\016 \\017 \\020 \\021 \\022 \\023 \\024 \\025 \\026 \\027 \\030 \\031 \\032 \\033 \\034 \\035 \\036 \\037 \\177 \\\\ abc \\a \\b \\t \\n \\v \\f \\r abc");
+}
+
+TEST(TestStringUtils, TestXMLEscapeEscapesSpecialXMLCharacters) // NOLINT
+{
+    std::string threatPath = "abc & < > \' \" abc";
+    escapeControlCharacters(threatPath);
+    EXPECT_EQ(threatPath, "abc &amp; &lt; &gt; &apos; &quot; abc");
+}
+
+TEST(TestStringUtils, TestXMLEscapeNotEscapingWeirdCharacters) // NOLINT
+{
+    std::string threatPath = "ありったけの夢をかき集め \1 \2 \3 \4 \5 \6 \016 \017 \020 \021 \022 \023 \024 \025 \026 \027 \030 \031 \032 \033 \034 \035 \036 \037 \177 \\ Ἄνδρα μοι ἔννεπε \a \b \t \n \v \f \r Ä Ö Ü ß";
+    escapeControlCharacters(threatPath);
+    EXPECT_EQ(threatPath, "ありったけの夢をかき集め \\1 \\2 \\3 \\4 \\5 \\6 \\016 \\017 \\020 \\021 \\022 \\023 \\024 \\025 \\026 \\027 \\030 \\031 \\032 \\033 \\034 \\035 \\036 \\037 \\177 \\\\ Ἄνδρα μοι ἔννεπε \\a \\b \\t \\n \\v \\f \\r Ä Ö Ü ß");
+}
+
+TEST(TestStringUtils, TestToUtf8) // NOLINT
+{
+    std::string threatPath = "abc  \1 \2 \3 \4 \5 \6 \\ efg \a \b \t \n \v \f \r hik";
+    std::string utf8Path = toUtf8(threatPath);
+    EXPECT_EQ(utf8Path, threatPath);
+}
+
+TEST(TestStringUtils, TestToUtf8WeirdCharacters) // NOLINT
+{
+    std::string threatPath = "ありったけの夢をかき集め \1 \2 \3 \4 \5 \6 \\ Ἄνδρα μοι ἔννεπε \a \b \t \n \v \f \r Ä Ö Ü ß";
+    std::string utf8Path = toUtf8(threatPath);
+    EXPECT_EQ(utf8Path, threatPath);
+}
 
 namespace
 {
