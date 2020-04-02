76d59289d9316a91bf0b968983966ee776535d54
Hamza Zaman <Hamza.Zaman@sophos.com>
2020-04-02 12:20:18 +0100

    [av] LINUXDAR-1631: Update loggers to and copyright header for common module introduction

--- av/modules/common/Logger.cpp
+++ av/modules/common/Logger.cpp
@@ -1,14 +1,14 @@
 /******************************************************************************************************
 
-Copyright 2018-2019, Sophos Limited.  All rights reserved.
+Copyright 2020, Sophos Limited.  All rights reserved.
 
 ******************************************************************************************************/
 #include "Logger.h"
 
 #include <Common/Logging/LoggerConfig.h>
 
-log4cplus::Logger& getFileWalkerLogger()
+log4cplus::Logger& getCommonLogger()
 {
-    static log4cplus::Logger STATIC_LOGGER = Common::Logging::getInstance("FileWalker");
+    static log4cplus::Logger STATIC_LOGGER = Common::Logging::getInstance("Common");
     return STATIC_LOGGER;
 }
--- av/modules/common/Logger.h
+++ av/modules/common/Logger.h
@@ -1,6 +1,6 @@
 /******************************************************************************************************
 
-Copyright 2018-2019, Sophos Limited.  All rights reserved.
+Copyright 2020, Sophos Limited.  All rights reserved.
 
 ******************************************************************************************************/
 
@@ -8,10 +8,10 @@ Copyright 2018-2019, Sophos Limited.  All rights reserved.
 
 #include <Common/Logging/SophosLoggerMacros.h>
 
-log4cplus::Logger& getFileWalkerLogger();
+log4cplus::Logger& getCommonLogger();
 
-#define LOGDEBUG(x) LOG4CPLUS_DEBUG(getFileWalkerLogger(), x)  // NOLINT
-#define LOGINFO(x) LOG4CPLUS_INFO(getFileWalkerLogger(), x)    // NOLINT
-#define LOGSUPPORT(x) LOG4CPLUS_SUPPORT(getFileWalkerLogger(), x) // NOLINT
-#define LOGWARN(x) LOG4CPLUS_WARN(getFileWalkerLogger(), x)    // NOLINT
-#define LOGERROR(x) LOG4CPLUS_ERROR(getFileWalkerLogger(), x)  // NOLINT
+#define LOGDEBUG(x) LOG4CPLUS_DEBUG(getCommonLogger(), x)  // NOLINT
+#define LOGINFO(x) LOG4CPLUS_INFO(getCommonLogger(), x)    // NOLINT
+#define LOGSUPPORT(x) LOG4CPLUS_SUPPORT(getCommonLogger(), x) // NOLINT
+#define LOGWARN(x) LOG4CPLUS_WARN(getCommonLogger(), x)    // NOLINT
+#define LOGERROR(x) LOG4CPLUS_ERROR(getCommonLogger(), x)  // NOLINT
