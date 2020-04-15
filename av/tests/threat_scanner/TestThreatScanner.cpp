be4accc296ed19539e7abcd540640725b887b4ed
DarrenSmith <darren.smith@sophos.com>
2020-04-15 11:05:04 +0100

    [av] [LINUXDAR-1565] Unit test for scanning with SUSI

--- av/tests/threat_scanner/TestThreatScanner.cpp
+++ av/tests/threat_scanner/TestThreatScanner.cpp
@@ -18,6 +18,54 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 
 using namespace testing;
 
+static std::string susiResponseStr =
+        "{\n"
+        "    \"results\":\n"
+        "    [\n"
+        "        {\n"
+        "            \"detections\": [\n"
+        "               {\n"
+        "                   \"threatName\": \"MAL/malware-A\",\n"
+        "                   \"threatType\": \"malware/trojan/PUA/...\",\n"
+        "                   \"threatPath\": \"...\",\n"
+        "                   \"longName\": \"...\",\n"
+        "                   \"identityType\": 0,\n"
+        "                   \"identitySubtype\": 0\n"
+        "               }\n"
+        "            ],\n"
+        "            \"Local\": {\n"
+        "                \"LookupType\": 1,\n"
+        "                \"Score\": 20,\n"
+        "                \"SignerStrength\": 30\n"
+        "            },\n"
+        "            \"Global\": {\n"
+        "                \"LookupType\": 1,\n"
+        "                \"Score\": 40\n"
+        "            },\n"
+        "            \"ML\": {\n"
+        "                \"ml-pe\": 50\n"
+        "            },\n"
+        "            \"properties\": {\n"
+        "                \"GENES/SUPPRESSML\": 1\n"
+        "            },\n"
+        "            \"telemetry\": [\n"
+        "                {\n"
+        "                    \"identityName\": \"xxx\",\n"
+        "                    \"dataHex\": \"6865646765686f67\"\n"
+        "                }\n"
+        "            ],\n"
+        "            \"mlscores\": [\n"
+        "                {\n"
+        "                    \"score\": 12,\n"
+        "                    \"secScore\": 34,\n"
+        "                    \"featuresHex\": \"6865646765686f67\",\n"
+        "                    \"mlDataVersion\": 56\n"
+        "                }\n"
+        "            ]\n"
+        "        }\n"
+        "    ]\n"
+        "}";
+
 TEST(TestThreatScanner, test_FakeSusiScannerConstruction) //NOLINT
 {
     threat_scanner::FakeSusiScannerFactory factory;
@@ -84,4 +132,52 @@ TEST(TestThreatScanner, test_SusiScannerConstruction) //NOLINT
             susiWrapper->m_scannerConfig)).WillOnce(Return(susiWrapper));
 
     threat_scanner::SusiScanner susiScanner(susiWrapperFactory);
+}
+
+TEST(TestThreatScanner, test_SusiScanner_scanFile_clean) //NOLINT
+{
+    setupFakeSophosThreatDetectorConfig();
+
+    auto susiWrapper = std::make_shared<MockSusiWrapper>("", "");
+    std::shared_ptr<MockSusiWrapperFactory> susiWrapperFactory = std::make_shared<MockSusiWrapperFactory>();
+
+    EXPECT_CALL(*susiWrapperFactory, createSusiWrapper(_, _)).WillOnce(Return(susiWrapper));
+
+    SusiResult susiResult = SUSI_S_OK;
+    SusiScanResult* scanResult = nullptr;
+    std::string filePath = "/tmp/eicar.txt";
+
+    EXPECT_CALL(*susiWrapper, scanFile(_, filePath.c_str(), _)).WillOnce(Return(susiResult));
+    EXPECT_CALL(*susiWrapper, freeResult(scanResult));
+
+    threat_scanner::SusiScanner susiScanner(susiWrapperFactory);
+    datatypes::AutoFd fd(1);
+    scan_messages::ScanResponse response = susiScanner.scan(fd, filePath);
+
+    EXPECT_EQ(response.clean(), true);
+}
+
+TEST(TestThreatScanner, test_SusiScanner_scanFile_threat) //NOLINT
+{
+    setupFakeSophosThreatDetectorConfig();
+
+    auto susiWrapper = std::make_shared<MockSusiWrapper>("", "");
+    std::shared_ptr<MockSusiWrapperFactory> susiWrapperFactory = std::make_shared<MockSusiWrapperFactory>();
+
+    EXPECT_CALL(*susiWrapperFactory, createSusiWrapper(_, _)).WillOnce(Return(susiWrapper));
+
+    SusiResult susiResult = SUSI_I_THREATPRESENT;
+    SusiScanResult scanResult;
+    scanResult.version = 1;
+    scanResult.scanResultJson = const_cast<char*>(susiResponseStr.c_str());
+    std::string filePath = "/tmp/eicar.txt";
+
+    EXPECT_CALL(*susiWrapper, scanFile(_, filePath.c_str(), _)).WillOnce(DoAll(SetArgPointee<2>(&scanResult), Return(susiResult)));
+    EXPECT_CALL(*susiWrapper, freeResult(&scanResult));
+
+    threat_scanner::SusiScanner susiScanner(susiWrapperFactory);
+    datatypes::AutoFd fd(1);
+    scan_messages::ScanResponse response = susiScanner.scan(fd, filePath);
+
+    EXPECT_EQ(response.clean(), false);
 }
