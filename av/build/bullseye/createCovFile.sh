ec1d4411a220f2ac883a78f77033181daa8a29fb
Tim Pickersgill <tim.pickersgill@sophos.com>
2020-04-17 09:56:58 +0100

    [av] [LINUXDAR-1574] exclude redist from coverage reports

--- av/build/bullseye/createCovFile.sh
+++ av/build/bullseye/createCovFile.sh
@@ -66,7 +66,7 @@ COVDIR="${COVFILE%/*}"
 [[ $COVDIR == "/tmp/root" ]] || echo "UNEXPECTED COVDIR = $COVDIR"
 
 ## Need to get between these dirs
-currentDir="../..$BASE_DIR"
+#currentDir="../..$BASE_DIR"
 
 SCRIPT_DIR=$(cd "${0%/*}"; echo "$PWD")
 BUILD_SRC_DIR="${SCRIPT_DIR%/*}"
@@ -78,7 +78,7 @@ echo "ORIG_CWD=$ORIG_CWD"
 echo "BASE_DIR=$BASE_DIR"
 echo "SCRIPT_DIR=$SCRIPT_DIR"
 echo "SRC_DIR=$SRC_DIR"
-echo "rel path = $currentDir"
+#echo "rel path = $currentDir"
 
 #
 #EXCLUSION_FILE=$COVDIR/$currentDir/src/regression/supportFiles/bullseye/bullseyeExclusionFile.txt
@@ -119,6 +119,7 @@ SRC_TEST_DIR=${SRC_DIR}/tests
 
 exclude \!../..${SRC_DIR}/build/
 exclude \!../..${SRC_DIR}/build64/
+exclude \!../..${SRC_DIR}/redist/
 exclude \!../..${SRC_TEST_DIR}/
 
 echo "Exclusions:"
