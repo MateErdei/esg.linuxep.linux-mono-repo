1d464568fcf219834591fbb7b3f74ab6cf202148
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2020-04-20 14:56:13 +0100

    [edr] [LINUXDAR-1546] change directory name to lowercase

--- edr/modules/livequery/livequery_main.cpp
+++ edr/modules/livequery/livequery_main.cpp
@@ -4,12 +4,14 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 
 ******************************************************************************************************/
 
+#include <iostream>
 #include "livequery_main.h"
 
 namespace livequery{
 
     int livequery_main::main(int argc, char* argv[])
     {
-
+        std::cout << argc << argv << std::endl;
+        return 0;
     }
 }
