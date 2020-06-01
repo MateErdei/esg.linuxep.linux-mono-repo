47fcd19651c8acf2b09e899a43059c9136186ca2
wellingtonm <wellington.machingauta@sophos.com>
2020-06-01 08:39:34 +0100

    Added extra print message for debugging

--- sdds/src/filer.py
+++ sdds/src/filer.py
@@ -164,6 +164,8 @@ def locate_artisan_package_on_filer6(name, branch, build, build_type, version):
     else:
         path_linux = os.path.join(path, build, name)
 
+    print("Linux11 path = {}".format(os.path.exists(os.path.join(UPSTREAM_DEV, path_linux))))
+
     if not os.path.exists(os.path.join(UPSTREAM_DEV, path_linux)):
         if not os.path.exists(os.path.join(UPSTREAM_DEV, path)):
             print("Path build name = {}".format(os.path.join(UPSTREAM_DEV, path)))
