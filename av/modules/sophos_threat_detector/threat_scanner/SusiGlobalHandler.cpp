12ba49edaf4d3cec70c35dbbda39d7421ad59dee
Douglas Leeder <douglas.leeder@sophos.com>
2020-07-15 16:50:35 +0100

    [av] [LINUXDAR-1826] Log SUSI messages at appropriate levels.

--- av/modules/sophos_threat_detector/threat_scanner/SusiGlobalHandler.cpp
+++ av/modules/sophos_threat_detector/threat_scanner/SusiGlobalHandler.cpp
@@ -58,7 +58,24 @@ void susiLogCallback(void* token, SusiLogLevel level, const char* message)
     std::string m(message);
     if (!m.empty())
     {
-        LOGERROR(level << ": " << m);
+        switch (level)
+        {
+            case SUSI_LOG_LEVEL_DETAIL:
+                LOGDEBUG(m);
+                break;
+            case SUSI_LOG_LEVEL_INFO:
+                LOGINFO(m);
+                break;
+            case SUSI_LOG_LEVEL_WARNING:
+                LOGWARN(m);
+                break;
+            case SUSI_LOG_LEVEL_ERROR:
+                LOGERROR(m);
+                break;
+            default:
+                LOGERROR(level << ": " << m);
+                break;
+        }
     }
 }
 
