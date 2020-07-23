bd3d753c33506fa1f0d92d68b6390ab73c484110
wellingtonm <wellington.machingauta@sophos.com>
2020-07-23 13:00:50 +0100

    [base] [LINUXDAR-1952] - fix race in unit tests

--- base/tests/CommsComponent/TestSplitProcesses.cpp
+++ base/tests/CommsComponent/TestSplitProcesses.cpp
@@ -40,50 +40,56 @@ public:
 
     UserConf m_lowPrivChildUser = {"lp", "lp", "child"};
     UserConf m_lowPrivParentUser = {"games", "lp", "parent"};
-    std::string m_logConfigPath = {"base/etc/logger.conf"};
+
     std::string m_rootPath;
-    std::unique_ptr<Tests::TempDir> m_tempDir;
     std::string m_chrootSophosInstall;
+    std::string m_sophosInstall;
 
 
     void setupAfterSkipIfNotRoot()
     {
-        m_tempDir.reset(new Tests::TempDir(m_rootPath, "TestSplitProcesses"));
+        std::string testName = ::testing::UnitTest::GetInstance()->current_test_info()->name();
+        m_sophosInstall = Common::FileSystem::join(m_rootPath, testName);
 
         Common::ApplicationConfiguration::applicationConfiguration().setData(
-                Common::ApplicationConfiguration::SOPHOS_INSTALL, m_tempDir->dirPath());
+                Common::ApplicationConfiguration::SOPHOS_INSTALL, m_sophosInstall);
         std::string content = R"(
 [global]
 VERBOSITY=DEBUG
 )";
+        auto fs = Common::FileSystem::fileSystem();
         auto fperms = Common::FileSystem::FilePermissionsImpl();
-        fperms.chmod(m_tempDir->dirPath(), 0777);
+        fs->makedirs(m_sophosInstall);
+        fperms.chmod(m_sophosInstall, 0777);
 
-        m_tempDir->createFile(m_logConfigPath, content);
-        m_tempDir->makeDirs(std::vector<std::string>{
-                "var/sophos-spl-comms/logs/base", "var/sophos-spl-comms/base/etc", "logs/base/sophosspl"
-        });
-
-
-        std::string sophosInstall = m_tempDir->dirPath();
-        m_chrootSophosInstall = CommsConfigurator::chrootPathForSSPL(sophosInstall);
-
-        //local user dirs permissions to be done by the installer
+        //create local user dirs and set permissions already done by the installer
         for (auto path : std::vector<std::string>{
-                "logs", "logs/base", "logs/base/sophosspl", "base", "base/etc/", "base/etc/logger.conf"
+                "logs", "logs/base", "logs/base/sophosspl", "base", "base/etc/",
         })
         {
-            fperms.chmod(Common::FileSystem::join(sophosInstall, path), 0777);
-        }
-        //network user dirs permissions to be done by the installer
-        fperms.chmod(m_tempDir->absPath("var"), 0777);
-        for (auto path : std::vector<std::string>{"logs", "base", "base/etc/", "logs/base"})
-        {
-            fperms.chmod(Common::FileSystem::join(m_chrootSophosInstall, path), 0777);
+            auto absPath = Common::FileSystem::join(m_sophosInstall, path);
+            fs->makedirs(absPath);
+            fperms.chmod(absPath, 0777);
         }
 
+        //create expected files and open permissions
+        auto loggerConf = Common::FileSystem::join(m_sophosInstall, "base/etc/logger.conf");
+        fs->writeFile(loggerConf, content);
+        fperms.chmod(loggerConf, 0666);
+
+        //network user dirs permissions to be done by the installer
+        m_chrootSophosInstall = CommsConfigurator::chrootPathForSSPL(
+                m_sophosInstall); //SOPHOS_INSTALL/var/sophos-spl-comms
+        fs->makedirs(m_chrootSophosInstall);
+        fperms.chmod(Common::FileSystem::join(m_sophosInstall, "var"), 0777);
         fperms.chmod(m_chrootSophosInstall, 0777);
 
+        for (auto path : std::vector<std::string>{"logs", "base", "base/etc/", "logs/base"})
+        {
+            auto abdPath = Common::FileSystem::join(m_chrootSophosInstall, path);
+            fs->makedirs(abdPath);
+            fperms.chmod(abdPath, 0777);
+        }
     }
 };
 
@@ -161,42 +167,16 @@ TEST_F(TestSplitProcessesWithNullConfigurator, ExchangeMessagesAndStop) // NOLIN
 }
 
 
-TEST_F(TestSplitProcesses, ExchangeMessagesAndStop) // NOLINT
+TEST_F(TestSplitProcesses, ParentExportsErrorCodeOfTheChild) // NOLINT
 {
     testing::FLAGS_gtest_death_test_style = "threadsafe";
     MAYSKIP;
     ASSERT_EXIT(
             {
                 setupAfterSkipIfNotRoot();
-                auto childProcess = CommNetworkSide();
-                auto parentProcess = [](std::shared_ptr<MessageChannel> channel, OtherSideApi& childProxy) {
-                    childProxy.pushMessage("hello");
+                auto childProcess = [](std::shared_ptr<MessageChannel> channel, OtherSideApi&) {
                     std::string message;
-                    channel->pop(message);
-                    if (message != "world fromchild")
-                    {
-                        throw std::runtime_error("Did not receive world");
-                    }
-                    childProxy.pushMessage("stop");
-                };
-
-                auto config = CommsConfigurator(m_chrootSophosInstall, m_lowPrivChildUser, m_lowPrivParentUser,
-                                                std::vector<ReadOnlyMount>());
-                int exitCode = splitProcessesReactors(parentProcess, childProcess, config);
-                exit(exitCode);
-            },
-            ::testing::ExitedWithCode(0), ".*");
-
-}
-
-TEST_F(TestSplitProcesses, ParentProcessExportTheErrorCodeOfTheChild) // NOLINT
-{
-    testing::FLAGS_gtest_death_test_style = "threadsafe";
-    MAYSKIP;
-    ASSERT_EXIT(
-            {
-                setupAfterSkipIfNotRoot();
-                auto childProcess = [](std::shared_ptr<MessageChannel>, OtherSideApi&) {
+                    channel->pop(message, std::chrono::milliseconds(500));
                     exit(3);
                 };
                 auto parentProcess = [](std::shared_ptr<MessageChannel> channel, OtherSideApi&) {
@@ -334,4 +314,56 @@ TEST_F(TestSplitProcesses, ParentStopIfChildSendStopNoHanging) // NOLINT
                 exit(exitCode);
             },
             ::testing::ExitedWithCode(0), ".*");
+}
+
+TEST_F(TestSplitProcesses, ParentAndChildCanUseLog4) // NOLINT
+{
+    MAYSKIP;
+    testing::FLAGS_gtest_death_test_style = "threadsafe";
+    ASSERT_EXIT({
+                    setupAfterSkipIfNotRoot();
+                    auto parentProcess = [](std::shared_ptr<MessageChannel> /*channel*/,
+                                            OtherSideApi& /*childProxy*/) {
+                        LOGDEBUG("Log from Parent");
+                    };
+
+                    auto childProcess = [](std::shared_ptr<MessageChannel>/*channel*/, OtherSideApi& /*parentProxy*/) {
+                        LOGDEBUG("Log from Child");
+                        std::cout << "also log from here" << std::endl;
+                        try
+                        {
+                            Common::FileSystem::fileSystem()->writeFile("/logs/base/another_child.log", "testing123");
+                        }
+                        catch (std::exception& ex)
+                        {
+                            std::cerr << "failed to write file: " << ex.what() << std::endl;
+                        }
+                    };
+                    auto config = CommsConfigurator(m_chrootSophosInstall, m_lowPrivChildUser, m_lowPrivParentUser,
+                                                    std::vector<ReadOnlyMount>());
+                    int exitCode = splitProcessesReactors(parentProcess, childProcess, config);
+
+
+                    std::string childLog = Common::FileSystem::join(m_chrootSophosInstall,
+                                                                    "logs/base/child.log");
+                    std::string parentLog = Common::FileSystem::join(m_sophosInstall, "logs/base/sophosspl/parent.log");
+                    auto fs = Common::FileSystem::fileSystem();
+                    std::string parentLogContent = fs->readFile(parentLog);
+                    if (parentLogContent.find("Log from Parent") == std::string::npos)
+                    {
+                        exitCode = 2;
+                    }
+                    for (auto& name : fs->listFilesAndDirectories(
+                            Common::FileSystem::join(m_chrootSophosInstall, "logs/base/")))
+                    {
+                        std::cout << "files inside the child log base: " << name << std::endl;
+                    }
+                    std::string childLogContent = fs->readFile(childLog);
+                    if (childLogContent.find("Log from Child") == std::string::npos)
+                    {
+                        exitCode = 3;
+                    }
+                    exit(exitCode);
+                },
+                ::testing::ExitedWithCode(0), ".*");
 }
