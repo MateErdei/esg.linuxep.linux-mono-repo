6824184a6799ac009bd3a5c975c6e325ca7d4f10
Douglas Leeder <douglas.leeder@sophos.com>
2020-07-28 14:09:32 +0100

    [av] [LINUXDAR-2062] Test doing a clean scan.

--- av/tests/unixsocket/TestThreatDetectorSocket.cpp
+++ av/tests/unixsocket/TestThreatDetectorSocket.cpp
@@ -4,13 +4,15 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 
 ******************************************************************************************************/
 
-#include "tests/common/WaitForEvent.h"
 #include "unixsocket/threatDetectorSocket/ScanningClientSocket.h"
 #include "unixsocket/threatDetectorSocket/ScanningServerSocket.h"
+#include <unixsocket/SocketUtils.h>
+
+#include "tests/common/WaitForEvent.h"
+#include "tests/common/LogInitializedTests.h"
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
-#include <unixsocket/SocketUtils.h>
 
 #include <fcntl.h>
 #include <memory>
@@ -29,18 +31,21 @@ namespace
     public:
         MOCK_METHOD1(createScanner, threat_scanner::IThreatScannerPtr(bool scanArchives));
     };
+
+    class TestThreatDetectorSocket : public LogInitializedTests
+    {};
 }
 
 using namespace ::testing;
 
-TEST(TestThreatDetectorSocket, test_construction) //NOLINT
+TEST_F(TestThreatDetectorSocket, test_construction) //NOLINT
 {
     std::string path = "TestThreatDetectorSocket_socket";
     auto scannerFactory = std::make_shared<StrictMock<MockScannerFactory>>();
     EXPECT_NO_THROW(unixsocket::ScanningServerSocket server(path, 0666, scannerFactory));
 }
 
-TEST(TestThreatDetectorSocket, test_running) // NOLINT
+TEST_F(TestThreatDetectorSocket, test_running) // NOLINT
 {
     std::string path = "TestThreatDetectorSocket_socket_test_running";
     auto scannerFactory = std::make_shared<StrictMock<MockScannerFactory>>();
@@ -60,10 +65,10 @@ static scan_messages::ScanResponse scan(unixsocket::ScanningClientSocket& socket
     return socket.scan(file_fd, request);
 }
 
-TEST(TestThreatDetectorSocket, test_scan) // NOLINT
+TEST_F(TestThreatDetectorSocket, test_scan_threat) // NOLINT
 {
     static const std::string THREAT_PATH = "/dev/null";
-    std::string path = "TestThreatDetectorSocket_socket_test_running";
+    std::string path = "TestThreatDetectorSocket_socket_test_scan_threat";
     auto scannerFactory = std::make_shared<StrictMock<MockScannerFactory>>();
     auto scanner = std::make_unique<StrictMock<MockScanner>>();
 
@@ -88,6 +93,42 @@ TEST(TestThreatDetectorSocket, test_scan) // NOLINT
 
         EXPECT_FALSE(response.clean());
         EXPECT_EQ(response.threatName(), "THREAT");
+    }
+
+    server.requestStop();
+    server.join();
+
+    testing::Mock::VerifyAndClearExpectations(scannerPtr);
+    testing::Mock::VerifyAndClearExpectations(scannerFactory.get());
+
+}
+
+TEST_F(TestThreatDetectorSocket, test_scan_clean) // NOLINT
+{
+    static const std::string THREAT_PATH = "/dev/null";
+    std::string path = "TestThreatDetectorSocket_socket_test_scan_clean";
+    auto scannerFactory = std::make_shared<StrictMock<MockScannerFactory>>();
+    auto scanner = std::make_unique<StrictMock<MockScanner>>();
+
+    auto expected_response = scan_messages::ScanResponse();
+    expected_response.setClean(true);
+
+    auto* scannerPtr = scanner.get();
+    testing::Mock::AllowLeak(scanner.get());
+
+    EXPECT_CALL(*scanner, scan(_, THREAT_PATH, _, _)).WillOnce(Return(expected_response));
+    EXPECT_CALL(*scannerFactory, createScanner(false)).WillOnce(Return(ByMove(std::move(scanner))));
+
+    unixsocket::ScanningServerSocket server(path, 0666, scannerFactory);
+    server.start();
+
+    // Create client connection
+    {
+        unixsocket::ScanningClientSocket client_socket(path);
+        datatypes::AutoFd devNull(::open(THREAT_PATH.c_str(), O_RDONLY));
+        auto response = scan(client_socket, devNull, THREAT_PATH);
+
+        EXPECT_TRUE(response.clean());
     }
 
     server.requestStop();
