be992567275e59084943d268a43c0c99248e7e94
efthymios.makaras <efthymios.makaras@sophos.com>
2020-07-28 12:57:50 +0100

    [av] [LINUXDAR-2056] Ensure that we throw the right exceptions in EpochToCentralTimeThrowsFailedToFormat EpochToCentralTimeThrowsFailedToConvert

--- av/tests/datatypes/TestTime.cpp
+++ av/tests/datatypes/TestTime.cpp
@@ -5,8 +5,9 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 ******************************************************************************************************/
 
 #include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
 #include "datatypes/Time.h"
-
+using namespace testing;
 using namespace datatypes;
 
 TEST(TestTime, CurrentToCentralTime) // NOLINT
@@ -19,10 +20,14 @@ TEST(TestTime, EpochToCentralTimeThrowsFailedToConvert) // NOLINT
     try
     {
         Time::epochToCentralTime(-111111111111111111);
+        FAIL() << "Did not throw exception as expected";
     }
     catch (std::runtime_error& e)
     {
         EXPECT_EQ(e.what(),std::string("Failed to convert timestamp to UTC"));
+    }
+    catch (...) {
+        FAIL() <<  "Unexpected exception type";
     }
 }
 
@@ -31,9 +36,13 @@ TEST(TestTime, EpochToCentralTimeThrowsFailedToFormat) // NOLINT
     try
     {
         Time::epochToCentralTime(9999999999999999);
+        FAIL() << "Did not throw exception as expected";
     }
     catch (std::runtime_error& e)
     {
         EXPECT_EQ(e.what(),std::string("Failed to format timestamp"));
+    }
+    catch (...) {
+        FAIL() <<  "Unexpected exception type";
     }
 }
