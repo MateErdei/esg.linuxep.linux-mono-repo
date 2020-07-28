dc0a3ae7f9116aaa47fc50fbbe26ff1449561b14
Douglas Leeder <douglas.leeder@sophos.com>
2020-07-28 11:03:43 +0100

    [av] [LINUXDAR-2062] Verify that we log messages for the conditionals that we are testing.

---
+++ av/tests/common/LogInitializedTests.h
@@ -0,0 +1,118 @@
+/******************************************************************************************************
+
+Copyright 2020, Sophos Limited.  All rights reserved.
+
+******************************************************************************************************/
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <log4cplus/logger.h>
+#include <Common/Logging/LoggerConfig.h>
+#include <Common/Logging/ConsoleLoggingSetup.h>
+
+namespace
+{
+    void initialize_logging()
+    {
+        static Common::Logging::ConsoleLoggingSetup m_loggingSetup;
+    }
+
+    /** Inherit from this class when the tests 'uses' log4cplus and the messages are used in the tests*/
+    class LogInitializedTests : public ::testing::Test
+    {
+    public:
+        LogInitializedTests()
+        {
+            initialize_logging();
+        }
+    };
+
+    class MemoryAppender : public log4cplus::Appender
+    {
+    public:
+        MemoryAppender() = default;
+        ~MemoryAppender() override;
+
+        bool contains(const std::string& expected) const;
+        std::vector<std::string> m_events;
+        void close() override {}
+        std::vector<std::string>::size_type size() const { return m_events.size(); }
+
+        void clear() noexcept
+        {
+            m_events.clear();
+        }
+    protected:
+        void append(const log4cplus::spi::InternalLoggingEvent& event) override;
+
+    };
+
+    MemoryAppender::~MemoryAppender()
+    {
+        destructorImpl();
+    }
+
+    void MemoryAppender::append(const log4cplus::spi::InternalLoggingEvent& event)
+    {
+        std::stringstream logOutput;
+        layout->formatAndAppend(logOutput, event);
+        m_events.emplace_back(logOutput.str());
+    }
+    bool MemoryAppender::contains(const std::string& expected) const
+    {
+        for (const auto& e : m_events)
+        {
+            if (e.find(expected) != std::string::npos)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    class MemoryAppenderUsingTests : public LogInitializedTests
+    {
+    public:
+        explicit MemoryAppenderUsingTests(std::string loggerInstanceName);
+        std::string m_loggerInstanceName;
+        MemoryAppender* m_memoryAppender = nullptr;
+        log4cplus::SharedAppenderPtr m_sharedAppender;
+
+        void setupMemoryAppender();
+        void clearMemoryAppender();
+
+        [[nodiscard]] log4cplus::Logger getLogger() const;
+    };
+
+    MemoryAppenderUsingTests::MemoryAppenderUsingTests(std::string loggerInstanceName)
+        : m_loggerInstanceName(std::move(loggerInstanceName))
+    {}
+
+    void MemoryAppenderUsingTests::setupMemoryAppender()
+    {
+        if (!m_sharedAppender)
+        {
+            log4cplus::Logger logger = getLogger();
+            m_memoryAppender = new MemoryAppender; // Declared separately so that we can access MemoryAppender
+            m_sharedAppender = log4cplus::SharedAppenderPtr(m_memoryAppender); // Donated pointer
+            logger.addAppender(m_sharedAppender);
+        }
+    }
+
+    void MemoryAppenderUsingTests::clearMemoryAppender()
+    {
+        if (m_sharedAppender)
+        {
+            log4cplus::Logger logger = getLogger();
+            logger.removeAppender(m_sharedAppender);
+            m_memoryAppender->clear();
+            m_memoryAppender = nullptr;
+            m_sharedAppender = log4cplus::SharedAppenderPtr();
+        }
+    }
+    log4cplus::Logger MemoryAppenderUsingTests::getLogger() const
+    {
+        return Common::Logging::getInstance(m_loggerInstanceName);
+    }
+
+}
--- av/tests/unixsocket/TestScanningServerConnectionThread.cpp
+++ av/tests/unixsocket/TestScanningServerConnectionThread.cpp
@@ -4,27 +4,32 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 
 ******************************************************************************************************/
 
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-
 #include "unixsocket/threatDetectorSocket/ScanningServerSocket.h"
 
-#include <Common/Logging/ConsoleLoggingSetup.h>
+#include "tests/common/LogInitializedTests.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
 #include <memory>
+#include <ctime>
 
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 using namespace unixsocket;
 
-static Common::Logging::ConsoleLoggingSetup consoleLoggingSetup;
-
 namespace
 {
+    class TestScanningServerConnectionThread : public MemoryAppenderUsingTests
+    {
+    public:
+        TestScanningServerConnectionThread()
+            : MemoryAppenderUsingTests("UnixSocket")
+        {}
+    };
+
     class MockScanner : public threat_scanner::IThreatScanner
     {
     public:
@@ -36,11 +41,12 @@ namespace
     public:
         MOCK_METHOD1(createScanner, threat_scanner::IThreatScannerPtr(bool scanArchives));
     };
+
 }
 
 using namespace ::testing;
 
-TEST(TestScanningServerConnectionThread, successful_construction) //NOLINT
+TEST_F(TestScanningServerConnectionThread, successful_construction) //NOLINT
 {
     auto scannerFactory = std::make_shared<StrictMock<MockScannerFactory>>();
     datatypes::AutoFd fdHolder(::open("/dev/null", O_RDONLY));
@@ -48,7 +54,7 @@ TEST(TestScanningServerConnectionThread, successful_construction) //NOLINT
     EXPECT_NO_THROW(unixsocket::ScanningServerConnectionThread connectionThread(fdHolder, scannerFactory));
 }
 
-TEST(TestScanningServerConnectionThread, fail_construction_with_bad_fd) //NOLINT
+TEST_F(TestScanningServerConnectionThread, fail_construction_with_bad_fd) //NOLINT
 {
     using namespace unixsocket;
     auto scannerFactory = std::make_shared<StrictMock<MockScannerFactory>>();
@@ -57,7 +63,7 @@ TEST(TestScanningServerConnectionThread, fail_construction_with_bad_fd) //NOLINT
     ASSERT_THROW(ScanningServerConnectionThread(fdHolder, scannerFactory), std::runtime_error);
 }
 
-TEST(TestScanningServerConnectionThread, fail_construction_with_null_factory) //NOLINT
+TEST_F(TestScanningServerConnectionThread, fail_construction_with_null_factory) //NOLINT
 {
     using namespace unixsocket;
     datatypes::AutoFd fdHolder(::open("/dev/null", O_RDONLY));
@@ -65,8 +71,10 @@ TEST(TestScanningServerConnectionThread, fail_construction_with_null_factory) //
     ASSERT_THROW(ScanningServerConnectionThread(fdHolder, nullptr), std::runtime_error);
 }
 
-TEST(TestScanningServerConnectionThread, stop_while_running) //NOLINT
+TEST_F(TestScanningServerConnectionThread, stop_while_running) //NOLINT
 {
+    setupMemoryAppender();
+
     auto scannerFactory = std::make_shared<StrictMock<MockScannerFactory>>();
     datatypes::AutoFd fdHolder(::open("/dev/null", O_RDONLY));
     ASSERT_GE(fdHolder.get(), 0);
@@ -74,23 +82,35 @@ TEST(TestScanningServerConnectionThread, stop_while_running) //NOLINT
     connectionThread.start();
     connectionThread.requestStop();
     connectionThread.join();
+
+    EXPECT_GT(m_memoryAppender->size(), 0);
+    EXPECT_TRUE(m_memoryAppender->contains("Closing scanning socket thread"));
+    clearMemoryAppender();
 }
 
-TEST(TestScanningServerConnectionThread, eof_while_running) //NOLINT
+TEST_F(TestScanningServerConnectionThread, eof_while_running) //NOLINT
 {
+    setupMemoryAppender();
+
     auto scannerFactory = std::make_shared<StrictMock<MockScannerFactory>>();
     datatypes::AutoFd fdHolder(::open("/dev/null", O_RDONLY));
     ASSERT_GE(fdHolder.get(), 0);
     ScanningServerConnectionThread connectionThread(fdHolder, scannerFactory);
     connectionThread.start();
-    struct timespec req{.tv_sec=0, .tv_nsec=100000};
+    struct timespec req{.tv_sec=0, .tv_nsec=200000};
     nanosleep(&req, nullptr);
     connectionThread.requestStop();
     connectionThread.join();
+
+    EXPECT_GT(m_memoryAppender->size(), 0);
+    EXPECT_TRUE(m_memoryAppender->contains("Scanning Server Connection closed: EOF"));
+    clearMemoryAppender();
 }
 
-TEST(TestScanningServerConnectionThread, send_zero_length) //NOLINT
+TEST_F(TestScanningServerConnectionThread, send_zero_length) //NOLINT
 {
+    setupMemoryAppender();
+
     auto scannerFactory = std::make_shared<StrictMock<MockScannerFactory>>();
     datatypes::AutoFd fdHolder(::open("/dev/zero", O_RDONLY));
     ASSERT_GE(fdHolder.get(), 0);
@@ -100,4 +120,29 @@ TEST(TestScanningServerConnectionThread, send_zero_length) //NOLINT
     nanosleep(&req, nullptr);
     connectionThread.requestStop();
     connectionThread.join();
+
+    EXPECT_GT(m_memoryAppender->size(), 0);
+    EXPECT_TRUE(m_memoryAppender->contains("Ignoring length of zero / No new messages"));
+    clearMemoryAppender();
+}
+
+TEST_F(TestScanningServerConnectionThread, closed_fd) //NOLINT
+{
+    setupMemoryAppender();
+
+    auto scannerFactory = std::make_shared<StrictMock<MockScannerFactory>>();
+    datatypes::AutoFd fdHolder(::open("/dev/zero", O_RDONLY));
+    ASSERT_GE(fdHolder.get(), 0);
+    int fd = fdHolder.get();
+    ScanningServerConnectionThread connectionThread(fdHolder, scannerFactory);
+    ::close(fd); // fd in connection Thread now broken
+    connectionThread.start();
+    struct timespec req{.tv_sec=0, .tv_nsec=100000};
+    nanosleep(&req, nullptr);
+    connectionThread.requestStop();
+    connectionThread.join();
+
+    EXPECT_GT(m_memoryAppender->size(), 0);
+    EXPECT_TRUE(m_memoryAppender->contains("Socket failed: 9"));
+    clearMemoryAppender();
 }
