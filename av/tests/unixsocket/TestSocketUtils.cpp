2ba6800e1dbbab9e196d89aaa22025fd10baa6e0
Douglas Leeder <douglas.leeder@sophos.com>
2020-07-28 13:47:28 +0100

    [av] [LINUXDAR-2062] Test EOF in readLength, and too large a length.

--- av/tests/unixsocket/TestSocketUtils.cpp
+++ av/tests/unixsocket/TestSocketUtils.cpp
@@ -4,13 +4,15 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 
 ******************************************************************************************************/
 
-
-#include <gtest/gtest.h>
-
 #include "unixsocket/SocketUtils.h"
 #include "unixsocket/SocketUtilsImpl.h"
 
+#include <datatypes/AutoFd.h>
+#include <datatypes/Print.h>
+#include <gtest/gtest.h>
+
 #include <deque>
+#include <fcntl.h>
 
 using namespace unixsocket;
 
@@ -87,4 +89,79 @@ TEST(TestBuffer, TestSplitTwoBytes) // NOLINT
     auto buffer = addTopBitAndPutInBuffer(bytes);
     EXPECT_EQ(buffer[0], static_cast<uint8_t>(0x81));
     EXPECT_EQ(buffer[1], static_cast<uint8_t>(0x7f));
+}
+
+namespace
+{
+    class TestFile
+    {
+    public:
+        TestFile(const char* name)
+            : m_name(name)
+        {
+            ::unlink(m_name.c_str());
+        }
+
+        ~TestFile()
+        {
+            ::unlink(m_name.c_str());
+        }
+
+        void create()
+        {
+            datatypes::AutoFd fd(::open(m_name.c_str(), O_WRONLY | O_CREAT, 0666));
+        }
+
+        void write(const char* buffer, size_t length)
+        {
+            datatypes::AutoFd fd(::open(m_name.c_str(), O_WRONLY | O_CREAT, 0666));
+            ASSERT_GE(fd.get(), 0);
+            int ret = ::write(fd.get(), buffer, length);
+            ASSERT_EQ(ret, length);
+        }
+
+        void write(const std::string& buffer)
+        {
+            datatypes::AutoFd fd(::open(m_name.c_str(), O_WRONLY | O_CREAT, 0666));
+            ASSERT_GE(fd.get(), 0);
+            int ret = ::write(fd.get(), buffer.data(), buffer.size());
+            ASSERT_EQ(ret, buffer.size());
+        }
+
+        int readFD()
+        {
+            datatypes::AutoFd fd(::open(m_name.c_str(), O_RDONLY));
+            return fd.release();
+        }
+
+        std::string m_name;
+    };
+}
+
+TEST(TestReadLength, EOFReturnsMinus2) // NOLINT
+{
+    TestFile tf("TestReadLength_EOFReturnsMinus2_buffer");
+    tf.create();
+
+    datatypes::AutoFd fd(tf.readFD());
+    if (fd.get() < 0)
+    {
+        PRINT("Open read-only failed: errno=" << errno << ": " << strerror(errno));
+        ASSERT_GE(fd.get(), 0);
+    }
+
+    int ret = unixsocket::readLength(fd.get());
+    EXPECT_EQ(ret, -2);
+}
+
+TEST(TestReadLength, TooLargeLengthReturnsMinusOne) // NOLINT
+{
+    TestFile tf("TestReadLength_TooLargeLengthReturnsMinusOne_buffer");
+    tf.write("\xFF\xFF\xFF\xFF\xFF");
+
+    datatypes::AutoFd fd(tf.readFD());
+    ASSERT_GE(fd.get(), 0);
+
+    int ret = unixsocket::readLength(fd.get());
+    EXPECT_EQ(ret, -1);
 }
