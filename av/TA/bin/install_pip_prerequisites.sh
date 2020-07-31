89ce4c1e42fd18645127ab829da43fb1e3cf9f7d
Douglas Leeder <douglas.leeder@sophos.com>
2020-07-31 16:14:04 +0100

    [av] [LINUXDAR-2070] Don't ping forever!
    Add sed command to try alternative repo location...

--- av/TA/bin/install_pip_prerequisites.sh
+++ av/TA/bin/install_pip_prerequisites.sh
@@ -7,9 +7,9 @@ then
     apt install -y python3.7-dev nfs-kernel-server
 elif [[ -x $(which yum) ]]
 then
-    ping abn-centosrepo || true
-    ping abn-engrepo.eng.sophos || true
-#    sed -i -e's/abn-engrepo.eng.sophos/abn-centosrepo/g' /etc/yum.repos.d/CentOS-Base.repo
+    ping -c2 abn-centosrepo || true
+    ping -c2 abn-engrepo.eng.sophos || true
+    sed -i -e's/abn-centosrepo/abn-engrepo.eng.sophos/g' /etc/yum.repos.d/CentOS-Base.repo
     cat /etc/yum.repos.d/CentOS-Base.repo
     yum install -y "gcc" "gcc-c++" "make" "capnproto-devel" "capnproto-libs" "capnproto" nfs-utils
 else
