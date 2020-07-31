391e5dc6c29f7ae0e66bc10b77d150e4b87474fd
Douglas Leeder <douglas.leeder@sophos.com>
2020-07-31 16:08:44 +0100

    [av] [LINUXDAR-2070] Don't fail if the ping fails.

--- av/TA/bin/install_pip_prerequisites.sh
+++ av/TA/bin/install_pip_prerequisites.sh
@@ -7,8 +7,8 @@ then
     apt install -y python3.7-dev nfs-kernel-server
 elif [[ -x $(which yum) ]]
 then
-    ping abn-centosrepo
-    ping abn-engrepo.eng.sophos
+    ping abn-centosrepo || true
+    ping abn-engrepo.eng.sophos || true
 #    sed -i -e's/abn-engrepo.eng.sophos/abn-centosrepo/g' /etc/yum.repos.d/CentOS-Base.repo
     cat /etc/yum.repos.d/CentOS-Base.repo
     yum install -y "gcc" "gcc-c++" "make" "capnproto-devel" "capnproto-libs" "capnproto" nfs-utils
