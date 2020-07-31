e98cbc4ba87833a8f2de317e93905bfdc6bbc390
Douglas Leeder <douglas.leeder@sophos.com>
2020-07-31 08:49:49 +0100

    [av] [LINUXDAR-2061] Test SUSI global certificate functions.

---
+++ av/tests/threat_scanner/TestSusiGlobalCertificateFunctions.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************************************
+
+Copyright 2020, Sophos Limited.  All rights reserved.
+
+******************************************************************************************************/
+
+
+#include "sophos_threat_detector/threat_scanner/SusiCertificateFunctions.h"
+
+#include "../common/LogInitializedTests.h"
+
+#include <gtest/gtest.h>
+
+namespace
+{
+    class TestSusiGlobalCertificateFunctions : public LogInitializedTests
+    {};
+}
+
+TEST_F(TestSusiGlobalCertificateFunctions, callEmptyFunctions) // NOLINT
+{
+    // SusiCertTrustType isTrustedCert(void *token, SusiHashAlg algorithm, const char *pkcs7, size_t size);
+    auto ret = threat_scanner::isTrustedCert(nullptr, SUSI_MD5_ALG, nullptr, 0);
+    EXPECT_EQ(ret, SUSI_TRUSTED);
+
+    // bool threat_scanner::isWhitelistedCert(void *token, const char *fileTopLevelCert, size_t size)
+    auto ret2 = threat_scanner::isWhitelistedCert(nullptr, nullptr, 0);
+    EXPECT_EQ(ret2, false);
+}
