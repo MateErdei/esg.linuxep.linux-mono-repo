c761dad62140e195615b4c6a3a4c57775d512275
wellingtonm <wellington.machingauta@sophos.com>
2020-07-31 09:31:55 +0100

    [base] [LINUXDAR-2012] - make fix proxy tests great again

--- base/testUtils/libs/LiveResponseUtils.py
+++ base/testUtils/libs/LiveResponseUtils.py
@@ -6,47 +6,59 @@ import LiveQueryUtils
 
 import BaseInfo as base_info
 
-TMP_ACTIONS_DIR = os.path.join("/opt/tmp/", "actions")
 BASE_ACTION_DIR = os.path.join(base_info.get_install(), "base", "mcs", "action")
-os.makedirs(TMP_ACTIONS_DIR, exist_ok=True)
-
-def get_live_response_file():
-    filelist = glob.glob(os.path.join( BASE_ACTION_DIR, "LiveTerminal_*.xml"))
-    if filelist is None:
-        raise AssertionError("no livequery response file found")
-    return filelist[0]
 
 
-def create_live_response_action(wss_url="wss://lr.url/", thumbprint="thumbprint", correlationId="correlation-id"):
-    now = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-    return f"""<?xml version="1.0"?><ns:commands xmlns:ns="http://www.sophos.com/xml/mcs/commands" schemaVersion="1.0">
-        <command>
-            <id>{correlationId}</id>
-            <seq>1</seq>
-            <appId>LiveTerminal</appId>
-            <creationTime>{now}</creationTime>
-            <ttl>PT10000S</ttl>'
-            <body>&lt;?xml version="1.0"?&gt;&lt;action type="sophos.mgt.action.InitiateLiveTerminal"&gt;&lt;url&gt;{wss_url}&lt;/url&gt; \
-                        &lt;thumbprint&gt;{thumbprint}&lt;/thumbprint&gt; \
-                                        &lt;/action&gt; \
-                                         </body>
-            </command>
-        </ns:commands>"""
+class LiveResponseUtils:
+    def __init__(self):
+        self.tmp_actions_dir = os.path.join("/opt/tmp/", "actions")
+        os.makedirs(self.tmp_actions_dir, exist_ok=True)
 
-def create_live_response_action_fake_cloud(wss_url="wss://lr.url/", thumbprint="thumbprint"):
-    return f"""<?xml version='1.0'?><action type="sophos.mgt.action.InitiateLiveTerminal"><url>{wss_url}</url><thumbprint>{thumbprint}</thumbprint></action>"""
+    def __del__(self):
+        if os.path.exists(self.tmp_actions_dir):
+            shutil.rmtree(self.tmp_actions_dir, ignore_errors=True)
 
-def get_correlation_id():
-    import uuid
-    return str(uuid.uuid4())
+    @staticmethod
+    def get_live_response_file():
+        filelist = glob.glob(os.path.join(BASE_ACTION_DIR, "LiveTerminal_*.xml"))
+        if filelist is None:
+            raise AssertionError("no livequery response file found")
+        return filelist[0]
 
-def run_live_response(query, correlationId):
+    @staticmethod
+    def create_live_response_action(wss_url="wss://lr.url/", thumbprint="thumbprint", correlationId="correlation-id"):
+        now = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        return f"""<?xml version="1.0"?><ns:commands xmlns:ns="http://www.sophos.com/xml/mcs/commands" schemaVersion="1.0">
+            <command>
+                <id>{correlationId}</id>
+                <seq>1</seq>
+                <appId>LiveTerminal</appId>
+                <creationTime>{now}</creationTime>
+                <ttl>PT10000S</ttl>'
+                <body>&lt;?xml version="1.0"?&gt;&lt;action type="sophos.mgt.action.InitiateLiveTerminal"&gt;&lt;url&gt;{wss_url}&lt;/url&gt; \
+                            &lt;thumbprint&gt;{thumbprint}&lt;/thumbprint&gt; \
+                                            &lt;/action&gt; \
+                                             </body>
+                </command>
+            </ns:commands>"""
 
-    query_file_name = "LiveTerminal_{}_action_{}.json".format(correlationId, LiveQueryUtils.get_valid_creation_time_and_ttl())
-    query_file_path = os.path.join(TMP_ACTIONS_DIR, query_file_name)
-    with open(query_file_path, 'w') as action_file:
-        action_file.write(query)
-        LiveQueryUtils.make_file_readable_by_mcs(query_file_path)
+    @staticmethod
+    def create_live_response_action_fake_cloud(wss_url="wss://lr.url/", thumbprint="thumbprint"):
+        return f"""<?xml version='1.0'?><action type="sophos.mgt.action.InitiateLiveTerminal"><url>{wss_url}</url><thumbprint>{thumbprint}</thumbprint></action>"""
 
-    # Move query file into mcs action dir to be picked up by management agent
-    shutil.move(query_file_path, BASE_ACTION_DIR)
+    @staticmethod
+    def get_correlation_id():
+        import uuid
+        return str(uuid.uuid4())
+
+    def run_live_response(self, query, correlationId):
+
+        query_file_name = "LiveTerminal_{}_action_{}.json".format(correlationId,
+                                                                  LiveQueryUtils.get_valid_creation_time_and_ttl())
+        query_file_path = os.path.join(self.tmp_actions_dir, query_file_name)
+        with open(query_file_path, 'w') as action_file:
+            action_file.write(query)
+            LiveQueryUtils.make_file_readable_by_mcs(query_file_path)
+
+        # Move query file into mcs action dir to be picked up by management agent
+        shutil.move(query_file_path, BASE_ACTION_DIR)
