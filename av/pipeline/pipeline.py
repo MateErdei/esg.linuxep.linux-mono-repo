71340f32b0e498c015adffb459e623e659766632
Douglas Leeder <douglas.leeder@sophos.com>
2020-08-03 08:58:26 +0100

    [av] [LINUXDAR-2070] Use python3 for robot tests.

--- av/pipeline/pipeline.py
+++ av/pipeline/pipeline.py
@@ -64,9 +64,9 @@ def install_requirements(machine: tap.Machine):
 def robot_task_with_env(machine: tap.Machine, environment=None):
     try:
         machine.run('bash', machine.inputs.test_scripts / "bin/install_nfs_server.sh")
-        machine.run('python', machine.inputs.test_scripts / 'RobotFramework.py', environment=environment)
+        machine.run(python(machine), machine.inputs.test_scripts / 'RobotFramework.py', environment=environment)
     finally:
-        machine.run('python', machine.inputs.test_scripts / 'move_robot_results.py')
+        machine.run(python(machine), machine.inputs.test_scripts / 'move_robot_results.py')
         machine.output_artifact('/opt/test/logs', 'logs')
         machine.output_artifact('/opt/test/results', 'results')
 
