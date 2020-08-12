61cdb777fc82e023de516d909856f0bf1d48c42b
Douglas Leeder <douglas.leeder@sophos.com>
2020-08-12 16:04:25 +0100

    [av] [LINUXDAR-2122] Remove __ from arguments since __<foo> identifier are reserved.

--- av/modules/avscanner/mountinfoimpl/ISystemCallWrapper.h
+++ av/modules/avscanner/mountinfoimpl/ISystemCallWrapper.h
@@ -14,8 +14,8 @@ namespace avscanner::avscannerimpl
     class ISystemCallWrapper
     {
     public:
-        virtual int _ioctl(int __fd, unsigned long int __request, char* buffer) = 0;
-        virtual int _statfs(const char *__file, struct ::statfs *__buf) = 0;
-        virtual int _open(const char *__file, int __oflag, mode_t mode) = 0;
+        virtual int _ioctl(int fd, unsigned long int request, char* buffer) = 0;
+        virtual int _statfs(const char *file, struct ::statfs *buf) = 0;
+        virtual int _open(const char *file, int oflag, mode_t mode) = 0;
     };
 }
