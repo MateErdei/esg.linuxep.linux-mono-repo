aab4df2795a6f9d456691e167f8d114bc0d39953
Douglas Leeder <douglas.leeder@sophos.com>
2020-08-17 09:03:29 +0100

    [av] [LINUXDAR-2122] Simplify grep_identifier.sh sed expression.

--- av/TA/manual/grep_identifier.sh
+++ av/TA/manual/grep_identifier.sh
@@ -6,7 +6,6 @@ BASE=$(pwd)
 cd ..
 cd ..
 
-I="$1"
-I2=$(echo "$I" | sed -e's/ /{SP}/g' -e's/_/{SP}/g' | sed -e's/{SP}/[ _]/g')
-#echo "$I2"
+I2=$(echo "$1" | sed -e's/[ _]/[ _]/g')
+# echo "$I2"
 exec grep -riI "$I2" TA/
