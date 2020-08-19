5d6a689455f851c3895b580188d4b2a916b05fa2
Douglas Leeder <douglas.leeder@sophos.com>
2020-08-19 11:43:25 +0100

    [av] [LINUXDAR-2165] Allow self-contained testing.

--- av/products/susi_experiment_scanner/runScanner2.sh
+++ av/products/susi_experiment_scanner/runScanner2.sh
@@ -5,10 +5,15 @@ cd ${0%/*}
 BASE=$(pwd)
 cd "${STARTINGDIR}"
 
-export SOPHOS_INSTALL=/opt/sophos-spl
-export PLUGIN_INSTALL=${SOPHOS_INSTALL}/plugins/av
+export BASE
+
+export GOOD=${GOOD:-good}
+export PLUGIN_INSTALL=${BASE}/${GOOD}
 export SUSI_DIR=${PLUGIN_INSTALL}/chroot/susi/distribution_version
+export LD_LIBRARY_PATH=$BASE:$BASE/lib64:$PLUGIN_INSTALL/lib64:$SUSI_DIR:$SUSI_DIR/version1
+ldd "$BASE/susi_scanner2"
 
-export LD_LIBRARY_PATH=$PLUGIN_INSTALL/lib64:$SUSI_DIR:$SUSI_DIR/version1
-
-exec $BASE/susi_scanner2 "$@"
+TOOL=
+# TOOL="gdb --args"
+TOOL=valgrind
+exec $TOOL "$BASE/susi_scanner2" "$@"
--- av/products/susi_experiment_scanner/susi_scanner2.cpp
+++ av/products/susi_experiment_scanner/susi_scanner2.cpp
@@ -23,6 +23,30 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 
 namespace fs = sophos_filesystem;
 
+static std::string getPluginInstall()
+{
+    const char* PLUGIN_INSTALL = getenv("PLUGIN_INSTALL");
+    if (PLUGIN_INSTALL != nullptr)
+    {
+        return PLUGIN_INSTALL;
+    }
+
+    const char* BASE = getenv("BASE");
+    if (BASE != nullptr)
+    {
+        return BASE;
+    }
+    const char* cwd_str = getcwd(nullptr, 0); // Linux extension to allocate space
+    fs::path cwd(cwd_str);
+    fs::path chroot = cwd / "chroot";
+    if (fs::is_directory(chroot))
+    {
+        return cwd;
+    }
+
+    return "/opt/sophos-spl/plugins/av";
+}
+
 static int scan(const char* filename)
 {
     datatypes::AutoFd fd(::open(filename, O_RDONLY));
@@ -30,7 +54,7 @@ static int scan(const char* filename)
 
     auto& appConfig = Common::ApplicationConfiguration::applicationConfiguration();
 
-    std::string PLUGIN_INSTALL = "/opt/sophos-spl/plugins/av";
+    std::string PLUGIN_INSTALL = getPluginInstall();
 
     appConfig.setData("PLUGIN_INSTALL", PLUGIN_INSTALL);
     fs::path pluginInstall = PLUGIN_INSTALL;
