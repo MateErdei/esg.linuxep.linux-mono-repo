3dc856405e1891505f4fd8b541de7121fb768253
Douglas Leeder <douglas.leeder@sophos.com>
2020-08-19 15:36:02 +0100

    [av] [LINUXDAR-2165] Automatically create symlinks for test directories if required.

--- av/products/susi_experiment_scanner/runScanner2.sh
+++ av/products/susi_experiment_scanner/runScanner2.sh
@@ -13,7 +13,26 @@ export SUSI_DIR=${PLUGIN_INSTALL}/chroot/susi/distribution_version
 export LD_LIBRARY_PATH=$BASE:$BASE/lib64:$PLUGIN_INSTALL/lib64:$SUSI_DIR:$SUSI_DIR/version1
 ldd "$BASE/susi_scanner2"
 
+function create_symlinks()
+{
+    local F=$1
+    local SHORTER=${F%.*}
+    local SO_CUT=${SHORTER%.so}
+
+    if [[ ! -e $SHORTER ]]
+    then
+        ln -snfv $(basename $F)  $SHORTER
+    fi
+
+    [[ $SO_CUT == $SHORTER ]] && create_symlinks $SHORTER
+}
+
+for F in $(find "${SUSI_DIR}" -name '*.so.*' -type f)
+do
+    create_symlinks "$F"
+done
+
 TOOL=
 # TOOL="gdb --args"
-TOOL=valgrind
+# TOOL="valgrind --leak-check=full"
 exec $TOOL "$BASE/susi_scanner2" "$@"
