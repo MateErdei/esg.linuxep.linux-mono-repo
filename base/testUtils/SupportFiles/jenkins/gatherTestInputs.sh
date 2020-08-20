18629e4cb11da7ecc0417c10b44b84fa136b5648
jakemclanaghan <jake.mclanaghan@sophos.com>
2020-08-20 17:52:46 +0100

    [base] [LINUXDAR-2160] push to test on jenkins

--- base/testUtils/SupportFiles/jenkins/gatherTestInputs.sh
+++ base/testUtils/SupportFiles/jenkins/gatherTestInputs.sh
@@ -10,7 +10,6 @@ exit 1
 sudo rm -rf /tmp/system-product-test-inputs/
 
 sudo $WORKSPACE/testUtils/SupportFiles/jenkins/SetupCIBuildScripts.sh || fail "Error: Failed to get CI scripts"
-pushd $WORKSPACE
+export BUILD_JWT=$(cat $WORKSPACE/testUtils/SupportFiles/jenkins/fake_jwt.txt)
 python3 -m build_scripts.artisan_fetch $WORKSPACE/testUtils/system-product-test-release-package.xml || fail "Error: Failed to fetch inputs"
-popd
 
