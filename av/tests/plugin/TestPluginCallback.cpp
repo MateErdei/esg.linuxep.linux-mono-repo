b4e27f39135317cc1baf1ed9b2f35d349f962b92
DarrenSmith <darren.smith@sophos.com>
2020-08-21 15:42:55 +0100

    [av] [LINUXDAR-2108] Add tests for the scanning of threats via symlinks

---
+++ av/tests/plugin/TestPluginCallback.cpp
@@ -0,0 +1,61 @@
+/******************************************************************************************************
+
+Copyright 2020, Sophos Limited.  All rights reserved.
+
+******************************************************************************************************/
+
+#include "datatypes/sophos_filesystem.h"
+#include "pluginimpl/PluginCallback.h"
+#include "pluginimpl/QueueTask.h"
+
+#include <tests/common/LogInitializedTests.h>
+
+#include <Common/ApplicationConfiguration/IApplicationConfiguration.h>
+
+#include <gtest/gtest.h>
+
+#include <fstream>
+
+
+#define BASE "/tmp/TestPluginCallback"
+
+namespace fs = sophos_filesystem;
+
+namespace
+{
+    class TestPluginCallback : public LogInitializedTests
+    {
+    public:
+        void SetUp() override
+        {
+            auto& appConfig = Common::ApplicationConfiguration::applicationConfiguration();
+            appConfig.setData("PLUGIN_INSTALL", BASE);
+            fs::create_directories(BASE);
+            fs::path versionFile = BASE;
+            versionFile /= "VERSION.ini";
+
+            std::ofstream ofs;
+            ofs.open(versionFile);
+            ofs << "PRODUCT_NAME = Sophos Server Protection Linux - av" << std::endl;
+            ofs << "PRODUCT_VERSION = 1.2.3.456" << std::endl;
+            ofs << "BUILD_DATE = 1970-00-01" << std::endl;
+            ofs.close();
+        };
+    };
+}
+
+TEST_F(TestPluginCallback, getTelemetry) //NOLINT
+{
+    std::string expectedJson{ R"sophos({
+	"Number of Scans" : 0,
+	"Number of Files Scanned" : 0,
+	"Number of Infections" : 0,
+	"Average Performance" : 0.000000,
+    "Average Performance Unit" : "MB/s",
+    "Version Number" : 1.2.3.456
+})sophos" };
+
+    std::shared_ptr<Plugin::QueueTask> task = nullptr;
+    Plugin::PluginCallback pluginCallback(task);
+    EXPECT_EQ(pluginCallback.getTelemetry(), expectedJson);
+}
