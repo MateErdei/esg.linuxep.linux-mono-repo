38e51d3d861bee91a9eca54eefbf7de7de0428c0
wellingtonm <wellington.machingauta@sophos.com>
2020-08-25 17:02:31 +0100

    [edr] [LINUXDAR-2159]- separate builds as we are now unified[LINUXDAR-2159]

--- edr/pipeline/pipeline.py
+++ edr/pipeline/pipeline.py
@@ -155,13 +155,13 @@ def edr_plugin(stage: tap.Root, context: tap.PipelineContext, parameters: tap.Pa
 
     with stage.parallel('test'):
         machines = (
-            ("ubuntu1804",
-             tap.Machine('ubuntu1804_x64_server_en_us', inputs=get_inputs(context, edr_build, parameters), platform=tap.Platform.Linux)),
-            ("centos77", tap.Machine('centos77_x64_server_en_us', inputs=get_inputs(context, edr_build, parameters), platform=tap.Platform.Linux))
+            # ("ubuntu1804",
+            #  tap.Machine('ubuntu1804_x64_server_en_us', inputs=get_inputs(context, edr_build, parameters), platform=tap.Platform.Linux)),
+            ("centos77", tap.Machine('centos77_x64_server_en_us', inputs=get_inputs(context, edr_build, parameters), platform=tap.Platform.Linux)),
             # add other distros here
         )
 
-        if parameters.coverage == 'yes' or has_coverage_build(context.branch):
+        if parameters.mode == 'coverage' or has_coverage_build(context.branch):
             with stage.parallel('combined'):
                 for template_name, machine in machines:
                     stage.task(task_name=template_name, func=combined_task, machine=machine)
