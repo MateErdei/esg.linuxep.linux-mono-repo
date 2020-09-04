ebdea830acb0c5c237a65465ee82ea77a1bb2fd5
Alex Cooper <alex.cooper@sophos.com>
2020-09-04 17:25:01 +0100

    [base] [LINUXDAR-2237] Move python venv

--- base/testUtils/SupportFiles/jenkins/gatherTestInputs.sh
+++ base/testUtils/SupportFiles/jenkins/gatherTestInputs.sh
@@ -12,8 +12,8 @@ sudo rm -rf /tmp/system-product-test-inputs/
 # Create venv
 # undo set -eu because venv/bin/activate script produces errors.
 set +eu
-python3 -m venv venv-for-ci
-source venv-for-ci/bin/activate
+python3 -m venv /tmp/venv-for-ci
+source /tmp/venv-for-ci/bin/activate
   $TEST_UTILS/SupportFiles/jenkins/SetupCIBuildScripts.sh || fail "Error: Failed to get CI scripts"
   export BUILD_JWT=$(cat $TEST_UTILS/SupportFiles/jenkins/fake_jwt.txt)
   python3 -m build_scripts.artisan_fetch $TEST_UTILS/system-product-test-release-package.xml || fail "Error: Failed to fetch inputs"
