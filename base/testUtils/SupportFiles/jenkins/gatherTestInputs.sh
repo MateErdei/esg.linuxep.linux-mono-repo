2f6f9faaac20013ccc63105eaf270d038aa74b72
Alex Cooper <alex.cooper@sophos.com>
2020-09-04 09:40:29 +0100

    [base] [LINUXDAR-2237] On some older distros there is a bug in the python activate script which means it errors when run in a strict bash session (-eu) so disable this while we run the venv scripts.

--- base/testUtils/SupportFiles/jenkins/gatherTestInputs.sh
+++ base/testUtils/SupportFiles/jenkins/gatherTestInputs.sh
@@ -7,8 +7,11 @@ echo $msg 1>&2
 exit 1
 }
 
-# Create venv
 sudo rm -rf /tmp/system-product-test-inputs/
+
+# Create venv
+# undo set -eu because venv/bin/activate script produces errors.
+set +eu
 python3 -m venv venv-for-ci
 source venv-for-ci/bin/activate
   $TEST_UTILS/SupportFiles/jenkins/SetupCIBuildScripts.sh || fail "Error: Failed to get CI scripts"
@@ -16,5 +19,5 @@ source venv-for-ci/bin/activate
   python3 -m build_scripts.artisan_fetch $TEST_UTILS/system-product-test-release-package.xml || fail "Error: Failed to fetch inputs"
   python3 -m build_scripts.artisan_build $TEST_UTILS/system-product-test-release-package.xml || fail "Error: Failed to set ostia vut address"
 deactivate
-
-
+# restore bash strictness (for scripts that source this one)
+set -eu
