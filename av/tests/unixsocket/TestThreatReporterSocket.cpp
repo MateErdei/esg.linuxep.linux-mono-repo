0b3d7875a41f56b394edd39b167df3f29647b93b
Douglas Leeder <douglas.leeder@sophos.com>
2020-09-10 15:58:33 +0100

    [av] [LINUDAR-2216] Remvoe extra colons.

--- av/tests/unixsocket/TestThreatReporterSocket.cpp
+++ av/tests/unixsocket/TestThreatReporterSocket.cpp
@@ -64,7 +64,7 @@ TEST_F(TestThreatReporterSocket, TestSendThreatReport) // NOLINT
     auto mock_callback = std::make_shared<StrictMock<MockIThreatReportCallbacks>>();
 
     EXPECT_CALL(*mock_callback, processMessage(_)).Times(1).WillOnce(
-            InvokeWithoutArgs(&serverWaitGuard, &WaitForEvent::onEventNoArgs));;
+            InvokeWithoutArgs(&serverWaitGuard, &WaitForEvent::onEventNoArgs));
 
     unixsocket::ThreatReporterServerSocket threatReporterServer(m_socketPath, 0600, mock_callback);
 
@@ -102,7 +102,7 @@ TEST_F(TestThreatReporterSocket, TestSendTwoThreatReports) // NOLINT
 
     EXPECT_CALL(*mock_callback, processMessage(_)).Times(2).WillOnce(
             InvokeWithoutArgs(&serverWaitGuard, &WaitForEvent::onEventNoArgs)).WillOnce(
-            InvokeWithoutArgs(&serverWaitGuard2, &WaitForEvent::onEventNoArgs));;
+            InvokeWithoutArgs(&serverWaitGuard2, &WaitForEvent::onEventNoArgs));
 
 
     unixsocket::ThreatReporterServerSocket threatReporterServer(m_socketPath, 0600, mock_callback);
