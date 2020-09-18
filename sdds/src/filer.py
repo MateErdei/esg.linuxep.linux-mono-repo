a99881ea29f99cacca858b56826417190df16cd5
wellingtonm <wellington.machingauta@sophos.com>
2020-09-18 16:14:07 +0100

    [sdds] Revert "[LINUXDAR-2186] - pick changes that are just improvements to python"
    This reverts commit 9bc14c7f

--- sdds/src/filer.py
+++ sdds/src/filer.py
@@ -2,16 +2,10 @@
 
 from __future__ import print_function
 import os
-import re
 import sys
 
 UPSTREAM_PROD = r'\\uk-filer5.prod.sophos\PRODRO\bir'
 UPSTREAM_DEV = r'\\uk-filer6.eng.sophos\bfr'
-
-UNIFIED_PIPELINE_INPUTS = ['sau_xg', 'esg']
-# Those are the inputs where the last good component build file is created too
-# early, for various reasons. For those, look into last good build file.
-UNRELIABLE_DEV_INPUTS = ['sed', 'ssp-release']
 
 
 def decompose_path(path):
@@ -33,28 +27,23 @@ def replace_placeholder(host, elements, index, replacement_file):
     return elements
 
 
-def read_file(path):
+def get_last_good_component_build(path, name):
+    if name == 'none':
+        # support unified pipeline builds.
+        good_build_file_name = os.path.join(path, 'lastgoodbuild.txt')
+    else:
+        good_build_file_name = os.path.join(path, name + '_lastgoodbuild.txt')
+
     try:
-        with open(path) as f:
-            return f.readline().strip()
+        with open(good_build_file_name) as f:
+            build = f.readline().strip()
+        # ensure the build directory exists, if not return None.
+        if not os.path.exists(os.path.join(path, build)):
+            return None
     except IOError:
         return None
 
-
-def get_last_good_component_build(path, name):
-    build = ''
-    lgb_path = os.path.join(path, 'lastgoodbuild.txt')
-    if os.path.exists(lgb_path):
-        build = read_file(lgb_path)
-    # regex for unified pipeline build name format
-    if(re.search("[0-9]{14}-[0-9a-zA-Z]{40}-[0-9a-zA-Z]{6}", build) or name in UNRELIABLE_DEV_INPUTS):
-        if os.path.isdir(os.path.join(path, build, name)):
-            return build
-        else:
-            return None
-
-    component_lgb_path = os.path.join(path, name + '_lastgoodbuild.txt')
-    return read_file(component_lgb_path)
+    return build
 
 
 def get_latest_version(path):
@@ -111,12 +100,21 @@ def locate_scaffold_package_on_filer6(branch, build, name, version):
     if not os.path.exists(os.path.join(UPSTREAM_DEV, path)):
         return not_found
     if not build:
-        build = get_last_good_component_build(os.path.join(UPSTREAM_DEV, path), name)
+        build = get_last_good_component_build(os.path.join(UPSTREAM_DEV, path), name + "_linux11")
         if not build:
-            return not_found
-    path = os.path.join(path, build, name)
+            build = get_last_good_component_build(os.path.join(UPSTREAM_DEV, path), name)
+            if not build:
+                # try to get path based on lastgood build file.
+                build = get_last_good_component_build(os.path.join(UPSTREAM_DEV, path), "none")
+                if not build:
+                    return not_found
+
+    path = os.path.join(path, build, name + "_linux11")
+
     if not os.path.exists(os.path.join(UPSTREAM_DEV, path)):
-        return not_found
+        path = os.path.join(path, build, name)
+        if not os.path.exists(os.path.join(UPSTREAM_DEV, path)):
+            return not_found
     if not version:
         version = get_latest_version(os.path.join(UPSTREAM_DEV, path))
         if not version:
@@ -126,37 +124,70 @@ def locate_scaffold_package_on_filer6(branch, build, name, version):
     return UPSTREAM_DEV, path, build
 
 
-def locate_artisan_package_on_filer6(name, branch, build, build_type, version, sub_component):
+def locate_artisan_package_on_filer6(name, branch, build, build_type, version):
     not_found = None, None, None, None
     path = name
-    component = name if sub_component is None else sub_component
+    print("Path name = {}".format(os.path.join(UPSTREAM_DEV, path)))
     if not os.path.exists(os.path.join(UPSTREAM_DEV, path)):
         return not_found
     if not branch:
         branch = 'develop'
     path = os.path.join(path, branch)
+    path_linux = path
+    print("Path name and branch = {}".format(os.path.join(UPSTREAM_DEV, path)))
+    print("Path_linux name and branch = {}".format(os.path.join(UPSTREAM_DEV, path_linux)))
     if not os.path.exists(os.path.join(UPSTREAM_DEV, path)):
         return not_found
     if not build:
         if build_type:
-            build = get_last_good_component_build(os.path.join(UPSTREAM_DEV, path), component + '-' + build_type)
+            build = get_last_good_component_build(os.path.join(UPSTREAM_DEV, path_linux), name + '_linux11-' + build_type)
         else:
-            build = get_last_good_component_build(os.path.join(UPSTREAM_DEV, path), component + '-release')
+            build = get_last_good_component_build(os.path.join(UPSTREAM_DEV, path_linux), name + '_linux11-release')
             if build:
                 build_type = 'release'
             else:
-                build = get_last_good_component_build(os.path.join(UPSTREAM_DEV, path), component)
+                build = get_last_good_component_build(os.path.join(UPSTREAM_DEV, path_linux), name + "_linux11")
+
         if not build:
-            return not_found
+            if build_type:
+                build = get_last_good_component_build(os.path.join(UPSTREAM_DEV, path), name + build_type)
+            else:
+                build = get_last_good_component_build(os.path.join(UPSTREAM_DEV, path), name + '-release')
+                if build:
+                    build_type = 'release'
+                else:
+                    build = get_last_good_component_build(os.path.join(UPSTREAM_DEV, path), name)
+
+            if not build:
+                build = get_last_good_component_build(os.path.join(UPSTREAM_DEV, path_linux), "none")
+
+            if not build:
+                return not_found
+
+    print("Build = {}".format(build))
 
     if build_type:
-        path = os.path.join(path, build, component + '-' + build_type)
+        path = os.path.join(path, build, name + '-' + build_type)
     else:
-        path = os.path.join(path, build, component)
+        path = os.path.join(path, build, name)
 
-    if not os.path.exists(os.path.join(UPSTREAM_DEV, path)):
-        print('expected path does not exist: ' + os.path.join(UPSTREAM_DEV, path))
-        return not_found
+    if build_type:
+        path_linux = os.path.join(path_linux, build, name + '_linux11-' + build_type)
+    else:
+        path_linux = os.path.join(path_linux, build, name + "_linux11")
+
+    print("Path build name = {}".format(os.path.join(UPSTREAM_DEV, path)))
+    print("Linux11 path = {}".format(os.path.join(UPSTREAM_DEV, path_linux)))
+    print("UPSTREAM_DEV = {}".format(UPSTREAM_DEV))
+
+    if not os.path.exists(os.path.join(UPSTREAM_DEV, path_linux)):
+        print("linux path not found {}".format(os.path.join(UPSTREAM_DEV, path_linux)))
+        if not os.path.exists(os.path.join(UPSTREAM_DEV, path)):
+            print("Path build name = {}".format(os.path.join(UPSTREAM_DEV, path)))
+            return not_found
+    else:
+        path = path_linux
+        print("Path build name = {}".format(os.path.join(UPSTREAM_DEV, path)))
 
     if not version:
         version = get_latest_version(os.path.join(UPSTREAM_DEV, path))
@@ -164,20 +195,11 @@ def locate_artisan_package_on_filer6(name, branch, build, build_type, version, s
             return not_found
     path = os.path.join(path, version)
 
+    print("path version = {}".format(os.path.join(UPSTREAM_DEV, path)))
+
     return UPSTREAM_DEV, path, branch, build
 
-
-def latest_directory_in_path(path):
-    entries = os.listdir(path)
-    if not entries:
-        raise RuntimeError(f'Unexpected empty folder: {path}')
-    folders = [f for f in entries if os.path.isdir(os.path.join(path, f))]
-    if not folders:
-        raise RuntimeError(f'Path contains no folders: {path}')
-    return sorted(folders, key=lambda f: os.path.getmtime(os.path.join(path, f)))[-1]
-
-
-def locate_package_from_clues(name, version, build_id, output):
+def locate_package_from_clues(name, branch, version, build, build_type):
     """
     Find a build on the production or dev filer from build metadata
 
@@ -186,34 +208,39 @@ def locate_package_from_clues(name, version, build_id, output):
     version - the version to use (dash separated, default: latest)
     build - the build identifier of the build to use (default: last good component build)
     build_type - release or debug (default: release)
-    sub_component - component name. incase there are multiple components in a repository
     """
 
-    print(f'Looking for name={name} version={version} build_id={build_id} output={output}')
+    print ('Looking for {}:{}:{}:{}:{}'.format(name, branch, version, build, build_type))
 
-    assert name, f'Expected a package name, got "{name}"'
+    try:
+        # production builds are fully specified <name>/<version>/<build>
+        if name and not branch and version and build and not build_type:
+            filer, package = locate_on_filer5(os.path.join(name, version.replace(".", "-"), build))
+            if package:
+                print ('Found on filer5')
+                return filer, package, "production", '/'.join([version, build]), None
 
-    base_path = os.path.join(UPSTREAM_PROD, name)
-    if not os.path.exists(base_path):
-        raise RuntimeError(f'Path not found: {base_path}')
+        #  not a filer5 package, try filer6, artisan builds take priority
+        #  Artisan packages are <name>/<branch>/<build>/<name>-<build-type>/<version>
+        filer, package, found_branch, found_build = locate_artisan_package_on_filer6(name, branch, build, build_type, version)
 
-    if not version:
-        version = latest_directory_in_path(base_path)
-    else:
-        version = version.replace('.', '-')
-    base_path = os.path.join(base_path, version)
+        print("artisan Filer {}, package {}, found_branch {}, found_build {}".format(filer, package, found_branch, found_build))
 
-    if not build_id:
-        build_id = latest_directory_in_path(base_path)
+        if package:
+            commit = found_build.split('-')[1]
+            print('Found an Artisan build on filer6')
+            return filer, package, found_branch, found_build, commit
 
-    full_path = os.path.join(base_path, build_id)
-    if output:
-        full_path = os.path.join(full_path, output)
-
-    if not os.path.exists(full_path):
-        raise RuntimeError(f'Path not found: {full_path}')
-
-    return full_path
+        #  Scaffold packages are <branch>/<build>/<name>/<version>
+        filer, package, found_build = locate_scaffold_package_on_filer6(branch, build, name, version)
+        print("Scaffold Filer {}, package {}, found_branch {}, found_build {}".format(filer, package, found_branch, found_build))
+        if package:
+            changelist = found_build.split('-')[1]
+            print('Found a Scaffold build on filer6')
+            return filer, package, None, found_build, changelist
+    except TypeError:
+        raise TypeError('Package not found {}:{}:{}:{}:{}'.format(name, branch, version, build, build_type))
+    raise OSError(2, 'Package not found {}:{}:{}:{}:{}'.format(name, branch, version, build, build_type))
 
 
 def main(args):
