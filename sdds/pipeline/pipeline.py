fea3525ba84633002cd87b11179f0a14c5c185e0
wellingtonm <wellington.machingauta@sophos.com>
2020-09-18 11:28:26 +0100

    [sdds] [LINUXDAR-2186] - move to Unified pipeline

---
+++ sdds/pipeline/pipeline.py
@@ -0,0 +1,10 @@
+import tap.v1 as tap
+
+
+@tap.pipeline(version=1, component='sspl-warehouse')
+def sspl_warehouse(stage: tap.Root):
+    component = tap.Component(name='sspl-warehouse', base_version='1.0.0')
+
+    with stage.parallel('build'):
+        _build = stage.artisan_build(name='release', component=component, image='JenkinsLinuxTemplate5',
+                                         mode='release', release_package='./build/release-package.xml')
