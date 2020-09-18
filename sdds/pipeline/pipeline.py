8a09214d1c9f5f06a4457b991c0c325e0951e12d
wellingtonm <wellington.machingauta@sophos.com>
2020-09-18 17:46:54 +0100

    [sdds] [LINUXDAR-2186] - signing changes

--- sdds/pipeline/pipeline.py
+++ sdds/pipeline/pipeline.py
@@ -6,5 +6,5 @@ def sspl_warehouse(stage: tap.Root):
     component = tap.Component(name='sspl-warehouse', base_version='1.0.0')
 
     with stage.parallel('build'):
-        _build = stage.artisan_build(name='release', component=component, image='JenkinsLinuxTemplate5',
+        _build = stage.artisan_build(name='release', component=component, image='Warehouse',
                                          mode='release', release_package='./build/release-package.xml')
