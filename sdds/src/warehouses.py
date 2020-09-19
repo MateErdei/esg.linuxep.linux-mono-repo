b186d8a90cd742a8c5a66496d87f2470506dc279
wellingtonm <wellington.machingauta@sophos.com>
2020-09-19 12:06:29 +0100

    Revert "[LINUXDAR-2186] - fix warehouse.py

--- sdds/src/warehouses.py
+++ sdds/src/warehouses.py
@@ -9,6 +9,7 @@ import logging
 import re
 from glob import glob
 
+
 class Supplement:
     def __init__(self, warehouse, id, tag):
         self._id = id
@@ -16,17 +17,20 @@ class Supplement:
         self._tag = tag
         self._base_version = None
         self._decode = None
+
     def set_decode_path(self, path):
         self._decode = path
+
     def set_base_version(self, base_version):
         self._base_version = str(base_version)
+
     def to_element_tree(self):
         root = etree.Element("Supplement")
-        etree.SubElement(root, "Warehouse", id = self._warehouse)
+        etree.SubElement(root, "Warehouse", id=self._warehouse)
         inst = etree.SubElement(root, "Instance",
-                                { 'line': self._id,
-                                  'versiontype': "generic",
-                                  'tag': self._tag })
+                                {'line': self._id,
+                                 'versiontype': "generic",
+                                 'tag': self._tag})
         if self._base_version != None:
             inst.set('baseversion', self._base_version)
         decode = etree.SubElement(root, "DecodePath")
@@ -34,27 +38,32 @@ class Supplement:
             decode.text = self._decode
         return root
 
+
 class Warehouse:
     def __init__(self, id, type):
         self._id = id
         self._type = type
         self._items = {}
+
     @property
     def id(self):
         return self._id
+
     def add_item(self, item):
         self._items.setdefault(item.id, [])
         self._items[item.id].append(item)
+
     def to_element_tree(self):
-        root = etree.Element("Warehouse", type = self._type, id = self._id, snapshot="true")
+        root = etree.Element("Warehouse", type=self._type, id=self._id, snapshot="true")
         lines = etree.SubElement(root, "Lines")
-        for line_id, items in self._items.iteritems():
-            line = etree.SubElement(lines, "Line", id = line_id)
+        for line_id, items in self._items.items():
+            line = etree.SubElement(lines, "Line", id=line_id)
             instances = etree.SubElement(line, "Instances")
             for i in items:
                 instances.append(i.to_element_tree())
         return root
-        
+
+
 class WarehouseItem:
     def __init__(self, line_id, version):
         self._id = line_id
@@ -63,23 +72,29 @@ class WarehouseItem:
         self._release_tags = []
         self._resubscription = None
         self._supplements = []
+
     @property
     def id(self):
         return self._id
+
     @property
     def version(self):
         return self._version
+
     def set_promotional_state(self, state):
         self._promotional_state = str(state)
+
     def set_release_tags(self, tags):
         self._release_tags = tags
+
     def set_resubscription(self, line_id, baseversion, version):
         self._resubscription = (line_id, str(baseversion), str(version))
+
     def add_supplement(self, supplement):
         self._supplements.append(supplement)
 
     def to_element_tree(self):
-        instance = etree.Element("Instance", version = self._version)
+        instance = etree.Element("Instance", version=self._version)
         if self._promotional_state != None:
             instance.set("promotionalstate", self._promotional_state)
         if len(self._supplements) != 0:
@@ -96,8 +111,9 @@ class WarehouseItem:
         if self._resubscription != None:
             resubs = etree.SubElement(instance, "Resubscriptions")
             (id, bv, v) = self._resubscription
-            etree.SubElement(resubs, "Resubscription", line = id, baseversion = bv, version = v)
+            etree.SubElement(resubs, "Resubscription", line=id, baseversion=bv, version=v)
         return instance
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     logging.basicConfig()
