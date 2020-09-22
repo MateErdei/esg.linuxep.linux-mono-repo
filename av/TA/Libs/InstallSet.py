d9410d5a46a068c09297962fe519189b4e854aef
Douglas Leeder <douglas.leeder@sophos.com>
2020-09-22 10:19:44 +0100

    [av] [LINUXDAR-2263] Add manual code to create an install set.
    Seems silly to download the supplements but not have a way to create the install set.
    Share the functionality with the automation system.

--- av/TA/Libs/InstallSet.py
+++ av/TA/Libs/InstallSet.py
@@ -12,38 +12,39 @@ except ImportError:
 
 from robot.libraries.BuiltIn import BuiltIn
 
+Libs = os.path.dirname(__file__)
+TA = os.path.dirname(Libs)
+manual = os.path.join(TA, "manual")
+assert os.path.isdir(manual)
+if manual not in sys.path:
+    sys.path.append(manual)
+
+import createInstallSet
+createInstallSet.LOGGER = logger
+
+import downloadSupplements
+downloadSupplements.LOGGER = logger
 
 def download_supplements(dest):
     # ensure manual dir is on sys.path
-    Libs = os.path.dirname(__file__)
-    TA = os.path.dirname(Libs)
-    manual = os.path.join(TA, "manual")
-    assert os.path.isdir(manual)
-    if manual not in sys.path:
-        sys.path.append(manual)
-    import downloadSupplements
-    downloadSupplements.LOGGER = logger
     ret = downloadSupplements.run(dest)
     assert ret == 0
+
+
+def susi_dir(install_set):
+    return createInstallSet.susi_dir(install_set)
+
+
+def setup_install_set(install_set, sdds_component, vdl, ml_model, local_rep):
+    return createInstallSet.setup_install_set(install_set, sdds_component, vdl, ml_model, local_rep)
 
 
 class InstallSet(object):
     def __init__(self):
         self.__m_install_set_verified = False
 
-    def susi_dir(self, install_set):
-        return os.path.join(install_set, "files", "plugins", "av", "chroot", "susi", "distribution_version", "version1")
-
-    def verify_install_set(self, install_set):
-        if not os.path.isdir(install_set):
-            return False
-
-        dest = self.susi_dir(install_set)
-        for x in "vdb", "mlmodel", "lrdata":
-            if not os.path.isdir(os.path.join(dest, x)):
-                return False
-
-        return True
+    def verify_install_set(self, install_set, sdds_component=None):
+        return createInstallSet.verify_install_set(install_set, sdds_component)
 
     def create_install_set(self, install_set):
         base = BuiltIn().get_variable_value("${TEST_INPUT_PATH}")
@@ -52,42 +53,11 @@ class InstallSet(object):
         sdds_component = BuiltIn().get_variable_value("${COMPONENT_SDDS_COMPONENT}")
         logger.info("sdds_component = %s" % sdds_component)
 
-        vdl = os.path.join(base, "vdl")
-        ml_model = os.path.join(base, "ml_model")
-        local_rep = os.path.join(base, "local_rep")
-
         if not os.path.isdir(sdds_component):
-            logger.error("Failed to find SDDS_COMONENT for INSTALL_SET: %s" % sdds_component)
+            logger.error("Failed to find SDDS_COMPONENT for INSTALL_SET: %s" % sdds_component)
             return
 
-        for x in (vdl, ml_model, local_rep):
-            if not os.path.isdir(x):
-                download_supplements(base)
-                assert os.path.isdir(x)
-
-        o = subprocess.check_output(['ls', base])
-        logger.info("BASE ls = %s" % o.decode("UTF-8"))
-
-        o = subprocess.check_output(['ls', vdl])
-        logger.info("VDL ls = %s" % o.decode("UTF-8"))
-
-        o = subprocess.check_output(['ls', ml_model])
-        logger.info("ml_model ls = %s" % o.decode("UTF-8"))
-
-        o = subprocess.check_output(['ls', local_rep])
-        logger.info("local_rep ls = %s" % o.decode("UTF-8"))
-
-        # Copy SDDS-Component
-        shutil.rmtree(install_set, ignore_errors=True)
-        shutil.copytree(sdds_component, install_set)
-
-        dest = self.susi_dir(install_set)
-        vdl_dest = os.path.join(dest, "vdb")
-        shutil.copytree(vdl, vdl_dest)
-        shutil.copytree(ml_model, os.path.join(dest, "mlmodel"))
-        shutil.copytree(local_rep, os.path.join(dest, "lrdata"))
-
-        self.__m_install_set_verified = self.verify_install_set(install_set)
+        return createInstallSet.create_install_set_if_required(install_set, sdds_component, base)
 
     def Create_Install_Set_If_Required(self):
         if self.__m_install_set_verified:
@@ -95,8 +65,8 @@ class InstallSet(object):
 
         # Read variables
         install_set = BuiltIn().get_variable_value("${COMPONENT_INSTALL_SET}")
-        self.__m_install_set_verified = self.verify_install_set(install_set)
+        sdds_component = BuiltIn().get_variable_value("${COMPONENT_SDDS_COMPONENT}")
+        self.__m_install_set_verified = self.verify_install_set(install_set, sdds_component)
         if self.__m_install_set_verified:
             return
         self.create_install_set(install_set)
-
---
+++ av/TA/manual/createInstallSet.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import filecmp
+import os
+import shutil
+import sys
+from distutils.core import setup
+
+LOGGER = None
+
+def log(*x):
+    global LOGGER
+    if LOGGER is None:
+        print(*x)
+    else:
+        LOGGER.info(" ".join(x))
+
+
+def download_supplements(dest):
+    # ensure manual dir is on sys.path
+    import downloadSupplements
+    downloadSupplements.LOGGER = LOGGER
+    ret = downloadSupplements.run(dest)
+    assert ret == 0
+
+
+def susi_dir(install_set):
+    return os.path.join(install_set, "files", "plugins", "av", "chroot", "susi", "distribution_version", "version1")
+
+
+def is_same(p1, p2):
+    try:
+        return filecmp.cmp(p1, p2, False)
+    except EnvironmentError:
+        return False
+
+
+def verify_install_set(install_set, sdds_component=None):
+    if not os.path.isdir(install_set):
+        return False
+
+    dest = susi_dir(install_set)
+    for x in "vdb", "mlmodel", "lrdata":
+        if not os.path.isdir(os.path.join(dest, x)):
+            return False
+
+    # Compare manifest.dat inside sdds_component
+    if sdds_component is not None:
+        sdds_manifest = os.path.join(sdds_component, b"manifest.dat")
+        install_manifest = os.path.join(install_set, b"manifest.dat")
+        if not is_same(sdds_manifest, install_manifest):
+            return False
+
+    return True
+
+
+def setup_install_set(install_set, sdds_component, vdl, ml_model, local_rep):
+    # Copy SDDS-Component
+    shutil.rmtree(install_set, ignore_errors=True)
+    shutil.copytree(sdds_component, install_set)
+
+    dest = susi_dir(install_set)
+    vdl_dest = os.path.join(dest, "vdb")
+    shutil.copytree(vdl, vdl_dest)
+    shutil.copytree(ml_model, os.path.join(dest, "mlmodel"))
+    shutil.copytree(local_rep, os.path.join(dest, "lrdata"))
+    return 0
+
+
+def create_install_set(install_set, sdds_component, base):
+    vdl = os.path.join(base, "vdl")
+    ml_model = os.path.join(base, "ml_model")
+    lrdata = os.path.join(base, "local_rep")
+    download_supplements(base)
+    return setup_install_set(install_set, sdds_component, vdl, ml_model, lrdata)
+
+
+def create_install_set_if_required(install_set, sdds_component, base):
+    if verify_install_set(install_set, sdds_component):
+        return 0
+    return create_install_set(install_set, sdds_component, base)
+
+def main(argv):
+    # Delete install set if you want it recreated
+    # Recreate install set if sdds_component is different
+    if len(argv) == 4:
+        install_set = argv[1]
+        sdds_component = argv[2]
+        base = argv[3]
+        return create_install_set_if_required(install_set, sdds_component, base)
+
+    return 2
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
