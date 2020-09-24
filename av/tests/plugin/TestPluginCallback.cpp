c74be62b54de1074932dc33bcf18db098e9994c4
yael.may <yael.may@sophos.com>
2020-09-24 13:39:02 +0100

    [av] [LINUXDAR-2279] Add missing test for ML model file does not exist

--- av/tests/plugin/TestPluginCallback.cpp
+++ av/tests/plugin/TestPluginCallback.cpp
@@ -31,6 +31,7 @@ namespace
     public:
         void SetUp() override
         {
+            //creating initial version file
             auto& appConfig = Common::ApplicationConfiguration::applicationConfiguration();
             appConfig.setData("PLUGIN_INSTALL", BASE);
             fs::create_directories(BASE);
@@ -44,17 +45,19 @@ namespace
             versionFileStream << "BUILD_DATE = 1970-00-01" << std::endl;
             versionFileStream.close();
 
+            //creating initial ml library file
             fs::path libDirPath (BASE);
             libDirPath /= "chroot/susi/distribution_version/version1";
             fs::create_directories(libDirPath);
-            m_mlLibFilePath = libDirPath;
-            m_mlLibFilePath /= "libmodel.so";
+            m_mlLibPath = libDirPath;
+            m_mlLibPath /= "libmodel.so";
 
             std::ofstream mlFilePathStream;
-            mlFilePathStream.open(m_mlLibFilePath);
+            mlFilePathStream.open(m_mlLibPath);
             mlFilePathStream << "1"  << std::endl;
             mlFilePathStream.close();
 
+            //creating initial lr data files
             fs::path lrDirPath (BASE);
             lrDirPath /= "chroot/susi/distribution_version/version1/lrdata";
             fs::create_directories(lrDirPath);
@@ -72,6 +75,14 @@ namespace
             lrSignerrepStream.open(m_lrSignerrepPath);
             lrSignerrepStream << "1"  << std::endl;
             lrSignerrepStream.close();
+
+            //creating initial ml model file
+            std::string initialHexString = "7374617469635f5f5f5f646574656374696f6e00ed03000004000000723b3401ec25b100010000000000000000001000";
+            fs::path mlModelDirPath = BASE;
+            mlModelDirPath /= "chroot/susi/distribution_version/version1/mlmodel";
+            fs::create_directories(mlModelDirPath);
+            m_mlModelPath = mlModelDirPath / "model.dat";
+            writeHexStringToFile(initialHexString, m_mlModelPath);
 
             //creating file for vdl version
             m_vdlDirPath = BASE;
@@ -120,17 +131,35 @@ namespace
             vdlVersionFileStream.close();
         }
 
+        void writeHexStringToFile(const std::string &hexString, const fs::path &filePath){
+            std::ofstream datafile(filePath, std::ios_base::binary | std::ios_base::out);
+
+            char buf[3];
+            buf[2] = 0;
+
+            std::stringstream input(hexString);
+            input.flags(std::ios_base::hex);
+            while (input)
+            {
+                input >> buf[0] >> buf[1];
+                long val = strtol(buf, nullptr, 16);
+                datafile << static_cast<unsigned char>(val & 0xff);
+            }
+        }
+
         std::shared_ptr<Plugin::PluginCallback> m_pluginCallback;
 
         fs::path m_lrFilerepPath;
         fs::path m_lrSignerrepPath;
-        fs::path m_mlLibFilePath;
+        fs::path m_mlLibPath;
+        fs::path m_mlModelPath;
         fs::path m_vdlDirPath;
         fs::path m_vdlVersionFilePath;
         fs::path m_versionFile;
 
         std::string m_initialExpectedLrHash = "ad0fadf63cc7cd779ce475e345bf4063565b63a3c2efef1eebc89790aaa6acba";
         std::string m_initialExpectedMlLibHash = "4355a46b19d348dc2f57c046f8ef63d4538ebb936000f3c9ee954a27460dd865";
+        std::string m_initialExpectedMlModelVersion = "20200306";
         unsigned long m_initialExpectedVdlIdeCount = 3;
         std::string m_initialExpectedVdlVersion = "5.78";
         std::string m_initialExpectedVersion = "1.2.3.456";
@@ -175,7 +204,7 @@ TEST_F(TestPluginCallback, getTelemetry_mlLibHash) //NOLINT
     EXPECT_EQ(initialTelemetry["ml-lib-hash"], m_initialExpectedMlLibHash);
 
     std::ofstream mlFilePathStream;
-    mlFilePathStream.open(m_mlLibFilePath);
+    mlFilePathStream.open(m_mlLibPath);
     mlFilePathStream << "2"  << std::endl;
     mlFilePathStream.close();
 
@@ -186,11 +215,39 @@ TEST_F(TestPluginCallback, getTelemetry_mlLibHash) //NOLINT
 
 TEST_F(TestPluginCallback, getTelemetry_mlLibHash_fileDoesNotExist) //NOLINT
 {
-    fs::remove(m_mlLibFilePath);
+    fs::remove(m_mlLibPath);
 
     json telemetry = json::parse(m_pluginCallback->getTelemetry());
 
     EXPECT_EQ(telemetry["ml-lib-hash"], "unknown");
+}
+
+TEST_F(TestPluginCallback, getTelemetry_mlModelVersion) //NOLINT
+{
+    std::string modifiedHexString = "7374617469635f5f5f5f646574656374696f6e00ed03000004000000733b3401ec25b100010000000000000000001000";
+    std::string modifiedExpectedMlModelVersion = "20200307";
+
+    std::string initJson = m_pluginCallback->getTelemetry();
+    json initialTelemetry = json::parse(initJson);
+
+    EXPECT_EQ(initialTelemetry["ml-pe-model-version"], m_initialExpectedMlModelVersion);
+
+    writeHexStringToFile(modifiedHexString, m_mlModelPath);
+
+    std::string modJson = m_pluginCallback->getTelemetry();
+    json modifiedTelemetry = json::parse(modJson);
+
+    EXPECT_EQ(modifiedTelemetry["ml-pe-model-version"], modifiedExpectedMlModelVersion);
+}
+
+TEST_F(TestPluginCallback, getTelemetry_mlModelVersion_fileDoesNotExist) //NOLINT
+{
+    fs::remove(m_mlModelPath);
+
+    std::string initJson = m_pluginCallback->getTelemetry();
+    json telemetry = json::parse(initJson);
+
+    EXPECT_EQ(telemetry["ml-pe-model-version"], "unknown");
 }
 
 TEST_F(TestPluginCallback, getTelemetry_lrData) //NOLINT
