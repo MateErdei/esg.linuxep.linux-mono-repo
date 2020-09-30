a308e7ee39c780702fa66e9c48f53b1677294a8a
yael.may <yael.may@sophos.com>
2020-09-30 11:12:33 +0100

    [av] [LINUXDAR-2223] Attempt to handle non-vagrant machine in runScanner2b.sh
    [+DLCL]

--- av/products/susi_experiment_scanner/runScanner2b.sh
+++ av/products/susi_experiment_scanner/runScanner2b.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
+failure()
+{
+    local CODE=$1
+    shift
+    echo "$@" >&2
+    exit $CODE
+}
+
 STARTINGDIR=$(pwd)
 cd ${0%/*}
 BASE=$(pwd)
 export BASE
+cd ..
+PRODUCTS=$(pwd)
+cd ..
+SOURCE_DIR=$(pwd)
+BUILD_DIR=${SOURCE_DIR}/build64
+[[ -d ${BUILD_DIR} ]] || BUILD_DIR=/vagrant/sspl-plugin-anti-virus/build64
+
 cd "${STARTINGDIR}"
 
-BUILD_DIR=/vagrant/sspl-plugin-anti-virus/build64
 EXE="$BASE/susi_scanner2"
 [[ -f "$EXE" ]] || EXE=${BUILD_DIR}/products/susi_experiment_scanner/susi_scanner2
-[[ -f "$EXE" ]] || {
-  echo "Failed to find $EXE"
-  exit 1
-}
+[[ -f "$EXE" ]] || failure 1 "Failed to find $EXE"
 
 file "$EXE"
 
 export PLUGIN_INSTALL=/opt/sophos-spl/plugins/av
 export SUSI_DIR=${PLUGIN_INSTALL}/chroot/susi/distribution_version
 export LD_LIBRARY_PATH=$BASE:$BASE/lib64:$PLUGIN_INSTALL/lib64:$SUSI_DIR:$SUSI_DIR/version1
-ldd "$EXE" || {
-  echo "Failed to ldd $EXE"
-  exit 2
-}
+ldd "$EXE" || failure 2 "Failed to ldd $EXE"
 
 TOOL=
 # TOOL="gdb --args"
