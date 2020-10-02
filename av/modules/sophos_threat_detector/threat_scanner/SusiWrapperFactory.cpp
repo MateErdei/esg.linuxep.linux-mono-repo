c13936eb94e180c976a5d89b1f845aa962b19d8d
Douglas Leeder <douglas.leeder@sophos.com>
2020-10-02 14:53:22 +0100

    [av] [LINUXDAR-2290] Correct formatting of globalReputation configuration.

--- av/modules/sophos_threat_detector/threat_scanner/SusiWrapperFactory.cpp
+++ av/modules/sophos_threat_detector/threat_scanner/SusiWrapperFactory.cpp
@@ -4,22 +4,14 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 
 ******************************************************************************************************/
 
-
 #include "SusiWrapperFactory.h"
 
 #include "ScannerInfo.h"
 #include "SusiWrapper.h"
-#include "Logger.h"
 
-#include <Common/ApplicationConfiguration/IApplicationConfiguration.h>
 #include "Common/UtilityImpl/StringUtils.h"
-#include <Common/XmlUtilities/AttributesMap.h>
-
-#include <fstream>
-#include <thirdparty/nlohmann-json/json.hpp>
 
 using namespace threat_scanner;
-using json = nlohmann::json;
 namespace fs = sophos_filesystem;
 
 std::shared_ptr<ISusiWrapper> SusiWrapperFactory::createSusiWrapper(const std::string& scannerConfig)
@@ -32,65 +24,8 @@ static fs::path susi_library_path()
     return pluginInstall() / "chroot/susi/distribution_version";
 }
 
-static std::string getEndpointId()
+static std::string create_runtime_config(const std::string& scannerInfo)
 {
-    auto& appConfig = Common::ApplicationConfiguration::applicationConfiguration();
-    fs::path machineIdPath = appConfig.getData("SOPHOS_INSTALL") + "/base/etc/machine_id.txt";
-
-    std::ifstream fs(machineIdPath);
-
-    if (fs.good())
-    {
-        try
-        {
-            std::stringstream endpointIdContents;
-            endpointIdContents << fs.rdbuf();
-
-            return endpointIdContents.str();
-        }
-        catch (std::exception& e)
-        {
-            LOGERROR("Unexpected error when reading endpoint id for global rep setup: " << e.what());
-        }
-    }
-
-    return "66b8fd8b39754951b87269afdfcb285c";
-}
-
-static std::string getCustomerId()
-{
-    auto& appConfig = Common::ApplicationConfiguration::applicationConfiguration();
-    fs::path customerIdPath = appConfig.getData("SOPHOS_INSTALL") + "/base/update/var/update_config.json";
-
-    std::ifstream fs(customerIdPath, std::ifstream::in);
-
-    if (fs.good())
-    {
-        try
-        {
-            std::stringstream updateConfigContents;
-            updateConfigContents << fs.rdbuf();
-
-            auto jsonContents = json::parse(updateConfigContents);
-            LOGINFO("json: " << jsonContents);
-            LOGINFO("creds: " << jsonContents["credential"]);
-            return jsonContents["credential"]["password"];
-
-        }
-        catch (std::exception& e)
-        {
-            LOGERROR("Unexpected error when reading customer id for global rep setup: " << e.what());
-        }
-    }
-
-    return "c1cfcf69a42311a6084bcefe8af02c8a";
-}
-
-
-static std::string create_runtime_config(const std::string& scannerInfo, const std::string& endpointId, const std::string& customerId)
-{
-    std::string customerID("0123456789abcdef");
-
     fs::path libraryPath = susi_library_path();
     std::string runtimeConfig = Common::UtilityImpl::StringUtils::orderedStringReplace(R"sophos({
     "library": {
@@ -101,22 +36,23 @@ static std::string create_runtime_config(const std::string& scannerInfo, const s
             "context": "File",
             "version": "1.0.0"
         },
-        "customerID": "@@CUSTOMER_ID@@",
-        "machineID": "@@MACHINE_ID@@"
+        "globalReputation": {
+            "customerID": "c1cfcf69a42311a6084bcefe8af02c8a",
+            "machineID":  "66b8fd8b39754951b87269afdfcb285c",
+            "timeout": 5,
+            "enableLookup": true
+        }
     },
     @@SCANNER_CONFIG@@
 })sophos", {{"@@LIBRARY_PATH@@", libraryPath},
-            {"@@CUSTOMER_ID@@", customerId},
-            {"@@MACHINE_ID@@", endpointId},
             {"@@SCANNER_CONFIG@@", scannerInfo}
     });
     return runtimeConfig;
 }
 
-//make a method that reads ALC-policy to get the Customer ID
 SusiWrapperFactory::SusiWrapperFactory()
 {
     std::string scannerInfo = create_scanner_info(false);
-    std::string runtimeConfig = create_runtime_config(scannerInfo, getEndpointId(), getCustomerId());
+    std::string runtimeConfig = create_runtime_config(scannerInfo);
     m_globalHandler = std::make_shared<SusiGlobalHandler>(runtimeConfig);
 }
