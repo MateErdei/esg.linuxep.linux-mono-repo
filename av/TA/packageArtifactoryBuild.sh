905c2b7764d56a6cadd551023bbbac4d32c41f95
DarrenSmith <darren.smith@sophos.com>
2020-10-09 14:47:33 +0100

    [av] [LINUXDAR-2242] Fast forward to changes from the make_installAV.sh_work_with_python branch

--- av/TA/packageArtifactoryBuild.sh
+++ av/TA/packageArtifactoryBuild.sh
@@ -11,13 +11,20 @@ then
 	echo Please pass an artifactory repository path
 	exit 1
 fi
-
 DEST_BASE=/tmp
+
+if [[ -f ${DEST_BASE}/output.zip ]]
+then
+    NEWER="--time-cond ${DEST_BASE}/output.zip"
+else
+    NEWER=""
+fi
+
+curl https://artifactory.sophos-ops.com/${REPO_PATH}/build/output.zip --output ${DEST_BASE}/output.zip $NEWER
+
 OUTPUT=${DEST_BASE}/output
 [ -d "$OUTPUT" ] && rm -rf $OUTPUT
 mkdir $OUTPUT
-
-curl https://artifactory.sophos-ops.com/${REPO_PATH}/build/output.zip --output ${DEST_BASE}/output.zip
 unzip ${DEST_BASE}/output.zip -d $OUTPUT
 
 TEST_DIR_NAME=test
@@ -28,10 +35,20 @@ mkdir -p $AV
 
 rsync -va --copy-unsafe-links --delete  "$BASE/../TA/"            "$INPUTS/test_scripts"
 ln -snf test_scripts "$INPUTS/TA"
-rsync -va --copy-unsafe-links --delete "$OUTPUT/SDDS-COMPONENT/" "$AV/SDDS-COMPONENT"
-rsync -va --copy-unsafe-links --delete "$OUTPUT/base-sdds/"      "$AV/base-sdds"
-rsync -va --copy-unsafe-links --delete "$OUTPUT/test-resources"  "$AV/"
+rm -rf "$AV/SDDS-COMPONENT"
+mv "$OUTPUT/SDDS-COMPONENT/" "$AV/SDDS-COMPONENT"
+chmod 700 "$AV/SDDS-COMPONENT/install.sh"
+rm -rf "$AV/base-sdds"
+mv "$OUTPUT/base-sdds/"      "$AV/base-sdds"
+chmod 700 "$AV/base-sdds/install.sh"
+rm -rf "$AV/test-resources"
+mv "$OUTPUT/test-resources"  "$AV/"
 
-python3 ${BASE}/manual/createInstallSet.py "$AV/INSTALL-SET/" "$AV/SDDS-COMPONENT/" "$INPUTS"
+PYTHON=${PYTHON:-python3}
+${PYTHON} ${BASE}/manual/downloadSupplements.py "$INPUTS"
 
-exec tar cjf /tmp/inputs.tar.bz2 -C ${DEST_BASE} ${TEST_DIR_NAME}
+exec tar cjf /tmp/inputs.tar.bz2 -C ${DEST_BASE} \
+    --exclude='test/inputs/vdl.zip' \
+    --exclude='test/inputs/model.zip' \
+    --exclude='test/inputs/reputation.zip' \
+    ${TEST_DIR_NAME}
