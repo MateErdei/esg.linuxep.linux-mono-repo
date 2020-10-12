a625b99abc35db3f9d1d2857eb9cce0610080c49
Douglas Leeder <douglas.leeder@sophos.com>
2020-10-12 15:51:44 +0100

    [av] [LINUXDAR-1788] Implement signal handler for SIGUSR1.

--- av/modules/unixsocket/threatDetectorSocket/SigURS1Monitor.cpp
+++ av/modules/unixsocket/threatDetectorSocket/SigURS1Monitor.cpp
@@ -5,6 +5,11 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 ******************************************************************************************************/
 
 #include "SigURS1Monitor.h"
+
+#include "../Logger.h"
+
+#include <csignal>
+
 int unixsocket::SigURS1Monitor::monitorFd()
 {
     return m_pipe.readFd();
@@ -15,15 +20,46 @@ void unixsocket::SigURS1Monitor::triggered()
     {
     }
 
+    LOGWARN("Reload triggered by USR1");
+
     m_reloader->reload();
 }
+
+static int GL_USR1_MONITOR_PIPE = -1; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+
+static void signal_handler(int)
+{
+    if (GL_USR1_MONITOR_PIPE >= 0)
+    {
+        ::write(GL_USR1_MONITOR_PIPE, "\0", 1);
+    }
+}
+
 unixsocket::SigURS1Monitor::SigURS1Monitor(unixsocket::IReloadablePtr reloadable)
     : m_reloader(std::move(reloadable))
 {
     // Setup signal handler
+    struct sigaction action{};
+    action.sa_handler = signal_handler;
+    action.sa_flags = SA_RESTART;
+    int ret = ::sigaction(SIGUSR1, &action, nullptr);
+    if (ret != 0)
+    {
+        LOGERROR("Failed to setup SIGUSR1 signal handler");
+    }
+    GL_USR1_MONITOR_PIPE = m_pipe.writeFd();
 }
 
 unixsocket::SigURS1Monitor::~SigURS1Monitor()
 {
     // clear signal handler
+    GL_USR1_MONITOR_PIPE = -1;
+    struct sigaction action{};
+    action.sa_handler = SIG_DFL;
+    action.sa_flags = SA_RESTART;
+    int ret = ::sigaction(SIGUSR1, &action, nullptr);
+    if (ret != 0)
+    {
+        LOGERROR("Failed to clear SIGUSR1 signal handler");
+    }
 }
