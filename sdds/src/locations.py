a8d7a49b701802be3a0e857495af6f108c8dd78b
Jake McLanaghan <jake.mclanaghan@sophos.com>
2020-10-15 14:39:29 +0100

    print environment

--- sdds/src/locations.py
+++ sdds/src/locations.py
@@ -5,6 +5,9 @@ cwd = os.getcwd()
 LOGS = os.path.join(cwd, "..", "logs")
 WAREHOUSE_WRITE = os.path.join(cwd, "..", "output", "warehouse")
 CUSTOMER_FILES_WRITE = os.path.join(cwd, "..", "output", "customer")
+print("JAKE LOOK HERE")
+print(os.environ)
+print("JAKE LOOK HERE")
 branch = os.environ.get("SOURCE_CODE_BRANCH").replace("/","-")
 WAREHOUSE_READ  = ["http://ostia.eng.sophos/dev/sspl-warehouse/{}/warehouse/warehouse".format(branch)]
 REPOSITORY_ID = "ES+C"
