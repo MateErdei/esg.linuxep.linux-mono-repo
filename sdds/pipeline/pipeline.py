91187431e074b992e328c29465251a34437e5366
Jake McLanaghan <jake.mclanaghan@sophos.com>
2020-10-15 14:20:43 +0100

    [sdds] fix duplicate outputs

--- sdds/pipeline/pipeline.py
+++ sdds/pipeline/pipeline.py
@@ -6,14 +6,14 @@ def sspl_warehouse(stage: tap.Root):
     component = tap.Component(name='sspl-warehouse', base_version='1.0.0')
 
     with stage.parallel('build'):
-        _build = stage.artisan_build(name='release', component=component, image='Warehouse',
+        _build = stage.artisan_build(name='develop', component=component, image='Warehouse',
                                          mode='release', release_package='./build/release-package.xml')
 
-        _build = stage.artisan_build(name='release', component=component, image='Warehouse',
+        _build = stage.artisan_build(name='edr-999', component=component, image='Warehouse',
                                      mode='release', release_package='./build/release-package-edr-999.xml')
 
-        _build = stage.artisan_build(name='release', component=component, image='Warehouse',
+        _build = stage.artisan_build(name='mdr-999', component=component, image='Warehouse',
                                      mode='release', release_package='./build/release-package-mdr-999.xml')
 
-        _build = stage.artisan_build(name='release', component=component, image='Warehouse',
+        _build = stage.artisan_build(name='edr-mdr-999', component=component, image='Warehouse',
                                      mode='release', release_package='./build/release-package-edr-mdr-999.xml')
