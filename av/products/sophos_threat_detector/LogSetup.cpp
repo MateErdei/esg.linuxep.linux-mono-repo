c60caa5731186b7d20fbb8776fd2bb119b30d18b
efthymios.makaras <efthymios.makaras@sophos.com>
2020-10-16 15:06:53 +0100

    [av] [LINUXDAR-2251] Change sophos_threat_detector logger to use setupFileLoggingWithPath instead of setupConsoleFileLoggingWithPath [+EH]

--- av/products/sophos_threat_detector/LogSetup.cpp
+++ av/products/sophos_threat_detector/LogSetup.cpp
@@ -7,7 +7,7 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 #include "LogSetup.h"
 
 #include "Common/ApplicationConfiguration/IApplicationPathManager.h"
-#include "Common/Logging/ConsoleFileLoggingSetup.h"
+#include "Common/Logging/FileLoggingSetup.h"
 #include "Common/Logging/LoggerConfig.h"
 #include "../common/config.h"
 
@@ -18,7 +18,7 @@ LogSetup::LogSetup()
     std::string logfilepath = Common::ApplicationConfiguration::applicationPathManager().sophosInstall();
     logfilepath += "/plugins/av/log/sophos_threat_detector/sophos_threat_detector.log";
 
-    Common::Logging::ConsoleFileLoggingSetup::setupConsoleFileLoggingWithPath(logfilepath);
+    Common::Logging::FileLoggingSetup::setupFileLoggingWithPath(logfilepath);
     Common::Logging::applyGeneralConfig(PLUGIN_NAME);
 }
 
