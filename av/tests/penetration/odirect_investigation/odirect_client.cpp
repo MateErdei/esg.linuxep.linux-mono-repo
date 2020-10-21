2cacb3f0f8f5fe5b6cc08a61bef69b36a4cec1ca
Douglas Leeder <douglas.leeder@sophos.com>
2020-10-21 16:04:41 +0100

    [av] [LINUXDAR-2381] Add test programs to verify that O_DIRECT is shared by the fds on either end of the unix socket!
    e.g.
    /etc/fstab is O_DIRECT
    /etc/fstab is O_DIRECT
    /etc/fstab is NOT O_DIRECT
    /etc/fstab is NOT O_DIRECT

---
+++ av/tests/penetration/odirect_investigation/odirect_client.cpp
@@ -0,0 +1,79 @@
+/******************************************************************************************************
+
+Copyright 2019, Sophos Limited.  All rights reserved.
+
+******************************************************************************************************/
+
+#include <datatypes/Print.h>
+#include <unixsocket/threatDetectorSocket/ScanningClientSocket.h>
+
+#include <Common/Logging/ConsoleLoggingSetup.h>
+
+#include <cassert>
+#include <string>
+#include <thread>
+
+#include <fcntl.h>
+
+static void flip_odirect(const std::string& file_path, int file_fd)
+{
+    auto flags = static_cast<unsigned>(::fcntl(file_fd, F_GETFL, 0));
+    auto without_odirect = flags & ~static_cast<unsigned>(O_DIRECT);
+    auto with_odirect = flags | O_DIRECT;
+    for (int i=0; i<300; i++)
+    {
+        ::fcntl(file_fd, F_SETFL, with_odirect);
+        PRINT("setting "<< file_path << " with O_DIRECT");
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        ::fcntl(file_fd, F_SETFL, without_odirect);
+        PRINT("setting "<< file_path << " without O_DIRECT");
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+}
+
+static scan_messages::ScanResponse scan(unixsocket::ScanningClientSocket& socket, datatypes::AutoFd& fd, const std::string& filename)
+{
+    scan_messages::ClientScanRequest request;
+    request.setPath(filename);
+    request.setScanInsideArchives(false);
+    request.setScanType(scan_messages::E_SCAN_TYPE_ON_DEMAND);
+    request.setUserID("root");
+    return socket.scan(fd, request);
+}
+
+int main(int argc, char* argv[])
+{
+    static Common::Logging::ConsoleLoggingSetup m_loggingSetup;
+
+    std::string filename = "/etc/fstab";
+    if (argc > 1)
+    {
+        filename = argv[1];
+    }
+
+    datatypes::AutoFd file_fd(open(filename.c_str(), O_RDONLY));
+    assert(file_fd.get() >= 0);
+
+    const std::string path = "/tmp/fd_chroot/tmp/unix_socket";
+    unixsocket::ScanningClientSocket socket(path);
+
+    std::thread flipper(&flip_odirect, filename, file_fd.get());
+
+    auto response = scan(socket, file_fd, filename); // takes ownership of file_fd
+
+    for (const auto& detection: response.getDetections())
+    {
+        if (detection.second.empty())
+        {
+            PRINT(filename << " is fake clean");
+        }
+        else
+        {
+            PRINT(filename << " is fake infected");
+        }
+    }
+
+    flipper.join();
+
+    return 0;
+}
---
+++ av/tests/penetration/odirect_investigation/odirect_server.cpp
@@ -0,0 +1,86 @@
+/******************************************************************************************************
+
+Copyright 2020, Sophos Limited.  All rights reserved.
+
+******************************************************************************************************/
+
+//
+// Created by Douglas Leeder on 19/12/2019.
+//
+
+#include "datatypes/Print.h"
+#include "sophos_threat_detector/threat_scanner/IThreatScannerFactory.h"
+#include "unixsocket/threatDetectorSocket/ScanningServerSocket.h"
+
+#include <Common/Logging/ConsoleLoggingSetup.h>
+
+#include <string>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define handle_error(msg) do { perror(msg); exit(EXIT_FAILURE); } while(0)
+namespace
+{
+    class FakeScanner : public threat_scanner::IThreatScanner
+    {
+        scan_messages::ScanResponse scan(
+            datatypes::AutoFd& fd,
+            const std::string& file_path,
+            int64_t /*scanType*/,
+            const std::string& /*userID*/) override
+        {
+            for (int i=0; i<1000; i++)
+            {
+                unsigned oldflags = ::fcntl(fd.get(), F_GETFL, 0);
+                if ((oldflags & O_DIRECT) > 0)
+                {
+                    PRINT(file_path << " is O_DIRECT");
+                }
+                else
+                {
+                    PRINT(file_path << " is NOT O_DIRECT");
+                }
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            }
+
+            fd.close();
+
+            scan_messages::ScanResponse response;
+            response.addDetection("/bin/bash", "");
+            return response;
+        }
+    };
+    class FakeScannerFactory : public threat_scanner::IThreatScannerFactory
+    {
+        threat_scanner::IThreatScannerPtr createScanner(bool) override
+        {
+            return std::make_unique<FakeScanner>();
+        }
+    };
+}
+
+int main()
+{
+    static Common::Logging::ConsoleLoggingSetup m_loggingSetup;
+
+    int ret = ::mkdir("/tmp/fd_chroot", 0700);
+    static_cast<void>(ret); // ignore
+
+    ret = chroot("/tmp/fd_chroot");
+    if (ret != 0)
+    {
+        handle_error("Failed to chroot to /tmp/fd_chroot");
+    }
+
+    ret = ::mkdir("/tmp", 0700);
+    static_cast<void>(ret); // ignore
+
+    const std::string path = "/tmp/unix_socket";
+    auto scannerFactory = std::make_shared<FakeScannerFactory>();
+    unixsocket::ScanningServerSocket server(path, 0666, scannerFactory);
+    server.run();
+
+    return 0;
+}
