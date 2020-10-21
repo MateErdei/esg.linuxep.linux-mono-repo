c7636cf90780e990e80cdd29c3c0a98e89fe55a2
Jake McLanaghan <jake.mclanaghan@sophos.com>
2020-10-21 10:24:56 +0100

    [base] [LINUXDAR-2249] passing the debug argument is handled better here. In python3 named arguments after *args are treated as "CAN ONLY BE SET VIA NAME".

--- base/testUtils/libs/FullInstallerUtils.py
+++ base/testUtils/libs/FullInstallerUtils.py
@@ -291,7 +291,7 @@ def run_full_installer_expecting_code(expected_code, *args):
     else:
         installer = get_full_installer()
     logger.info("Installer path: " + str(installer))
-    return run_full_installer_from_location_expecting_code(installer, expected_code, debug=True, *args)
+    return run_full_installer_from_location_expecting_code(installer, expected_code, *args, debug=True)
 
 def run_full_installer_without_x_set():
     if get_variable("PUB_SUB_LOGGING"):
@@ -301,7 +301,7 @@ def run_full_installer_without_x_set():
     logger.info("Installer path: " + str(installer))
     return run_full_installer_from_location_expecting_code(installer, 0, debug=False)
 
-def run_full_installer_from_location_expecting_code(install_script_location, expected_code, debug, *args):
+def run_full_installer_from_location_expecting_code(install_script_location, expected_code, *args, debug=True):
     # Robot passes in strings.
     expected_code = int(expected_code)
     if debug:
