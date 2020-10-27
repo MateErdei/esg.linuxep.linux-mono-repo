efa46af31d9cd8f178100b4d96ba5ccd9096ed84
Paul-Vlad Avadanei <paulvlad.avadanei@sophos.com>
2020-10-27 17:17:55 +0000

    [base] [LINUXDAR-2245] Removed unnecessary methods

--- base/modules/Common/ProcUtilImpl/ProcUtilities.h
+++ base/modules/Common/ProcUtilImpl/ProcUtilities.h
@@ -35,18 +35,6 @@ namespace Proc
         std::string comm;
     };
 
-    class CommMatcher
-    {
-        std::string m_comm;
-
-    public:
-        CommMatcher(const std::string& filename) : m_comm(filename) {};
-        bool operator()(const std::string& comm) const;
-        bool operator()(const ProcStat& procStat) const
-        {
-            return operator()(procStat.comm);
-        }
-    };
 
     /** Parse the content of /proc/pid/stat and return the ProcStat as optional for cases where the parse can not suceed
      * This method does not aim to be generic to work for all possible cases, but to be capable of correctly parsing the
@@ -55,29 +43,7 @@ namespace Proc
      * */
     std::optional<ProcStat> parseProcStat(const std::string& contentOfProcStat);
 
-    std::vector<ProcStat> listAndFilterProcesses(std::function<bool(const Proc::ProcStat&)> predicate);
-
-    std::vector<ProcStat> listProcesses();
-
-    std::vector<ProcStat> filterProcesses(const std::vector<ProcStat>&, std::function<bool(const ProcStat&)> filter);
-
     void killProcess(int pid);
-
-    std::vector<ProcStat> listProcessesByComm(
-        const std::string& partOfComm,
-        const std::string& requiresFullPathContainsPath);
-
-    /**
-     *
-     * @param partOfComm
-     * @param requiresFullPathContainsPath for each candidate that matches the comm it will require that the full path
-     * contains this string. This is to avoid the possible side effect of killing processes that have same name as the
-     * one of the partOfComm. Example: killing other instances of osquery for example that are not managed by
-     * sophos-spl.
-     */
-    void ensureNoExecWithThisCommIsRunning(
-        const std::string& partOfComm,
-        const std::string& requiresFullPathContainsPath);
 
     int getUserIdFromStatus(const long pid);
 
