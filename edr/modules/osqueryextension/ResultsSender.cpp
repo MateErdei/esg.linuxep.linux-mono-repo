9fa4e66b56e019caed328f2c5891fad05e2e893f
Alex Cooper <Alex.Cooper@Sophos.com>
2020-10-27 13:56:06 +0000

    [edr] [LINUXDAR-2201] adding ability to tag results in result sender used by logger extension. Implement filesize function in result sender.

--- edr/modules/osqueryextension/ResultsSender.cpp
+++ edr/modules/osqueryextension/ResultsSender.cpp
@@ -9,21 +9,86 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 #include "Logger.h"
 
 #include <Common/UtilityImpl/TimeUtils.h>
+#include <json/json.h>
+#include <modules/pluginimpl/ApplicationPaths.h>
 #include <iostream>
 
 ResultsSender::ResultsSender()
 {
     LOGDEBUG("Created results sender");
+    try
+    {
+        Send();
+    }
+    catch (const std::exception& e)
+    {
+        LOGERROR("Failed to send previous batched scheduled results on start up. " << e.what());
+    }
+
+    try
+    {
+        loadScheduledQueryTags();
+    }
+    catch (const std::exception& e)
+    {
+        LOGERROR("Failed to load scheduled query tags on start up. " << e.what());
+    }
+
 }
 
 ResultsSender::~ResultsSender()
 {
-    Send();
+    try
+    {
+        Send();
+    }
+    catch (const std::exception& e)
+    {
+        LOGERROR("Failed to send batched scheduled results on shutdown. " << e.what());
+    }
 }
 
 void ResultsSender::Add(const std::string& result)
 {
     LOGDEBUG("Adding XDR results to intermediary file: " << result);
+
+    Json::Value logLine;
+    try
+    {
+        std::stringstream logLineStream(result);
+        logLineStream >> logLine;
+    }
+    catch (const std::exception& e)
+    {
+        LOGERROR("Invalid JSON log message. " << e.what());
+        throw;
+    }
+
+    std::map<std::string, std::pair<std::string, std::string>> queryTagMap;
+    for (const auto& query : m_scheduledQueryTags)
+    {
+        queryTagMap.insert(std::make_pair(query.queryNameWithPack, std::make_pair(query.queryName, query.tag)));
+    }
+
+    auto queryName = logLine["name"].asString();
+    if (!queryName.empty())
+    {
+        auto correctQueryNameAndTag = queryTagMap[queryName];
+        if (logLine["name"] != correctQueryNameAndTag.first && !correctQueryNameAndTag.first.empty())
+        {
+            logLine["name"] = correctQueryNameAndTag.first;
+        }
+        if (!correctQueryNameAndTag.second.empty())
+        {
+            logLine["tag"] = correctQueryNameAndTag.second;
+        }
+    }
+
+    std::stringstream ss;
+    Json::StreamWriterBuilder writerBuilder;
+    writerBuilder["indentation"] = "";
+    ss << Json::writeString(writerBuilder, logLine);
+    auto taggedResult = ss.str();
 
     std::string stringToAppend;
     if (!m_firstEntry)
@@ -31,11 +96,9 @@ void ResultsSender::Add(const std::string& result)
         stringToAppend =",";
     }
 
-    stringToAppend.append(result);
-
+    stringToAppend.append(taggedResult);
     auto filesystem = Common::FileSystem::fileSystem();
     filesystem->appendFile(INTERMEDIARY_PATH, stringToAppend);
-
     m_firstEntry = false;
 }
 
@@ -75,6 +138,68 @@ void ResultsSender::Reset()
 
 uintmax_t ResultsSender::GetFileSize()
 {
-    LOGDEBUG("GetFileSize, returning 0 for now.");
-    return 0;
+    uintmax_t size = 0;
+    auto filesystem = Common::FileSystem::fileSystem();
+    if (filesystem->exists(INTERMEDIARY_PATH))
+    {
+        // Add 2 (wide characters) here for the , and ] that gets added
+        size = filesystem->fileSize(INTERMEDIARY_PATH) + 2;
+    }
+    return size;
 }
+
+
+Json::Value ResultsSender::readJsonFile(const std::string& path)
+{
+    Json::CharReaderBuilder builder;
+    auto reader = std::unique_ptr<Json::CharReader>(builder.newCharReader());
+    Json::Value root;
+    std::string errors;
+    try
+    {
+        auto filesystem = Common::FileSystem::fileSystem();
+        std::string jsonFile = filesystem->readFile(path);
+        reader->parse(jsonFile.c_str(), jsonFile.c_str() + jsonFile.size(), &root, &errors);
+    }
+    catch (const std::exception& err)
+    {
+        throw std::runtime_error("Failed to open " + path + " to read. Error:" + err.what());
+    }
+
+    return root;
+}
+
+void ResultsSender::loadScheduledQueryTags()
+{
+    std::vector<ScheduledQuery> scheduledQueries;
+    auto confJsonRoot = readJsonFile(Plugin::osqueryXDRConfigFilePath());
+    auto scheduleRoot = confJsonRoot["schedule"];
+    for (Json::Value::const_iterator scheduledItr = scheduleRoot.begin(); scheduledItr != scheduleRoot.end();
+         scheduledItr++)
+    {
+        auto query = *scheduledItr;
+        scheduledQueries.push_back(ScheduledQuery{
+            scheduledItr.key().asString(),
+            scheduledItr.key().asString(),
+            query["tag"].asString() });
+    }
+    auto otherQueryPacks = confJsonRoot["packs"];
+    for (Json::Value::const_iterator packItr = otherQueryPacks.begin(); packItr != otherQueryPacks.end();
+         packItr++)
+    {
+        auto packName = packItr.key().asString();
+        auto packNode = *packItr;
+        auto packQueries = packNode["queries"];
+        for (Json::Value::const_iterator packQueriesItr = packQueries.begin();
+             packQueriesItr != packQueries.end();
+             packQueriesItr++)
+        {
+            auto query = *packQueriesItr;
+            std::string packAppendedName = "pack_" + packName + "_" + packQueriesItr.key().asString();
+            scheduledQueries.push_back(ScheduledQuery{ packAppendedName, packQueriesItr.key().asString(), query["tag"].asString() });
+        }
+    }
+
+    m_scheduledQueryTags = scheduledQueries;
+}
+
--- edr/modules/osqueryextension/ResultsSender.h
+++ edr/modules/osqueryextension/ResultsSender.h
@@ -4,10 +4,19 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 
 ******************************************************************************************************/
 
-#include <ResultsSenderInterface.h>
-#include <Common/FileSystem/IFileSystem.h>
 #include <Common/FileSystem/IFilePermissions.h>
-#include <thirdparty/nlohmann-json/json.hpp>
+#include <Common/FileSystem/IFileSystem.h>
+#include <redist/jsoncpp/include/json/value.h>
+
+#include <ResultsSenderInterface.h>
+
+struct ScheduledQuery
+{
+    std::string queryNameWithPack;
+    std::string queryName;
+    std::string tag;
+};
+
 class ResultsSender : public ResultsSenderInterface
 {
 public:
@@ -22,4 +31,7 @@ private:
     bool m_firstEntry = true;
     inline static const std::string INTERMEDIARY_PATH = "/opt/sophos-spl/plugins/edr/var/tmp_file";
     inline static const std::string DATAFEED_PATH = "/opt/sophos-spl/base/mcs/datafeed";
+    std::vector<ScheduledQuery> m_scheduledQueryTags;
+    Json::Value readJsonFile(const std::string& path);
+    void loadScheduledQueryTags();
 };
