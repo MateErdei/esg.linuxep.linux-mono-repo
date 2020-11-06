0f2f990ce04f23b831e83f644b614d584d417b1e
Wellington Machingauta <Wellington.Machingauta@Sophos.com>
2020-11-06 14:30:31 +0000

    [edr] [LINUXDAR-2435]-remove hard coded query pack

---
+++ edr/tests/manual/TestLoggerExtention/TestLoggerExtention.cpp
@@ -0,0 +1,9 @@
+/******************************************************************************************************
+
+Copyright 2018-2020 Sophos Limited.  All rights reserved.
+
+******************************************************************************************************/
+
+#include <Common/FileSystem/IFileSystem.h>
+#include <Common/FileSystem/IFilePermissions.h>
+#include <Common/FileSystem/IFileSystemException.h>
