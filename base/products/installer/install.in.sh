16b81f74eaed0c5b6fc8e9ebc01d63b00be363b9
Alex Cooper <alex.cooper@sophos.com>
2020-11-26 15:24:07 +0000

    [base] [LINUXDAR-2495] allow spl group to write to telem cache dir.

--- base/products/installer/install.in.sh
+++ base/products/installer/install.in.sh
@@ -424,7 +424,7 @@ chown "root:${GROUP_NAME}" "${SOPHOS_INSTALL}/base/mcs/certs"
 # Telemetry
 makedir 750 "${SOPHOS_INSTALL}/base/telemetry"
 makedir 750 "${SOPHOS_INSTALL}/base/telemetry/var"
-makedir 750 "${SOPHOS_INSTALL}/base/telemetry/cache"
+makedir 770 "${SOPHOS_INSTALL}/base/telemetry/cache"
 chown -R "${USER_NAME}:${GROUP_NAME}" "${SOPHOS_INSTALL}/base/telemetry"
 if [ -L "${SOPHOS_INSTALL}/opt/sophos-spl/base/bin/versionedcopy" ]
 then
