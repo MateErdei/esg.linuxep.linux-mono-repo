64d92292d6f8b3d0e6ed3e7d6c3a21cfe9fe8fc5
Douglas Leeder <douglas.leeder@sophos.com>
2020-11-30 08:53:29 +0000

    [av] [LINUXDAR-2511] Move the system test script into git.
    Make the code simpler in Jenkins.

---
+++ av/TA/bin/runSystemTests.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -ex
+
+export MCS_REGION=${MCS_REGION:-DEV}
+
+SCRIPT_DIR=$PWD
+
+echo "Test scripts from git branch: $GIT_BRANCH"
+VUT_BRANCH=${VUT_BRANCH:-master}
+echo "Product version under test from $VUT_BRANCH"
+
+apt-get install -y libcap-dev
+apt-get install -y python3.7 python3.7-dev python3.7-venv
+
+# Create and enter TAP Python Virtual Environment
+python3.7 -m venv tapvenv
+. ./tapvenv/bin/activate
+
+export PYTHONUNBUFFERED=1
+
+# Get Build
+python3.7 ${SCRIPT_DIR}/TA/bin/downloadBuild.py ${SCRIPT_DIR}/download ${VUT_BRANCH}
+#ls -l ${SCRIPT_DIR}/download ${SCRIPT_DIR}/download/output
+
+#ls -l $SCRIPT_DIR
+#env
+
+python3.7 -m pip install --upgrade keyrings.alt 2>/dev/null
+python3.7 -m pip install pip --upgrade
+python3.7 -m pip install -i https://artifactory.sophos-ops.com/api/pypi/pypi/simple -r TA/requirements.txt
+
+# Setup test env
+[ -d /opt/test ] && rm -rf /opt/test
+mkdir -p /opt/test/inputs/av
+ln -s $SCRIPT_DIR/TA /opt/test/inputs/test_scripts
+OUTPUT=${SCRIPT_DIR}/download/output
+ln -s $OUTPUT/test-resources /opt/test/inputs/av/test-resources
+ln -s $OUTPUT/SDDS-COMPONENT /opt/test/inputs/av/SDDS-COMPONENT
+ln -s $OUTPUT/base-sdds /opt/test/inputs/av/base-sdds
+
+#ls -l /opt/test/inputs/av /opt/test/inputs/av/base-sdds/ /opt/test/inputs/av/SDDS-COMPONENT/ /opt/test/inputs/av/test-resources/
+
+cd /opt/test/inputs/test_scripts
+
+# Set log level
+sed -i "s/SUPPORT/${LOGLEVEL}/g" shared/AVResources.robot
+
+## run system tests
+[ -f /opt/sophos-spl/bin/uninstall.sh ] && /opt/sophos-spl/bin/uninstall.sh --force
+python3.7 -m robot --include system .
+cd $SCRIPT_DIR
+
+if [[ -d /opt/sophos-spl ]]
+then
+	/opt/sophos-spl/bin/uninstall.sh --force
+fi
