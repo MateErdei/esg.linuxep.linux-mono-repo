e0313ec443b7b7fa0d6d184e27212e4b5ed9d49b
Douglas Leeder <douglas.leeder@sophos.com>
2020-12-02 14:20:22 +0000

    [av] [LINUXDAR-2533] Alter scan times, to try to and avoid creating a policy that specifies a scan time in the past.

--- av/TA/Libs/CloudClient/NextScanTime.py
+++ av/TA/Libs/CloudClient/NextScanTime.py
@@ -7,9 +7,13 @@ import datetime
 
 def get_next_scan_time():
     now = datetime.datetime.now()
-    if now.minute < 30:
+    if now.minute < 28:
         now = now.replace(minute=30, second=0, microsecond=0)
-    else:
+    if now.minute < 58:
         now = now.replace(minute=0, second=0, microsecond=0)
         now += datetime.timedelta(hours=1)
+    else:
+        now = now.replace(minute=30, second=0, microsecond=0)
+        now += datetime.timedelta(hours=1)
+
     return now
