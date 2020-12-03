b3ad8247cc1a0f0706463dd0575851d31fc45998
Douglas Leeder <douglas.leeder@sophos.com>
2020-12-03 10:57:10 +0000

    [av] [LINUXDAR-2533] Missing el(if) - causes <28 times to be more than 32 minutes in the future.
    Fixed and tested.
    Assert added to check time is not more that 32 minutes in the future.

--- av/TA/Libs/CloudClient/NextScanTime.py
+++ av/TA/Libs/CloudClient/NextScanTime.py
@@ -3,17 +3,53 @@
 # Copyright (C) 2020 Sophos Plc, Oxford, England.
 # All rights reserved.
 
+from __future__ import print_function
+
 import datetime
+import sys
+
+def internal_get_next_scan_time(now):
+    if now.minute < 28:
+        nextTime = now.replace(minute=30, second=0, microsecond=0)
+    elif now.minute < 58:
+        nextTime = now.replace(minute=0, second=0, microsecond=0)
+        nextTime += datetime.timedelta(hours=1)
+    else:
+        nextTime = now.replace(minute=30, second=0, microsecond=0)
+        nextTime += datetime.timedelta(hours=1)
+
+    assert((nextTime-now) <= datetime.timedelta(minutes=32))
+
+    return nextTime
 
 def get_next_scan_time():
     now = datetime.datetime.now()
-    if now.minute < 28:
-        now = now.replace(minute=30, second=0, microsecond=0)
-    if now.minute < 58:
-        now = now.replace(minute=0, second=0, microsecond=0)
-        now += datetime.timedelta(hours=1)
-    else:
-        now = now.replace(minute=30, second=0, microsecond=0)
-        now += datetime.timedelta(hours=1)
+    return internal_get_next_scan_time(now)
 
-    return now
+
+def main():
+    now = datetime.datetime.now()
+    now = now.replace(minute=5, second=0, microsecond=0)
+    nextTime = internal_get_next_scan_time(now)
+    print(str(now), "->", str(nextTime))
+
+    now = now.replace(minute=28, second=0, microsecond=0)
+    nextTime = internal_get_next_scan_time(now)
+    print(str(now), "->", str(nextTime))
+
+    now = now.replace(minute=45, second=0, microsecond=0)
+    nextTime = internal_get_next_scan_time(now)
+    print(str(now), "->", str(nextTime))
+
+    now = now.replace(minute=58, second=0, microsecond=0)
+    nextTime = internal_get_next_scan_time(now)
+    print(str(now), "->", str(nextTime))
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
+
+
