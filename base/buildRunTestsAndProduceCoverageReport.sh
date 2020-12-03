08591a6ac57c77486df575fe5315a05dd150ef1f
Menaga Karunanandan <Menaga.Karunanandan@sophos.com>
2020-12-03 10:57:19 +0000

    [base] [BUGFIX] try to call combine after system tests are run

--- base/buildRunTestsAndProduceCoverageReport.sh
+++ base/buildRunTestsAndProduceCoverageReport.sh
@@ -16,7 +16,7 @@ export PATH=$PATH:/usr/local/bin/
 
 which sb_manifest_sign
 
-./fetchandbuild.sh --python-coverage  --no-unittests
+./fetchandbuild.sh --python-coverage
 SDDS_COMPONENT="${BASE}/output/SDDS-COMPONENT"
 echo "Keep the coverage for unit tests"
 pushd modules
@@ -28,26 +28,27 @@ sudo rm -rf /usr/local/lib64/python3.6/site-packages/Crypto/*
 sudo cp -r redist/pycryptodome/Crypto/*   /usr/local/lib64/python3.6/site-packages/Crypto/
 
 pushd testUtils
-#echo 'run system tests'
-#TESTS2RUN="-e AMAZON_LINUX -i CENTRAL -i FAKE_CLOUD -i MCS -i MCS_ROUTER -i MESSAGE_RELAY -i REGISTRATION -i THIN_INSTALLER -i UPDATE_CACHE -e EDR_PLUGIN -e OSTIA -e LIVERESPONSE_PLUGIN -e MDR_PLUGIN"
-#USER=$(whoami)
-#if [[ ${USER} == "jenkins" ]]; then
-##  COVERAGE_BASE_BUILD="${SDDS_COMPONENT}" bash SupportFiles/jenkins/jenkinsBuildCommand.sh  ${TESTS2RUN} || echo "Test failure does not prevent the coverage report. "
-#  COVERAGE_BASE_BUILD="${SDDS_COMPONENT}" bash SupportFiles/jenkins/jenkinsBuildCommand.sh  -s mcs_router-update_scheduler -s mcs_router || echo "Test failure does not prevent the coverage report. "
-#  sudo chown ${USER} .coverage
-#else
-#  ./robot ${TESTS2RUN}
-#fi
+echo 'run system tests'
+TESTS2RUN="-e AMAZON_LINUX -i CENTRAL -i FAKE_CLOUD -i MCS -i MCS_ROUTER -i MESSAGE_RELAY -i REGISTRATION -i THIN_INSTALLER -i UPDATE_CACHE -e EDR_PLUGIN -e OSTIA -e LIVERESPONSE_PLUGIN -e MDR_PLUGIN"
+USER=$(whoami)
+if [[ ${USER} == "jenkins" ]]; then
+#  COVERAGE_BASE_BUILD="${SDDS_COMPONENT}" bash SupportFiles/jenkins/jenkinsBuildCommand.sh  ${TESTS2RUN} || echo "Test failure does not prevent the coverage report. "
+  COVERAGE_BASE_BUILD="${SDDS_COMPONENT}" bash SupportFiles/jenkins/jenkinsBuildCommand.sh  -s TestProxy || echo "Test failure does not prevent the coverage report. "
+  sudo chown ${USER} .coverage
+else
+  ./robot ${TESTS2RUN}
+fi
 echo 'replace path to the original source'
 
 sed -i "s#/opt/sophos-spl/base/lib64#${BASE}/modules/mcsrouter#g" .coverage
 sed -i "s/py.0/py/g" .coverage
 sed -i "s_/opt/sophos-spl/base/lib_${SDDS_COMPONENT}/files/base/lib_g" .coverage
 
+python3 -m coverage combine || echo 'ignore error'
 # create the xml report that is used by jenkins
 python3 -m coverage xml -i  --omit="*python3.7*,*site-packages*,*build64*,*tests"
 # publish the report to filer 6
-
+find . -name "*coverage*"  || echo "could not perfrom find"
 if [[ ${USER} == "jenkins" ]]; then
   TARGET_PATH=/mnt/filer6/linux/SSPL/testautomation/pythoncoverage/
   sudo cp .coverage ${TARGET_PATH}/latest_python_coverage
