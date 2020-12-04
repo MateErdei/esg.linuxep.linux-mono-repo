d20a3e15b0ded34c33c8e5c7a423abbb5a4d9306
Menaga Karunanandan <Menaga.Karunanandan@sophos.com>
2020-12-04 09:12:25 +0000

    [base] [BUGFIX] run more tests

--- base/buildRunTestsAndProduceCoverageReport.sh
+++ base/buildRunTestsAndProduceCoverageReport.sh
@@ -16,7 +16,7 @@ export PATH=$PATH:/usr/local/bin/
 
 which sb_manifest_sign
 
-./fetchandbuild.sh --python-coverage  --no-unittests
+./fetchandbuild.sh --python-coverage
 SDDS_COMPONENT="${BASE}/output/SDDS-COMPONENT"
 echo "Keep the coverage for unit tests"
 pushd modules
@@ -29,11 +29,10 @@ sudo cp -r redist/pycryptodome/Crypto/*   /usr/local/lib64/python3.6/site-packag
 
 pushd testUtils
 echo 'run system tests'
-TESTS2RUN="-e AMAZON_LINUX -i CENTRAL -i FAKE_CLOUD -i MCS -i MCS_ROUTER -i MESSAGE_RELAY -i REGISTRATION -i THIN_INSTALLER -i UPDATE_CACHE -e EDR_PLUGIN -e OSTIA -e LIVERESPONSE_PLUGIN -e MDR_PLUGIN"
+TESTS2RUN="-e AMAZON_LINUX -i CENTRAL -i FAKE_CLOUD -i MCS -i MCS_ROUTER -i MESSAGE_RELAY -i REGISTRATION -i THIN_INSTALLER -i UPDATE_CACHE -e OSTIA"
 USER=$(whoami)
 if [[ ${USER} == "jenkins" ]]; then
 #  COVERAGE_BASE_BUILD="${SDDS_COMPONENT}" bash SupportFiles/jenkins/jenkinsBuildCommand.sh  ${TESTS2RUN} || echo "Test failure does not prevent the coverage report. "
-  COVERAGE_BASE_BUILD="${SDDS_COMPONENT}" bash SupportFiles/jenkins/jenkinsBuildCommand.sh  -s TestProxy || echo "Test failure does not prevent the coverage report. "
   sudo chown ${USER} .coverage
 else
   ./robot ${TESTS2RUN}
@@ -44,8 +43,10 @@ sed -i "s#/opt/sophos-spl/base/lib64#${BASE}/modules/mcsrouter#g" .coverage
 sed -i "s/py.0/py/g" .coverage
 sed -i "s_/opt/sophos-spl/base/lib_${SDDS_COMPONENT}/files/base/lib_g" .coverage
 
+
 find . -name '*coverage*' || echo "could not perform find"
 # create the xml report that is used by jenkins
+python3 -m coverage combine || echo 'ignore error'
 python3 -m coverage xml -i  --omit="*python3.7*,*site-packages*,*build64*,*tests"
 # publish the report to filer 6
 find . -name "*coverage*"  || echo "could not perform find"
