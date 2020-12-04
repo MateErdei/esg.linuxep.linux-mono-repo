1016bf3673e834edecb8214d015c9082ccefca33
Yael May <yael.may@sophos.com>
2020-12-04 14:04:54 +0000

    [av] [LINUXDAR-2431] Re-protecting a getter,

--- av/modules/avscanner/avscannerimpl/ScanCallbackImpl.h
+++ av/modules/avscanner/avscannerimpl/ScanCallbackImpl.h
@@ -31,11 +31,10 @@ namespace avscanner::avscannerimpl
         void scanStarted() override { m_startTime = time(nullptr); }
         void logSummary() override;
 
-        [[nodiscard]] int getNoOfScanErrors() { return m_noOfErrors; }
-
     protected:
         [[nodiscard]] time_t  getStartTime() { return m_startTime; }
         [[nodiscard]] int getNoOfInfectedFiles() { return m_noOfInfectedFiles; }
+        [[nodiscard]] int getNoOfScanErrors() { return m_noOfErrors; }
         [[nodiscard]] int getNoOfCleanFiles() { return m_noOfCleanFiles; }
         [[nodiscard]] int getNoOfScannedFiles() { return m_noOfCleanFiles + m_noOfInfectedFiles; }
         [[nodiscard]] std::map<std::string, int> getThreatTypes() { return m_threatCounter; }
