3595476c5a6b4d8edc87e56e7a8e66ab061630a7
Jake McLanaghan <jake.mclanaghan@sophos.com>
2020-12-04 11:03:04 +0000

    [thininstaller] [LINUXDAR-2355] pipeline and CIJenkinsfile, adjustments to release-package.xml

---
+++ thininstaller/pipeline/pipeline.py
@@ -0,0 +1,14 @@
+import tap.v1 as tap
+
+
+@tap.pipeline(version=1, component='sspl-thininstaller')
+def sspl_thininstaller(stage: tap.Root, parameters: tap.Parameters):
+    component = tap.Component(name='sspl-thininstaller', base_version='1.0.7')
+    mode = parameters.mode or 'release'
+
+    #export TAP_PARAMETER_MODE=release|analysis|coverage*(requires bullseye)
+    _thininstaller_build = None
+    with stage.parallel('build'):
+        _thininstaller_build = stage.artisan_build(name=mode, component=component, image='JenkinsLinuxTemplate5',
+                                        mode=mode, release_package='./release-package.xml')
+
