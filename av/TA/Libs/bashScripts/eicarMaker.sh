cfa1da06c9c185c6230f2e1314613c34dd321f64
efthymios.makaras <efthymios.makaras@sophos.com>
2020-12-07 10:37:52 +0000

    [av] [LINUXDAR-2541] change eicar creation location in eicarMaker.sh  [+EH]

--- av/TA/Libs/bashScripts/eicarMaker.sh
+++ av/TA/Libs/bashScripts/eicarMaker.sh
@@ -2,5 +2,5 @@
 mkdir -p /tmp/three_hundred_eicars
 for x in $(seq 1 1 300)
 do
-        echo "X5O!P%@AP[4\\PZX54(P^)7CC)7}\$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!\$H+H*" > /tmp/three_hundred_eicars/eicar$x
+        echo "X5O!P%@AP[4\\PZX54(P^)7CC)7}\$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!\$H+H*" > /tmp_test/three_hundred_eicars/eicar$x
 done
