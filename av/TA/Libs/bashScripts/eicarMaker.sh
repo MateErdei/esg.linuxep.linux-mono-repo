4b8c77e1f9badb47aea3d4bb5bfb3ab27df85956
efthymios.makaras <efthymios.makaras@sophos.com>
2020-12-07 11:07:16 +0000

    [av] [LINUXDAR-2541] update mkdir in eicarMaker.sh  [+EH]

--- av/TA/Libs/bashScripts/eicarMaker.sh
+++ av/TA/Libs/bashScripts/eicarMaker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mkdir -p /tmp/three_hundred_eicars
+mkdir -p /tmp_test/three_hundred_eicars
 for x in $(seq 1 1 300)
 do
         echo "X5O!P%@AP[4\\PZX54(P^)7CC)7}\$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!\$H+H*" > /tmp_test/three_hundred_eicars/eicar$x
