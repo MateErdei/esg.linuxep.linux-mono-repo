1d33504ae67f31b9e50f5363833218dd4fa096e2
Paul-Vlad Avadanei <paulvlad.avadanei@sophos.com>
2020-12-09 09:44:44 +0000

    [base] [LINUXDAR-1816] test workspace location

--- base/testUtils/SupportFiles/jenkins/jenkinsBuildCommand.sh
+++ base/testUtils/SupportFiles/jenkins/jenkinsBuildCommand.sh
@@ -72,66 +72,66 @@ if [[ $WORKSPACE =~ $EXPECTED_WORKSPACE ]]
 then
     sudo cp $WORKSPACE/testUtils/SupportFiles/jenkins/auditdConfig.txt /etc/audit/auditd.conf || fail "ERROR: failed to copy auditdConfig from $WORKSPACE/SupportFiles to /etc/audit/auditd.conf"
 fi
-
-export TEST_UTILS=$WORKSPACE/testUtils
-[[ -n $NO_GATHER ]] || source $WORKSPACE/testUtils/SupportFiles/jenkins/gatherTestInputs.sh                || fail "Error: failed to gather test inputs"
-source $WORKSPACE/testUtils/SupportFiles/jenkins/exportInputLocations.sh            || fail "Error: failed to export expected input locations"
-source $WORKSPACE/testUtils/SupportFiles/jenkins/checkTestInputsAreAvailable.sh     || fail "Error: failed to validate gathered inputs"
-
-#setup coverage inputs and exports
-COVERAGE_STAGING=/tmp/system-product-test-inputs/coverage
-if [[ -n $BASE_COVERAGE ]]; then
-  mv $COVERAGE_STAGING/sspl-base-unittest.cov $COVERAGE_STAGING/sspl-base-combined.cov
-  export COVFILE=$COVERAGE_STAGING/sspl-base-combined.cov
-  export htmldir=$COVERAGE_STAGING/sspl-base-combined
-  export COV_HTML_BASE=sspl-base-combined
-  export BULLSEYE_UPLOAD=1
-elif [[ -n $MDR_COVERAGE ]]; then
-  mv $COVERAGE_STAGING/sspl-mdr-unittest.cov $COVERAGE_STAGING/sspl-mdr-combined.cov
-  export COVFILE=$COVERAGE_STAGING/sspl-mdr-combined.cov
-  export htmldir=$COVERAGE_STAGING/sspl-mdr-combined
-  export COV_HTML_BASE=sspl-mdr-combined
-  export BULLSEYE_UPLOAD=1
-fi
-
-bash ${JENKINS_DIR}/install_dependencies.sh
-
-ROBOT_BASE_COMMAND="sudo -E python3 -m robot -x robot.xml --loglevel TRACE "
-RERUNFAILED=${RERUNFAILED:-false}
-HasFailure=false
-
-inputArguments="$@"
-
-platform_exclude_tag
-EXCLUDED_BY_DEFAULT_TAGS="MANUAL PUB_SUB FUZZ AUDIT_PLUGIN EVENT_PLUGIN TESTFAILURE MCS_FUZZ AMAZON_LINUX EXAMPLE_PLUGIN "$PLATFORM_EXCLUDE_TAG
-
-EXTRA_ARGUMENTS=""
-for EXCLUDED_TAG in ${EXCLUDED_BY_DEFAULT_TAGS}; do
-# if EXCLUDED_TAG is not mentioned in the input argument, add it to the default exclusion
-if ! [[ $inputArguments =~ "${EXCLUDED_TAG}" ]]; then
-  EXTRA_ARGUMENTS="${EXTRA_ARGUMENTS} -e ${EXCLUDED_TAG}"
-fi
-done
-
-ROBOT_BASE_COMMAND="${ROBOT_BASE_COMMAND} ${EXTRA_ARGUMENTS}"
-
-#Setup crash dumps for when a segfault occurs during system tests
-sudo sysctl kernel.core_pattern=/tmp/core-%e-%s-%u-%g-%p-%t
-ulimit -c unlimited
-
-echo "Running command: ${ROBOT_BASE_COMMAND} $@"
-eval $ROBOT_BASE_COMMAND $@ . || HasFailure=true
-
-if [[ ${RERUNFAILED} == true && ${HasFailure} == true ]]; then
-    echo "Running for re-run"
-    sudo -E mv output.xml output1.xml
-    sudo -E python3 -m robot --rerunfailed output1.xml --output output2.xml  . || echo "Failed tests on rerun"
-    sudo -E rebot --merge --output output.xml -l log.html -r report.html output1.xml output2.xml
-fi
+#
+#export TEST_UTILS=$WORKSPACE/testUtils
+#[[ -n $NO_GATHER ]] || source $WORKSPACE/testUtils/SupportFiles/jenkins/gatherTestInputs.sh                || fail "Error: failed to gather test inputs"
+#source $WORKSPACE/testUtils/SupportFiles/jenkins/exportInputLocations.sh            || fail "Error: failed to export expected input locations"
+#source $WORKSPACE/testUtils/SupportFiles/jenkins/checkTestInputsAreAvailable.sh     || fail "Error: failed to validate gathered inputs"
+#
+##setup coverage inputs and exports
+#COVERAGE_STAGING=/tmp/system-product-test-inputs/coverage
+#if [[ -n $BASE_COVERAGE ]]; then
+#  mv $COVERAGE_STAGING/sspl-base-unittest.cov $COVERAGE_STAGING/sspl-base-combined.cov
+#  export COVFILE=$COVERAGE_STAGING/sspl-base-combined.cov
+#  export htmldir=$COVERAGE_STAGING/sspl-base-combined
+#  export COV_HTML_BASE=sspl-base-combined
+#  export BULLSEYE_UPLOAD=1
+#elif [[ -n $MDR_COVERAGE ]]; then
+#  mv $COVERAGE_STAGING/sspl-mdr-unittest.cov $COVERAGE_STAGING/sspl-mdr-combined.cov
+#  export COVFILE=$COVERAGE_STAGING/sspl-mdr-combined.cov
+#  export htmldir=$COVERAGE_STAGING/sspl-mdr-combined
+#  export COV_HTML_BASE=sspl-mdr-combined
+#  export BULLSEYE_UPLOAD=1
+#fi
+#
+#bash ${JENKINS_DIR}/install_dependencies.sh
+#
+#ROBOT_BASE_COMMAND="sudo -E python3 -m robot -x robot.xml --loglevel TRACE "
+#RERUNFAILED=${RERUNFAILED:-false}
+#HasFailure=false
+#
+#inputArguments="$@"
+#
+#platform_exclude_tag
+#EXCLUDED_BY_DEFAULT_TAGS="MANUAL PUB_SUB FUZZ AUDIT_PLUGIN EVENT_PLUGIN TESTFAILURE MCS_FUZZ AMAZON_LINUX EXAMPLE_PLUGIN "$PLATFORM_EXCLUDE_TAG
+#
+#EXTRA_ARGUMENTS=""
+#for EXCLUDED_TAG in ${EXCLUDED_BY_DEFAULT_TAGS}; do
+## if EXCLUDED_TAG is not mentioned in the input argument, add it to the default exclusion
+#if ! [[ $inputArguments =~ "${EXCLUDED_TAG}" ]]; then
+#  EXTRA_ARGUMENTS="${EXTRA_ARGUMENTS} -e ${EXCLUDED_TAG}"
+#fi
+#done
+#
+#ROBOT_BASE_COMMAND="${ROBOT_BASE_COMMAND} ${EXTRA_ARGUMENTS}"
+#
+##Setup crash dumps for when a segfault occurs during system tests
+#sudo sysctl kernel.core_pattern=/tmp/core-%e-%s-%u-%g-%p-%t
+#ulimit -c unlimited
+#
+#echo "Running command: ${ROBOT_BASE_COMMAND} $@"
+#eval $ROBOT_BASE_COMMAND $@ . || HasFailure=true
+#
+#if [[ ${RERUNFAILED} == true && ${HasFailure} == true ]]; then
+#    echo "Running for re-run"
+#    sudo -E mv output.xml output1.xml
+#    sudo -E python3 -m robot --rerunfailed output1.xml --output output2.xml  . || echo "Failed tests on rerun"
+#    sudo -E rebot --merge --output output.xml -l log.html -r report.html output1.xml output2.xml
+#fi
 
 #upload coverage results
 if [[ -n $BASE_COVERAGE || -n $MDR_COVERAGE ]]; then
-  bash -x build/bullseye/uploadResults.sh || exit $?
+  bash -x $WORKSPACE/build/bullseye/uploadResults.sh || exit $?
 fi
 
 if [[ $WORKSPACE =~ $EXPECTED_WORKSPACE ]]
