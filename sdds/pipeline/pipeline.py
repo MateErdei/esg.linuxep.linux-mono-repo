1911276e89f724be27024b96df61918c0bcba79e
Douglas Leeder <douglas.leeder@sophos.com>
2020-12-09 13:55:35 +0000

    Add options for EDR-999 and MDR-999 builds.

--- sdds/pipeline/pipeline.py
+++ sdds/pipeline/pipeline.py
@@ -12,9 +12,14 @@ def build_dev_warehouse(stage: tap.Root, name="release-package"):
 
 @tap.pipeline(root_sequential=False)
 def warehouse(stage: tap.Root, context: tap.PipelineContext, parameters: tap.Parameters):
+    mdr999 = parameters.mdr_999 != 'false'
+    edr999 = parameters.edr_999 != 'false'
 
     with stage.parallel('build'):
         build_dev_warehouse(stage=stage, name="release-package")
-        build_dev_warehouse(stage=stage, name="release-package-edr-999")
-        # build_dev_warehouse(stage=stage, name="release-package-mdr-999")
-        # build_dev_warehouse(stage=stage, name="release-package-edr-mdr-999")
+        if edr999:
+            build_dev_warehouse(stage=stage, name="release-package-edr-999")
+        if mdr999:
+            build_dev_warehouse(stage=stage, name="release-package-mdr-999")
+        if edr999 and mdr999:
+            build_dev_warehouse(stage=stage, name="release-package-edr-mdr-999")
