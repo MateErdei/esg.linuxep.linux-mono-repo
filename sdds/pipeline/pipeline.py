52a9b162cdedd841034340cea2352f87b22c6f90
Douglas Leeder <douglas.leeder@sophos.com>
2020-12-09 09:59:09 +0000

    Try setting up for using mode for different build options.

--- sdds/pipeline/pipeline.py
+++ sdds/pipeline/pipeline.py
@@ -1,13 +1,17 @@
 import tap.v1 as tap
 
 
-def build_dev_warehouse(stage: tap.Root):
-    component = tap.Component(name='dev-warehouse', base_version='1.0.0')
-    stage.artisan_build(name='sdds2', component=component, image='Warehouse', release_package='./build/dev.xml', mode='sdds2')
+def build_dev_warehouse(stage: tap.Root, name="release-package"):
+    component = tap.Component(name='dev-warehouse-'+name, base_version='1.0.0')
+    stage.artisan_build(name=name,
+                        component=component,
+                        image='Warehouse',
+                        release_package='./build/dev.xml',
+                        mode=name)
 
 
 @tap.pipeline(root_sequential=False)
 def warehouse(stage: tap.Root, context: tap.PipelineContext, parameters: tap.Parameters):
 
     with stage.parallel('build'):
-        build_dev_warehouse(stage=stage)
+        build_dev_warehouse(stage=stage, name="release-package")
