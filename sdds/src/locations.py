4d48eea79eba4d8da918ecde161bb6e01d7f59f4
Douglas Leeder <douglas.leeder@sophos.com>
2020-12-15 13:43:13 +0000

    [sdds] [LINUXDAR-1796] Add SSPL URL to DCI files.
    Keep Windows DCI file location, so either work.

--- sdds/src/locations.py
+++ sdds/src/locations.py
@@ -5,5 +5,9 @@ cwd = os.getcwd()
 LOGS = os.path.join(cwd, "..", "logs")
 WAREHOUSE_WRITE = os.path.join(cwd, "..", "output", "warehouse", "content")
 CUSTOMER_FILES_WRITE = os.path.join(cwd, "..", "output", "warehouse")
-WAREHOUSE_READ = ["http://d1.sophosupd.net/update/"]
+branch = os.environ.get("PIPELINE_NAME")
+WAREHOUSE_READ = [
+    "http://ostia.eng.sophos/dev/sspl-warehouse/{}/warehouse/warehouse".format(branch),
+    "http://d1.sophosupd.net/update/"
+]
 REPOSITORY_ID = "ES+C"
