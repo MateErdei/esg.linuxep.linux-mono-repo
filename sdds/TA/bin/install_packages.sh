2e90ab0cf3d4a36f98cad0cd5dce6bcc294d2236
Douglas Leeder <douglas.leeder@sophos.com>
2021-01-18 08:56:21 +0000

    [sdds] [LINUXDAR-1796] Re-try installing packages.

--- sdds/TA/bin/install_packages.sh
+++ sdds/TA/bin/install_packages.sh
@@ -18,14 +18,34 @@ echo "B" >/opt/test/results/B
 
 ls -lR /opt/test >/opt/test/logs/LS
 
+
+function wait_for_apt()
+{
+  while fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+    sleep 1
+  done
+}
+
+
+function apt_install()
+{
+    for x in 1 2 3 4 5
+    do
+        wait_for_apt
+        apt-get install -y "$@" && return
+    done
+    echo "Failed to install $*"
+    exit 1
+}
+
 if [[ -x $(which apt) ]]
 then
+    COMMON="zip unzip"
     if [[ $(lsb_release -rs) == "18.04" ]]; then
-        apt-get install -y python3.7 python3.7-dev
+        apt_install python3.7 python3.7-dev $COMMON
     else
-        apt-get install -y python3.8 python3.8-dev
+        apt_install python3.8 python3.8-dev $COMMON
     fi
-    apt-get install -y zip unzip
 elif [[ -x $(which yum) ]]
 then
     ping -c2 abn-centosrepo || true
