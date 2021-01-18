df7e970603be1472ce565b5b3525834e3e438b1d
Menaga Karunanandan <Menaga.Karunanandan@sophos.com>
2021-01-18 13:18:54 +0000

    [base] [LINUXDAR-2616]remove fail as it is not defined here

--- base/testUtils/SupportFiles/jenkins/gatherBuildInputs.sh
+++ base/testUtils/SupportFiles/jenkins/gatherBuildInputs.sh
@@ -1,6 +1,6 @@
 python3 -m venv /tmp/venv-for-ci
 source /tmp/venv-for-ci/bin/activate
-  $WORKSPACE/testUtils/SupportFiles/jenkins/SetupCIBuildScripts.sh || fail "Error: Failed to get CI scripts"
+  $WORKSPACE/testUtils/SupportFiles/jenkins/SetupCIBuildScripts.sh
   export BUILD_JWT=$(cat $WORKSPACE/testUtils/SupportFiles/jenkins/jwt_token.txt)
-  python3 -m build_scripts.artisan_fetch $WORKSPACE/build/release-package.xml || fail "Error: Failed to fetch inputs"
+  python3 -m build_scripts.artisan_fetch $WORKSPACE/build/release-package.xml
 deactivate
