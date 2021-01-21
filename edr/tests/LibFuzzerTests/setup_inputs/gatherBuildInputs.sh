adb1c409716b9e1671232ced5a917ed090aa8cec
Menaga Karunanandan <Menaga.Karunanandan@sophos.com>
2021-01-21 15:17:19 +0000

    [edr] [LINUXDAR-2616]fix paths

--- edr/tests/LibFuzzerTests/setup_inputs/gatherBuildInputs.sh
+++ edr/tests/LibFuzzerTests/setup_inputs/gatherBuildInputs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 python3 -m venv /tmp/venv-for-ci
 source /tmp/venv-for-ci/bin/activate
-  $WORKSPACE/tests/LibFuzzerTests/SetupCIBuildScripts.sh
-  export BUILD_JWT=$(cat $WORKSPACE/tests/LibFuzzerTests/jwt_token.txt)
+  $WORKSPACE/tests/LibFuzzerTests/setup_inputs/SetupCIBuildScripts.sh
+  export BUILD_JWT=$(cat $WORKSPACE/tests/LibFuzzerTests/setup_inputs/jwt_token.txt)
   python3 -m build_scripts.artisan_fetch $WORKSPACE/build-files/release-package.xml
 deactivate
