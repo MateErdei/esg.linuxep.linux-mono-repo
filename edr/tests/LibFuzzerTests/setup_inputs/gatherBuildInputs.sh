71fc68248fe6e5e0dfea4625113003f492c87bfa
Menaga Karunanandan <Menaga.Karunanandan@sophos.com>
2021-01-22 11:44:25 +0000

    [edr] [LINUXDAR-2616]check if we still need fix for detached head

--- edr/tests/LibFuzzerTests/setup_inputs/gatherBuildInputs.sh
+++ edr/tests/LibFuzzerTests/setup_inputs/gatherBuildInputs.sh
@@ -5,6 +5,6 @@ source /tmp/venv-for-ci/bin/activate
   $WORKSPACE/tests/LibFuzzerTests/setup_inputs/SetupCIBuildScripts.sh
   export BUILD_JWT=$(cat $WORKSPACE/tests/LibFuzzerTests/setup_inputs/jwt_token.txt)
   #temp fix for detached head
-  git checkout bugfix/LINUXDAR-2616-fix-fuzzing-jobs
+
   python3 -m build_scripts.artisan_fetch $WORKSPACE/build-files/release-package.xml
 deactivate
