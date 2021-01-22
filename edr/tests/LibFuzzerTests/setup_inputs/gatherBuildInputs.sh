1a79d55845b897932863802bf57ee51dab12c62d
Menaga Karunanandan <Menaga.Karunanandan@sophos.com>
2021-01-22 11:54:33 +0000

    [edr] [LINUXDAR-2616]try another fix for detached head

--- edr/tests/LibFuzzerTests/setup_inputs/gatherBuildInputs.sh
+++ edr/tests/LibFuzzerTests/setup_inputs/gatherBuildInputs.sh
@@ -5,6 +5,7 @@ source /tmp/venv-for-ci/bin/activate
   $WORKSPACE/tests/LibFuzzerTests/setup_inputs/SetupCIBuildScripts.sh
   export BUILD_JWT=$(cat $WORKSPACE/tests/LibFuzzerTests/setup_inputs/jwt_token.txt)
   #temp fix for detached head
-
+  git branch temp
+  git checkout temp
   python3 -m build_scripts.artisan_fetch $WORKSPACE/build-files/release-package.xml
 deactivate
