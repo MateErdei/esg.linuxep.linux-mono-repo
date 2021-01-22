bef98e639250288f053771f9cd56be30e0282812
Menaga Karunanandan <Menaga.Karunanandan@sophos.com>
2021-01-22 12:09:08 +0000

    [edr] [LINUXDAR-2616]update comment

--- edr/tests/LibFuzzerTests/setup_inputs/gatherBuildInputs.sh
+++ edr/tests/LibFuzzerTests/setup_inputs/gatherBuildInputs.sh
@@ -4,7 +4,7 @@ python3 -m venv /tmp/venv-for-ci
 source /tmp/venv-for-ci/bin/activate
   $WORKSPACE/tests/LibFuzzerTests/setup_inputs/SetupCIBuildScripts.sh
   export BUILD_JWT=$(cat $WORKSPACE/tests/LibFuzzerTests/setup_inputs/jwt_token.txt)
-  #temp fix for detached head
+  #fix for detached head
   git branch temp
   git checkout temp
   python3 -m build_scripts.artisan_fetch $WORKSPACE/build-files/release-package.xml
