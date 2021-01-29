8370bba161a9fec2414347e6b704e5b9244c3e07
Douglas Leeder <douglas.leeder@sophos.com>
2021-01-29 08:53:08 +0000

    [av] [LINUXDAR-2569] Pull setupFileLoggingWithPath into LogSetup.cpp
    Preparing to make logging changes for sophos_threat_detector.

--- av/products/sophos_threat_detector/LogSetup.cpp
+++ av/products/sophos_threat_detector/LogSetup.cpp
@@ -9,10 +9,13 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 #include "Common/ApplicationConfiguration/IApplicationPathManager.h"
 #include "Common/Logging/FileLoggingSetup.h"
 #include "Common/Logging/LoggerConfig.h"
+#include <Common/Logging/LoggingSetup.h>
 #include <Common/Logging/SophosLoggerMacros.h>
 #include "../common/config.h"
 
 #include <log4cplus/logger.h>
+#include <log4cplus/consoleappender.h>
+#include <log4cplus/fileappender.h>
 
 #include <iterator>
 
@@ -28,6 +31,70 @@ static log4cplus::Logger& getLogSetupLogger()
 
 #define LOGERROR(x) LOG4CPLUS_ERROR(getLogSetupLogger(), x)  // NOLINT
 
+namespace
+{
+    std::string linkLocationCreator(const std::string& pluginInstall)
+    {
+        auto chroot = pluginInstall + "/chroot";
+        auto chrootLinkDir = chroot + pluginInstall;
+        return chrootLinkDir + "/log/sophos_threat_detector";
+    }
+
+    std::tuple<bool, int> ensureLoggingSymlinkPresent(const std::string& pluginInstall, const std::string& linkLocation)
+    {
+        int linkCreatedErrno = 0;
+        struct stat statbuf
+        {
+        };
+        int ret = lstat(linkLocation.c_str(), &statbuf);
+        if (ret == 0)
+        {
+            return {true, 0};
+        }
+        else
+        {
+            // Create the symlink
+            size_t depth = std::count(pluginInstall.begin(), pluginInstall.end(), '/');
+            std::ostringstream os;
+            std::fill_n(std::ostream_iterator<std::string>(os), depth, "../");
+            os << "../log";
+            auto linkDestination = os.str();
+            ret = ::symlink(linkDestination.c_str(), linkLocation.c_str());
+            if (ret != 0)
+            {
+                linkCreatedErrno = errno;
+            }
+        }
+        return {false, linkCreatedErrno};
+    }
+
+    void setupFileLoggingWithPath(const std::string& logfilepath)
+    {
+        log4cplus::initialize();
+
+        log4cplus::tstring datePattern;
+        const long maxFileSize = 10 * 1024 * 1024;
+        const int maxBackupIndex = 10;
+        const bool immediateFlush = true;
+        const bool createDirs = true;
+        log4cplus::SharedAppenderPtr appender(
+            new log4cplus::RollingFileAppender(logfilepath, maxFileSize, maxBackupIndex, immediateFlush, createDirs));
+        Common::Logging::LoggingSetup::applyDefaultPattern(appender);
+        log4cplus::Logger::getRoot().addAppender(appender);
+
+        // Log error messages to stderr
+        log4cplus::SharedAppenderPtr stderr_appender(new log4cplus::ConsoleAppender(true));
+        stderr_appender->setThreshold(log4cplus::ERROR_LOG_LEVEL);
+        Common::Logging::LoggingSetup::applyPattern(stderr_appender, Common::Logging::LoggingSetup::GL_CONSOLE_PATTERN);
+        log4cplus::Logger::getRoot().addAppender(stderr_appender);
+    }
+
+    void applyGeneralConfig(const std::string& logbase)
+    {
+        Common::Logging::applyGeneralConfig(logbase);
+    }
+}
+
 LogSetup::LogSetup()
 {
     auto sophosInstall = Common::ApplicationConfiguration::applicationPathManager().sophosInstall();
@@ -40,38 +107,17 @@ LogSetup::LogSetup()
     // ln -snf "${BACKLINK}/log" "${CHROOT_LINK_DIR}/log/sophos_threat_detector"
 
     auto pluginInstall = sophosInstall + "/plugins/av";
-    auto chroot = pluginInstall + "/chroot";
-    auto chrootLinkDir = chroot + pluginInstall;
-    auto linkLocation = chrootLinkDir + "/log/sophos_threat_detector";
+    auto linkLocation = linkLocationCreator(pluginInstall); // also needed for reporting
 
     // check if link is present
-    bool linkAlreadyPresent = false;
-    int linkCreatedErrno = 0;
-    struct stat statbuf{};
-    int ret = lstat(linkLocation.c_str(), &statbuf);
-    if (ret == 0)
-    {
-        linkAlreadyPresent = true;
-    }
-    else
-    {
-        // Create the symlink
-        size_t depth = std::count(pluginInstall.begin(), pluginInstall.end(), '/');
-        std::ostringstream os;
-        std::fill_n(std::ostream_iterator<std::string>(os), depth, "../");
-        os << "../log";
-        auto linkDestination = os.str();
-        ret = ::symlink(linkDestination.c_str(), linkLocation.c_str());
-        if (ret != 0)
-        {
-            linkCreatedErrno = errno;
-        }
-    }
+    auto linkCreatedResult = ensureLoggingSymlinkPresent(pluginInstall, linkLocation);
+    auto linkAlreadyPresent = std::get<0>(linkCreatedResult);
+    auto linkCreatedErrno = std::get<1>(linkCreatedResult);
 
-    auto logfilepath =  pluginInstall + "/log/sophos_threat_detector/sophos_threat_detector.log";
+    auto logfilepath = pluginInstall + "/log/sophos_threat_detector/sophos_threat_detector.log";
 
-    Common::Logging::FileLoggingSetup::setupFileLoggingWithPath(logfilepath);
-    Common::Logging::applyGeneralConfig(PLUGIN_NAME);
+    setupFileLoggingWithPath(logfilepath);
+    applyGeneralConfig(PLUGIN_NAME); // Sets the threshold for the root logger
 
     // Log after the logging has been setup
     if (!linkAlreadyPresent)
