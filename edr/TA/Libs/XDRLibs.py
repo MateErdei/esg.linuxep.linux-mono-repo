1332f5d4d4c429a091086c9853a248c19cbf7148
Jake McLanaghan <jake.mclanaghan@sophos.com>
2021-02-04 12:07:59 +0000

    [edr] [LINUXDAR-2619] increase timeout because it's flakey

--- edr/TA/Libs/XDRLibs.py
+++ edr/TA/Libs/XDRLibs.py
@@ -84,7 +84,7 @@ def get_current_epoch_time():
 
 def wait_for_scheduled_query_file_and_return_filename():
     n = 0
-    while n < 30:
+    while n < 40:
         files = os.listdir("/opt/sophos-spl/base/mcs/datafeed")
         if len(files) == 0:
             time.sleep(1)
