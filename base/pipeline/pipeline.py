e88e68b03ba6c2ce473eadee972622dedf39f59b
Ethan Vince-Urwin <ethan.vince-urwin@sophos.com>
2021-02-09 09:26:40 +0000

    [base] Update base version to 1.1.5

--- base/pipeline/pipeline.py
+++ base/pipeline/pipeline.py
@@ -150,7 +150,7 @@ def get_inputs(context: tap.PipelineContext, base_build: ArtisanInput, mode: str
 
 @tap.pipeline(version=1, component='sspl-base', root_sequential=False)
 def sspl_base(stage: tap.Root, context: tap.PipelineContext, parameters: tap.Parameters):
-    component = tap.Component(name='sspl-base', base_version='1.1.4')
+    component = tap.Component(name='sspl-base', base_version='1.1.5')
     
     RELEASE_MODE = 'release'
     ANALYSIS_MODE = 'analysis'
