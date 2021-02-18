e9e84aae6c096daddf150dc988a644e887653e23
Paul-Vlad Avadanei <paulvlad.avadanei@sophos.com>
2021-02-18 09:40:18 +0000

    [edr] [LINUXDAR-2641] Minor code clean-up changes

--- edr/modules/pluginimpl/OsqueryLogStringUtil.cpp
+++ edr/modules/pluginimpl/OsqueryLogStringUtil.cpp
@@ -6,8 +6,6 @@ Copyright 2021 Sophos Limited.  All rights reserved.
 
 #include "OsqueryLogStringUtil.h"
 
-#include "TelemetryConsts.h"
-
 #include <Common/TelemetryHelperImpl/TelemetryHelper.h>
 #include <Common/UtilityImpl/StringUtils.h>
 
@@ -17,13 +15,13 @@ std::tuple<bool, std::string> OsqueryLogStringUtil::processOsqueryLogLineForSche
     std::string lineToFind= "Executing scheduled query ";
     if (Common::UtilityImpl::StringUtils::isSubstring(logLine, lineToFind))
     {
-        std::vector<std::string> words = Common::UtilityImpl::StringUtils::splitString(logLine, lineToFind);
-        if(words.size() < 2)
+        std::vector<std::string> items = Common::UtilityImpl::StringUtils::splitString(logLine, lineToFind);
+        if(items.size() < 2)
         {
             return std::make_tuple(false, "");
         }
 
-        std::vector<std::string> timeString = Common::UtilityImpl::StringUtils::splitString(words[0], " ");
+        std::vector<std::string> timeString = Common::UtilityImpl::StringUtils::splitString(items[0], " ");
         if(timeString.size() < 2)
         {
             return std::make_tuple(false, "");
@@ -31,7 +29,7 @@ std::tuple<bool, std::string> OsqueryLogStringUtil::processOsqueryLogLineForSche
 
         std::string time = timeString[1];
 
-        std::vector<std::string> nameString = Common::UtilityImpl::StringUtils::splitString(words[1], ":");
+        std::vector<std::string> nameString = Common::UtilityImpl::StringUtils::splitString(items[1], ":");
         if(timeString.empty())
         {
             return std::make_tuple(false, "");
