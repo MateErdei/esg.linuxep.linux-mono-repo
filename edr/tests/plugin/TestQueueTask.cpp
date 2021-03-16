831211311c9cabf085c25637bb3298cd69810935
Alex Cooper <Alex.Cooper@Sophos.com>
2021-03-16 17:29:49 +0000

    [edr] Add unit tests for pushing restarts

--- edr/tests/plugin/TestQueueTask.cpp
+++ edr/tests/plugin/TestQueueTask.cpp
@@ -43,3 +43,43 @@ TEST(TestQueueTask, popReturnsTrueWhenTaskIsPushedDuringWait)
     ASSERT_TRUE(return_value);
     ASSERT_EQ(task.m_taskType,Plugin::Task::TaskType::STOP);
 }
+
+TEST(TestQueueTask, pushIfNotAlreadyInQueue)
+{
+    Plugin::QueueTask queueTask;
+    Plugin::Task task1;
+    Plugin::Task task2;
+    Plugin::Task task3;
+    task1.m_taskType = Plugin::Task::TaskType::STOP;
+    task1.m_content = "1";
+    task2.m_taskType = Plugin::Task::TaskType::QUEUE_OSQUERY_RESTART;
+    task2.m_content = "2";
+    task3.m_taskType = Plugin::Task::TaskType::STOP;
+    task3.m_content = "3";
+    queueTask.pushIfNotAlreadyInQueue(task1);
+    queueTask.pushIfNotAlreadyInQueue(task2);
+    queueTask.pushIfNotAlreadyInQueue(task3);
+
+    Plugin::Task task;
+    ASSERT_TRUE(queueTask.pop(task, 1));
+    ASSERT_EQ(task.m_content, "1");
+    ASSERT_EQ(task.m_taskType, Plugin::Task::TaskType::STOP);
+
+    ASSERT_TRUE(queueTask.pop(task, 1));
+    ASSERT_EQ(task.m_content, "2");
+    ASSERT_EQ(task.m_taskType, Plugin::Task::TaskType::QUEUE_OSQUERY_RESTART);
+
+    ASSERT_FALSE(queueTask.pop(task, 1));
+}
+
+TEST(TestQueueTask, pushOsqueryRestart)
+{
+    Plugin::QueueTask queueTask;
+    queueTask.pushOsqueryRestart("First reason to restart");
+    queueTask.pushOsqueryRestart("Second reason to restart");
+    Plugin::Task task;
+    ASSERT_TRUE(queueTask.pop(task, 1));
+    ASSERT_EQ(task.m_content, "First reason to restart");
+    ASSERT_EQ(task.m_taskType, Plugin::Task::TaskType::QUEUE_OSQUERY_RESTART);
+    ASSERT_FALSE(queueTask.pop(task, 1));
+}
