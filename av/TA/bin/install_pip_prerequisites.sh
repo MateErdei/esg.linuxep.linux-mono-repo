296ee1b49dc855cc4955a25adee9dc0baa7cc67d
DarrenSmith <darren.smith@sophos.com>
2021-03-17 14:54:43 +0000

    [av] Retry package installs re-using code from sspl-warehouse repo

--- av/TA/bin/install_pip_prerequisites.sh
+++ av/TA/bin/install_pip_prerequisites.sh
@@ -2,14 +2,32 @@
 
 set -ex
 
+function wait_for_apt()
+{
+  while fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+    sleep 1
+  done
+}
+
+function apt_install()
+{
+    for x in 1 2 3 4 5
+    do
+        wait_for_apt
+        apt-get install -y "$@" && return
+    done
+    echo "Failed to install $*"
+    exit 1
+}
+
 if [[ -x $(which apt-get) ]]
 then
+    COMMON="nfs-kernel-server zip unzip python3-pkgconfig capnproto libcapnp-dev"
     if [[ $(lsb_release -rs) == "18.04" ]]; then
-        apt-get install -y python3.7 python3.7-dev
+        apt_install python3.7 python3.7-dev $COMMON
     else
-        apt-get install -y python3.8 python3.8-dev
+        apt_install python3.8 python3.8-dev $COMMON
     fi
-    apt-get install -y nfs-kernel-server zip unzip
 elif [[ -x $(which yum) ]]
 then
     ping -c2 abn-centosrepo || true
@@ -20,7 +38,7 @@ then
     cat /etc/yum.conf
     grep -r abn-centosrepo /etc/yum.repos.d/* /etc/yum.conf || true
 
-    yum install -y "gcc" "gcc-c++" "make" "capnproto-devel" "capnproto-libs" "capnproto" nfs-utils zip
+    yum install -y gcc gcc-c++ make capnproto-devel capnproto-libs capnproto nfs-utils zip python3-pkgconfig
 else
     echo "Can't find package management system"
     exit 1
