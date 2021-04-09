c9aea62e2c0be459486262c36db8d95795e674d1
Paul-Vlad Avadanei <paulvlad.avadanei@sophos.com>
2021-04-09 11:14:41 +0100

    [base] [LINUXDAR-2606] fix path of covfile

--- base/testUtils/SupportFiles/jenkins/jenkinsBuildCommand.sh
+++ base/testUtils/SupportFiles/jenkins/jenkinsBuildCommand.sh
@@ -98,9 +98,7 @@ elif [[ -n "${MDR_COVERAGE:-}" ]]; then
   export COV_HTML_BASE=sspl-mtr-combined
   export BULLSEYE_UPLOAD=1
 elif [[ -n "${EDR_COVERAGE:-}" ]]; then
-  #mv $COVERAGE_STAGING/sspl-edr-unittest.cov $COVERAGE_STAGING/sspl-edr-combined.cov
-  ls $COVERAGE_STAGING
-  ls $COVERAGE_STAGING/covfile
+  mv $COVERAGE_STAGING/sspl-plugin-edr-unit.cov $COVERAGE_STAGING/sspl-edr-combined.cov
   export COVFILE=$COVERAGE_STAGING/sspl-edr-combined.cov
   export htmldir=$COVERAGE_STAGING/sspl-edr-combined
   export COV_HTML_BASE=sspl-edr-combined
