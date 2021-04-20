33925bc3f784e143f6bc66d7ef744a3083ded2ff
Paul-Vlad Avadanei <paulvlad.avadanei@sophos.com>
2021-04-20 15:35:39 +0100

    [edr] [LINUXDAR-2606] Make download script download specified file.

--- edr/build/bullseye/downloadFromAllegro.sh
+++ edr/build/bullseye/downloadFromAllegro.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 echo "Download specified file/folder from Allegro in /tmp/allegro"
 
+#sspl-plugin-edr-taptest/sspl-plugin-edr-tap.cov
+[[ -n ${FILESTODOWNLOAD} ]] || exitFailure 3 "Files to download not specified"
+
 PRIVATE_KEY=/opt/test/inputs/bullseye_files/private.key
 [[ -f ${PRIVATE_KEY} ]] || PRIVATE_KEY=${BASE}/build/bullseye/private.key
 [[ -f ${PRIVATE_KEY} ]] || exitFailure 3 "Unable to find private key for download"
@@ -11,5 +14,5 @@ chmod 600 ${PRIVATE_KEY}
 mkdir /tmp/allegro
 
 rsync -va --rsh="ssh -i ${PRIVATE_KEY} -o StrictHostKeyChecking=no" \
-    upload@allegro.eng.sophos:public_html/bullseye/sspl-plugin-edr-taptest/sspl-plugin-edr-tap.cov  \
+    upload@allegro.eng.sophos:public_html/bullseye/"${FILESTODOWNLOAD}" \
     /tmp/allegro
