beb65c9d183d943c9a351e82c0032df9ea80965f
trevormcging <trevor.mcging@sophos.com>
2021-04-20 10:42:46 +0100

    [edr] [LINUXDAR-2937] Add running state to template plugin so that the signal handler does not exit before the main process has completed shutdown which meant that a second sigterm would uncleanly kill the plugin.

--- edr/modules/pluginimpl/PluginCallback.cpp
+++ edr/modules/pluginimpl/PluginCallback.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************************************
 
-Copyright 2018-2019 Sophos Limited.  All rights reserved.
+Copyright 2018 Sophos Limited.  All rights reserved.
 
 ******************************************************************************************************/
 
@@ -9,15 +9,16 @@ Copyright 2018-2019 Sophos Limited.  All rights reserved.
 #include "Logger.h"
 #include "Telemetry.h"
 #include "TelemetryConsts.h"
-#include "PluginUtils.h"
 
 #include <Common/TelemetryHelperImpl/TelemetryHelper.h>
+
+#include <unistd.h>
 
 namespace Plugin
 {
     PluginCallback::PluginCallback(std::shared_ptr<QueueTask> task) : m_task(std::move(task))
     {
-        std::string noPolicySetStatus {
+        std::string noPolicySetStatus{
             R"sophos(<?xml version="1.0" encoding="utf-8" ?>
                     <status xmlns="http://www.sophos.com/EE/EESavStatus">
                         <CompRes xmlns="com.sophos\msys\csc" Res="NoRef" RevID="" policyType="2" />
@@ -28,32 +29,26 @@ namespace Plugin
         LOGDEBUG("Plugin Callback Started");
     }
 
-    void PluginCallback::applyNewPolicy(const std::string& /* policyXml */)
+    void PluginCallback::applyNewPolicy(const std::string& policyXml)
     {
-        LOGERROR("This method should never be called.");
+        LOGSUPPORT("Applying new policy");
+        m_task->push(Task{ Task::TaskType::Policy, policyXml });
     }
 
-    void PluginCallback::applyNewPolicyWithAppId(const std::string& appId, const std::string& policyXml)
-    {
-        LOGSUPPORT("Applying new policy with APPID: " << appId);
-        m_task->push(Task { Task::TaskType::POLICY, policyXml, "", appId });
-    }
-
-    void PluginCallback::queueAction(const std::string& /* actionXml */)
-    {
-        LOGERROR("This method should never be called.");
-    }
-
-    void PluginCallback::queueActionWithCorrelation(const std::string& queryJson, const std::string& correlationId)
-    {
-        LOGSUPPORT("Receive new query");
-        m_task->push(Task { Task::TaskType::QUERY, queryJson, correlationId });
-    }
+    void PluginCallback::queueAction(const std::string& /* actionXml */) { LOGSUPPORT("Queueing action"); }
 
     void PluginCallback::onShutdown()
     {
         LOGSUPPORT("Shutdown signal received");
         m_task->pushStop();
+        int timeoutCounter = 0;
+        int shutdownTimeout = 30;
+        while(isRunning() && timeoutCounter < shutdownTimeout)
+        {
+            LOGSUPPORT("Shutdown waiting for EDR processes to complete");
+            sleep(1);
+            timeoutCounter++;
+        }
     }
 
     Common::PluginApi::StatusInfo PluginCallback::getStatus(const std::string& /* appId */)
@@ -73,46 +68,25 @@ namespace Plugin
         LOGSUPPORT("Received get telemetry request");
         auto& telemetry = Common::Telemetry::TelemetryHelper::getInstance();
 
-        std::optional<std::string> version = plugin::getVersion();
+        std::optional<std::string> version = Plugin::getVersion();
         if (version)
         {
-            telemetry.set(plugin::version, version.value());
+            telemetry.set(Plugin::Telemetry::version, version.value());
         }
-        bool isXDR;
-        try
-        {
-            isXDR = Plugin::PluginUtils::retrieveGivenFlagFromSettingsFile(PluginUtils::MODE_IDENTIFIER);
-        }
-        catch (const std::runtime_error& ex)
-        {
-            // not set in plugin.conf default to false
-            isXDR = false;
-        }
-        telemetry.set(plugin::telemetryIsXdrEnabled, isXDR);
-
-        std::optional<unsigned long> osqueryDatabaseSize = plugin::getOsqueryDatabaseSize();
-        if (osqueryDatabaseSize)
-        {
-            telemetry.set(plugin::telemetryOSQueryDatabaseSize, osqueryDatabaseSize.value());
-        }
-        //TODO LINUXDAR-2484 remove this function call
-        plugin::readOsqueryInfoFiles();
-        telemetry.updateTelemetryWithStats();
-        telemetry.updateTelemetryWithAllStdDeviationStats();
-
         std::string telemetryJson = telemetry.serialiseAndReset();
         LOGDEBUG("Got telemetry JSON data: " << telemetryJson);
 
-        initialiseTelemetry();
         return telemetryJson;
     }
 
-    void PluginCallback::initialiseTelemetry()
+    void PluginCallback::setRunning(bool running)
     {
-        auto& telemetry = Common::Telemetry::TelemetryHelper::getInstance();
-        telemetry.increment(plugin::telemetryOsqueryRestarts, 0L);
-        telemetry.increment(plugin::telemetryOSQueryRestartsCPU, 0L);
-        telemetry.increment(plugin::telemetryOSQueryRestartsMemory, 0L);
-        telemetry.increment(plugin::telemetryOSQueryDatabasePurges, 0L);
+        m_running = running;
     }
+
+    bool PluginCallback::isRunning()
+    {
+        return m_running;
+    }
+
 } // namespace Plugin
