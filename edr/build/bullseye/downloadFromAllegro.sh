8ba62c02b18c02d1f2b9d4761d759ed6e74bf043
Paul-Vlad Avadanei <paulvlad.avadanei@sophos.com>
2021-04-21 12:40:14 +0100

    [edr] [LINUXDAR-2606] Fix pathing.

--- edr/build/bullseye/downloadFromAllegro.sh
+++ edr/build/bullseye/downloadFromAllegro.sh
@@ -2,7 +2,10 @@
 echo "Download specified file/folder from Allegro in /tmp/allegro"
 
 #sspl-plugin-edr-taptest/sspl-plugin-edr-tap.cov
-[[ -n ${FILESTODOWNLOAD} ]] || exitFailure 3 "Files to download not specified"
+[[ -n ${FILESTODOWNLOAD} ]] || exitFailure 4 "Files to download not specified"
+
+SCRIPT_DIR=$(cd "${0%/*}"; echo "$PWD")
+[[ -n ${BASE} ]] || BASE=${SCRIPT_DIR}/../..
 
 PRIVATE_KEY=/opt/test/inputs/bullseye_files/private.key
 [[ -f ${PRIVATE_KEY} ]] || PRIVATE_KEY=${BASE}/build/bullseye/private.key
