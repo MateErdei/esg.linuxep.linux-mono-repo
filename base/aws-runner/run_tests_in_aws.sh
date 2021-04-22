a0c268c8661faeaf6dc790eabe564dc526d1a780
Jake McLanaghan <jake.mclanaghan@sophos.com>
2021-04-22 14:08:19 +0100

    [base] [LINUXDAR-2878] remove accidental exit

--- base/aws-runner/run_tests_in_aws.sh
+++ base/aws-runner/run_tests_in_aws.sh
@@ -54,7 +54,6 @@ if [[ -z "$SKIP_GATHER" ]]
 then
     bash ./gather.sh $@ || failure "Failed to gather test files: $?"
 fi
-exit
 [[ -f "$TEST_TAR" ]] || failure "Failed to gather test files: $TEST_TAR doesn't exist"
 
 ## Create template
