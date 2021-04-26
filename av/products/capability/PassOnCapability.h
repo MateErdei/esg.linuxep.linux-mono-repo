a1d886eea6849b395fb69f130156e2b0bd31a25d
Douglas Leeder <douglas.leeder@sophos.com>
2021-04-26 15:53:02 +0100

    [av] [LINUXDAR-2496] Add unit test for pass_on_capability.

--- av/products/capability/PassOnCapability.h
+++ av/products/capability/PassOnCapability.h
@@ -7,10 +7,19 @@ Copyright 2020, Sophos Limited.  All rights reserved.
 #ifndef SSPL_PLUGIN_MAV_PASSONCAPABILITY_H
 #define SSPL_PLUGIN_MAV_PASSONCAPABILITY_H
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #include <sys/capability.h>
 
 int pass_on_capability(cap_value_t cap);
 
 void set_no_new_privs();
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif //SSPL_PLUGIN_MAV_PASSONCAPABILITY_H
---
+++ av/tests/capability/TestPassOnCapability.cpp
@@ -0,0 +1,59 @@
+/******************************************************************************************************
+
+Copyright 2021, Sophos Limited.  All rights reserved.
+
+******************************************************************************************************/
+
+#include "capability/PassOnCapability.h"
+
+#include "common/ErrorCodes.h"
+#include "datatypes/Print.h"
+
+#include <gtest/gtest.h>
+
+#include <unistd.h>
+
+TEST(TestPassOnCapability, pass_on_capability_root_only) // NOLINT
+{
+    if (::getuid() != 0)
+    {
+        return;
+    }
+
+    int ret = pass_on_capability(CAP_SYS_CHROOT);
+    EXPECT_EQ(ret, 0);
+}
+
+
+static void drop_root()
+{
+    PRINT("Drop root");
+    int ret = setregid(1, 1);
+    ASSERT_EQ(ret, 0);
+    ret = setreuid(1, 1);
+    ASSERT_EQ(ret, 0);
+}
+
+
+TEST(TestPassOnCapability, pass_on_capability_no_root) // NOLINT
+{
+    if (::getuid() == 0)
+    {
+        drop_root();
+    }
+
+    ASSERT_NE(::getuid(), 0);
+
+    int ret = pass_on_capability(CAP_SYS_CHROOT);
+    EXPECT_NE(ret, 0);
+
+    // non-root process returns 62 - cap_set_proc fails
+    // ex-root process returns 64 - cap_set_ambient fails - cap_set_proc works
+
+//    EXPECT_EQ(ret, E_CAP_SET_PROC);
+}
+
+TEST(TestPassOnCapability, no_new_privs) // NOLINT
+{
+    set_no_new_privs();
+}
