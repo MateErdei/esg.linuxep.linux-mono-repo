d5061cae268e6c9f8a202a8b357ba4a19ebf9cf3
Alex Cooper <alex.cooper@sophos.com>
2021-05-20 17:30:08 +0100

    [edr] [LINUXDAR-2414] bump version number to 1.1.2

--- edr/pipeline/pipeline.py
+++ edr/pipeline/pipeline.py
@@ -169,7 +169,7 @@ def edr_plugin(stage: tap.Root, context: tap.PipelineContext, parameters: tap.Pa
     coverage_mode = 'coverage'
     nine_nine_nine_mode = '999'
     mode = parameters.mode or release_mode
-    component = tap.Component(name='edr', base_version='1.1.1')
+    component = tap.Component(name='edr', base_version='1.1.2')
 
     #export TAP_PARAMETER_MODE=release|analysis|coverage*(requires bullseye)
     edr_build = None
