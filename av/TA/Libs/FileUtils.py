f8f1e1ed1132a0d575f2e823a262ad862b62f7de
edwardhathaway <Edward.Hathaway@sophos.com>
2021-06-10 14:59:40 +0100

    [av] [LINUXDAR-2950] Remove correct file and re-add FileUtils.py

---
+++ av/TA/Libs/FileUtils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+
+def copy_file_if_destination_missing(source, destination):
+    if os.path.isfile(destination):
+        return False
+    shutil.copy2(source, destination)
+    return True
+
+def ensure_list_appears_once(path, line_to_append):
+    assert os.path.isfile(path)
+    lines = open(path).readlines()
+    output = []
+    found = False
+    for line in lines:
+        if line == line_to_append:
+            if found:
+                continue
+            found = True
+        output.append(line)
+    if not found:
+        output.append(line_to_append)
+
+    if output != lines:
+        open(path, "w").writelines(output)
+        return True
+    return False
