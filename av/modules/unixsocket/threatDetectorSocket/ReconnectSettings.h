ddbdf221216b9c24cc9c61352d4924e5d2ae2a0c
Douglas Leeder <douglas.leeder@sophos.com>
2021-06-17 10:29:56 +0100

    [av] [LINUXDAR-3121] Change timeout from 20 seconds to 60 seconds.
    With a total of 600 seconds (10 minutes) allowed before avscanner aborts.
    
    Maintains current 10 files complete failure before aborting.

--- av/modules/unixsocket/threatDetectorSocket/ReconnectSettings.h
+++ av/modules/unixsocket/threatDetectorSocket/ReconnectSettings.h
@@ -7,12 +7,15 @@ Copyright 2021, Sophos Limited.  All rights reserved.
 #ifndef SSPL_PLUGIN_MAV_RECONNECTSETTINGS_H
 #define SSPL_PLUGIN_MAV_RECONNECTSETTINGS_H
 
-#define TOTAL_MAX_RECONNECTS 200
-#define MAX_CONN_RETRIES 20
+#define TOTAL_MAX_RECONNECTS 600
+/* Number of attempts for initial connection to socket, before scanning any file */
+#define MAX_CONN_RETRIES 60
+
 #ifdef USING_LIBFUZZER
 #define MAX_SCAN_RETRIES 1
 #else
-#define MAX_SCAN_RETRIES 20
+/* Number of attempt to scan a particular file, reconnecting if required */
+#define MAX_SCAN_RETRIES 60
 #endif
 
 #endif // SSPL_PLUGIN_MAV_RECONNECTSETTINGS_H
