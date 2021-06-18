c2fabc82fd8fd518b76de82fb4b04bfc69bdeb45
Ethan Vince-Urwin <ethan.vince-urwin@sophos.com>
2021-06-18 11:41:21 +0100

    [edr] [LINUXDAR-2963] Remove the need to have the version in two locations. Now the pipeline.py parses the release-package.xml for the version

--- edr/pipeline/pipeline.py
+++ edr/pipeline/pipeline.py
@@ -1,8 +1,18 @@
 import tap.v1 as tap
+import xml.etree.ElementTree as ET
+
+def get_package_version(package_path):
+    """ Read version from package.xml """
+    package_tree = ET.parse(package_path)
+    package_node = package_tree.getroot()
+    return package_node.attrib['version']
+
+PACKAGE_PATH = './build-files/release-package.xml'
+PACKAGE_VERSION = get_package_version(PACKAGE_PATH)
 
 @tap.pipeline(version=1, component='sspl-plugin-template')
 def template_plugin(stage: tap.Root, parameters: tap.Parameters):
-    component = tap.Component(name='sspl-plugin-template', base_version='1.0.0')
+    component = tap.Component(name='sspl-plugin-template', base_version=PACKAGE_VERSION)
 
     release_mode = 'release'
     nine_nine_nine_mode = '999'
@@ -14,8 +24,8 @@ def template_plugin(stage: tap.Root, parameters: tap.Parameters):
     with stage.parallel('build'):
         if mode == release_mode:
             template_build = stage.artisan_build(name=release_mode, component=component, image='JenkinsLinuxTemplate5',
-                                            mode=release_mode, release_package='./build-files/release-package.xml')
+                                            mode=release_mode, release_package=PACKAGE_PATH)
             nine_nine_nine_build = stage.artisan_build(name=nine_nine_nine_mode, component=component, image='JenkinsLinuxTemplate5',
-                                                       mode=nine_nine_nine_mode, release_package='./build-files/release-package.xml')
+                                                       mode=nine_nine_nine_mode, release_package=PACKAGE_PATH)
             zero_six_zero = stage.artisan_build(name=zero_six_zero_mode, component=component, image='JenkinsLinuxTemplate5',
-                                                mode=zero_six_zero_mode, release_package='./build-files/release-package.xml')
+                                                mode=zero_six_zero_mode, release_package=PACKAGE_PATH)
