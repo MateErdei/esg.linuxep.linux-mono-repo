afad6378c40e4dee598b6dd0d0f9ca8abd0ace49
Menaga Karunanandan <Menaga.Karunanandan@sophos.com>
2021-06-23 16:52:12 +0100

    [eventjournaler] [LINUXDAR-3132] fix build

--- eventjournaler/build.sh
+++ eventjournaler/build.sh
@@ -305,7 +305,7 @@ function build()
 
     [[ -f build64/sdds/SDDS-Import.xml ]] || exitFailure $FAILURE_COPY_SDDS_FAILED "Failed to create SDDS-Import.xml"
     cp -a build64/sdds output/SDDS-COMPONENT || exitFailure $FAILURE_COPY_SDDS_FAILED "Failed to copy SDDS component to output"
-
+    cp -a ${INPUT}/base-sdds  output/base-sdds  || exitFailure $FAILURE_COPY_SDDS_FAILED  "Failed to copy base SDDS component to output"
     if [[ -d build64/symbols ]]
     then
         cp -a build64/symbols output/
