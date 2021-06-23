95bf0dc12bf760d43678fde0d523dd41cca702a2
Menaga Karunanandan <Menaga.Karunanandan@sophos.com>
2021-06-23 16:37:03 +0100

    [eventjournaler] [LINUXDAR-3132] fix build

--- eventjournaler/pipeline/pipeline.py
+++ eventjournaler/pipeline/pipeline.py
@@ -69,13 +69,13 @@ def event_journaler(stage: tap.Root, context: tap.PipelineContext, parameters: t
                                                        mode=nine_nine_nine_mode, release_package=PACKAGE_PATH)
             zero_six_zero = stage.artisan_build(name=zero_six_zero_mode, component=component, image='JenkinsLinuxTemplate6',
                                                 mode=zero_six_zero_mode, release_package=PACKAGE_PATH)
-    ej_build = None
+
     with stage.parallel('test'):
         machines = (
             ("ubuntu1804",
-             tap.Machine('ubuntu1804_x64_server_en_us', inputs=get_inputs(context, ej_build, mode), platform=tap.Platform.Linux)),
-            ("centos77", tap.Machine('centos77_x64_server_en_us', inputs=get_inputs(context, ej_build, mode), platform=tap.Platform.Linux)),
-            ("centos82", tap.Machine('centos82_x64_server_en_us', inputs=get_inputs(context, ej_build, mode), platform=tap.Platform.Linux)),
+             tap.Machine('ubuntu1804_x64_server_en_us', inputs=get_inputs(context, release_build, mode), platform=tap.Platform.Linux)),
+            ("centos77", tap.Machine('centos77_x64_server_en_us', inputs=get_inputs(context, release_build, mode), platform=tap.Platform.Linux)),
+            ("centos82", tap.Machine('centos82_x64_server_en_us', inputs=get_inputs(context, release_build, mode), platform=tap.Platform.Linux)),
             # add other distros here
         )
 
