b821958b26a42ef0056127f51d7108b0a33a07ae
Menaga Karunanandan <Menaga.Karunanandan@sophos.com>
2021-06-24 15:37:15 +0100

    [eventjournaler] [LINUXDAR-3132] build eventPubSub tool as part of products

--- eventjournaler/build.sh
+++ eventjournaler/build.sh
@@ -305,7 +305,8 @@ function build()
 
     [[ -f build64/sdds/SDDS-Import.xml ]] || exitFailure $FAILURE_COPY_SDDS_FAILED "Failed to create SDDS-Import.xml"
     cp -a build64/sdds output/SDDS-COMPONENT || exitFailure $FAILURE_COPY_SDDS_FAILED "Failed to copy SDDS component to output"
-    cp -a build64/tests/manualTools/EventPubSub output/manualTools/ || exitFailure $FAILURE_COPY_SDDS_FAILED "Failed to copy EventPubSub Tool to output"
+    mkdir -p  output/manualTools/
+    cp -a build64/products/manualTools/EventPubSub output/manualTools/ || exitFailure $FAILURE_COPY_SDDS_FAILED "Failed to copy EventPubSub Tool to output"
     cp -a ${INPUT}/base-sdds  output/base-sdds  || exitFailure $FAILURE_COPY_SDDS_FAILED  "Failed to copy base SDDS component to output"
     if [[ -d build64/symbols ]]
     then
