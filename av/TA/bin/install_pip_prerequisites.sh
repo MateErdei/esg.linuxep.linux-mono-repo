35de91ac5267cca779cc982c4cb318e585826174
edwardhathaway <Edward.Hathaway@sophos.com>
2021-06-28 18:21:18 +0100

    [av] Add Richard's suggested fixes

--- av/TA/bin/install_pip_prerequisites.sh
+++ av/TA/bin/install_pip_prerequisites.sh
@@ -30,10 +30,30 @@ then
     fi
 elif [[ -x $(which yum) ]]
 then
+# Change the ping check to check abn-centosrepo to abn-centosrepo.eng.sophos -> This will sanity check that you can actually reach the suggested update location
+
+# Change all the sed commands to change abn-centosrepo to abn.centosrepo.eng.sophos for all files in /etc/yum.repos.d/, i.e. iterate through all files
+
+# Try and make the above sed command only change abn-centosrepo to include the .eng.sophos if it requires it. Looks like
+# the centos7 template has the correct path and the centos8 template doesn't so you want it to fix both. Perhaps gate
+# the sed command being run on a grep of the files in the repo not containing abn-centosrepo.eng.sophos
+
+    if [[ -f /etc/os-release ]]; then
+      . /etc/os-release
+      OS=$NAME
+    fi
+
+    if [[ "$OS" -eq "CentOS" ]]; then
+      for FILE in /etc/yum.repos.d/*; do
+        if [ -f "$FILE" ]; then
+          if grep -q "abn-centosrepo/" "$FILE"; then
+            sed -i -e's/abn-centosrepo/abn-engrepo.eng.sophos/g' "FILE"
+          fi
+        fi
+      done
+    fi
     ping -c2 abn-centosrepo || true
     ping -c2 abn-engrepo.eng.sophos || true
-    sed -i -e's/abn-centosrepo/abn-engrepo.eng.sophos/g' /etc/yum.repos.d/CentOS-Base.repo
-    sed -i -e's/abn-centosrepo/abn-engrepo.eng.sophos/g' /etc/yum.repos.d/epel.repo
     cat /etc/yum.repos.d/CentOS-Base.repo
     cat /etc/yum.conf
     grep -r abn-centosrepo /etc/yum.repos.d/* /etc/yum.conf || true
