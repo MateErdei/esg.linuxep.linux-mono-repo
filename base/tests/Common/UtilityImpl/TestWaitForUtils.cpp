9d71f79933ff3b9c5f9060e16d8d4eeea4b640c9
Menaga Karunanandan <Menaga.Karunanandan@sophos.com>
2021-06-30 15:25:19 +0100

    [base] [LINUXDAR-3132]add unitTest for waitForUtils

---
+++ base/tests/Common/UtilityImpl/TestWaitForUtils.cpp
@@ -0,0 +1,48 @@
+/******************************************************************************************************
+
+Copyright 2021, Sophos Limited.  All rights reserved.
+
+******************************************************************************************************/
+#include <Common/UtilityImpl/WaitForUtils.h>
+#include <gtest/gtest.h>
+
+using namespace Common::UtilityImpl;
+
+
+TEST(WaitForUtils, waitLoopDoesNotThrowIfItReachesTheEndOftheLoop) // NOLINT
+{
+    auto simpleFunc = []() {return false;};
+    double waitTime = 0.1;
+//    get time in ms
+    EXPECT_NO_THROW(Common::UtilityImpl::waitFor(waitTime,0.01,simpleFunc));
+//    get time in ms
+//    diff > waitTime
+}
+
+TEST(WaitForUtils, checkWaitLoopReturnsWhenConditionFulfiled) // NOLINT
+{
+    int counter = 0;
+    auto simpleFunc = [&]()
+    {
+      counter++;
+      return true;
+    };
+    double waitTime = 0.1;
+    EXPECT_NO_THROW(Common::UtilityImpl::waitFor(waitTime,0.1,simpleFunc));
+//    get time in ms
+//    diff < 1 second
+    ASSERT_EQ(counter, 1);
+}
+
+TEST(WaitForUtils, waitLoopRespectsTheIntervalAndWaitTimeGiven) // NOLINT
+{
+    int counter = 0;
+    auto simpleFunc = [&]()
+    {
+        counter++;
+        return false;
+    };
+    double waitTime = 0.1;
+    EXPECT_NO_THROW(Common::UtilityImpl::waitFor(waitTime,0.01,simpleFunc));
+    ASSERT_GE(counter, 10);
+}
