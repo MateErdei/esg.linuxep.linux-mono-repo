0e96c53eccc12edf638481e5297fa06e0ff649fa
Garry McNulty <garry.mcnulty@sophos.com>
2021-07-15 11:42:34 +0100

    [base] [LINUXDAR-3147] Add event journals path to mock interface

--- base/tests/Common/ApplicationConfiguration/MockedApplicationPathManager.h
+++ base/tests/Common/ApplicationConfiguration/MockedApplicationPathManager.h
@@ -20,6 +20,7 @@ public:
     MOCK_CONST_METHOD0(getWatchdogSocketAddress, std::string(void));
     MOCK_CONST_METHOD0(getEventSubscriberSocketAddress, std::string(void));
     MOCK_CONST_METHOD0(getEventSubscriberSocketFile, std::string(void));
+    MOCK_CONST_METHOD0(getEventJournalsPath, std::string(void));
     MOCK_CONST_METHOD0(sophosInstall, std::string(void));
     MOCK_CONST_METHOD0(getRelativeLogConfFilePath, std::string(void));
     MOCK_CONST_METHOD0(getRelativeLocalLogConfFilePath, std::string(void));
