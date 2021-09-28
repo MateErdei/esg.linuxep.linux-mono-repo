73362ce1b8ed2fe0c63a14db5e4c085f80973595
Mollie Bourke <mollie.bourke@sophos.com>
2021-09-28 12:02:24 +0100

    [thininstaller] [LINUXDAR-3518] Move to JenkinsLinuxTemplate6

--- thininstaller/pipeline/pipeline.py
+++ thininstaller/pipeline/pipeline.py
@@ -9,6 +9,6 @@ def sspl_thininstaller(stage: tap.Root, parameters: tap.Parameters):
     #export TAP_PARAMETER_MODE=release|analysis|coverage*(requires bullseye)
     _thininstaller_build = None
     with stage.parallel('build'):
-        _thininstaller_build = stage.artisan_build(name=mode, component=component, image='JenkinsLinuxTemplate5',
+        _thininstaller_build = stage.artisan_build(name=mode, component=component, image='JenkinsLinuxTemplate6',
                                         mode=mode, release_package='./release-package.xml')
 
