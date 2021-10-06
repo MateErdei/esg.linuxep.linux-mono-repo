02a4a279626513882eb2585bb0f1b183b10ead05
Mollie Bourke <mollie.bourke@sophos.com>
2021-10-06 16:44:41 +0100

    [base] [TESTFIX] Create function for installing packages to allow for retries on test machines

--- base/testUtils/SupportFiles/jenkins/install_dependencies.sh
+++ base/testUtils/SupportFiles/jenkins/install_dependencies.sh
@@ -13,6 +13,39 @@ function pip_install()
   fi
 }
 
+function install_system_packages
+{
+  local pkg_manager
+  if [ -n "$(which apt-get)" ]
+    then
+      pkg_manager="apt-get"
+  elif [ -n "$(which yum)" ]
+    then
+      pkg_manager="yum"
+  else
+    echo "System is not rhel-based or ubuntu, cannot install packages"
+    exit 1
+  fi
+
+  local installed_ok=0
+  for (( i=1; i<=20; i++ ))
+  do
+    if sudo $pkg_manager -y install $@
+    then
+      installed_ok=1
+      break
+    else
+       sleep 3
+     fi
+  done
+
+  if [ $installed_ok = 0 ]
+  then
+    echo "Package install failed"
+    exit 1
+  fi
+}
+
 python3 -m pip install pip --upgrade
 
 # install dependencies
@@ -21,13 +54,4 @@ do
   pip_install ${python_package}
 done
 
-if [ -n "$(which apt-get)" ]
-then
-  sudo apt-get -y install zip unzip || ( echo "package install failed" && exit 1 )
-elif [ -n "$(which yum)" ]
-then
-  sudo yum -y install zip unzip || ( echo "package install failed" && exit 1 )
-else
-  echo "System is not rhel-based or ubuntu, cannot install packages"
-  exit 1
-fi
+install_system_packages zip unzip
