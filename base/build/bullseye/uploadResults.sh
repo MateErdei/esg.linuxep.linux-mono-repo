fc64aaeba39fd82f3768b2932a55a8b01352f2ab
Casimir Overell <casimir.overell@sophos.com>
2021-10-11 09:29:02 +0100

    [base] [LINUXDAR-2945] correcting jenkins build coverage script path

--- base/build/bullseye/uploadResults.sh
+++ base/build/bullseye/uploadResults.sh
@@ -72,7 +72,6 @@ then
       </dev/null \
       || echo "Failed to upload bullseye html to Allegro"
 
-  find /tmp/system-product-test-inputs/bazel-tools
   sudo PATH=$PATH python3 -u $COVERAGE_SCRIPT                   \
       "$COVFILE"                                                \
       --output /opt/test/results/coverage/test_coverage.json    \
