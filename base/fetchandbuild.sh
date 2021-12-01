6a19b355d885a0cb85fac79ea87fd861a588f95f
Jake McLanaghan <jake.mclanaghan@sophos.com>
2021-12-01 17:41:47 +0000

    [base] revert change to use tap fetch

--- base/fetchandbuild.sh
+++ base/fetchandbuild.sh
@@ -3,6 +3,7 @@ set -e
 #Fetch inputs using python build scripts and then build full product passing all arguments
 
 rm -rf ./input
-#python3 -m build_scripts.artisan_fetch build/release-package.xml
-tap fetch sspl_base
+python3 -m build_scripts.artisan_fetch build/release-package.xml
+# tap fetch is the more "modern" way of doing this. But requires at least python3.7, which not all our test templates have.
+#tap fetch sspl_base
 ./build.sh $@
