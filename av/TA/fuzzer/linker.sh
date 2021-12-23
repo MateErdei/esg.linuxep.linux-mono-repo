1bfd18bbf056911a371f771658b9a04044d7929b
efthymios.makaras <efthymios.makaras@sophos.com>
2021-12-23 21:18:29 +0000

    [av] [LINUXDAR-2401] script that creates the necessary symlinks for the executables

---
+++ av/TA/fuzzer/linker.sh
@@ -0,0 +1,12 @@
+if [[ $USE_LIBFUZZER ]]
+then
+  cwd=$(pwd)
+  rm $cwd/output/base-sdds/files/base/lib64/liblog4cplus-2.0.so.3
+  rm $cwd/output/SDDS-COMPONENT/files/plugins/av/lib64/libzmq.so.5
+  rm $cwd/output/SDDS-COMPONENT/files/plugins/av/lib64/libprotobuf.so.28
+
+  ln -s $cwd/output/base-sdds/files/base/lib64/liblog4cplus-2.0.so.3.4.5 $cwd/output/base-sdds/files/base/lib64/liblog4cplus-2.0.so.3
+  ln -s $cwd/output/SDDS-COMPONENT/files/plugins/av/lib64/liblog4cplus-2.0.so.3.4.5 $cwd/output/SDDS-COMPONENT/files/plugins/av/lib64/liblog4cplus-2.0.so.3
+  ln -s $cwd/output/SDDS-COMPONENT/files/plugins/av/lib64/libzmq.so.5.2.4 $cwd/output/SDDS-COMPONENT/files/plugins/av/lib64/libzmq.so.5
+  ln -s $cwd/output/SDDS-COMPONENT/files/plugins/av/lib64/libprotobuf.so.28.0.3 $cwd/output/SDDS-COMPONENT/files/plugins/av/lib64/libprotobuf.so.28
+fi
