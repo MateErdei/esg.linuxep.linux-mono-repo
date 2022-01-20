c452f064411b730d3a928c63c9c3c48523fa8927
Douglas Leeder <douglas.leeder@sophos.com>
2022-01-20 14:57:19 +0000

    [av] [LINUXDAR-4038] Lower case tags.
    See:
    https://github.com/robotframework/robotframework/issues/2778
    http://robotframework.org/robotframework/latest/RobotFrameworkUserGuide.html#tag-patterns

--- av/pipeline/aws-runner/test.sh
+++ av/pipeline/aws-runner/test.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-INCLUDE_TAG="${1:-INTEGRATION OR PRODUCT}"
+INCLUDE_TAG="${1:-integration OR product}"
 
 function failure()
 {
@@ -22,9 +22,9 @@ PLATFORM_EXCLUDE_TAG=""
 #    release_pattern="CentOS.*8.*"
 #    if [[ ${current_release} =~ ${release_pattern} ]]
 #    then
-#        PLATFORM_EXCLUDE_TAG="-e EXCLUDE_CENTOS8"
+#        PLATFORM_EXCLUDE_TAG="-e exclude_centos8"
 #    else
-#        PLATFORM_EXCLUDE_TAG="-e EXCLUDE_CENTOS"
+#        PLATFORM_EXCLUDE_TAG="-e exclude_centos"
 #    fi
 #elif [[ -f /etc/redhat-release ]]
 #then
@@ -33,9 +33,9 @@ PLATFORM_EXCLUDE_TAG=""
 #
 #    if [[ ${current_release} =~ ${release_pattern} ]]
 #    then
-#        PLATFORM_EXCLUDE_TAG="-e EXCLUDE_RHEL8"
+#        PLATFORM_EXCLUDE_TAG="-e exclude_rhel8"
 #    else
-#        PLATFORM_EXCLUDE_TAG="-e EXCLUDE_RHEL"
+#        PLATFORM_EXCLUDE_TAG="-e exclude_rhel"
 #    fi
 #elif [[ -n `lsb_release -a` ]]
 #then
@@ -43,9 +43,9 @@ PLATFORM_EXCLUDE_TAG=""
 #    release_pattern="PRETTY_NAME=\"Ubuntu 20.*"
 #    if [[ ${current_release} =~ ${release_pattern} ]]
 #    then
-#        PLATFORM_EXCLUDE_TAG="-e EXCLUDE_UBUNTU20"
+#        PLATFORM_EXCLUDE_TAG="-e exclude_ubuntu20"
 #    else
-#        PLATFORM_EXCLUDE_TAG="-e EXCLUDE_UBUNTU"
+#        PLATFORM_EXCLUDE_TAG="-e exclude_ubuntu"
 #    fi
 #elif [[ -f /etc/os-release ]]
 #then
@@ -53,7 +53,7 @@ PLATFORM_EXCLUDE_TAG=""
 #    release_pattern="PRETTY_NAME=\"Amazon Linux 2\""
 #    if [[ ${current_release} =~ ${release_pattern} ]]
 #    then
-#        PLATFORM_EXCLUDE_TAG="-e EXCLUDE_AMAZON_LINUX"
+#        PLATFORM_EXCLUDE_TAG="-e exclude_amazon_linux"
 #    fi
 #fi
 
@@ -61,11 +61,11 @@ SAMBA_EXCLUDE_TAG=
 if [[ ! -f /etc/samba/smb.conf ]]
 then
     echo "Excluding SMB as /etc/samba/smb.conf doesn't exist"
-    SAMBA_EXCLUDE_TAG="-e SMB"
+    SAMBA_EXCLUDE_TAG="-e smb"
 elif [[ ! -x $(which mount.cifs) ]]
 then
     echo "Excluding SMB as mount.cifs doesn't exist"
-    SAMBA_EXCLUDE_TAG="-e SMB"
+    SAMBA_EXCLUDE_TAG="-e smb"
 fi
 
 export TEST_UTILS=$SCRIPT_DIR
@@ -78,7 +78,7 @@ python3 -m pip install -r $SCRIPT_DIR/requirements.txt
 echo "Running tests on $HOSTNAME"
 RESULT=0
 python3 -m robot --include "${INCLUDE_TAG}" \
-    --exclude "OSTIA OR MANUAL OR DISABLED OR STRESS" \
+    --exclude "manual OR disabled OR stress" \
     $PLATFORM_EXCLUDE_TAG \
     $SAMBA_EXCLUDE_TAG \
     --removekeywords WUKS  .
