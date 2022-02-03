2fd0957cd15fe6a52cc4716515db854f01f9550d
Douglas Leeder <douglas.leeder@sophos.com>
2022-02-03 13:52:57 +0000

    [av] [LINUDAR-3864][Blackduck] Needs a path.

--- av/build/blackduck.sh
+++ av/build/blackduck.sh
@@ -8,8 +8,8 @@ set -ex
 
 if [[ -z $NO_BUILD ]]
 then
-    bash build.sh --setup
-    bash build.sh --dev --no-unpack
+    bash ./build.sh --setup
+    bash ./build.sh --dev --no-unpack
 fi
 
 # MTUyNTdjMmItYWU0NS00MTM2LWIzN2QtNmFiZmI5NDVkMWJmOjExMDI0NDE5LWM1ZmMtNDRmNC05NDBhLTY2YzRiMDg2MmZmZA==
