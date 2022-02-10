247cd8375be2b3f3dc8f93d68f947e43471e98bb
DarrenSmith <darren.smith@sophos.com>
2022-02-10 14:48:18 +0000

    [av] [LINUXDAR-4145] - Change expected age value to accommodate different kernel clock speeds.

--- av/tests/plugin/TestPluginCallback.cpp
+++ av/tests/plugin/TestPluginCallback.cpp
@@ -1033,7 +1033,7 @@ TEST_F(TestPluginCallback, getProcessInfoReturnsCorrectValuesWhenSuccessful) //N
     EXPECT_CALL(*sysCallsMock, getSystemUpTime()).WillOnce(Return(sysCallUptime));
 
     long expectedMemoryValue = 500;
-    long expectedAgeValue = 996;
+    long expectedAgeValue = sysCallUptime.second - (450/sysconf(_SC_CLK_TCK));
     std::pair<long, long> expectedResult(expectedMemoryValue,expectedAgeValue);
     std::pair<long, long> result = m_pluginCallback->getThreatScannerProcessinfo(sysCallsMock);
 
