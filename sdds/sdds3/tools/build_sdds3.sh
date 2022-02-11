79ad02e5c09c392531a415d0393f6bbd8e6c4409
Jake McLanaghan <jake.mclanaghan@sophos.cim>
2022-02-11 17:33:21 +0000

    [sdds] [LINUXDAR-4179] Build SDDS3 style warehouse

---
+++ sdds/sdds3/tools/build_sdds3.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+REDIST=./redist
+
+SDDS3_BUILDER=${REDIST}/sdds3/sdds3-builder
+chmod +x ${SDDS3_BUILDER}
+
+OUTPUT=./output
+rm -rf ${OUTPUT}
+REPO=${OUTPUT}/repo
+
+PACKAGE_OUTPUT=${REPO}/package
+SUITE_OUTPUT=${REPO}/suite
+
+LAUNCH_DARKLY=${OUTPUT}/launchdarkly
+
+PACKAGE_INPUT=${REDIST}/package
+SUPPLEMENT_INPUT=${REDIST}/package
+SUITE_INPUT=./sdds3/suite
+
+#move packages to repo
+find ${PACKAGE_INPUT}/ -name SDDS-Import.xml | xargs -i ${SDDS3_BUILDER} --build-package --package-dir ${PACKAGE_OUTPUT} \
+                                                                  --sdds-import {} --nonce DNR
+
+#create suites
+find ${SUITE_INPUT}/ -type f | xargs -i ${SDDS3_BUILDER} --build-suite --xml {} --dir ${SUITE_OUTPUT} --nonce DNR
+
+
+
+#create launchdarkly files
+cp -r sdds3/launchdarkly ${LAUNCH_DARKLY}
