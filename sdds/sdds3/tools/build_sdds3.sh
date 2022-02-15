c49494b836c75d1261594e39a93b660550b2ed34
Jake McLanaghan <jake.mclanaghan@sophos.com>
2022-02-15 14:34:49 +0000

    [LINUXDAR-4179] fix published artifact source paths, fix debugging

--- sdds/sdds3/tools/build_sdds3.sh
+++ sdds/sdds3/tools/build_sdds3.sh
@@ -6,21 +6,13 @@ set -x
 env
 
 REDIST=./redist
-export LIBRARY_PATH="${LIBRARY_PATH}:/usr/lib/x86_64-linux-gnu"
 tar xvzf redist/gcc/gcc-8.1.0-linux.tar.gz -C redist/gcc/
 export LD_LIBRARY_PATH="${REDIST}/gcc/gcc/lib64/:/usr/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH}"
 #objdump -x binary-or-library |grep RPATH
 #readelf -d binary-or-library |head -20
 
-## debug
-ls -l /usr/lib/x86_64-linux-gnu
-find /lib64/
-find / -name "libstdc++.so*"
-
 SDDS3_BUILDER=${REDIST}/sdds3/sdds3-builder
 chmod +x ${SDDS3_BUILDER}
-
-ldd "$SDDS3_BUILDER"
 
 OUTPUT=./output
 rm -rf ${OUTPUT}
