78833b8716f1479759a25fd16c9e94c7c3224a59
efthymios.makaras <efthymios.makaras@sophos.com>
2022-02-28 17:46:22 +0000

    [av] [LINUXDAR-4331] Log to sophos_threat_detector log the reason for the exception before throwing

--- av/modules/sophos_threat_detector/sophosthreatdetectorimpl/Reloader.cpp
+++ av/modules/sophos_threat_detector/sophosthreatdetectorimpl/Reloader.cpp
@@ -5,15 +5,18 @@ Copyright 2021, Sophos Limited.  All rights reserved.
 ******************************************************************************************************/
 
 #include "Reloader.h"
+#include "Logger.h"
 
 void sspl::sophosthreatdetectorimpl::Reloader::update()
 {
     if (!m_scannerFactory)
     {
+        LOGERROR("Failed to update threat scanner: No scanner factory available. Threat detector will restart");
         throw std::runtime_error("Failed to update threat scanner: No scanner factory available");
     }
     else if (!m_scannerFactory->update())
     {
+        LOGERROR("Failed to update threat scanner. Threat detector will restart");
         throw std::runtime_error("Failed to update threat scanner");
     }
 }
@@ -22,10 +25,12 @@ void sspl::sophosthreatdetectorimpl::Reloader::reload()
 {
     if (!m_scannerFactory)
     {
+        LOGERROR("Failed to reload threat scanner: No scanner factory available. Threat detector will restart");
         throw std::runtime_error("Failed to reload threat scanner: No scanner factory available");
     }
     else if (!m_scannerFactory->reload())
     {
+        LOGERROR("Failed to update threat scanner. Threat detector will restart");
         throw std::runtime_error("Failed to reload threat scanner");
     }
 }
