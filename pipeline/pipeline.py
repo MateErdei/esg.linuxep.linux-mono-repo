94cd8fca26cab990abd356c2249b0903680ed4d4
Ethan Vince-Urwin <ethan.vince-urwin@sophos.com>
2022-03-01 17:21:00 +0000

    [LINUXDAR-4061] Update build template to version 7 and rename BUILD_IMAGE to BUILD_TEMPLATE for stash project consistency

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,11 +1,11 @@
 import tap.v1 as tap
 
 PACKAGE_PATH = "./release-package.xml"
-BUILD_IMAGE = 'JenkinsLinuxTemplate6'
+BUILD_TEMPLATE = 'JenkinsLinuxTemplate7'
 @tap.pipeline(version=1, component='linux-mono-repo')
 def linux_mono_repo(stage: tap.Root, context: tap.PipelineContext, parameters: tap.Parameters):
     component = tap.Component(name='linux-mono-repo', base_version="1.0.0")
     release_mode = 'release'
     with stage.parallel('build'):
-            stage.artisan_build(name=release_mode, component=component, image=BUILD_IMAGE,
+            stage.artisan_build(name=release_mode, component=component, image=BUILD_TEMPLATE,
                                                  mode=release_mode, release_package=PACKAGE_PATH)
