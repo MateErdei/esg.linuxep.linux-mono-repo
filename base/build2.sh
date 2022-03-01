9f75ea742fb3f1c1f03f554affb292259f559d5f
Alex Cooper <alex.cooper@sophos.com>
2022-03-01 12:51:04 +0000

    [base] [LINUXDAR-4092] Begin work on lighter build.sh, will rename build2.sh to build.sh when it's at the point that it supports the non-standard build modes we need, e.g. coverage or static analysis.

---
+++ base/build2.sh
@@ -0,0 +1,249 @@
+#!/bin/bash
+PRODUCT=sspl-base
+export PRODUCT_NAME="Sophos Server Protection Linux - Base Component"
+export PRODUCT_LINE_ID="ServerProtectionLinux-Base-component"
+export DEFAULT_HOME_FOLDER="sspl-base"
+
+FAILURE_INPUT_NOT_AVAILABLE=50
+FAILURE_BULLSEYE_FAILED_TO_CREATE_COVFILE=51
+FAILURE_BULLSEYE=52
+FAILURE_BAD_ARGUMENT=53
+FAILURE_COPY_CPPCHECK_RESULT_FAILED=61
+FAILURE_CPPCHECK=62
+
+set -ex
+set -o pipefail
+
+STARTINGDIR=$(pwd)
+
+cd ${0%/*}
+BASE=$(pwd)
+OUTPUT=$BASE/output
+export BASE
+export OUTPUT
+
+
+function source_file()
+{
+  local path=$1
+  [ -f "$path" ] || { echo "Can't find $path" ; exit 11 ; }
+  source "$path"
+}
+
+source_file "$BASE/build/common.sh"
+source_file "$BASE/setup_env_vars.sh"
+
+LOG=$BASE/log/build.log
+mkdir -p $BASE/log || exit 1
+
+PythonCoverage="OFF"
+STRACE_SUPPORT="OFF"
+CLEAN=0
+#BULLSEYE=0
+#BULLSEYE_UPLOAD=0
+#BULLSEYE_SYSTEM_TESTS=0
+#export NO_REMOVE_GCC=1
+#INPUT=/build/input
+
+#COVFILE="/tmp/root/sspl-base-unittest.cov"
+#BULLSEYE_SYSTEM_TEST_BRANCH=develop
+#export TEST_SELECTOR=
+
+#CMAKE_BUILD_TYPE=RelWithDebInfo
+CMAKE_BUILD_TYPE=Debug
+
+#DEBUG=0
+export ENABLE_STRIP=1
+#VALGRIND=0
+UNIT_TESTS=1
+
+function build()
+{
+    echo "STARTINGDIR=$STARTINGDIR"
+    echo "BASE=$BASE"
+    echo "Initial PATH=$PATH"
+    echo "Initial LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-unset}"
+    echo "Build type=${CMAKE_BUILD_TYPE}"
+    echo "Debug=${DEBUG}"
+
+    if [[ ! -d "$FETCHED_INPUTS_DIR" ]]
+    then
+        exitFailure $FAILURE_INPUT_NOT_AVAILABLE "No input available"
+    fi
+
+    if [[ ! -d "$REDIST" ]]
+    then
+        exitFailure $FAILURE_INPUT_NOT_AVAILABLE "REDIST does not exist"
+    fi
+
+    # TODO LINUXDAR-1506: remove the patching when the related issue is incorporated into the released version of boost
+    # https://github.com/boostorg/process/issues/62
+    BOOST_PROCESS_TARGET=${REDIST}/boost/include/boost/process/detail/posix/executor.hpp
+    diff -u patched_boost_executor.hpp ${BOOST_PROCESS_TARGET} && DIFFERS=0 || DIFFERS=1
+    if [[ "${DIFFERS}" == "1" ]]; then
+      echo "Patch Boost executor"
+      cp patched_boost_executor.hpp  ${BOOST_PROCESS_TARGET}
+    else
+      echo 'Boost executor already patched'
+    fi
+
+    echo "After setup: PATH=$PATH"
+    echo "After setup: LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-unset}"
+    echo "After setup: LIBRARY_PATH=${LIBRARY_PATH}"
+    echo "After setup: CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}"
+    COMMON_LDFLAGS="${LINK_OPTIONS:-}"
+    COMMON_CFLAGS="${OPTIONS:-} ${CFLAGS:-} ${COMMON_LDFLAGS}"
+
+    [[ $CLEAN == 1 ]] && rm -rf "$BASE/$DEBUG_BUILD_DIR"
+    [[ $CLEAN == 1 ]] && rm -rf "$BASE/$RELEASE_BUILD_DIR"
+    [[ $CLEAN == 1 ]] && rm -rf $OUTPUT
+
+#    # Run static analysis
+#    if [[ $ANALYSIS == 1 ]]
+#    then
+#      cppcheck_build  build${BITS} || exitFailure $FAILURE_CPPCHECK "Cppcheck static analysis build failed: $?"
+#    fi
+#
+    if [[ "${NO_BUILD}" == "1" ]]
+    then
+        exit 0
+    fi
+
+    if [[ "$CMAKE_BUILD_TYPE" == "RelWithDebInfo" ]]
+    then
+      echo "Build type is release"
+      BUILD_DIR="$BASE/$RELEASE_BUILD_DIR"
+    elif [[ "$CMAKE_BUILD_TYPE" == "Debug" ]]
+    then
+      echo "Build type is debug"
+      BUILD_DIR="$BASE/$DEBUG_BUILD_DIR"
+    else
+      # https://cmake.org/cmake/help/latest/variable/CMAKE_BUILD_TYPE.html
+      exitFailure $FAILURE_BAD_ARGUMENT "CMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE but should be either Debug or RelWithDebInfo"
+    fi
+
+    mkdir -p "$BUILD_DIR"
+    cd "$BUILD_DIR"
+#    echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >env
+#    echo "export PATH=$PATH" >>env
+
+    [[ -n ${NPROC:-} ]] || { which nproc > /dev/null 2>&1 && NPROC=$((`nproc`)); } || NPROC=2
+    (( $NPROC < 1 )) && NPROC=1
+    cmake -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+          .. \
+          || exitFailure 14 "Failed to configure $PRODUCT"
+#    cmake -v \
+#        -DPRODUCT_NAME="${PRODUCT_NAME}" \
+#        -DPRODUCT_LINE_ID="${PRODUCT_LINE_ID}" \
+#        -DDEFAULT_HOME_FOLDER="${DEFAULT_HOME_FOLDER}" \
+#        -DREDIST="${REDIST}" \
+#        -DINPUT="${REDIST}" \
+#        -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+#        -DNO_GCOV="true" \
+#        -DPythonCoverage="${PythonCoverage}" \
+#        -DSTRACE_SUPPORT="${STRACE_SUPPORT}" \
+#        .. \
+#        || exitFailure 14 "Failed to configure $PRODUCT"
+
+
+#    make -j${NPROC} copy_libs || exitFailure 15 "Failed to build $PRODUCT"
+    make -j${NPROC} || exitFailure 15 "Failed to build $PRODUCT"
+
+
+#    if (( ${UNIT_TESTS} == 1 ))
+#    then
+#        if (( ${VALGRIND} == 1 ))
+#        then
+#            ## -VV --debug
+#            export NPROC
+#            bash ${BASE}/build/valgrind/runValgrind.sh \
+#             || {
+#                local EXITCODE=$?
+#                exitFailure 16 "Unit tests failed for $PRODUCT: $EXITCODE"
+#            }
+#            echo 'Valgrind test finished'
+#            exit 0
+#
+#        [[ -n ${TEST_NPROC:-} ]] || TEST_NPROC=$NPROC
+#        elif (( ${BULLSEYE_SYSTEM_TESTS} == 0 ))
+#        then
+            ## If we are doing bullseye system tests then don't run unit test first
+            ## Otherwise run the unit-tests now
+            ctest \
+                --test-action test \
+                --no-compress-output --output-on-failure \
+                --timeout 300 \
+                || {
+                  local EXITCODE=$?
+                  echo "Unit tests failed with $EXITCODE"
+                  cat Testing/Temporary/LastTest.log || true
+                  cat /tmp/unitTest.log || true
+                  exitFailure 16 "Unit tests failed for $PRODUCT: $EXITCODE"
+                }
+            exit 0
+#            ctest \
+#                --test-action test \
+#                --parallel ${TEST_NPROC} \
+#                --no-compress-output --output-on-failure \
+#                --timeout 300 \
+#                || {
+#                local EXITCODE=$?
+#                echo "Unit tests failed with $EXITCODE"
+#                cat Testing/Temporary/LastTest.log || true
+#                cat /tmp/unitTest.log || true
+#                exitFailure 16 "Unit tests failed for $PRODUCT: $EXITCODE"
+#            }
+            cat /tmp/unitTest.log || true
+            exit 0
+#        fi
+
+        # python would have been executed in unit tests creating pyc and or pyo files
+        # need to make sure these are removed before creating distribution files.
+#        find ../build${BITS} -name "*.pyc" -type f | xargs -r rm -f
+#        find ../build${BITS} -name "*.pyo" -type f | xargs -r rm -f
+#    fi
+#    make -j${NPROC} install || exitFailure 17 "Failed to install $PRODUCT"
+#    make dist || exitFailure 18 "Failed to create distribution"
+#    cd ..
+#
+#    mkdir -p output
+#    echo "STARTINGDIR=$STARTINGDIR" >output/STARTINGDIR
+#    echo "BASE=$BASE" >output/BASE
+#    echo "PATH=$PATH" >output/PATH
+#    echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >output/LD_LIBRARY_PATH
+#
+#    rm -rf output/SDDS-COMPONENT
+#    cp -a build${BITS}/distribution/ output/SDDS-COMPONENT || exitFailure 21 "Failed to copy SDDS package: $?"
+#    cp -a build${BITS}/products/PluginApi/pluginapi.tar.gz output/pluginapi.tar.gz || exitFailure 22 "Failed to copy pluginapi.tar.gz package: $?"
+#    pushd build${BITS}
+#    tar -zcvf ../output/SystemProductTestOutput.tar.gz SystemProductTestOutput/ || exitFailure 23 "Failed to tar SystemProductTestOutput package: $?"
+#    popd
+#
+#    if [[ -d build${BITS}/symbols ]]
+#    then
+#        cp -a build${BITS}/symbols output/
+#    fi
+
+#    if [[ ${BULLSEYE} == 1 ]]
+#    then
+#      ## upload unit tests
+#      if [[ ${UNIT_TESTS} == 1 ]]
+#      then
+#            ## Process bullseye output
+#            ## upload unit tests
+#            cd $BASE
+#
+#            #keep the local jenkins tests seperated
+#            export COV_HTML_BASE=sspl-base-unittest
+#            export BULLSEYE_UPLOAD
+#            bash -x build/bullseye/uploadResults.sh || exit $?
+#      fi
+#      cp -a ${COVFILE}  output   || exitFailure $FAILURE_BULLSEYE_FAILED_TO_CREATE_COVFILE "Failed to copy covfile: $?"
+#    fi
+    echo "Build completed"
+}
+
+build 2>&1 | tee -a "$LOG"
+EXIT=$?
+cp $LOG $OUTPUT/ || true
+exit $EXIT
--- base/setup_build_tools.sh
+++ base/setup_build_tools.sh
@@ -10,43 +10,99 @@ then
 fi
 
 BASEDIR=$(dirname "$0")
-
 # Just in case this script ever gets symlinked
 BASEDIR=$(readlink -f "$BASEDIR")
+
+function install_package()
+{
+  local package=$1
+  if which apt 2>/dev/null
+  then
+    if ! apt list --installed $package 2>/dev/null | grep -q $package
+    then
+      sudo apt-get install $package -y || exit 1
+    fi
+  else
+    # TODO support yum
+    echo "Not implemented yet - currently only support apt"
+  fi
+}
+
+
 cd "$BASEDIR"
 
 # Where the TAP Python venv will be put
 TAP_VENV="$BASEDIR/tap_venv"
 
 # Install python3 venv for TAP and then install TAP
-# TODO deal with OSs with python3.6 as default = e.g. 18.04
-if ! apt list --installed python3-venv 2>/dev/null | grep -q python3-venv
+# For TAP we try to work out which python to use or install and then set PYTHON_TO_USE for use by TAP and TAP venv.
+if which python3
 then
-  sudo apt-get install python3-venv -y
+  PYTHON_TO_USE=python3
+else
+  echo "Python3 not installed"
+  exit 1
 fi
+
+if python3 -c "import sys; print(sys.version_info.major >= 3 and sys.version_info.minor >= 7)" | grep False
+then
+  echo "python3 version is too old, trying 'python3.8' explicitly"
+  if which python3.8
+  then
+    PYTHON_TO_USE=python3.8
+  else
+    echo "Python3.8 not installed, trying python3.7"
+    if which python3.7
+      then
+        PYTHON_TO_USE=python3.7
+      else
+        echo "Python3.7 not installed, will try installing python3.8"
+        sleep 3
+        sudo apt-get install python3.8-venv -y
+        if which python3.8
+        then
+          PYTHON_TO_USE=python3.8
+        else
+          echo "Could not install python3.8, will try installing python3.7"
+          sudo apt-get install python3.7-venv -y
+          if which python3.7
+          then
+            PYTHON_TO_USE=python3.7
+          else
+            echo "Could not find or install a python version of 3.7 or newer."
+            exit 1
+          fi
+        fi
+      fi
+  fi
+fi
+echo "Using $PYTHON_TO_USE for python commands"
+
+install_package $PYTHON_TO_USE-venv
 
 if [ -d "$TAP_VENV" ]
 then
  echo "$TAP_VENV already exists, not re-creating it"
  source "$TAP_VENV/bin/activate"
 else
-  python3 -m venv "$TAP_VENV"
+  $PYTHON_TO_USE -m venv "$TAP_VENV"
   pushd "$TAP_VENV"
     echo "[global]" > pip.conf
     echo "index-url = https://tap-artifactory1.eng.sophos/artifactory/api/pypi/pypi/simple" >> pip.conf
     echo "trusted-host = tap-artifactory1.eng.sophos" >> pip.conf
   popd
+  # Within TAP python venv just use "python3"
   source "$TAP_VENV/bin/activate"
-  echo "Using python:"
-  which python
-  python3 -m pip install pip --upgrade
-  python3 -m pip install wheel
-  ##python3 -m pip install build_scripts
-  python3 -m pip install keyrings.alt
-  python3 -m pip install tap
+    echo "Using python:"
+    which python
+    python3 -m pip install pip --upgrade
+    python3 -m pip install wheel
+    ##python3 -m pip install build_scripts
+    python3 -m pip install keyrings.alt
+    python3 -m pip install tap
 fi
 
-# TODO - raise ticket to get this dependency removed - Not sure why tap needs this, seems to never be used?
+# TODO - raise ticket to get this dependency removed - Not sure why tap needs this, seems to never be used and is always empty.
 TAP_CACHE="/SophosPackages"
 if [ -d "$TAP_CACHE" ]
 then
@@ -72,6 +128,8 @@ fi
 tap --version
 tap ls
 tap fetch sspl_base.build.release
+# We're done with TAP now, so deactivate the venv.
+deactivate
 
 BUILD_TOOLS_DIR="$ROOT_LEVEL_BUILD_DIR/build_tools"
 FETCHED_INPUTS_DIR="$ROOT_LEVEL_BUILD_DIR/input"
@@ -150,10 +208,7 @@ echo "---"
 # Make
 # TODO
 # Make is not yet available in artifactory so for now we'll install it directly
-if ! apt list --installed make 2>/dev/null | grep -q make
-then
-  sudo apt-get install make -y
-fi
+install_package make
 MAKE_DIR="$BUILD_TOOLS_DIR/make"
 mkdir -p "$MAKE_DIR"
 which make
@@ -167,10 +222,7 @@ echo "---"
 # AS (assembler)
 # TODO
 # AS (assembler) is not yet available in artifactory so for now we'll install it directly
-if ! apt list --installed binutils 2>/dev/null | grep -q binutils
-then
-  sudo apt-get install binutils -y
-fi
+install_package binutils
 AS_DIR="$BUILD_TOOLS_DIR/as"
 mkdir -p "$AS_DIR"
 which as
@@ -184,19 +236,29 @@ echo "---"
 # TODO - try and use crosstoolng to build a GCC toolchain
 # This install works around this error:
 # /usr/bin/ld: cannot find crt1.o: No such file or directory
-if ! apt list --installed libc6-dev 2>/dev/null | grep -q libc6-dev
-then
-  sudo apt-get install libc6-dev -y
-fi
+install_package libc6-dev
 
 # zip is used when packing up mcs router
-if ! apt list --installed zip 2>/dev/null | grep -q zip
+install_package zip
+
+# For python unit tests install required packages.
+install_package python3-pip
+# Also need python3-dev for building certain python packages
+install_package python3-dev
+if [[ -f $BASEDIR/python_unit_test_requirements.txt ]]
 then
-  sudo apt install zip -y
+  python3 -m pip install -r $BASEDIR/python_unit_test_requirements.txt
+else
+  echo "Could not find python_unit_test_requirements.txt to install dependencies for MCSRouter python unit tests"
+  exit 1
 fi
 
 echo "---"
-echo "Done, please 'source setup_env_vars.sh'"
+echo "Done"
+echo "To build you can use these options:"
+echo "In your IDE set the toolchain environment file to be: 'source setup_env_vars.sh' and then build."
+echo "Build.sh"
+echo "cmdline 'source setup_env_vars.sh'"
 
 # TODO For SSPL only dev machines this may be ok but might break other things?
 # [[ -f /etc/profile.d/setup_env_vars.sh ]] || sudo ln -s "$BASEDIR/setup_env_vars.sh" /etc/profile.d/setup_env_vars.sh
