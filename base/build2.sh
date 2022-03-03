4a84cb5d1b900a3f7a56c6395e36f9d8010f1b55
Alex Cooper <alex.cooper@sophos.com>
2022-03-03 16:59:11 +0000

    [base] [LINUXDAR-4092] copy sdds3 from dist to output in build script, for now.

--- base/build2.sh
+++ base/build2.sh
@@ -330,8 +330,11 @@ function build()
     echo "BASE=$BASE" >$OUTPUT/BASE
     echo "PATH=$PATH" >$OUTPUT/PATH
     echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >$OUTPUT/LD_LIBRARY_PATH
+
     rm -rf $OUTPUT/SDDS-COMPONENT
     cp -a "$BUILD_DIR/distribution/" "$OUTPUT/SDDS-COMPONENT" || exitFailure 21 "Failed to copy SDDS package: $?"
+    cp -a "$BUILD_DIR/SDDS3-PACKAGE" output/SDDS3-PACKAGE || exitFailure 21 "Failed to copy SDDS3-PACKAGE: $?"
+    cp -a "$BUILD_DIR/distribution/SDDS-Import.xml" output/SDDS3-PACKAGE || exitFailure 21 "Failed to copy SDDS-Import.xml to SDDS3-PACKAGE: $?"
     cp -a "$BUILD_DIR/products/PluginApi/pluginapi.tar.gz" "$OUTPUT/pluginapi.tar.gz" || exitFailure 22 "Failed to copy pluginapi.tar.gz package: $?"
     pushd "$BUILD_DIR"
       tar -zcvf "$OUTPUT/SystemProductTestOutput.tar.gz" SystemProductTestOutput/ || exitFailure 23 "Failed to tar SystemProductTestOutput package: $?"
