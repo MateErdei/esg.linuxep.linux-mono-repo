13b4ea769627ba69d38f8a3a7dd19345a94ef828
Mollie Bourke <Mollie.Bourke@Sophos.com>
2022-03-24 16:45:23 +0000

    [base] Pull request #1564: Feature/LINUXDAR-4399 add debian 11 for spl
    Merge in LINUXEP/everest-base from feature/LINUXDAR-4399-add-debian-11-for-spl to develop
    
    * commit 'ba3404f133fe37c0bbc805c7a39d06dbe1159b5c':
      [LINUXDAR-4399] debian not in platform.platform() on debian11
      [LINUXDAR-4399] Add debian11 to overnight test infrastructure

--- base/aws-runner/run_tests_in_aws.sh
+++ base/aws-runner/run_tests_in_aws.sh
@@ -337,6 +337,7 @@ combineResults()
   python3 -m robot.rebot --merge -o ./results-combine-workspace/centos7x64-output.xml -l none -r none -N centos7x64  ./results/centos7x64*
   python3 -m robot.rebot --merge -o ./results-combine-workspace/centosstreamx64-output.xml -l none -r none -N centosstreamx64  ./results/centosstreamx64*
   python3 -m robot.rebot --merge -o ./results-combine-workspace/debian10x64-output.xml -l none -r none -N debian10x64  ./results/debian10x64*
+  python3 -m robot.rebot --merge -o ./results-combine-workspace/debian11x64-output.xml -l none -r none -N debian11x64  ./results/debian11x64*
   python3 -m robot.rebot --merge -o ./results-combine-workspace/oracle8x64-output.xml -l none -r none -N oracle8x64  ./results/oracle8x64*
   python3 -m robot.rebot --merge -o ./results-combine-workspace/rhel78x64-output.xml -l none -r none -N rhel78x64  ./results/rhel78x64*
   python3 -m robot.rebot --merge -o ./results-combine-workspace/rhel81x64-output.xml -l none -r none -N rhel81x64  ./results/rhel81x64*
@@ -350,6 +351,7 @@ combineResults()
   python3 -m robot.rebot -l ./results/centos7x64-log.html -r ./results/centos7x64-report.html -N combined ./results-combine-workspace/centos7x64-output.xml
   python3 -m robot.rebot -l ./results/centosstreamx64-log.html -r ./results/centosstreamx64-report.html -N combined ./results-combine-workspace/centosstreamx64-output.xml
   python3 -m robot.rebot -l ./results/debian10x64-log.html -r ./results/debian10x64-report.html -N combined ./results-combine-workspace/debian10x64-output.xml
+  python3 -m robot.rebot -l ./results/debian11x64-log.html -r ./results/debian11x64-report.html -N combined ./results-combine-workspace/debian11x64-output.xml
   python3 -m robot.rebot -l ./results/oracle8x64-log.html -r ./results/oracle8x64-report.html -N combined ./results-combine-workspace/oracle8x64-output.xml
   python3 -m robot.rebot -l ./results/rhel78x64-log.html -r ./results/rhel78x64-report.html -N combined ./results-combine-workspace/rhel78x64-output.xml
   python3 -m robot.rebot -l ./results/rhel81x64-log.html -r ./results/rhel81x64-report.html -N combined ./results-combine-workspace/rhel81x64-output.xml
