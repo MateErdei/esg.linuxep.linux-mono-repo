201f8909bb8c79b6a219d715a282eb5c34da0ab2
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2022-03-28 14:12:42 +0100

    [base] [LINUXDAR-4266] remove password from git

--- base/tests/cmscrouter/TestMcsHttpClient.cpp
+++ base/tests/cmscrouter/TestMcsHttpClient.cpp
@@ -11,8 +11,8 @@ class McsClientTests : public LogInitializedTests
 TEST_F(McsClientTests, jwtToken)
 {
     MCS::MCSHttpClient client("https://mcs2-cloudstation-eu-west-1.prod.hydra.sophos.com/sophos/management/ep","027858598e1b2b7b22625bd075a78e220c172d6b725e49db666ec8ad23f953ab");
-    client.setID("7e70a78d-407b-a479-db17-c4820ad3d80d");
-    client.setPassword("r6ElR8oT8Upo3nUq");
+    client.setID("");
+    client.setPassword("");
     Common::HttpRequests::Headers requestHeaders;
     requestHeaders.insert({"Content-Type","application/xml; charset=utf-8"});
     Common::HttpRequests::Response response = client.sendMessageWithIDAndRole("/authenticate/endpoint/",requestHeaders,Common::HttpRequests::RequestType::POST);
