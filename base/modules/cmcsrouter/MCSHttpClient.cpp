ce664eaa78d2bd2b6ec6c359f52cec42d73163d8
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2022-03-28 11:08:24 +0100

    [base] [LINUXDAR-4266] initial commit of mcs http code

--- base/modules/cmcsrouter/MCSHttpClient.cpp
+++ base/modules/cmcsrouter/MCSHttpClient.cpp
@@ -5,125 +5,48 @@ Copyright 2022, Sophos Limited.  All rights reserved.
 
 #include "MCSHttpClient.h"
 
+#include <Common/CurlWrapper/CurlWrapper.h>
 #include <Common/HttpRequestsImpl/HttpRequesterImpl.h>
-#include <Common/ObfuscationImpl/Base64.h>
-
+#include <vector>
 #include <memory>
 namespace MCS
 {
-    MCSHttpClient::MCSHttpClient(std::string mcsUrl, std::string registerToken,std::shared_ptr<Common::HttpRequests::IHttpRequester> client):
-        m_base_url(mcsUrl),m_registerToken(registerToken),m_client(client){}
-
-    Common::HttpRequests::Response  MCSHttpClient::sendRegistration(const std::string& statusXml, const std::string& token)
+    Common::HttpRequests::Response  MCSHttpClient::sendMessage(const std::string& url)
     {
-        Common::HttpRequests::Headers requestHeaders
-            {
-                {"Authorization",getRegistrationAuthorizationHeader(token)},
-                {"User-Agent", "Sophos MCS Client/" + m_version + " Linux sessions "+ m_registerToken},
-                {"Content-Type","application/xml; charset=utf-8"}
-            };
+        std::map<std::string,std::string> headers;
+        headers.insert({"Authorization",getV1AuthorizationHeader()});
 
-        Common::HttpRequests::RequestConfig request{ .url = m_base_url + "/register", .headers = requestHeaders, .data = statusXml};
-        updateProxyInfo(request);
-        updateCertPath(request);
-
-        return m_client->post(request);
-    }
-
-    Common::HttpRequests::Response  MCSHttpClient::sendPreregistration(const std::string& statusXml, const std::string& customerToken)
-    {
-        Common::HttpRequests::Headers requestHeaders
-            {
-                {"Authorization",getDeploymentInfoV2AuthorizationHeader(customerToken)},
-                {"User-Agent", "Sophos MCS Client/" + m_version + " Linux sessions "+ m_registerToken},
-                {"Content-Type","application/json;charset=UTF-8"}
-            };
-
-        Common::HttpRequests::RequestConfig request{ .url = m_base_url + "/install/deployment-info/2", .headers = requestHeaders, .data = statusXml};
-        updateProxyInfo(request);
-        updateCertPath(request);
-
-        return m_client->post(request);
-    }
-
-    Common::HttpRequests::Response  MCSHttpClient::sendMessage(
-        const std::string& url,
-        Common::HttpRequests::RequestType requestType,
-        Common::HttpRequests::Headers headers = {}
-    )
-    {
-        Common::HttpRequests::Headers requestHeaders;
-        requestHeaders.insert({"Authorization",getV1AuthorizationHeader()});
-        requestHeaders.insert({"User-Agent", "Sophos MCS Client/" + m_version + " Linux sessions "+ m_registerToken});
-        for (const auto& head : headers)
-        {
-            requestHeaders.insert({head.first,head.second});
-        }
-
-        Common::HttpRequests::RequestConfig request{ .url = m_base_url + url ,.headers = requestHeaders};
-        updateProxyInfo(request);
-        updateCertPath(request);
-
-        Common::HttpRequests::Response response;
-        switch (requestType)
-        {
-            case Common::HttpRequests::RequestType::POST:
-                response = m_client->post(request);
-                break;
-            case Common::HttpRequests::RequestType::PUT:
-                response = m_client->put(request);
-                break;
-            case Common::HttpRequests::RequestType::GET:
-                response = m_client->get(request);
-                break;
-            default :
-                response = m_client->get(request);
-                break;
-        }
+//        std::shared_ptr<Common::CurlWrapper::ICurlWrapper> curlWrapper =
+//            std::make_shared<Common::CurlWrapper::CurlWrapper>();
+//        std::shared_ptr<Common::HttpRequests::IHttpRequester> client = std::make_shared<Common::HttpRequestsImpl::HttpRequesterImpl>(curlWrapper);
+        std::shared_ptr<Common::CurlWrapper::ICurlWrapper> curlWrapper;
+        Common::HttpRequestsImpl::HttpRequesterImpl client = Common::HttpRequestsImpl::HttpRequesterImpl(curlWrapper);
+        Common::HttpRequests::Response response =
+            client.get(Common::HttpRequests::RequestConfig{ .url = url ,.headers = headers});
         return response;
     }
 
-    Common::HttpRequests::Response  MCSHttpClient::sendMessageWithID(
-        const std::string& url,
-        Common::HttpRequests::RequestType requestType,
-        Common::HttpRequests::Headers headers = {})
+    Common::HttpRequests::Response  MCSHttpClient::sendMessageWithID(const std::string& url)
     {
-        return sendMessage(url + getID(),requestType, headers);
+        return sendMessage(url+getID());
     }
 
-    Common::HttpRequests::Response  MCSHttpClient::sendMessageWithIDAndRole(
-        const std::string& url,
-        Common::HttpRequests::RequestType requestType,
-        Common::HttpRequests::Headers headers = {})
+    Common::HttpRequests::Response  MCSHttpClient::sendMessageWithIDAndRole(const std::string& url)
     {
-        return sendMessage(url + getID() + "/role/endpoint", requestType, headers);
+        return sendMessage(url+getID() + "/role/endpoint");
     }
 
     std::string MCSHttpClient::getV1AuthorizationHeader()
     {
-        return getAuthorizationHeader(getID() + ":" + getPassword());
-    }
-
-    std::string MCSHttpClient::getRegistrationAuthorizationHeader(const std::string& token)
-    {
-        return getAuthorizationHeader(getID() + ":" + getPassword() + ":" + token);
-    }
-
-    std::string MCSHttpClient::getDeploymentInfoV2AuthorizationHeader(const std::string& customerToken)
-    {
-        return getAuthorizationHeader(customerToken);
-    }
-
-    std::string MCSHttpClient::getAuthorizationHeader(const std::string& toBeEncoded)
-    {
-        return "Basic " + Common::ObfuscationImpl::Base64::Encode(toBeEncoded);
+        std::string tobeEncoded = getID() + ":" + getPassword();
+        std::string header = "Basic " + tobeEncoded;
+        return header;
     }
 
     std::string MCSHttpClient::getID()
     {
         return m_id;
     }
-
     std::string MCSHttpClient::getPassword()
     {
         return m_password;
@@ -134,46 +57,9 @@ namespace MCS
         m_id = id;
     }
 
-    void MCSHttpClient::setVersion(const std::string& version)
-    {
-        m_version = version;
-    }
-
     void MCSHttpClient::setPassword(const std::string& password)
     {
         m_password = password;
     }
 
-    void MCSHttpClient::setCertPath(const std::string& certPath)
-    {
-        m_certPath = certPath;
-    }
-
-    void MCSHttpClient::setProxyInfo(const std::string& proxy,const std::string& proxyUser,const std::string& proxyPassword)
-    {
-        m_proxy = proxy;
-        m_proxyUser = proxyUser;
-        m_proxyPassword = proxyPassword;
-    }
-
-    void MCSHttpClient::updateProxyInfo(Common::HttpRequests::RequestConfig& request)
-    {
-        if (!m_proxy.empty())
-        {
-            request.proxy = m_proxy;
-            if (!m_proxyUser.empty())
-            {
-                request.proxyUsername = m_proxyUser;
-                request.proxyPassword = m_proxyPassword;
-            }
-        }
-    }
-
-    void MCSHttpClient::updateCertPath(Common::HttpRequests::RequestConfig& request)
-    {
-        if (!m_certPath.empty())
-        {
-            request.certPath = m_certPath;
-        }
-    }
 }
--- base/modules/cmcsrouter/MCSHttpClient.h
+++ base/modules/cmcsrouter/MCSHttpClient.h
@@ -3,48 +3,25 @@ Copyright 2022, Sophos Limited.  All rights reserved.
 ******************************************************************************************************/
 #pragma once
 
-#include <Common/CurlWrapper/CurlWrapper.h>
-#include <Common/HttpRequestsImpl/HttpRequesterImpl.h>
 
+#include <Common/HttpRequestsImpl/HttpRequesterImpl.h>
 namespace MCS
 {
     class MCSHttpClient
     {
     public:
-        MCSHttpClient(std::string mcsUrl, std::string registerToken,std::shared_ptr<Common::HttpRequests::IHttpRequester>);
-        Common::HttpRequests::Response  sendRegistration(const std::string& statusXml, const std::string& token);
-        Common::HttpRequests::Response  sendPreregistration(const std::string& statusXml, const std::string& customerToken);
-        Common::HttpRequests::Response  sendMessage(const std::string& url,Common::HttpRequests::RequestType requestType,Common::HttpRequests::Headers headers);
-        Common::HttpRequests::Response  sendMessageWithID(const std::string& url,Common::HttpRequests::RequestType requestType,Common::HttpRequests::Headers headers);
-        Common::HttpRequests::Response  sendMessageWithIDAndRole(const std::string& url,Common::HttpRequests::RequestType requestType,Common::HttpRequests::Headers headers);
+        Common::HttpRequests::Response  sendMessage(const std::string& url);
+        Common::HttpRequests::Response  sendMessageWithID(const std::string& url);
+        Common::HttpRequests::Response  sendMessageWithIDAndRole(const std::string& url);
         std::string getID();
         std::string getPassword();
-
         void setID(const std::string& id);
-        void setVersion(const std::string& version);
-        void setPassword(const std::string& password);
-        void setCertPath(const std::string& password);
-        void setProxyInfo(const std::string& proxy,const std::string& proxyUser,const std::string& proxyPassword);
+        void setPassword(const std::string& id);
 
     private:
         std::string getV1AuthorizationHeader();
-        std::string getRegistrationAuthorizationHeader(const std::string& token);
-        std::string getDeploymentInfoV2AuthorizationHeader(const std::string& customerToken);
-        std::string getAuthorizationHeader(const std::string& toBeEncoded);
-
-        void updateProxyInfo(Common::HttpRequests::RequestConfig& request);
-        void updateCertPath(Common::HttpRequests::RequestConfig& request);
-
-        std::string m_base_url;
-        std::string m_registerToken;
-        std::shared_ptr<Common::HttpRequests::IHttpRequester> m_client;
         std::string m_id;
-        std::string m_version;
         std::string m_password;
-        std::string m_proxy;
-        std::string m_proxyUser;
-        std::string m_proxyPassword;
-        std::string m_certPath;
 
     };
 }
