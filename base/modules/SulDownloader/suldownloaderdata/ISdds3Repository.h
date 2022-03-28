d582e1f98edd0550bfa3e9b9ed05797592d0440a
Jake McLanaghan <jake.mclanaghan@sophos.com>
2022-03-28 17:34:24 +0100

    [base] [LINUXDAR-4178] remove unwanted comments

--- base/modules/SulDownloader/suldownloaderdata/ISdds3Repository.h
+++ base/modules/SulDownloader/suldownloaderdata/ISdds3Repository.h
@@ -46,17 +46,6 @@ namespace SulDownloader
                 const suldownloaderdata::ConnectionSetup& connectionSetup,
                 bool supplementOnly,
                 const suldownloaderdata::ConfigurationData& configurationData) = 0;
-
-//            /**
-//             * Get the logs for this SUL connection
-//             * @return
-//             */
-//            virtual SulLogsVector getLogs() const = 0;
-//
-//            virtual void dumpLogs() const = 0;
-//
-//            virtual void reset() = 0;
-
         };
 
         using ISDDS3RepositoryPtr = std::unique_ptr<ISdds3Repository>;
