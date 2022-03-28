27c289ae1ea412a16b0f2b9769641444693e0232
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2022-03-28 17:09:17 +0100

    [base] [LINUXDAR-4345] create getJWTtoken method

--- base/modules/cmcsrouter/MCSApiCalls.cpp
+++ base/modules/cmcsrouter/MCSApiCalls.cpp
@@ -3,17 +3,10 @@ Copyright 2022, Sophos Limited.  All rights reserved.
 ******************************************************************************************************/
 
 #include "MCSApiCalls.h"
-#include "AgentAdapter.h"
-#include "Config.h"
-#include "Logger.h"
-#include <Common/ObfuscationImpl/Base64.h>
-#include <Common/UtilityImpl/StringUtils.h>
-
-#include <sstream>
 
 namespace MCS
 {
-    std::string MCSApiCalls::getJwToken(MCSHttpClient client)
+    std::string getJwToken(MCSHttpClient client)
     {
         Common::HttpRequests::Headers requestHeaders;
         requestHeaders.insert({"Content-Type","application/xml; charset=utf-8"});
@@ -26,68 +19,5 @@ namespace MCS
             return response.body;
         }
         return "";
-    }
-
-    std::string MCSApiCalls::preregisterEndpoint(
-        MCSHttpClient& client,
-        MCS::ConfigOptions& configOptions,
-        const std::string& statusXml,
-        const std::string& proxy)
-    {
-        client.setVersion(configOptions.config[MCS::MCS_PRODUCT_VERSION]);
-
-        if (!configOptions.config[MCS::MCS_CA_OVERRIDE].empty())
-        {
-            client.setCertPath(configOptions.config[MCS::MCS_CA_OVERRIDE]);
-        }
-        else
-        {
-            client.setCertPath(configOptions.config[MCS::MCS_CERT]);
-        }
-        
-        client.setProxyInfo(proxy, configOptions.config[MCS::MCS_PROXY_USERNAME], configOptions.config[MCS::MCS_PROXY_PASSWORD]);
-        Common::HttpRequests::Response response = client.sendPreregistration(statusXml, configOptions.config[MCS::MCS_CUSTOMER_TOKEN]);
-        return response.body;
-    }
-
-    bool MCSApiCalls::registerEndpoint(
-        MCSHttpClient& client,
-        MCS::ConfigOptions& configOptions,
-        const std::string& statusXml,
-        const std::string& proxy)
-    {
-        client.setID(configOptions.config[MCS::MCS_ID]);
-        client.setPassword(configOptions.config[MCS::MCS_PASSWORD]);
-        client.setVersion(configOptions.config[MCS::MCS_PRODUCT_VERSION]);
-
-        if (!configOptions.config[MCS::MCS_CA_OVERRIDE].empty())
-        {
-            client.setCertPath(configOptions.config[MCS::MCS_CA_OVERRIDE]);
-        }
-        else
-        {
-            client.setCertPath(configOptions.config[MCS::MCS_CERT]);
-        }
-        
-        client.setProxyInfo(proxy, configOptions.config[MCS::MCS_PROXY_USERNAME], configOptions.config[MCS::MCS_PROXY_PASSWORD]);
-        Common::HttpRequests::Response response = client.sendRegistration(statusXml, configOptions.config[MCS::MCS_TOKEN]);
-        if (response.status == 200)
-        {
-            std::string messageBody = Common::ObfuscationImpl::Base64::Decode(response.body);
-            std::vector<std::string> responseValues = Common::UtilityImpl::StringUtils::splitString(messageBody, ":");
-            if (responseValues.size() == 2)
-            {
-                // Note that updating the configOptions here should be propagated back to the caller as it is all
-                // passed by reference.
-                configOptions.config[MCS::MCS_ID] = responseValues[0]; // endpointId
-                configOptions.config[MCS::MCS_PASSWORD] = responseValues[1]; //MCS Password
-                return true;
-            }
-        }
-        else
-        {
-            LOGWARN("Error during registration: " << response.status);
-        }
-        return false;
     }
 }
--- base/modules/cmcsrouter/MCSApiCalls.h
+++ base/modules/cmcsrouter/MCSApiCalls.h
@@ -2,32 +2,14 @@
 Copyright 2022, Sophos Limited.  All rights reserved.
 ******************************************************************************************************/
 #pragma once
-
 #include "MCSHttpClient.h"
-#include "ConfigOptions.h"
-
 #include <string>
-#include <map>
-
 namespace MCS
 {
 class MCSApiCalls
 {
     public:
         std::string getJwToken(MCSHttpClient client);
-        bool registerEndpoint(
-            MCSHttpClient& client,
-            MCS::ConfigOptions& configOptions,
-            const std::string& statusXml,
-            const std::string& proxy
-        );
-        std::string preregisterEndpoint(
-            MCSHttpClient& client,
-            MCS::ConfigOptions& registerConfig,
-            const std::string& statusXml,
-            const std::string& proxy
-        );
-
 };
 
 }
