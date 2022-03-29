0fdd1b4c0f3e0a357c6cc5950ddc8f5ecd2c786c
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2022-03-29 11:49:00 +0100

    [base] [LINUXDAR-4345] add unit tests

--- base/tests/cmscrouter/TestMcsHttpClient.cpp
+++ base/tests/cmscrouter/TestMcsHttpClient.cpp
@@ -32,6 +32,16 @@ namespace
 
     EXPECT_CALL(*curlWrapper, curlEasySetFuncOpt(fakeCurlHandle, CURLOPT_WRITEFUNCTION, _)).WillOnce(Return(CURLE_OK));
     EXPECT_CALL(*curlWrapper, curlEasySetDataOpt(fakeCurlHandle, CURLOPT_WRITEDATA, _)).WillOnce(Return(CURLE_OK));
+    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_SSLVERSION, _)).WillOnce(Return(CURLE_OK));
+    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_TIMEOUT, _)).WillOnce(Return(CURLE_OK));
+    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_VERBOSE, _)).WillOnce(Return(CURLE_OK));
+    EXPECT_CALL(*curlWrapper, curlEasyPerform(fakeCurlHandle)).WillOnce(Return(CURLE_OK));
+    EXPECT_CALL(*curlWrapper, curlEasyCleanup(fakeCurlHandle));
+
+    EXPECT_CALL(*curlWrapper, curlGetResponseCode(fakeCurlHandle, _))
+        .WillOnce(DoAll(SetArgPointee<1>(200), Return(CURLE_OK)));
+
+    EXPECT_CALL(*curlWrapper, curlGlobalCleanup());
 }
 }
 
@@ -42,32 +52,89 @@ TEST_F(McsClientTests, checkFieldsAreSet)
     MCS::MCSHttpClient client("https://mcsUrl","registerToken",curlWrapper);
     client.setID("ThisIsAnMCSID+1001");
     client.setPassword("password");
+    client.setVersion("1.0.0");
     Common::HttpRequests::Headers requestHeaders;
     std::variant<std::string, long> urlVariant = "https://mcsUrl/authenticate/endpoint/ThisIsAnMCSID+1001/role/endpoint";
 
     EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_URL, urlVariant)).WillOnce(Return(CURLE_OK));
-    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_SSLVERSION, _)).WillOnce(Return(CURLE_OK));
-    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_TIMEOUT, _)).WillOnce(Return(CURLE_OK));
-    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_CUSTOMREQUEST, requestTypePost)).WillOnce(Return(CURLE_OK));
-    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_VERBOSE, _)).WillOnce(Return(CURLE_OK));
+
     EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_CAINFO, _)).WillOnce(Return(CURLE_OK));
     EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_CAPATH, _)).WillOnce(Return(CURLE_OK));
+
+    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_CUSTOMREQUEST, requestTypePost)).WillOnce(Return(CURLE_OK));
+    curl_slist* curlHeaders = nullptr;
+    curlHeaders = curl_slist_append(curlHeaders, "placeholder only");
+    EXPECT_CALL(*curlWrapper, curlSlistAppend(_, "Authorization:Basic VGhpc0lzQW5NQ1NJRCsxMDAxOnBhc3N3b3Jk")).WillOnce(Return(curlHeaders));
+    EXPECT_CALL(*curlWrapper, curlSlistAppend(_, "User-Agent:Sophos MCS Client/1.0.0 Linux sessions registerToken")).WillOnce(Return(curlHeaders));
+    EXPECT_CALL(*curlWrapper, curlEasySetOptHeaders(fakeCurlHandle, _)).WillOnce(Return(CURLE_OK));
+    EXPECT_CALL(*curlWrapper, curlSlistFreeAll(_));
+
+
+    client.sendMessageWithIDAndRole("/authenticate/endpoint/",Common::HttpRequests::RequestType::POST,requestHeaders);
+    curl_slist_free_all(curlHeaders);
+
+}
+
+TEST_F(McsClientTests, checkProxyFieldsAreSet)
+{
+    auto curlWrapper = std::make_shared<StrictMock<MockCurlWrapper>>();
+    setCommonExpectations(curlWrapper);
+    MCS::MCSHttpClient client("https://mcsUrl","registerToken",curlWrapper);
+    client.setID("ThisIsAnMCSID+1001");
+    client.setPassword("password");
+    client.setVersion("1.0.0");
+    client.setProxyInfo("10.55.36.66","user","password");
+    Common::HttpRequests::Headers requestHeaders;
+    std::variant<std::string, long> urlVariant = "https://mcsUrl/authenticate/endpoint/ThisIsAnMCSID+1001/role/endpoint";
+
+    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_URL, urlVariant)).WillOnce(Return(CURLE_OK));
+
+    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_CUSTOMREQUEST, requestTypeGet)).WillOnce(Return(CURLE_OK));
+
+    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_CAINFO, _)).WillOnce(Return(CURLE_OK));
+    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_CAPATH, _)).WillOnce(Return(CURLE_OK));
+
+    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_PROXY, _)).WillOnce(Return(CURLE_OK));
+    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_PROXYAUTH, _)).WillOnce(Return(CURLE_OK));
+    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_PROXYUSERPWD, _)).WillOnce(Return(CURLE_OK));
+    curl_slist* curlHeaders = nullptr;
+    curlHeaders = curl_slist_append(curlHeaders, "placeholder only");
+    EXPECT_CALL(*curlWrapper, curlSlistAppend(_, "Authorization:Basic VGhpc0lzQW5NQ1NJRCsxMDAxOnBhc3N3b3Jk")).WillOnce(Return(curlHeaders));
+    EXPECT_CALL(*curlWrapper, curlSlistAppend(_, "User-Agent:Sophos MCS Client/1.0.0 Linux sessions registerToken")).WillOnce(Return(curlHeaders));
+    EXPECT_CALL(*curlWrapper, curlEasySetOptHeaders(fakeCurlHandle, _)).WillOnce(Return(CURLE_OK));
+    EXPECT_CALL(*curlWrapper, curlSlistFreeAll(_));
+
+    client.sendMessageWithIDAndRole("/authenticate/endpoint/",Common::HttpRequests::RequestType::GET,requestHeaders);
+    curl_slist_free_all(curlHeaders);
+}
+
+TEST_F(McsClientTests, setCertPath)
+{
+    auto curlWrapper = std::make_shared<StrictMock<MockCurlWrapper>>();
+    setCommonExpectations(curlWrapper);
+    MCS::MCSHttpClient client("https://mcsUrl","registerToken",curlWrapper);
+    client.setID("ThisIsAnMCSID+1001");
+    client.setPassword("password");
+    client.setVersion("1.0.0");
+    client.setCertPath("/tmp/random");
+    Common::HttpRequests::Headers requestHeaders;
+    std::variant<std::string, long> urlVariant = "https://mcsUrl/authenticate/endpoint/ThisIsAnMCSID+1001/role/endpoint";
+    std::variant<std::string, long> certPath = "/tmp/random";
+
+    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_URL, urlVariant)).WillOnce(Return(CURLE_OK));
+
+    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_CUSTOMREQUEST, requestTypePut)).WillOnce(Return(CURLE_OK));
+
+    EXPECT_CALL(*curlWrapper, curlEasySetOpt(fakeCurlHandle, CURLOPT_CAINFO, certPath)).WillOnce(Return(CURLE_OK));
+
 
     curl_slist* curlHeaders = nullptr;
     curlHeaders = curl_slist_append(curlHeaders, "placeholder only");
     EXPECT_CALL(*curlWrapper, curlSlistAppend(_, "Authorization:Basic VGhpc0lzQW5NQ1NJRCsxMDAxOnBhc3N3b3Jk")).WillOnce(Return(curlHeaders));
-    EXPECT_CALL(*curlWrapper, curlSlistAppend(_, "User-Agent:Sophos MCS Client/ Linux sessions registerToken")).WillOnce(Return(curlHeaders));
+    EXPECT_CALL(*curlWrapper, curlSlistAppend(_, "User-Agent:Sophos MCS Client/1.0.0 Linux sessions registerToken")).WillOnce(Return(curlHeaders));
     EXPECT_CALL(*curlWrapper, curlEasySetOptHeaders(fakeCurlHandle, _)).WillOnce(Return(CURLE_OK));
     EXPECT_CALL(*curlWrapper, curlSlistFreeAll(_));
 
-    EXPECT_CALL(*curlWrapper, curlEasyPerform(fakeCurlHandle)).WillOnce(Return(CURLE_OK));
-    EXPECT_CALL(*curlWrapper, curlEasyCleanup(fakeCurlHandle));
-
-    EXPECT_CALL(*curlWrapper, curlGetResponseCode(fakeCurlHandle, _))
-        .WillOnce(DoAll(SetArgPointee<1>(200), Return(CURLE_OK)));
-
-    EXPECT_CALL(*curlWrapper, curlGlobalCleanup());
-    client.sendMessageWithIDAndRole("/authenticate/endpoint/",Common::HttpRequests::RequestType::POST,requestHeaders);
+    client.sendMessageWithIDAndRole("/authenticate/endpoint/",Common::HttpRequests::RequestType::PUT,requestHeaders);
     curl_slist_free_all(curlHeaders);
-
 }
