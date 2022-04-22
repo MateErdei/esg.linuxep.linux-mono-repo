5c45af4b8301303f774b01aff89d13a80176e960
Alex Cooper <Alex.Cooper@Sophos.com>
2022-04-22 14:42:35 +0000

    [base] Pull request #1593: Feature/base improvements fixes
    Merge in LINUXEP/everest-base from feature/base_improvements_fixes to develop
    
    Squashed commit of the following:
    
    commit f2b6d4652aa411d55165d06cf8b5dfbdead1bb4f
    Author: Alex Cooper <alex.cooper@sophos.com>
    Date:   Fri Apr 22 13:50:13 2022 +0100
    
        [LINUXDAR-4116] replace SDDS3 builder non-trivial command line call with simple script to make sure that the sha256 cmd is evaluated correctly.
    
    commit 450c8c5a6a8a1928c5010950f641f815c5f58528
    Author: Alex Cooper <alex.cooper@sophos.com>
    Date:   Fri Apr 22 13:01:42 2022 +0100
    
        [LINUXDAR-4116] revert changes to plugin api tar, log4cplu missing from tar, I suspect copy_if_different was not working as advertised. Locally the right files are being output, commiting now to check CI build.

---
+++ base/PostInstall/RunSdds3Builder.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ex
+SDDS3_BUILDER=$1
+PACKAGE_DIR=$2
+SDDS_IMPORT=$3
+NONCE=$(sha256sum $SDDS_IMPORT | head -c 10)
+echo $SDDS3_BUILDER --build-package --package-dir "$PACKAGE_DIR" --sdds-import "$SDDS_IMPORT" --nonce $NONCE
+$SDDS3_BUILDER --build-package --package-dir "$PACKAGE_DIR" --sdds-import "$SDDS_IMPORT" --nonce $NONCE
