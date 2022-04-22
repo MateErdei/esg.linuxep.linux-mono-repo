c4bd31a42b9f6417f8dd2e225e097f1a9fb39c00
trevormcging <trevor.mcging@sophos.com>
2022-04-22 08:26:58 +0100

    [base] [LINUXDAR-4274] fixing output libraries for central registration as artifacts from base build

--- base/build.sh
+++ base/build.sh
@@ -268,7 +268,7 @@ function build()
     then
         if (( DELETE_GCC == 1 ))
         then
-            rm -f ${INPUT}/gcc-*.tar.gz ${INPUT}/cmake-*.tar.gz
+            rm -f ${INPUT}/gcc-*.tar.gz ${INPUT}/cmake
         fi
 
         unpack_scaffold_gcc_make "$INPUT"
@@ -279,7 +279,7 @@ function build()
         local GCC_TAR=$(ls $INPUT/gcc-*.tar.gz)
         if [[ -f ${GCC_TAR} ]]
         then
-          untar_input gcc-8.1.0-linux
+          untar_input gcc-11.2.0-linux
         else
           echo "**** NO GCC tar so not unpacking"
           ls $INPUT/gcc-*.tar.gz || true
@@ -295,10 +295,9 @@ function build()
             exitFailure 12 "Failed to find openssl"
         fi
 
-        local CMAKE_TAR=$(ls $INPUT/cmake-*.tar.gz)
-        if [[ -f "$CMAKE_TAR" ]]
+        if [[ -f "$INPUT/cmake/bin/cmake" && ! -d $REDIST/cmake ]]
         then
-            tar xzf "$CMAKE_TAR" -C "$REDIST"
+            ln -sf $INPUT/cmake $REDIST/cmake
         else
             echo "WARNING: using system cmake"
         fi
@@ -306,6 +305,7 @@ function build()
         untar_input versig
         untar_input curl
         untar_input SUL
+        untar_input sdds3lib
         untar_input boost
         # TODO LINUXDAR-1506: remove the patching when the related issue is incorporated into the released version of boost
         # https://github.com/boostorg/process/issues/62
@@ -335,6 +335,7 @@ function build()
         untar_input python-urllib3
         untar_input pycryptodome
         untar_input $GOOGLETESTTAR
+        cp -r ${INPUT}/sophlib "$REDIST"
         cp -r ${INPUT}/sdds3 "$REDIST"/sdds3 && chmod +x "$REDIST"/sdds3/*
 
 
@@ -366,8 +367,10 @@ function build()
         fi
     fi
 
-    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${REDIST}/openssl/lib${BITS}:${REDIST}/curl/lib64:${REDIST}/log4cplus/lib:${REDIST}/zeromq/lib:${REDIST}/protobuf/install${BITS}/lib
-    export PATH=${REDIST}/cmake/bin:${REDIST}/protobuf/install${BITS}/bin:${PATH}
+    export LD_LIBRARY_PATH=${REDIST}/gcc/lib64:${REDIST}/openssl/lib${BITS}:${REDIST}/curl/lib64:${REDIST}/log4cplus/lib:${REDIST}/zeromq/lib:${REDIST}/protobuf/install${BITS}/lib:${LD_LIBRARY_PATH}
+    export PATH=${REDIST}/gcc/bin:${REDIST}/cmake/bin:${REDIST}/protobuf/install${BITS}/bin:${PATH}
+    chmod 700 $REDIST/cmake/bin/cmake || exitFailure "Unable to chmod cmake"
+    chmod 700 $REDIST/cmake/bin/ctest || exitFailure "Unable to chmod ctest"
     cp -r $REDIST/$GOOGLETESTTAR $BASE/tests/googletest
 
 
@@ -423,7 +426,7 @@ function build()
 
     [[ -n ${NPROC:-} ]] || { which nproc > /dev/null 2>&1 && NPROC=$((`nproc`)); } || NPROC=2
     (( $NPROC < 1 )) && NPROC=1
-    cmake -v \
+    cmake \
         -DPRODUCT_NAME="${PRODUCT_NAME}" \
         -DPRODUCT_LINE_ID="${PRODUCT_LINE_ID}" \
         -DDEFAULT_HOME_FOLDER="${DEFAULT_HOME_FOLDER}" \
@@ -492,6 +495,7 @@ function build()
     cp -a build${BITS}/SDDS3-PACKAGE output/SDDS3-PACKAGE || exitFailure 21 "Failed to copy SDDS3-PACKAGE: $?"
     cp -a build${BITS}/distribution/SDDS-Import.xml output/SDDS3-PACKAGE || exitFailure 21 "Failed to copy SDDS-Import.xml to SDDS3-PACKAGE: $?"
     cp -a build${BITS}/products/PluginApi/pluginapi.tar.gz output/pluginapi.tar.gz || exitFailure 22 "Failed to copy pluginapi.tar.gz package: $?"
+    cp -a build${BITS}/products/CentralRegistrationApi/centralregistrationapi.tar output/centralregistrationapi.tar || exitFailure 22 "Failed to copy centralregistrationapi.tar.gz package: $?"
     cp -a build${BITS}/products/cmcsrouter/mcs.tar.gz output/mcs.tar.gz || exitFailure 22 "Failed to copy mcs.tar.gz package: $?"
     pushd build${BITS}
     tar -zcvf ../output/SystemProductTestOutput.tar.gz SystemProductTestOutput/ || exitFailure 23 "Failed to tar SystemProductTestOutput package: $?"
