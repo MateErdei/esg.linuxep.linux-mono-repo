b0774f0e1d5e1ab82da583adede2c641d9b64602
Douglas Leeder <douglas.leeder@sophos.com>
2022-04-29 13:39:33 +0100

    [av] [LINUXDAR-4711] Stop trying to upload robot.xml which doesn't exist.

--- av/pipeline/aws-runner/testAndSendResults.sh
+++ av/pipeline/aws-runner/testAndSendResults.sh
@@ -18,7 +18,6 @@ UPLOAD_RESULT=0
 aws s3 cp $TEST_SCRIPTS/log.html s3://sspl-testbucket/test-results/$STACKNAME/$HOSTNAME-log.html && \
     aws s3 cp $TEST_SCRIPTS/output.xml s3://sspl-testbucket/test-results/$STACKNAME/$HOSTNAME-output.xml && \
     aws s3 cp $TEST_SCRIPTS/report.html s3://sspl-testbucket/test-results/$STACKNAME/$HOSTNAME-report.html && \
-    aws s3 cp $TEST_SCRIPTS/robot.xml s3://sspl-testbucket/test-results/$STACKNAME/$HOSTNAME-robot.xml && \
     aws s3 cp /tmp/cloudFormationInit.log s3://sspl-testbucket/test-results/$STACKNAME/$HOSTNAME-cloudFormationInit.log || UPLOAD_RESULT=0
 
 
