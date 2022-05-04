0a3c71b07c318b02620ab9920965c49f99c0744b
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2022-05-04 15:47:32 +0100

    [LINUXDAR-4777] update component version

--- thininstaller/pipeline/pipeline.py
+++ thininstaller/pipeline/pipeline.py
@@ -1,9 +1,14 @@
 import tap.v1 as tap
-
-
+import xml.etree.ElementTree as ET
+def get_package_version():
+    """ Read version from package.xml """
+    package_path = './release-package.xml'
+    package_tree = ET.parse(package_path)
+    package_node = package_tree.getroot()
+    return package_node.attrib['version']
 @tap.pipeline(version=1, component='sspl-thininstaller')
 def sspl_thininstaller(stage: tap.Root, parameters: tap.Parameters):
-    component = tap.Component(name='sspl-thininstaller', base_version='1.1.3')
+    component = tap.Component(name='sspl-thininstaller', base_version=get_package_version())
     mode = parameters.mode or 'release'
 
     #export TAP_PARAMETER_MODE=release|analysis|coverage*(requires bullseye)
