f563d69d8954ea570029f74d509a6469c3e79f0c
Jake McLanaghan <jake.mclanaghan@sophos.com>
2022-05-09 15:31:07 +0100

    [spl-tools] [LINUXDAR-4777] remove debug comment

--- spl-tools/jenkins/generateReleaseInputs.py
+++ spl-tools/jenkins/generateReleaseInputs.py
@@ -87,7 +87,6 @@ def process_release_files(release_files):
                     input_dict["artifacts"].append(input_pkg['include'])
                 elif type(input_pkg['include']) is list:
                     for artifact in input_pkg['include']:
-                        print(f"jake3: {artifact}")
                         input_dict["artifacts"].append(artifact)
 
                 print(input_dict["name"])
