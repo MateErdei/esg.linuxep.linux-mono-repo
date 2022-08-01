04b7f7251cd69e12d47efe60dddf5a36a9d09ae0
DarrenSmith <darren.smith@sophos.com>
2022-08-01 11:48:28 +0100

    [av] [LINUXDAR-5312] Use Gmock 1.10+ mocking style

--- av/tests/datatypes/MockSysCalls.h
+++ av/tests/datatypes/MockSysCalls.h
@@ -13,19 +13,19 @@ namespace
     class MockSystemCallWrapper : public datatypes::ISystemCallWrapper
     {
     public:
-        MOCK_METHOD0(getSystemUpTime, std::pair<const int, const long>());
-        MOCK_METHOD3(_ioctl, int(int __fd, unsigned long int __request, char* buffer));
-        MOCK_METHOD2(_statfs, int(const char *__file, struct ::statfs *__buf));
-        MOCK_METHOD3(_open, int(const char *__file, int __oflag, mode_t mode));
-        MOCK_METHOD6(pselect, int(int __nfds,
-                                  fd_set *__restrict __readfds,
-                                  fd_set *__restrict __writefds,
-                                  fd_set *__restrict __exceptfds,
-                                  const struct timespec *__restrict __timeout,
-                                  const __sigset_t *__restrict __sigmask));
-        MOCK_METHOD4(ppoll, int(struct pollfd* fd,
-                          nfds_t num_fds,
-                          const struct timespec* timeout,
-                          const __sigset_t* ss));
+        MOCK_METHOD((std::pair<const int, const long>), getSystemUpTime, ());
+        MOCK_METHOD(int, _ioctl, (int __fd, unsigned long int __request, char* buffer));
+        MOCK_METHOD(int, _statfs, (const char *__file, struct ::statfs *__buf));
+        MOCK_METHOD(int, _open, (const char *__file, int __oflag, mode_t mode));
+        MOCK_METHOD(int, pselect, (int __nfds,
+                                   fd_set *__restrict __readfds,
+                                   fd_set *__restrict __writefds,
+                                   fd_set *__restrict __exceptfds,
+                                   const struct timespec *__restrict __timeout,
+                                   const __sigset_t *__restrict __sigmask));
+        MOCK_METHOD(int, ppoll, (struct pollfd* fd,
+                                 nfds_t num_fds,
+                                 const struct timespec* timeout,
+                                 const __sigset_t* ss));
     };
 }
