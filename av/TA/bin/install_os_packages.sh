1b4c3854d26df758718d23fcb768291e16eab091
Douglas Leeder <douglas.leeder@sophos.com>
2022-08-03 12:26:42 +0100

    [av] [LINUXDAR-4543] Install gdb for tests.

--- av/TA/bin/install_os_packages.sh
+++ av/TA/bin/install_os_packages.sh
@@ -8,7 +8,7 @@ then
     # Retry 10 times before timeout
     for (( i=0; i<10; i++ ))
     do
-       if DEBIAN_FRONTEND=noninteractive apt-get install -y nfs-kernel-server zip unzip samba p7zip-full
+       if DEBIAN_FRONTEND=noninteractive apt-get install -y nfs-kernel-server zip unzip samba p7zip-full gdb
        then
           echo "Installation succeeded"
           break
@@ -61,7 +61,7 @@ then
             exit $EXIT
         fi
     }
-    yum install -y "gcc" "gcc-c++" "make" nfs-utils zip samba p7zip
+    yum install -y "gcc" "gcc-c++" "make" nfs-utils zip samba p7zip gdb
 else
     echo "Can't find package management system"
     exit 1
