b0ae523077d451842388c6eae27bdd30002e68ee
TimPickersgill <tim.pickersgill@sophos.com>
2022-08-05 17:34:01 +0100

    [av] Correct the logger name.

--- av/modules/sophos_on_access_process/onaccessimpl/Logger.cpp
+++ av/modules/sophos_on_access_process/onaccessimpl/Logger.cpp
@@ -1,14 +1,11 @@
-/******************************************************************************************************
+// Copyright 2022, Sophos Limited.  All rights reserved.
 
-Copyright 2022, Sophos Limited.  All rights reserved.
-
-******************************************************************************************************/
 #include "Logger.h"
 
 #include <Common/Logging/LoggerConfig.h>
 
 log4cplus::Logger& getOnAccessImplLogger()
 {
-    static log4cplus::Logger STATIC_LOGGER = Common::Logging::getInstance("OnAccessConfig");
+    static log4cplus::Logger STATIC_LOGGER = Common::Logging::getInstance("OnAccessImpl");
     return STATIC_LOGGER;
 }
