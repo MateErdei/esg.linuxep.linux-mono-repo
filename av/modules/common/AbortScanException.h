69f4fdca9d329b4e3953eab4c4255b3c347fe5c5
DarrenSmith <darren.smith@sophos.com>
2022-08-05 18:01:36 +0100

    [av] [LINUXDAR-5322] Revert unintentional deletion of AbortScanException.h

---
+++ av/modules/common/AbortScanException.h
@@ -0,0 +1,20 @@
+/******************************************************************************************************
+
+Copyright 2020, Sophos Limited.  All rights reserved.
+
+******************************************************************************************************/
+
+#pragma once
+
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+namespace common
+{
+    class AbortScanException : virtual public std::runtime_error
+    {
+    public:
+        explicit AbortScanException(const std::string& errorMsg) : std::runtime_error(errorMsg) {}
+    };
+}
