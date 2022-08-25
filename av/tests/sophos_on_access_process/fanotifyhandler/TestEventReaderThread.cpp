ef095ba7efdd65090c9ddc118689be55c5d58e3c
Peter.C <peter.c@sophos.com>
2022-08-25 11:23:24 +0100

    [av] [LINUXDAR-5404]: Add test which reproduced the issue.

--- av/tests/sophos_on_access_process/fanotifyhandler/TestEventReaderThread.cpp
+++ av/tests/sophos_on_access_process/fanotifyhandler/TestEventReaderThread.cpp
@@ -327,3 +327,40 @@ TEST_F(TestEventReaderThread, TestReaderSkipsEventsInPluginLogDir)
     EXPECT_TRUE(waitForLog("Skip event caused by soapd"));
     EXPECT_EQ(scanRequestQueue->size(), 0);
 }
+
+TEST_F(TestEventReaderThread, TestReaderDoesntInvalidateFd)
+{
+    UsingMemoryAppender memoryAppenderHolder(*this);
+    auto scanRequestQueue = std::make_shared<ScanRequestQueue>();
+    int fanotifyFD = 123;
+    struct fanotify_event_metadata metadata = {
+        .event_len = FAN_EVENT_METADATA_LEN, .vers = FANOTIFY_METADATA_VERSION, .reserved = 0, .metadata_len = FAN_EVENT_METADATA_LEN,
+        .mask = FAN_CLOSE_WRITE, .fd = 345, .pid = 2345 };
+
+    struct pollfd fds1[2]{};
+    fds1[1].revents = POLLIN;
+    struct pollfd fds2[2]{};
+    fds2[0].revents = POLLIN;
+    EXPECT_CALL(*m_mockSysCallWrapper, ppoll(_, 2, _, nullptr))
+        .WillOnce(DoAll(SetArrayArgument<0>(fds1, fds1+2), Return(1)))
+        .WillOnce(DoAll(SetArrayArgument<0>(fds2, fds2+2), Return(1)));
+    EXPECT_CALL(*m_mockSysCallWrapper, read(fanotifyFD, _, _)).WillOnce(DoAll(
+        Invoke([metadata] (int, void* arg2, size_t) { *static_cast<struct fanotify_event_metadata*>(arg2) = metadata; }),
+        Return(sizeof(metadata))));
+    const char* filePath = "/tmp/test";
+    EXPECT_CALL(*m_mockSysCallWrapper, readlink(_, _, _)).WillOnce(DoAll(SetArrayArgument<1>(filePath, filePath + strlen(filePath) + 1), Return(strlen(filePath) + 1)));
+    struct ::stat statbuf{};
+    statbuf.st_uid = 1;
+    EXPECT_CALL(*m_mockSysCallWrapper, _stat(_, _)).WillOnce(DoAll(SetArgPointee<1>(statbuf), Return(0)));
+
+
+    auto eventReader = std::make_shared<EventReaderThread>(fanotifyFD, m_mockSysCallWrapper, m_pluginInstall, scanRequestQueue);
+    common::ThreadRunner eventReaderThread(eventReader, "eventReader", true);
+
+    EXPECT_TRUE(waitForLog("got event: size "));
+    EXPECT_TRUE(waitForLog("Stopping the reading of Fanotify events"));
+    eventReaderThread.requestStopIfNotStopped();
+
+    EXPECT_EQ(scanRequestQueue->size(), 1);
+    EXPECT_NE(scanRequestQueue->pop().second->fd(), -1);
+}
