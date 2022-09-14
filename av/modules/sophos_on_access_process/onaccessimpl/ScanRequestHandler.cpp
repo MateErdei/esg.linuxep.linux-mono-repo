047f6c38b38edae0f13519388351db010e174aee
efthymios.makaras <efthymios.makaras@sophos.com>
2022-09-14 10:21:07 +0100

    [av] [LINUXDAR-5629] Calculate scan duration in on-access and print it in TRACE

--- av/modules/sophos_on_access_process/onaccessimpl/ScanRequestHandler.cpp
+++ av/modules/sophos_on_access_process/onaccessimpl/ScanRequestHandler.cpp
@@ -3,85 +3,109 @@
 #include "ScanRequestHandler.h"
 
 #include "Logger.h"
-
-#include "common/SaferStrerror.h"
-#include "common/StringUtils.h"
 #include "unixsocket/threatDetectorSocket/ScanningClientSocket.h"
 
-#include <memory>
+#include "common/PathUtils.h"
+#include "common/StringUtils.h"
+#include "datatypes/sophos_filesystem.h"
+
 #include <sstream>
+#include <fcntl.h>
+#include <chrono>
 
 using namespace sophos_on_access_process::onaccessimpl;
+namespace fs = sophos_filesystem;
 
 ScanRequestHandler::ScanRequestHandler(
    ScanRequestQueueSharedPtr scanRequestQueue,
-    std::shared_ptr<unixsocket::IScanningClientSocket> socket,
-    fanotifyhandler::IFanotifyHandlerSharedPtr fanotifyHandler)
-    : m_scanRequestQueue(std::move(scanRequestQueue))
-    , m_socket(std::move(socket))
-    , m_fanotifyHandler(std::move(fanotifyHandler))
+    std::shared_ptr<unixsocket::IScanningClientSocket> socket)
+    : m_scanRequestQueue(scanRequestQueue)
+    , m_socket(socket)
 {
+
+}
+
+std::string ScanRequestHandler::failedToOpen(const int error)
+{
+    switch (error)
+    {
+        case EACCES:
+            return "Failed to open as permission denied: ";
+        case ENAMETOOLONG:
+            return "Failed to open as the path is too long: ";
+        case ELOOP:
+            return "Failed to open as couldn't follow the symlink further: ";
+        case ENODEV:
+            return "Failed to open as path is a device special file and no corresponding device exists: ";
+        case ENOENT:
+            return "Failed to open as path is a dangling symlink or a directory component is missing: ";
+        case ENOMEM:
+            return "Failed to open due to a lack of kernel memory: ";
+        case EOVERFLOW:
+            return "Failed to open as the file is too large: ";
+        default:
+            return "Failed to open with error " + std::to_string(error) + ": ";
+    }
 }
 
 void ScanRequestHandler::scan(
-    const scan_messages::ClientScanRequestPtr& scanRequest,
+    scan_messages::ClientScanRequestPtr scanRequest,
     const struct timespec& retryInterval)
 {
+    std::string fileToScanPath = scanRequest->getPath();
+
     ScanResponse response;
     try
     {
-        if (!m_socketWrapper)
-        {
-            m_socketWrapper = std::make_unique<ClientSocketWrapper>(*m_socket, m_notifyPipe, retryInterval);
-        }
-        response = m_socketWrapper->scan(scanRequest);
+        ClientSocketWrapper socketWrapper(*m_socket, m_notifyPipe, retryInterval);
+        response = socketWrapper.scan(scanRequest);
     }
     catch (const ScanInterruptedException& e)
     {
         LOGWARN("Scan aborted: " << e.what());
-        throw;
     }
     catch (const std::exception& e)
     {
-        std::string fileToScanPath(common::escapePathForLogging(scanRequest->getPath()));
         LOGERROR("Failed to scan " << fileToScanPath << " : " << e.what());
-        m_socketWrapper.reset();
         return;
     }
 
     std::string errorMsg = common::toUtf8(response.getErrorMsg());
-    if (!errorMsg.empty())
+    if (response.getDetections().empty())
     {
-        LOGERROR(errorMsg);
-    }
-
-    auto detections = response.getDetections();
-    if (detections.empty())
-    {
-        if (errorMsg.empty() && scanRequest->isOpenEvent())
+        if (!errorMsg.empty())
+        {
+            LOGERROR(errorMsg);
+        }
+        else
         {
             // Clean file
-            int ret = m_fanotifyHandler->cacheFd(scanRequest->getFd(), scanRequest->getPath());
-            if (ret < 0)
-            {
-                int error = errno;
-                std::ignore = error; // Fuzz builds compile out LOGDEBUG
-                std::string escapedPath(common::escapePathForLogging(scanRequest->getPath()));
-                LOGWARN("Caching " << escapedPath << " failed: " << common::safer_strerror(error));
-            }
-
         }
     }
     else
     {
-        auto scanType = scan_messages::getScanTypeAsStr(scanRequest->getScanType());
+        if (!errorMsg.empty())
+        {
+            LOGERROR(errorMsg);
+        }
 
-        for(const auto& detection : detections)
+        for(const auto& detection : response.getDetections())
         {
             std::string escapedPath(common::escapePathForLogging(detection.path));
             std::string threatName = detection.name;
 
-            LOGWARN("Detected \"" << escapedPath << "\" is infected with " << threatName << " (" << scanType << ")");
+            fs::file_status symlinkStatus = fs::symlink_status(common::PathUtils::removeForwardSlashFromPath(detection.path));
+            if (fs::is_symlink(symlinkStatus))
+            {
+                std::string escapedTargetPath(common::escapePathForLogging(fs::canonical(detection.path)));
+                LOGWARN(
+                    "Detected \"" << escapedPath << "\" (symlinked to " << escapedTargetPath << ") is infected with "
+                                  << threatName);
+            }
+            else
+            {
+                LOGWARN("Detected \"" << escapedPath << "\" is infected with " << threatName);
+            }
         }
     }
 }
@@ -90,23 +114,20 @@ void ScanRequestHandler::run()
 {
     announceThreadStarted();
 
-    LOGDEBUG("Starting ScanRequestHandler");
-    try
+    LOGDEBUG("Entering Main Loop");
+    while (!stopRequested())
     {
-        while (!stopRequested())
+        auto queueItem = m_scanRequestQueue->pop();
+        if(queueItem)
         {
-            auto queueItem = m_scanRequestQueue->pop();
-            if (queueItem)
-            {
-                scan(queueItem);
-            }
+            std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+            scan(queueItem);
+
+            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+            long scanDuration = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+
+            std::string escapedPath(common::escapePathForLogging(queueItem->getPath()));
+            LOGTRACE("Scan for " << escapedPath << "completed in " << scanDuration << "ms");
         }
     }
-    catch (const ScanInterruptedException& e)
-    {
-        // Aborting the scan thread - stop requested while scan waiting for response
-        // Already logged
-    }
-    m_socketWrapper.reset();
-    LOGDEBUG("Finished ScanRequestHandler");
 }
