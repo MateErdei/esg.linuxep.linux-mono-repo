60e439814daa9392e6678e29f6734acbed4e61bc
Peter.Clark <peter.c@sophos.com>
2022-09-20 12:03:36 +0100

    [av] Add 3 x robot test to check caching is working.
    - Open without detections is cached
    - Open with detections is not cached
    - Close without detections is not cached

--- av/TA/Libs/FileUtils.py
+++ av/TA/Libs/FileUtils.py
@@ -89,10 +89,8 @@ def set_old_timestamps(directory):
             os.utime(os.path.join(d, f), times=(atime, mtime))
 
 def generate_only_open_event(path):
-    assert os.path.isfile(path), f"path doesnt exist: {path}"
-    open(path, 'r')
+    assert os.path.isfile(path)
+    f = open(path, 'r')
 
-def copy_file_no_temp_directory(src, destdir):
-    if not os.path.exists(destdir):
-        os.makedirs(destdir)
-    shutil.copy2(src, destdir)
+def copy_file_no_temp_directory(src, dest):
+    shutil.copy2(src, dest)
