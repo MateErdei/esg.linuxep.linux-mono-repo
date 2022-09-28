571d2aff3ff2679eeef6f94c24ff9512cc4f3fa7
Peter.C <peter.c@sophos.com>
2022-09-28 12:22:24 +0100

    [av] [LINUXDAR-5452] - Re introduce E_SCAN_TYPE_ON_ACCESS as a scantype we can send to central. ON_ACCESS_OPEN and ON_ACCESS_CLOSE kept for internal use.

--- av/modules/scan_messages/ScanType.h
+++ av/modules/scan_messages/ScanType.h
@@ -7,11 +7,14 @@ namespace scan_messages
     enum E_SCAN_TYPE : int
     {
         E_SCAN_TYPE_UNKNOWN = 200,
-        E_SCAN_TYPE_ON_ACCESS_OPEN = 201,
-        E_SCAN_TYPE_ON_ACCESS_CLOSE = 202,
+        E_SCAN_TYPE_ON_ACCESS = 201,
         E_SCAN_TYPE_ON_DEMAND = 203,
         E_SCAN_TYPE_SCHEDULED = 205,
-        E_SCAN_TYPE_MEMORY = 206
+        E_SCAN_TYPE_MEMORY = 206,
+
+        //For products internal use
+        E_SCAN_TYPE_ON_ACCESS_OPEN = 207,
+        E_SCAN_TYPE_ON_ACCESS_CLOSE = 208
     };
 }
 
