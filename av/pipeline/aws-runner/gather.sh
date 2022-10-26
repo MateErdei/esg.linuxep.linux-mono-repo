2530d66a64cf7eff8d2ddc6d411081976068ea61
Alex Cooper <alex.cooper@sophos.com>
2022-10-26 09:03:44 +0100

    [av] [LINUXDAR-5675] Add tap_test_output.tar.gz to AV AWS tests manually as pipeline inputs don't seem to be available. #3

--- av/pipeline/aws-runner/gather.sh
+++ av/pipeline/aws-runner/gather.sh
@@ -57,6 +57,7 @@ chmod 700 "$AV/base-sdds/install.sh"
 rm -rf "$AV/test-resources"
 $COPY "$OUTPUT/test-resources"  "$AV/"
 
+mkdir -p "$INPUTS/tap_test_output_from_build/"
 rm -rf "$INPUTS/tap_test_output_from_build/tap_test_output.tar.gz"
 $COPY "$OUTPUT/tap_test_output.tar.gz"  "$INPUTS/tap_test_output_from_build/"
 
