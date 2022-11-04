7b4528d5cbdf05b0ab1558ddb090e4a1f938ed2e
Alex Cooper <alex.cooper@sophos.com>
2022-11-04 03:06:31 +0000

    [LINUXDAR-6005] Fix SDDS3 thin installer so that the MCS cert that is shipped with the thin installer is passed through to the register central code.

--- thininstaller/installer.cpp
+++ thininstaller/installer.cpp
@@ -420,18 +420,13 @@ int main(int argc, char** argv)
         auto thisBinary = fs->readlink("/proc/self/exe");
         if (thisBinary.has_value())
         {
-            auto thisDir = Common::FileSystem::dirName(thisBinary.value());
-            mcsRootCert = Common::FileSystem::join(thisDir,"..", "mcs_rootca.crt");
-            if (!fs->isFile(mcsRootCert))
-            {
-                logError("Shipped MCS cert cannot be found here: " + mcsRootCert);
-                return 55;
-            }
+            auto thisDir = Common::FileSystem::basename(thisBinary.value());
+            mcsRootCert = Common::FileSystem::join(thisDir,"mcs_rootca.crt");
             logDebug("Using shipped MCS cert: " + mcsRootCert);
         }
         else
         {
-            return 56;
+            return 55;
         }
 
         MCS::ConfigOptions rootConfigOptions = CentralRegistration::innerCentralRegistration(registerArgValues, mcsRootCert);
