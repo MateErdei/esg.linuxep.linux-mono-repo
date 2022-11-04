ef00fd846ae927928e081e24ab9da08201346f0c
Alex Cooper <alex.cooper@sophos.com>
2022-11-04 03:10:02 +0000

    [LINUXDAR-6005] Fix thin installer build script to work with no native cmake installed and correctly use the unpacked version.

--- thininstaller/buildLinux.sh
+++ thininstaller/buildLinux.sh
@@ -165,7 +165,6 @@ function build()
     installer_dir=$BASE/installer
     output=$BASE/output
     pushd ${BASE}/build
-    chmod +x "$INPUT/cmake/bin/cmake"
     "$INPUT/cmake/bin/cmake" -DREDIST="${REDIST}" -DINSTALLERDIR="${installer_dir}" -DOUTPUT="${output}" ..
     make || exitFailure 15 "Failed to build thininstaller"
     make copyInstaller || exitFailure $FAILED_TO_COPY_INSTALLED "Failed to copy installer"
