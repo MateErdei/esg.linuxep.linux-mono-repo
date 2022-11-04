57acfb5b685a15f5b8d1d8475ad1aef8a6de8c3f
TimPickersgill <tim.pickersgill@sophos.com>
2022-11-04 13:51:00 +0000

    [av] Simplify unit test / google test includes.

--- av/buildconfig/SophosAddTest.cmake
+++ av/buildconfig/SophosAddTest.cmake
@@ -31,8 +31,26 @@ macro(SophosAddTest TARGET)
     # setup the google test target
     add_executable(${TARGET} ${INPUT_FILES})
 
-    target_include_directories(${TARGET} SYSTEM BEFORE PUBLIC ${GMOCK_INCLUDE} ${GTEST_INCLUDE})
-    target_link_libraries(${TARGET} PUBLIC ${GTEST_LIBRARY} ${GTEST_MAIN_LIBRARY} ${GMOCK_LIBRARY} pthread)
+    target_include_directories(${TARGET}
+            PUBLIC
+            ${CMAKE_SOURCE_DIR}
+            ${CMAKE_SOURCE_DIR}/modules
+            ${CMAKE_SOURCE_DIR}/tests
+            )
+
+    target_include_directories(${TARGET}
+            SYSTEM BEFORE PUBLIC
+            ${GTEST_INCLUDE}
+            ${GMOCK_INCLUDE}
+            )
+
+    target_link_libraries(${TARGET}
+            PUBLIC
+            ${GTEST_LIBRARY}
+            ${GTEST_MAIN_LIBRARY}
+            ${GMOCK_LIBRARY}
+            pthread
+            )
 
     if (AddTest_PROJECTS)
         target_link_libraries(${TARGET} PUBLIC ${AddTest_PROJECTS})
@@ -45,6 +63,7 @@ macro(SophosAddTest TARGET)
     if (AddTest_LIBS)
         target_link_libraries(${TARGET} PUBLIC ${AddTest_LIBS})
     endif ()
+
     gtest_discover_tests(${TARGET})
 
     add_dependencies(${TARGET} copy_libs)
