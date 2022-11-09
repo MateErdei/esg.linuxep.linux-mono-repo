aa07064e03f9b87375f4d5248f745f063aa8ddd7
mollie.bourke <mollie.bourke@sophos.com>
2022-11-10 09:59:41 +1100

    [av] [LINUXDAR-5790] Add telemetry keys to SafeStoreTelemetryConsts.h

---
+++ av/modules/safestore/SafeStoreTelemetryConsts.h
@@ -0,0 +1,9 @@
+// Copyright 2022, Sophos Limited.  All rights reserved.
+
+namespace safestore
+{
+    const char* const telemetrySafeStoreHealth = "health";
+    const char* const telemetrySafeStoreDormantMode = "dormant-mode";
+
+    const char* const telemetrySafeStoreDatabaseSize = "database-size";
+}
