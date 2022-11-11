0ce6e4e68ac51f3b94e284e1d5fcf8dd49e660c4
TimPickersgill <tim.pickersgill@sophos.com>
2022-11-11 14:01:19 +0000

    [av] TA vagrant setup script

---
+++ av/TA/bin/setupVagrant.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+set -e
+[[ -d /vagrant ]]
+(( UID == 0 )) || exec sudo -H "$0" "$@"
+mkdir -p /opt/test/inputs/av/
+ln -snf /vagrant/sspl-plugin-anti-virus/TA/ /opt/test/inputs/test_scripts
+ln -snf /vagrant/sspl-plugin-anti-virus/output/SDDS-COMPONENT/ /opt/test/inputs/av/SDDS-COMPONENT
+ln -snf /vagrant/sspl-plugin-anti-virus/output/test-resources/ /opt/test/inputs/av/test-resources
+ln -snf /vagrant/sspl-plugin-anti-virus/output/base-sdds/ /opt/test/inputs/av/base-sdds
+apt-get -y install python3.7 python3-pip
+apt-get -y install nfs-kernel-server samba p7zip-full
+python3.7 -m pip install pyzmq protobuf pytest-html robotframework
