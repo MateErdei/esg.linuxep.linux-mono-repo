3944b8fe8bab20e08448b4cc7d9294259da85b49
Peter.Clark <peter.c@sophos.com>
2022-11-28 16:56:18 +0000

    [av] [LINUXDAR-5978] - Add list of filesystems to exclude that arent covered by the system list

--- av/modules/sophos_on_access_process/soapd_bootstrap/OnAccessProductConfigDefaults.h
+++ av/modules/sophos_on_access_process/soapd_bootstrap/OnAccessProductConfigDefaults.h
@@ -1,17 +1,49 @@
 // Copyright 2022, Sophos Limited.  All rights reserved.
 
+#pragma once
+
 #include <stddef.h>
+
+#include <unordered_set>
+#include <string>
+
+
 
 namespace sophos_on_access_process::OnAccessConfig
 {
-    const unsigned long int onAccessProcessFdLimit = 1048576;
+   //Event Reading Queue
+   const unsigned long int onAccessProcessFdLimit = 1048576;
 
-    const size_t maxAllowedQueueSize = 1048000;
-    const size_t minAllowedQueueSize = 1000;
+   const size_t maxAllowedQueueSize = 1048000;
+   const size_t minAllowedQueueSize = 1000;
+   const size_t defaultMaxScanQueueSize = 4000;
+
+    //FileSystem
+    static const std::unordered_set<std::string> FILE_SYSTEMS_TO_EXCLUDE
+    {
+           "aufs",
+           "autofs",
+           "bpf",
+           "efivarfs",
+           "fuse.gvfs",
+           "fuse.vmware-vmblock",
+           "inotifyfs",
+           "nssadmin",
+           "nsfs",
+           "nsspool",
+           "romfs",
+           "rootfs",
+           "subfs",
+           "usbdevfs",
+    };
+
+    //Performance
+    const bool defaultDumpPerfData = false;
+
+    //Scanning Threads
     const int maxAllowedScanningThreads = 100;
     const int minAllowedScanningThreads = 1;
-
-    const size_t defaultMaxScanQueueSize = 4000;
     const int defaultScanningThreads = 10;
-    const bool defaultDumpPerfData = false;
+
+
 }
