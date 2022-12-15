f86e87bd15085b8ab4c7cbe16c1ea1a8ae3a4df4
Peter.C <peter.c@sophos.com>
2022-12-15 10:01:16 +0000

    [av] [LINUXDAR-5863] - Missed change for fuzzing

--- av/tests/sophos_on_access_process/OnAccessConfigReaderFuzzer.cpp
+++ av/tests/sophos_on_access_process/OnAccessConfigReaderFuzzer.cpp
@@ -21,7 +21,8 @@ Copyright 2022, Sophos Limited.  All rights reserved.
 static int fuzzConfigProcessor(const uint8_t *Data, size_t Size)
 {
     std::string fuzzString(reinterpret_cast<const char*>(Data), Size);
-    sophos_on_access_process::OnAccessConfig::parseOnAccessPolicySettingsFromJson(fuzzString);
+    sophos_on_access_process::OnAccessConfig::OnAccessConfiguration oaConfig;
+    sophos_on_access_process::OnAccessConfig::parseOnAccessPolicySettingsFromJson(fuzzString, oaConfig);
 
     return 0;
 }
