09ebd2d4f9b21e3084f56784a93f36975f7d890e
Peter.C <peter.c@sophos.com>
2023-01-10 12:35:17 +0000

    [av] [LINUXDAR-6030] - MockShutdownTimer now has a default for call to timeout

--- av/tests/sophos_threat_detector/sophosthreatdetectorimpl/MockShutdownTimer.h
+++ av/tests/sophos_threat_detector/sophosthreatdetectorimpl/MockShutdownTimer.h
@@ -6,12 +6,23 @@
 
 #include <gmock/gmock.h>
 
+using namespace ::testing;
+
 namespace
 {
     class MockShutdownTimer : public threat_scanner::IScanNotification
     {
     public:
+        MockShutdownTimer(time_t timeout = 0)
+        {
+            m_timeout = timeout;
+
+            ON_CALL(*this, timeout).WillByDefault(Return(m_timeout));
+        }
+
         MOCK_METHOD(void, reset, ());
         MOCK_METHOD(time_t, timeout, ());
+    private:
+        time_t m_timeout;
     };
 }
