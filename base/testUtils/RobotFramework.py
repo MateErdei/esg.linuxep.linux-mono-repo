a4d3a2068069de25a52344ec31108c99323c6b93
Casimir Overell <casimir.overell@sophos.com>
2023-01-10 16:28:38 +0000

    [base] [BUGFIX] Got keyerror for os.environ check on bullseye machine -- making dict check explicit.

--- base/testUtils/RobotFramework.py
+++ base/testUtils/RobotFramework.py
@@ -5,11 +5,9 @@ import os
 from pubtap.robotframework.tap_result_listener import tap_result_listener
 
 
-
-
 def main():
     tags = {'include': ['TAP_TESTS'], 'exclude': ["OSTIA", "CENTRAL", "AMAZON_LINUX", "EXAMPLE_PLUGIN", "MANUAL", "MESSAGE_RELAY", "PUB_SUB", "SAV", "SLOW", "TESTFAILURE", "UPDATE_CACHE", "FUZZ", "MDR_REGRESSION_TESTS", "FAULTINJECTION"]}
-    if os.environ['DEBUG']:
+    if os.environ.get('DEBUG'):
         tags['exclude'].append('BREAKS_DEBUG')
     log_files = ['log.html', 'output.xml', 'report.html']
 
