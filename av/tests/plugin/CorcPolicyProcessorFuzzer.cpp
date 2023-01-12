535dab8c5c6ca343b77bc203a9037d6b2d6448f8
Casimir Overell <casimir.overell@sophos.com>
2023-01-12 14:44:22 +0000

    [av] [LINUXDAR-6353] Added CORE and CORC policy fuzz tests.

---
+++ av/tests/plugin/CorcPolicyProcessorFuzzer.cpp
@@ -0,0 +1,67 @@
+// Copyright 2023 Sophos Limited. All rights reserved.
+
+#include "datatypes/sophos_filesystem.h"
+#include "pluginimpl/PolicyProcessor.h"
+#include "pluginimpl/TaskQueue.h"
+
+#include "Common/FileSystem/IFileSystem.h"
+#include "Common/Logging/ConsoleLoggingSetup.h"
+
+#include <fstream>
+#include <string>
+
+#define ERROR(x) std::cerr << x << '\n'
+#define PRINT(x) std::cout << x << '\n'
+
+#ifdef USING_LIBFUZZER
+
+static int fuzzCorcPolicy(const uint8_t *Data, size_t Size)
+{
+    std::string fuzzString(reinterpret_cast<const char*>(Data), Size);
+    auto parsedFuzzString = Common::XmlUtilities::parseXml(fuzzString);
+    auto taskQueue = std::make_shared<Plugin::TaskQueue>();
+    Plugin::PolicyProcessor policyProcessor(taskQueue);
+    policyProcessor.processCorcPolicy(parsedFuzzString);
+
+    return 0;
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
+{
+    return fuzzCorcPolicy(Data, Size);
+}
+#else
+
+static void initializeLogging()
+{
+    Common::Logging::ConsoleLoggingSetup::consoleSetupLogging();
+}
+
+int main(int argc, char* argv[])
+{
+    initializeLogging();
+    PRINT("arguments: " << argc);
+    if( argc < 2 )
+    {
+        ERROR("missing arg");
+        return 1;
+    }
+
+    if (sophos_filesystem::is_regular_file(argv[1]))
+    {
+        std::string fuzzString = Common::FileSystem::fileSystem()->readFile(argv[1]);
+        auto parsedFuzzString = Common::XmlUtilities::parseXml(fuzzString);
+        auto taskQueue = std::make_shared<Plugin::TaskQueue>();
+        Plugin::PolicyProcessor policyProcessor(taskQueue);
+        policyProcessor.processCorcPolicy(parsedFuzzString);
+    }
+    else
+    {
+        ERROR("Error: not a file ");
+        return 1;
+    }
+
+    return 0;
+}
+
+#endif
