8141694c7c29addf12596d893034a67cdaa96e6c
Alex Cooper <alex.cooper@sophos.com>
2023-01-16 15:57:33 +0000

    [av] [LINUXDAR-6354] Add threat name empty test case. Add reportsource option to SendThreatDetectedEvent tool. (AV was defaulting to seeing everything as ML)

--- av/TA/fault_injection/AVThreatFaultInjection.robot
+++ av/TA/fault_injection/AVThreatFaultInjection.robot
@@ -15,10 +15,20 @@ Test Teardown  Fault Injection Test Teardown
 
 *** Keywords ***
 
+Check Product Logs For Errors
+    check_all_product_logs_do_not_contain_error
+
+Mark Expected Error In AV Log
+    [Arguments]  ${error_log}
+    mark_expected_error_in_log  ${AV_LOG_PATH}  ${error_log}
+
 Fault Injection Test Setup
     Wait Until AV Plugin running
 
 Fault Injection Test Teardown
+    Exclude MCS Router is dead
+    Check Product Logs For Errors
+    # TODO - other checks to make sure nothing else is broken.
     Dump and Reset Logs
 
 Dump and Reset Logs
@@ -26,8 +36,8 @@ Dump and Reset Logs
     Dump Log  ${AV_LOG_PATH}
 
 Send Threat Object To AV
-    [Arguments]  ${filepath}=/tmp/testfile  ${threatname}=ThreatName  ${sha}=sha256
-    ${result} =  Run Shell Process  ${SEND_THREAT_DETECTED_TOOL} --socketpath /opt/sophos-spl/plugins/av/chroot/var/threat_report_socket --filepath "${filepath}" --threatname "${threatname}" --sha ${sha}    OnError=Failed to run SendThreatDetectedEvent binary   timeout=10
+    [Arguments]  ${file_path}=/tmp/testfile  ${threat_name}=ThreatName  ${sha}=sha256    ${report_source}=vdl
+    ${result} =  Run Shell Process  ${SEND_THREAT_DETECTED_TOOL} --socketpath /opt/sophos-spl/plugins/av/chroot/var/threat_report_socket --filepath "${file_path}" --threatname "${threat_name}" --sha ${sha} --reportsource ${report_source}    OnError=Failed to run SendThreatDetectedEvent binary   timeout=10
 
 Create File With Automatic Cleanup
     [Arguments]  ${filepath}
@@ -46,6 +56,8 @@ Disable SafeStore
 
 
 *** Test Cases ***
+
+# SafeStore disabled tests
 Send A Valid Threat Detected Object To AV with SafeStore Disabled
     Disable SafeStore
     Create File With Automatic Cleanup  /tmp/${TEST NAME}
@@ -53,10 +65,32 @@ Send A Valid Threat Detected Object To AV with SafeStore Disabled
     Send Threat Object To AV    /tmp/${TEST NAME}
     Wait For Log Contains From Mark  ${av_mark}   '/tmp/${TEST NAME}' was not quarantined due to SafeStore being disabled   timeout=60
 
-#Send A Valid Threat Detected Object To AV with SafeStore Enabled
-#    Enable SafeStore
-#    Create File With Automatic Cleanup  /tmp/${TEST NAME}
-#    ${av_mark} =  Get AV Log Mark
-#    Send Threat Object To AV    /tmp/${TEST NAME}
-#    Wait For Log Contains From Mark  ${av_mark}   something   timeout=60
+Send Empty Threat Name Threat Detected Object To AV with SafeStore Disabled
+    Disable SafeStore
+    Create File With Automatic Cleanup  /tmp/${TEST NAME}
+    ${av_mark} =  Get AV Log Mark
+    Send Threat Object To AV    /tmp/${TEST NAME}    threat_name=
+    ${expected_error} =  Set Variable  Aborting Threat Reporter connection thread: Failed to parse detection: Empty threat name
+    Wait For Log Contains From Mark  ${av_mark}   ${expected_error}   timeout=60
+    Mark Expected Error In AV Log  ${expected_error}
+
+# SafeStore enabled tests
+Send A Valid Threat Detected Object To AV with SafeStore Enabled
+    Enable SafeStore
+    Create File With Automatic Cleanup  /tmp/${TEST NAME}
+    ${av_mark} =  Get AV Log Mark
+    Send Threat Object To AV    /tmp/${TEST NAME}
+    Wait For Log Contains From Mark  ${av_mark}   Threat cleaned up at path: '/tmp/${TEST NAME}'   timeout=60
+
+Send Empty Threat Name Threat Detected Object To AV with SafeStore Enabled
+    Enable SafeStore
+    Create File With Automatic Cleanup  /tmp/${TEST NAME}
+    ${av_mark} =  Get AV Log Mark
+    Send Threat Object To AV    /tmp/${TEST NAME}    threat_name=
+    ${expected_error} =  Set Variable  Aborting Threat Reporter connection thread: Failed to parse detection: Empty threat name
+    Wait For Log Contains From Mark  ${av_mark}   ${expected_error}   timeout=60
+    Mark Expected Error In AV Log  ${expected_error}
+
+
+
 
--- av/tests/unixsocket/SendThreatDetectedExecutable/SendThreatDetectedEvent.cpp
+++ av/tests/unixsocket/SendThreatDetectedExecutable/SendThreatDetectedEvent.cpp
@@ -1,63 +1,146 @@
-// Copyright 2023 Sophos Limited. All rights reserved.
+// Copyright 2023 Sophos Limited.  All rights reserved.
 
 #include "scan_messages/ThreatDetected.h"
 #include "unixsocket/TestClient.h"
 
-#include "Common/Exceptions/IException.h"
-#include "Common/Logging/ConsoleLoggingSetup.h"
-#include "Common/Logging/SophosLoggerMacros.h"
-#include "Common/Logging/LoggerConfig.h"
+#include <Common/Logging/ConsoleLoggingSetup.h>
 
+#include <unistd.h>
 #include <iostream>
 #include <fcntl.h>
 #include <getopt.h>
-
 // Generate AV TDO
 //  SendThreatDetectedEvent -p /opt/sophos-spl/plugins/av/var/safestore_socket -f /tmp/testfile -t threatName -s e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
 
-static log4cplus::Logger& getSendThreatDetectedEventLogger()
+using namespace common::CentralEnums;
+
+std::string reportSourceToString(const ReportSource& reportSource)
 {
-    static log4cplus::Logger STATIC_LOGGER = Common::Logging::getInstance("SendThreatDetectedEvent");
-    return STATIC_LOGGER;
+    switch (reportSource)
+    {
+        case ReportSource::ml:
+            return "ml";
+        case ReportSource::rep:
+            return "repo";
+        case ReportSource::blocklist:
+            return "blocklist";
+        case ReportSource::amsi:
+            return "amsi";
+        case ReportSource::ips:
+            return "ips";
+        case ReportSource::behavioral:
+            return "behavioral";
+        case ReportSource::vdl:
+            return "vdl";
+        case ReportSource::hbt:
+            return "hbt";
+        case ReportSource::mtd:
+            return "mtd";
+        case ReportSource::web:
+            return "web";
+        case ReportSource::deviceControl:
+            return "deviceControl";
+        case ReportSource::hmpa:
+            return "hmpa";
+        case ReportSource::webControl:
+            return "webControl";
+    }
+    return {};
 }
 
-#define LOGFATAL(x) LOG4CPLUS_FATAL(getSendThreatDetectedEventLogger(), x)  // NOLINT
+ReportSource stringToReportSource(const std::string& reportSource)
+{
+    if (reportSource == "ml")
+    {
+        return ReportSource::ml;
+    }
+    else if (reportSource == "rep")
+    {
+        return ReportSource::rep;
+    }
+    else if (reportSource == "blocklist")
+    {
+        return ReportSource::blocklist;
+    }
+    else if (reportSource == "amsi")
+    {
+        return ReportSource::amsi;
+    }
+    else if (reportSource == "ips")
+    {
+        return ReportSource::ips;
+    }
+    else if (reportSource == "behavioral")
+    {
+        return ReportSource::behavioral;
+    }
+    else if (reportSource == "vdl")
+    {
+        return ReportSource::vdl;
+    }
+    else if (reportSource == "hbt")
+    {
+        return ReportSource::hbt;
+    }
+    else if (reportSource == "mtd")
+    {
+        return ReportSource::mtd;
+    }
+    else if (reportSource == "web")
+    {
+        return ReportSource::web;
+    }
+    else if (reportSource == "deviceControl")
+    {
+        return ReportSource::deviceControl;
+    }
+    else if (reportSource == "hmpa")
+    {
+        return ReportSource::hmpa;
+    }
+    else if (reportSource == "webControl")
+    {
+        return ReportSource::webControl;
+    }
+    return {};
+}
 
-static void printUsageAndExit(const std::string& name)
+void printUsageAndExit(const std::string name)
 {
     std::cout << "usage: " << name << std::endl
-        << "--socketpath <socket path>"<< std::endl
-        << "--filepath <file path of threat>" << std::endl
-        << "--sha <sha of threat>" << std::endl
-        << "--threattype <type of threat>" << std::endl
-        << "--threatname <name of threat>" << std::endl
-        << "--threatid <threatid>" << std::endl
-        << "--filedescriptor <filedescriptor>" << std::endl;
+              << "--socketpath <socket path>" << std::endl
+              << "--filepath <file path of threat>" << std::endl
+              << "--sha <sha of threat>" << std::endl
+              << "--threatname <name of threat>" << std::endl
+              << "--threatid <threatid>" << std::endl
+              << "--filedescriptor <filedescriptor>" << std::endl
+              << "--reportsource <"
+              << reportSourceToString(common::CentralEnums::ReportSource::ml) << ", "
+              << reportSourceToString(common::CentralEnums::ReportSource::vdl) << ", ...>"
+              << std::endl;
     exit(EXIT_FAILURE);
 }
 
-static int inner_main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
     if (argc < 2)
     {
         printUsageAndExit(argv[0]);
     }
+    Common::Logging::ConsoleLoggingSetup();
     std::string socketPath = "event.sock";
     std::string sha;
     std::string filePath;
-    std::string threatType;
     std::string threatName;
     std::string threatID = "00010203-0405-0607-0809-0a0b0c0d0e0f";
-    /**
-     * 0 is used as a marker from tests to open the file ourselves
-     */
     int fd = 0;
     bool sendFD = true;
     bool sendMessage = true;
-    const char* const short_opts = "p:u:t:s:f:i:d:mn";
+    ReportSource reportSource;
+
+    const auto short_opts = "p:t:s:f:i:d:mnr:";
     const option long_opts[] = {
         {"socketpath", required_argument, nullptr, 'p'},
-        {"threattype", required_argument, nullptr, 'u'},
         {"threatname", required_argument, nullptr, 't'},
         {"threatid", required_argument, nullptr, 'i'},
         {"sha", required_argument, nullptr, 's'},
@@ -65,6 +148,7 @@ static int inner_main(int argc, char* argv[])
         {"filedescriptor", required_argument, nullptr, 'd'},
         {"nosendmessage", no_argument, nullptr, 'm'},
         {"nosendfd", no_argument, nullptr, 'n'},
+        {"reportsource", required_argument, nullptr, 'r'},
         {nullptr, no_argument, nullptr, 0}
     };
     int opt = 0;
@@ -73,40 +157,38 @@ static int inner_main(int argc, char* argv[])
     {
         switch (opt)
         {
-
             case 'p':
                 socketPath = optarg;
-                std::cout << "Socket-path: " <<  socketPath << std::endl;
-                break;
-            case 'u':
-                threatType = optarg;
-                std::cout << "Threat-type: " << threatType << std::endl;
+                std::cout << socketPath << std::endl;
                 break;
             case 't':
                 threatName = optarg;
-                std::cout << "Threat-name: " << threatName << std::endl;
+                std::cout << threatName << std::endl;
                 break;
             case 'i':
                 threatID = optarg;
-                std::cout << "Threat ID: " << threatID << std::endl;
+                std::cout << threatID << std::endl;
                 break;
             case 'f':
                 filePath = optarg;
-                std::cout << "File path: " << filePath << std::endl;
+                std::cout << filePath << std::endl;
                 break;
             case 's':
                 sha = optarg;
-                std::cout << "SHA: " << sha << std::endl;
+                std::cout << sha << std::endl;
                 break;
             case 'd':
-                fd = std::stoi(optarg);
-                std::cout << "FD: " << fd << std::endl;
+                fd = std::atoi(optarg);
+                std::cout << fd << std::endl;
                 break;
             case 'm':
                 sendMessage = false;
                 break;
             case 'n':
                 sendFD = false;
+                break;
+            case 'r':
+                reportSource = stringToReportSource(optarg);
                 break;
             default:
                 printUsageAndExit(argv[0]);
@@ -129,10 +211,10 @@ static int inner_main(int argc, char* argv[])
             scan_messages::ThreatDetected threatDetected;
             threatDetected.filePath = filePath;
             threatDetected.sha256 = sha;
-            threatDetected.threatType = threatType;
             threatDetected.threatName = threatName;
             threatDetected.threatId = threatID;
             threatDetected.correlationId = threatID;
+            threatDetected.reportSource = reportSource;
             std::string dataAsString = threatDetected.serialise(false);
             if (sendFD)
             {
@@ -140,7 +222,7 @@ static int inner_main(int argc, char* argv[])
                 {
                     fd = open(filePath.c_str(), O_PATH);
                 }
-                client.sendRequestAndFD(dataAsString, fd);
+                client.sendRequestAndFD(dataAsString,fd);
             }
             else
             {
@@ -151,29 +233,4 @@ static int inner_main(int argc, char* argv[])
 
 
     return EXIT_SUCCESS;
-}
-
-
-int main(int argc, char* argv[])
-{
-    Common::Logging::ConsoleLoggingSetup logging;
-    try
-    {
-        return inner_main(argc, argv);
-    }
-    catch (const Common::Exceptions::IException& ex)
-    {
-        LOGFATAL("Caught IException at top-level: " << ex.what_with_location());
-        return 30;
-    }
-    catch (const std::exception& ex)
-    {
-        LOGFATAL("Caught std::exception at top-level: " << ex.what());
-        return 10;
-    }
-    catch (...)
-    {
-        LOGFATAL("Caught ... at top-level");
-        return 20;
-    }
 }
