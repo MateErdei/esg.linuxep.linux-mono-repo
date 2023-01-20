bc19b0ac2ee277c4698a2184f3bd407e8acc7e1b
Peter.C <peter.c@sophos.com>
2023-01-20 12:11:51 +0000

    [av] [LINUXDAR-5693] - Missed safestoreclient

--- av/modules/unixsocket/safeStoreSocket/SafeStoreClient.cpp
+++ av/modules/unixsocket/safeStoreSocket/SafeStoreClient.cpp
@@ -25,7 +25,7 @@ unixsocket::SafeStoreClient::SafeStoreClient(
     Common::Threads::NotifyPipe& notifyPipe,
     const duration_t& sleepTime,
     IStoppableSleeperSharedPtr sleeper) :
-    BaseClient(std::move(socket_path), "SafeStore", sleepTime, std::move(sleeper)), m_notifyPipe(notifyPipe)
+    BaseClient(std::move(socket_path), "SafeStoreClient", sleepTime, std::move(sleeper)), m_notifyPipe(notifyPipe)
 {
     BaseClient::connectWithRetries();
 }
