1be6c1a55aeabd5131c6e68e439a75a722083a20
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2023-01-20 17:10:19 +0000

    [base] [LINUXDAR-6256] add unit test

--- base/tests/cmscrouter/TestAgentAdapter.cpp
+++ base/tests/cmscrouter/TestAgentAdapter.cpp
@@ -230,6 +230,44 @@ TEST_F(AgentAdapterTests, testGetStatusXmlReturnsExpectedXmlWithMigrateFromSAV) 
     EXPECT_THAT(actualStatusXml, HasSubstr("<migratedFromSAV>1</migratedFromSAV></posixPlatformDetails>"));
 }
 
+TEST_F(AgentAdapterTests, testGetStatusXmlReturnsExpectedXmlWithMigrateFromSAV) // NOLINT
+{
+    auto mockPlatformUtil = std::make_shared<StrictMock<MockPlatformUtils>>();
+    auto mockLocalIP = std::make_shared<StrictMock<MockILocalIP>>();
+    std::vector<std::string> emptyStringVector;
+
+    EXPECT_CALL(*mockLocalIP, getLocalInterfaces()).WillRepeatedly(Return(MockILocalIP::buildInterfacesHelper()));
+    EXPECT_CALL(*mockPlatformUtil, sortInterfaces(_));
+    EXPECT_CALL(*mockPlatformUtil, getHostname()).WillRepeatedly(Return(basicXmlContent["hostname"]));
+    EXPECT_CALL(*mockPlatformUtil, getPlatform()).WillRepeatedly(Return(basicXmlContent["platform"]));
+    EXPECT_CALL(*mockPlatformUtil, getVendor()).WillRepeatedly(Return(basicXmlContent["vendor"]));
+    EXPECT_CALL(*mockPlatformUtil, getArchitecture()).WillRepeatedly(Return(basicXmlContent["architecture"]));
+    EXPECT_CALL(*mockPlatformUtil, getOsName()).WillRepeatedly(Return(basicXmlContent["osName"]));
+    EXPECT_CALL(*mockPlatformUtil, getKernelVersion()).WillRepeatedly(Return(basicXmlContent["kernelVersion"]));
+    EXPECT_CALL(*mockPlatformUtil, getOsMajorVersion()).WillRepeatedly(Return(basicXmlContent["osMajorVersion"]));
+    EXPECT_CALL(*mockPlatformUtil, getOsMinorVersion()).WillRepeatedly(Return(basicXmlContent["osMinorVersion"]));
+    EXPECT_CALL(*mockPlatformUtil, getDomainname()).WillRepeatedly(Return(basicXmlContent["domainName"]));
+    EXPECT_CALL(*mockPlatformUtil, getIp4Addresses(_)).WillRepeatedly(Return(ip4Addresses));
+    EXPECT_CALL(*mockPlatformUtil, getFirstIpAddress(ip4Addresses))
+        .WillRepeatedly(Return(basicXmlContent["ip4Address"]));
+    EXPECT_CALL(*mockPlatformUtil, getIp6Addresses(_)).WillRepeatedly(Return(ip6Addresses));
+    EXPECT_CALL(*mockPlatformUtil, getFirstIpAddress(ip6Addresses))
+        .WillRepeatedly(Return(basicXmlContent["ip6Address"]));
+    EXPECT_CALL(*mockPlatformUtil, getCloudPlatformMetadata(_)).WillRepeatedly(Return(fakeCloudXml));
+    EXPECT_CALL(*mockPlatformUtil, getMacAddresses()).WillRepeatedly(Return(emptyStringVector));
+
+    MCS::AgentAdapter adapter(mockPlatformUtil, mockLocalIP);
+
+    std::map<std::string, std::string> emptyOptions;
+    setenv("FORCE_UNINSTALL_SAV", "1", 1);
+    std::string actualStatusXml = adapter.getStatusXml(emptyOptions);
+    unsetenv("FORCE_UNINSTALL_SAV");
+
+    std::string expectedXmlHeader = expectedXmlHeaderContent();
+    EXPECT_THAT(actualStatusXml, HasSubstr(expectedXmlHeader));
+    EXPECT_THAT(actualStatusXml, HasSubstr("<migratedFromSAV>1</migratedFromSAV></posixPlatformDetails>"));
+}
+
 TEST_F(AgentAdapterTests, testGetStatusXmlReturnsExpectedXmlWithOptions) // NOLINT
 {
     auto mockPlatformUtil = std::make_shared<StrictMock<MockPlatformUtils>>();
