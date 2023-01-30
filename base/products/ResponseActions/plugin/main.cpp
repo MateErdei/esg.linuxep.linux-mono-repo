7af18fe37502422bad7625e4861192b0fa1292a5
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2023-01-30 17:13:16 +0000

    [base] [LINUXDAR-6598] add products folder for RA

--- base/products/ResponseActions/plugin/main.cpp
+++ base/products/ResponseActions/plugin/main.cpp
@@ -1,15 +1,19 @@
-// Copyright 2023 Sophos Limited. All rights reserved.
+/******************************************************************************************************
+
+Copyright 2018 Sophos Limited.  All rights reserved.
+
+******************************************************************************************************/
+
+#include "modules/ResponseActions/pluginimpl/config.h"
+
 #include <Common/FileSystem/IFileSystem.h>
 #include <Common/Logging/PluginLoggingSetup.h>
-#include <Common/PluginApi/ApiException.h>
-#include <Common/PluginApi/ErrorCodes.h>
 #include <Common/PluginApi/IBaseServiceApi.h>
 #include <Common/PluginApi/IPluginResourceManagement.h>
-#include <ResponseActions/ResponsePlugin/Logger.h>
-#include <ResponseActions/ResponsePlugin/PluginAdapter.h>
-#include <ResponsePlugin/config.h>
-
-#include <sstream>
+#include <Common/PluginApi/ApiException.h>
+#include <Common/PluginApi/ErrorCodes.h>
+#include <modules/ResponseActions/pluginimpl/Logger.h>
+#include <modules/ResponseActions/pluginimpl/PluginAdapter.h>
 
 const char* PluginName = RA_PLUGIN_NAME;
 
@@ -20,14 +24,14 @@ enum returnCode {
 
 int main()
 {
-    using namespace ResponsePlugin;
+    using namespace Plugin;
     int ret = SUCCESS;
     Common::Logging::PluginLoggingSetup loggerSetup(PluginName);
 
     std::unique_ptr<Common::PluginApi::IPluginResourceManagement> resourceManagement =
         Common::PluginApi::createPluginResourceManagement();
 
-    auto queueTask = std::make_shared<TaskQueue>();
+    auto queueTask = std::make_shared<QueueTask>();
     auto sharedPluginCallBack = std::make_shared<PluginCallback>(queueTask);
 
     std::unique_ptr<Common::PluginApi::IBaseServiceApi> baseService;
@@ -56,11 +60,6 @@ int main()
     catch (const std::exception& ex)
     {
         LOGERROR("Plugin threw an exception at top level: " << ex.what());
-        ret = EXCEPTIONTHROWN;
-    }
-    catch (...)
-    {
-        LOGERROR("Plugin threw an unknown exception at top level");
         ret = EXCEPTIONTHROWN;
     }
     sharedPluginCallBack->setRunning(false);
