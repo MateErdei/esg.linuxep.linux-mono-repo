843270c757269f89d277ac459a1e01a10a187396
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2023-02-09 16:06:22 +0000

    [base] [LINUXDAR-6660] do upload

--- base/modules/ResponseActions/ResponseActionsImpl/UploadFileAction.cpp
+++ base/modules/ResponseActions/ResponseActionsImpl/UploadFileAction.cpp
@@ -6,6 +6,11 @@
 #include "Logger.h"
 
 #include <Common/FileSystem/IFileSystem.h>
+#include <Common/FileSystem/IFileTooLargeException.h>
+#include <Common/UtilityImpl/TimeUtils.h>
+#include <Common/CurlWrapper/CurlWrapper.h>
+#include <Common/HttpRequestsImpl/HttpRequesterImpl.h>
+
 #include <json.hpp>
 namespace ResponseActionsImpl
 {
@@ -54,6 +59,34 @@ namespace ResponseActionsImpl
             return response.dump();
         }
 
+        Common::UtilityImpl::FormattedTime time;
+        u_int64_t start = time.currentEpochTimeInSecondsAsInteger();
+        response["startedAt"] = start;
+        std::string sha256;
+        //scoping so we don't keep content in memory any longer than we have to
+        {
+            try
+            {
+                std::string content = fs->readFile(info.targetPath, 1024 * 1024 * 1024);
+                sha256 = fs->calculateDigest(Common::SslImpl::Digest::sha256, content);
+            }
+            catch (const Common::FileSystem::IFileTooLargeException&)
+            {
+                std::string error = "File to be uploaded is being written to and has gone over the max size";
+                actionsUtils.setErrorInfo(response,1,error,"exceed_size_limit");
+            }
+        }
+        response["sha256"] = sha256;
+        Common::HttpRequests::RequestConfig request{ .url = info.url, .fileToUpload=info.targetPath, .timeout = info.timeout};
+        std::shared_ptr<Common::CurlWrapper::ICurlWrapper> curlWrapper =
+            std::make_shared<Common::CurlWrapper::CurlWrapper>();
+        std::shared_ptr<Common::HttpRequests::IHttpRequester> client =
+            std::make_shared<Common::HttpRequestsImpl::HttpRequesterImpl>(curlWrapper);
+        Common::HttpRequests::Response httpresponse = client->put(request);
+        response["httpStatus"] = httpresponse.status;
+        u_int64_t end = time.currentEpochTimeInSecondsAsInteger();
+        response["duration"] = start - end;
         return response.dump();
     }
+
 }
