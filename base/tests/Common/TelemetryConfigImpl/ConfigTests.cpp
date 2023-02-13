4f226ae699402cb8929899dd3a0291796b3d093f
Mollie Bourke <mollie.bourke@sophos.com>
2023-02-13 14:03:41 +0000

    [base] [LINUXDAR-6633] Update copyrights

--- base/tests/Common/TelemetryConfigImpl/ConfigTests.cpp
+++ base/tests/Common/TelemetryConfigImpl/ConfigTests.cpp
@@ -1,8 +1,4 @@
-/******************************************************************************************************
-
-Copyright 2019, Sophos Limited.  All rights reserved.
-
-******************************************************************************************************/
+// Copyright 2019-2023 Sophos Limited. All rights reserved.
 
 #include <Common/TelemetryConfigImpl/Config.h>
 #include <Common/TelemetryConfigImpl/Serialiser.h>
