93995286f100855e99b8837c7462de50304539d7
Menaga Karunanandan <menaga.karunanandan@sophos.com>
2023-02-13 12:00:59 +0000

    [base] [LINUXDAR-6660] set init values

--- base/modules/ResponseActions/ResponseActionsImpl/ActionStructs.h
+++ base/modules/ResponseActions/ResponseActionsImpl/ActionStructs.h
@@ -10,9 +10,9 @@ namespace ResponseActionsImpl
         std::string url;
         bool compress = false;
         std::string password;
-        u_int64_t expiration;
-        int timeout;
-        int maxSize;
+        u_int64_t expiration = 0;
+        int timeout = 0;
+        int maxSize = 0;
     };
     enum class UploadType
     {
