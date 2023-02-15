c23fa5dc2b951fe398a4fc1b1e57576822d66de2
Peter.C <peter.c@sophos.com>
2023-02-15 13:40:40 +0000

    [edr] [LINUXDAR-6721] - Remove GMOCK_LIBRARY reference

--- edr/buildconfig/SophosAddTest.cmake
+++ edr/buildconfig/SophosAddTest.cmake
@@ -30,7 +30,7 @@ macro(SophosAddTest TARGET)
     add_executable(${TARGET} ${INPUTFILES})
 
     target_include_directories(${TARGET} SYSTEM BEFORE PUBLIC ${GMOCK_INCLUDE} ${GTEST_INCLUDE})
-    target_link_libraries(${TARGET}  PUBLIC  ${GTEST_LIBRARY} ${GTEST_MAIN_LIBRARY} ${GMOCK_LIBRARY})
+    target_link_libraries(${TARGET}  PUBLIC  ${GTEST_LIBRARY} ${GTEST_MAIN_LIBRARY})
 
     if ( AddTest_PROJECTS )
         target_link_libraries(${TARGET} PUBLIC ${AddTest_PROJECTS})
