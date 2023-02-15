3998af2fdf6ef983355a34b2661e1fcdb8eca121
Peter.C <peter.c@sophos.com>
2023-02-15 11:56:50 +0000

    [base] [LINUXDAR-6721] - add /include to googlemock/test path

--- base/unpack_build_inputs.sh
+++ base/unpack_build_inputs.sh
@@ -267,7 +267,6 @@ function unpack_zips()
 
 function google_test()
 {
-    ls /build/input/googletest
     if [[ -d "$FETCHED_INPUTS_DIR/googletest" ]]
     then
         if [[ ! -d $REDIST/googletest ]]
