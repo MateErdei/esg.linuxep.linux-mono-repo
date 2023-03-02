c82a2b93a6b87a138ee10f7ade2be237573377cf
Douglas Leeder <douglas.leeder@sophos.com>
2023-03-02 11:27:39 +0000

    [base] [LINUXDAR-6874] Update copyright for FDQN fixes.

--- base/tests/cmscrouter/TestAgentAdapter.cpp
+++ base/tests/cmscrouter/TestAgentAdapter.cpp
@@ -1,8 +1,4 @@
-/******************************************************************************************************
-
-Copyright 2022, Sophos Limited.  All rights reserved.
-
-******************************************************************************************************/
+// Copyright 2022-2023 Sophos Limited. All rights reserved.
 #include <cmcsrouter/AgentAdapter.h>
 #include <gtest/gtest.h>
 #include <tests/Common/Helpers/LogInitializedTests.h>
