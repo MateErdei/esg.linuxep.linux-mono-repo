e6ea1aad87d29ab82577f7e029d87b0a85ab57d1
DarrenSmith <darren.smith@sophos.com>
2023-03-07 13:37:38 +0000

    [base] [LINUXDAR-6600] Remove unnecessary dependency on logger

--- base/tests/manualTools/DiskSpaceUtil.cpp
+++ base/tests/manualTools/DiskSpaceUtil.cpp
@@ -1,19 +1,17 @@
 // Copyright 2023 Sophos Limited. All rights reserved.
 
-#include "Common/Logging/ConsoleLoggingSetup.h"
 #include "Common/FileSystemImpl/FileSystemImpl.h"
 
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
-    Common::Logging::ConsoleLoggingSetup m_loggingSetup;
     auto filesystem = std::make_unique<Common::FileSystem::FileSystemImpl>();
     if (argc == 2)
     {
         std::string mountPoint(argv[1]);
         std::filesystem::space_info diskSpaceInfo = filesystem->getDiskSpaceInfo(mountPoint);
-        std::cout << "Disk space info for " << mountPoint;
+        std::cout << "Disk space info for " << mountPoint << std::endl;
         std::cout << "   Total size: " << diskSpaceInfo.capacity << std::endl;
         std::cout << "   Free space: " << diskSpaceInfo.free << std::endl;
         std::cout << "   Available space: " << diskSpaceInfo.available << std::endl;
