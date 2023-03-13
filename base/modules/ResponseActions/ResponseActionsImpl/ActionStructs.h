3634f1b4ac7e994f4335072df44d0cb9362338cb
Peter.C <peterc@sophos.com>
2023-03-13 14:13:35 +0000

    [base] [LINUXDAR-6669] - Add DownloadInfo to ActionStructs

--- base/modules/ResponseActions/ResponseActionsImpl/ActionStructs.h
+++ base/modules/ResponseActions/ResponseActionsImpl/ActionStructs.h
@@ -1,15 +1,9 @@
 // Copyright 2023 Sophos Limited. All rights reserved.
 
 #pragma once
-#include "ResponseActions/RACommon/ResponseActionsCommon.h"
-
 #include <string>
-#include <vector>
 namespace ResponseActionsImpl
 {
-
-    // Upload action
-
     struct UploadInfo
     {
         std::string targetPath;
@@ -20,38 +14,21 @@ namespace ResponseActionsImpl
         int timeout = 0;
         int maxSize = 0;
     };
-
     enum class UploadType
     {
         FILE,
         FOLDER
     };
 
-    // Run command action
-
-    struct CommandRequest
+    struct DownloadInfo
     {
-        std::vector<std::string> commands;
-        long timeout;
-        bool ignoreError;
+        std::string targetPath;
+        std::string url;
+        std::string sha256;
+        u_int32_t sizeBytes; //max 2GB
+        bool decompress;
+        std::string password;
         u_int64_t expiration = 0;
+        int timeout = 0;
     };
-
-    struct SingleCommandResult
-    {
-        std::string stdOut;
-        std::string stdErr;
-        int exitCode = 0;
-        unsigned long duration = 0;
-    };
-
-    struct CommandResponse
-    {
-        std::string type = "sophos.mgt.response.RunCommands";
-        ResponseActions::RACommon::ResponseResult result;
-        std::vector<SingleCommandResult> commandResults;
-        unsigned long startedAt = 0;
-        unsigned long duration = 0;
-    };
-
-} // namespace ResponseActionsImpl
+}
