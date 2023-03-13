8dc7cc95e94ca198a7d17a2fac15bfb8bdfefe6c
Mollie Bourke <mollie.bourke@sophos.com>
2023-03-13 16:35:57 +0000

    [base] [LINUXDAR-6704] Add doc strings

--- base/modules/watchdog/watchdogimpl/UserGroupUtils.h
+++ base/modules/watchdog/watchdogimpl/UserGroupUtils.h
@@ -9,48 +9,80 @@ namespace watchdog::watchdogimpl
 {
     using WatchdogUserGroupIDs = nlohmann::json;
 
-    /*
-     * TODO Docs
+    /**
+     * Strips comments in user-group-ids-requested.conf used to inform usage of file
+     * @param fileContents
+     * @return Json object without file usage string
      */
     std::string stripCommentsFromRequestedUserGroupIdFile(const std::vector<std::string>& fileContents);
 
-    /*
-     * TODO Docs
+    /**
+     * Validates requested user and group ID changes, removing root or duplicated user/group ID
+     * @param configJson
+     * @return Valid requested user and group ID changes as a Json object
      */
     WatchdogUserGroupIDs validateUserAndGroupIds(WatchdogUserGroupIDs configJson);
 
-    /*
-     * TODO Docs
+    /**
+     * Read user-group-ids-requested.conf and parse to remove usage string
+     * @return Json object with requested user and group ID changes if valid else empty object
      */
     WatchdogUserGroupIDs readRequestedUserGroupIds();
 
-    /*
-     * TODO Docs
+    /**
+     * Change user ID of a file or directory
+     * @param filePath
+     * @param userId
      */
     void setUserIdOfFile(const std::string& filePath, uid_t userId);
 
-    /*
-     * TODO Docs
+    /**
+     * Change group ID of a file or directory
+     * @param filePath
+     * @param groupId
      */
     void setGroupIdOfFile(const std::string& filePath, gid_t groupId);
 
-    /*
-     * TODO Docs
+    /**
+     * Change existing user ID to requested ID
+     * @param username
+     * @param newUserId
+     * @throw std::runtime_error
      */
     void changeUserId(const std::string& username, uid_t newUserId);
 
-    /*
-     * TODO Docs
+    /**
+     * Change existing group ID to requested ID
+     * @param groupname
+     * @param newGroupId
+     * @throw std::runtime_error
      */
     void changeGroupId(const std::string& groupname, gid_t newGroupId);
 
-    /*
+    /**
      * For every file and directory in rootPath that matches the current IDs passed in, change the file owner
-     * user and group IDs to the new values.
+     * user IDs to the new values.
+     * @param rootPath
+     * @param currentUserId
+     * @param newUserId
+     * @throw std::runtime_error
      */
     void remapUserIdOfFiles(const std::string& rootPath, uid_t currentUserId, uid_t newUserId);
+
+    /**
+     * For every file and directory in rootPath that matches the current IDs passed in, change the file owner
+     * group IDs to the new values.
+     * @param rootPath
+     * @param currentUserId
+     * @param newUserId
+     * @throw std::runtime_error
+     */
     void remapGroupIdOfFiles(const std::string& rootPath, gid_t currentGroupId, gid_t newGroupId);
 
+    /**
+     * Apply requested user and group ID changes to Sophos SPL installation
+     * @param ids - Json object extracted from user-group-ids-requested.conf
+     */
     void applyUserIdConfig(const WatchdogUserGroupIDs& ids);
 
-}
+} // namespace watchdog::watchdogimpl
