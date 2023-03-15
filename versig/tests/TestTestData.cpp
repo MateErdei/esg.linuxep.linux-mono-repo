e3db18273c45cb04d366b76b44a15dad7d2221ed
Douglas Leeder <douglas.leeder@sophos.com>
2023-03-15 09:26:30 +0000

    [LINUXDAR-5854] Add test for extended signatures only.

---
+++ versig/tests/TestTestData.cpp
@@ -0,0 +1,28 @@
+// Copyright 2023 Sophos All rights reserved.
+
+#include "libcryptosupport.h"
+#include "versig.h"
+
+#include <gtest/gtest.h>
+
+#define TESTS "../tests/TestData"
+
+namespace
+{
+    class TestTestData : public ::testing::Test
+    {
+    };
+}
+
+TEST_F(TestTestData, ExtendedSignaturesOnly)
+{
+    std::vector<std::string> argv { "versig_test",
+                                    "-c" TESTS "/ExtendedSignaturesOnly",
+                                    "-f" TESTS "/ExtendedSignaturesOnly/manifest.dat",
+                                    "-d" TESTS "/ExtendedSignaturesOnly",
+                                    "--silent-off"
+    };
+
+    int ret = versig_main(argv);
+    EXPECT_EQ(ret, 0);
+}
