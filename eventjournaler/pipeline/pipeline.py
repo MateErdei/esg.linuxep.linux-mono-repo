3e973c144afaf95e0044857ec250b16d6f493aa1
DarrenASmith <darren.smith@sophos.com>
2023-03-17 16:55:01 +0000

    [eventjournaler] [LINUXDAR-6024] Add SLES15 to Event Journaler TAP tests

--- eventjournaler/pipeline/pipeline.py
+++ eventjournaler/pipeline/pipeline.py
@@ -243,6 +243,9 @@ def event_journaler(stage: tap.Root, context: tap.PipelineContext, parameters: t
 
         ("amazonlinux2",
          tap.Machine('amzlinux2_x64_server_en_us', inputs=test_inputs, platform=tap.Platform.Linux)),
+
+        ("sles15",
+         tap.Machine('sles15_x64_sp4_aws_server_en_us', inputs=test_inputs, platform=tap.Platform.Linux)),
         # add other distros here
     )
 
