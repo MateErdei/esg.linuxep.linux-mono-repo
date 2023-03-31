41649d28f19d28d947541e8c20f64f06a120325c
Mollie Bourke <mollie.bourke@sophos.com>
2023-03-31 13:54:54 +0100

    [base] [LINUXDAR-6953] Use pluginapi lib in response actions

--- base/modules/ResponseActions/ResponsePlugin/ActionRunner.cpp
+++ base/modules/ResponseActions/ResponsePlugin/ActionRunner.cpp
@@ -5,9 +5,9 @@
 #include "Logger.h"
 #include "json.hpp"
 
-#include "FileSystem/IFilePermissions.h"
-#include "FileSystem/IFileSystem.h"
-#include "ApplicationConfiguration/IApplicationPathManager.h"
+#include "Common/FileSystem/IFilePermissions.h"
+#include "Common/FileSystem/IFileSystem.h"
+#include "Common/ApplicationConfiguration/IApplicationPathManager.h"
 
 namespace ResponsePlugin
 {
