3623937125ef068052a7dc4a7ebf3cb8f8ea6abf
Peter.C <peterc@sophos.com>
2023-04-13 09:43:56 +0100

    [base] Revert "[LINUXDAR-6669] - Temporarily remove SLES to get a test pass"
    This reverts commit aee335945329f068bc7eeb6899d09066918c14d8.

--- base/pipeline/pipeline.py
+++ base/pipeline/pipeline.py
@@ -339,6 +339,11 @@ def sspl_base(stage: tap.Root, context: tap.PipelineContext, parameters: tap.Par
         ("amazonlinux2",
          tap.Machine('amzlinux2_x64_server_en_us', inputs=test_inputs, platform=tap.Platform.Linux)),
 
+        ("sles12",
+         tap.Machine('sles12_x64_sp5_aws_server_en_us', inputs=test_inputs, platform=tap.Platform.Linux)),
+
+        ("sles15",
+         tap.Machine('sles15_x64_sp4_aws_server_en_us', inputs=test_inputs, platform=tap.Platform.Linux)),
         # add other distros here
     )
 
