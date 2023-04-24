2e585e6c55a10ad58e0c3f24d4a23ee2c20eecd0
claraSophos <clara.falconer@sophos.com>
2023-04-24 18:08:21 +0100

    [base] merge develop text fixes

--- base/modules/ResponseActions/ResponsePlugin/ActionRunner.cpp
+++ base/modules/ResponseActions/ResponsePlugin/ActionRunner.cpp
@@ -9,31 +9,34 @@
 #include "Common/ApplicationConfiguration/IApplicationPathManager.h"
 #include "Common/FileSystem/IFilePermissions.h"
 #include "Common/FileSystem/IFileSystem.h"
+#include "Common/ApplicationConfiguration/IApplicationPathManager.h"
+
+using namespace ResponseActions::RACommon;
+using namespace Common::Process;
 
 namespace ResponsePlugin
 {
-
     void sendFailedResponse(
-        ResponseActions::RACommon::ResponseResult result,
+        ResponseResult result,
         const std::string& requestType,
         const std::string& correlationId)
     {
         LOGINFO("Response Actions plugin sending failed response to Central on behalf of Action Runner process");
         nlohmann::json response;
-        if (requestType == ResponseActions::RACommon::RUN_COMMAND_REQUEST_TYPE)
+        if (requestType == RUN_COMMAND_REQUEST_TYPE)
         {
-            response["type"] = ResponseActions::RACommon::RUN_COMMAND_RESPONSE_TYPE;
+            response["type"] = RUN_COMMAND_RESPONSE_TYPE;
         }
-        else if (requestType == ResponseActions::RACommon::UPLOAD_FILE_REQUEST_TYPE)
+        else if (requestType == UPLOAD_FILE_REQUEST_TYPE)
         {
-            response["type"] = ResponseActions::RACommon::UPLOAD_FILE_RESPONSE_TYPE;
+            response["type"] = UPLOAD_FILE_RESPONSE_TYPE;
         }
-        else if (requestType == ResponseActions::RACommon::UPLOAD_FOLDER_REQUEST_TYPE)
+        else if (requestType == UPLOAD_FOLDER_REQUEST_TYPE)
         {
-            response["type"] = ResponseActions::RACommon::UPLOAD_FOLDER_RESPONSE_TYPE;
+            response["type"] = UPLOAD_FOLDER_RESPONSE_TYPE;
         }
         response["result"] = static_cast<int>(result);
-        ResponseActions::RACommon::sendResponse(correlationId, response.dump());
+        sendResponse(correlationId, response.dump());
     }
 
     void ActionRunner::runAction(
@@ -51,20 +54,25 @@ namespace ResponsePlugin
             {
                 std::string exePath =
                     Common::ApplicationConfiguration::applicationPathManager().getResponseActionRunnerPath();
-                this->m_process = Common::Process::createProcess();
+                this->m_process = createProcess();
                 LOGINFO("Trigger process at: " << exePath << " for action: " << correlationId);
                 std::vector<std::string> arguments = { correlationId, action, type };
                 this->m_process->exec(exePath, arguments, {});
-                Common::Process::ProcessStatus processStatus = this->m_process->wait(std::chrono::seconds(timeout), 1);
-                bool timedOut = false;
-                if (processStatus != Common::Process::ProcessStatus::FINISHED)
+                ProcessStatus processStatus = this->m_process->wait(std::chrono::seconds(timeout), 1);
+                if (processStatus == ProcessStatus::TIMEOUT)
                 {
-                    this->m_process->kill();
-                    timedOut = true;
-                    LOGWARN(
-                        "Action process was stopped due to a timeout after "
-                        << timeout << " secs, correlation ID: " << correlationId);
+                    LOGWARN("Action runner reached time out of " << timeout << " secs, correlation ID: " << correlationId);
+
+                    this->m_process->sendSIGUSR1();
+                    //Wait for process to handle signal and exit which should be done within 3 seconds
+                    this->m_process->wait(std::chrono::seconds(3), 1);
                 }
+
+                if (this->m_process->getStatus() != ProcessStatus::FINISHED)
+                {
+                    kill(" it carried on running unexpectedly");
+                }
+
                 auto output = this->m_process->output();
                 if (!output.empty())
                 {
@@ -80,24 +88,19 @@ namespace ResponsePlugin
                     LOGWARN("Failed action " << correlationId << " with exit code " << code);
                     TelemetryUtils::incrementFailedActions(type);
 
-                    if (code != 1)
+                    if (code != 1 && code != 4)
                     {
-                        ResponseActions::RACommon::ResponseResult result;
-                        if (timedOut)
+                        auto result = ResponseResult::INTERNAL_ERROR;
+                        if (processStatus == ProcessStatus::TIMEOUT)
                         {
-                            result = ResponseActions::RACommon::ResponseResult::TIMEOUT;
+                            result = ResponseResult::TIMEOUT;
                             TelemetryUtils::incrementTimedOutActions(type);
                         }
-                        else if (code == 4)
-                        {
-                            result = ResponseActions::RACommon::ResponseResult::EXPIRED;
-                            TelemetryUtils::incrementExpiredActions(type);
-                        }
-                        else
-                        {
-                            result = ResponseActions::RACommon::ResponseResult::INTERNAL_ERROR;
-                        }
                         sendFailedResponse(result, type, correlationId);
+                    }
+                    if (code == 4)
+                    {
+                        TelemetryUtils::incrementExpiredActions(type);
                     }
                 }
                 isRunning = false;
@@ -106,11 +109,26 @@ namespace ResponsePlugin
 
     void ActionRunner::killAction()
     {
-        m_process->kill();
+        kill("plugin received stop request");
     }
 
     bool ActionRunner::getIsRunning()
     {
         return isRunning;
     }
+
+    bool ActionRunner::kill(const std::string& msg)
+    {
+        if (this->m_process->kill())
+        {
+            LOGWARN("Action Runner had to be killed after " + msg);
+            return true;
+        }
+        else
+        {
+            LOGWARN("Action Runner was stopped after " + msg);
+            return false;
+        }
+    }
+
 } // namespace ResponsePlugin
