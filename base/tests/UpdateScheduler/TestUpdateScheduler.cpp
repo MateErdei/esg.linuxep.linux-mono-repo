c7cf1b751a74abf95aa845993377e8d7024a9890
Alex Cooper <alex.cooper@sophos.com>
2023-04-27 21:01:20 +0100

    [base] [LINUXDAR-7027] reformat test code

--- base/tests/UpdateScheduler/TestUpdateScheduler.cpp
+++ base/tests/UpdateScheduler/TestUpdateScheduler.cpp
@@ -19,97 +19,104 @@ Copyright 2021, Sophos Limited.  All rights reserved.
 using namespace Common::UtilityImpl;
 using namespace ::testing;
 
-class TestUpdateSchedulerProcessorHelperMethods : public LogOffInitializedTests{};
+class TestUpdateSchedulerProcessorHelperMethods : public LogOffInitializedTests
+{
+};
 
-TEST_F(TestUpdateSchedulerProcessorHelperMethods, featuresAreReadFromJsonFile) // NOLINT
+TEST_F(TestUpdateSchedulerProcessorHelperMethods, featuresAreReadFromJsonFile)
 {
     auto filesystemMock = new StrictMock<MockFileSystem>();
-    EXPECT_CALL(*filesystemMock,
-        exists(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
+    EXPECT_CALL(
+        *filesystemMock, exists(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
         .WillOnce(Return(true));
 
-    EXPECT_CALL(*filesystemMock,
-        readFile(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
+    EXPECT_CALL(
+        *filesystemMock, readFile(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
         .WillOnce(Return(R"(["featureA","featureB"])"));
 
-    Tests::ScopedReplaceFileSystem ScopedReplaceFileSystem{std::unique_ptr<Common::FileSystem::IFileSystem>(filesystemMock)};
-    std::vector<std::string> expectedFeatures = {"featureA", "featureB"};
+    Tests::ScopedReplaceFileSystem ScopedReplaceFileSystem{ std::unique_ptr<Common::FileSystem::IFileSystem>(
+        filesystemMock) };
+    std::vector<std::string> expectedFeatures = { "featureA", "featureB" };
     std::vector<std::string> actualFeatures = UpdateSchedulerImpl::readInstalledFeatures();
     EXPECT_EQ(expectedFeatures, actualFeatures);
 }
 
-TEST_F(TestUpdateSchedulerProcessorHelperMethods, featuresAreReadFromJsonFileWithEmptyList) // NOLINT
+TEST_F(TestUpdateSchedulerProcessorHelperMethods, featuresAreReadFromJsonFileWithEmptyList)
 {
     auto filesystemMock = new StrictMock<MockFileSystem>();
-    EXPECT_CALL(*filesystemMock,
-                exists(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
+    EXPECT_CALL(
+        *filesystemMock, exists(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
         .WillOnce(Return(true));
 
-    EXPECT_CALL(*filesystemMock,
-                readFile(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
+    EXPECT_CALL(
+        *filesystemMock, readFile(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
         .WillOnce(Return("[]"));
 
-    Tests::ScopedReplaceFileSystem ScopedReplaceFileSystem{std::unique_ptr<Common::FileSystem::IFileSystem>(filesystemMock)};
+    Tests::ScopedReplaceFileSystem ScopedReplaceFileSystem{ std::unique_ptr<Common::FileSystem::IFileSystem>(
+        filesystemMock) };
     std::vector<std::string> expectedFeatures = {};
     std::vector<std::string> actualFeatures = UpdateSchedulerImpl::readInstalledFeatures();
     EXPECT_EQ(expectedFeatures, actualFeatures);
 }
 
-TEST_F(TestUpdateSchedulerProcessorHelperMethods, featuresAreReadFromEmptyJsonFile) // NOLINT
+TEST_F(TestUpdateSchedulerProcessorHelperMethods, featuresAreReadFromEmptyJsonFile)
 {
     auto filesystemMock = new StrictMock<MockFileSystem>();
-    EXPECT_CALL(*filesystemMock,
-                exists(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
+    EXPECT_CALL(
+        *filesystemMock, exists(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
         .WillOnce(Return(true));
 
-    EXPECT_CALL(*filesystemMock,
-                readFile(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
+    EXPECT_CALL(
+        *filesystemMock, readFile(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
         .WillOnce(Return(""));
-    Tests::ScopedReplaceFileSystem ScopedReplaceFileSystem{std::unique_ptr<Common::FileSystem::IFileSystem>(filesystemMock)};
+    Tests::ScopedReplaceFileSystem ScopedReplaceFileSystem{ std::unique_ptr<Common::FileSystem::IFileSystem>(
+        filesystemMock) };
     std::vector<std::string> expectedFeatures = {};
     std::vector<std::string> actualFeatures = UpdateSchedulerImpl::readInstalledFeatures();
     EXPECT_EQ(expectedFeatures, actualFeatures);
 }
 
-TEST_F(TestUpdateSchedulerProcessorHelperMethods, noThrowWhenFeaturesAreReadFromMalformedJsonFile) // NOLINT
+TEST_F(TestUpdateSchedulerProcessorHelperMethods, noThrowWhenFeaturesAreReadFromMalformedJsonFile)
 {
     auto filesystemMock = new StrictMock<MockFileSystem>();
-    EXPECT_CALL(*filesystemMock,
-                exists(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
+    EXPECT_CALL(
+        *filesystemMock, exists(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
         .WillOnce(Return(true));
 
-    EXPECT_CALL(*filesystemMock,
-                readFile(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
+    EXPECT_CALL(
+        *filesystemMock, readFile(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
         .WillOnce(Return("this is not json..."));
 
-    Tests::ScopedReplaceFileSystem ScopedReplaceFileSystem{std::unique_ptr<Common::FileSystem::IFileSystem>(filesystemMock)};
+    Tests::ScopedReplaceFileSystem ScopedReplaceFileSystem{ std::unique_ptr<Common::FileSystem::IFileSystem>(
+        filesystemMock) };
     std::vector<std::string> expectedFeatures = {};
     std::vector<std::string> actualFeatures = UpdateSchedulerImpl::readInstalledFeatures();
     EXPECT_EQ(expectedFeatures, actualFeatures);
 }
 
-TEST_F(TestUpdateSchedulerProcessorHelperMethods, featuresDefaultToEmptyListWhenFileDoesNotExist) // NOLINT
+TEST_F(TestUpdateSchedulerProcessorHelperMethods, featuresDefaultToEmptyListWhenFileDoesNotExist)
 {
     auto filesystemMock = new StrictMock<MockFileSystem>();
-    EXPECT_CALL(*filesystemMock,
-                exists(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
+    EXPECT_CALL(
+        *filesystemMock, exists(Common::ApplicationConfiguration::applicationPathManager().getFeaturesJsonPath()))
         .WillOnce(Return(false));
 
-    Tests::ScopedReplaceFileSystem ScopedReplaceFileSystem{std::unique_ptr<Common::FileSystem::IFileSystem>(filesystemMock)};
+    Tests::ScopedReplaceFileSystem ScopedReplaceFileSystem{ std::unique_ptr<Common::FileSystem::IFileSystem>(
+        filesystemMock) };
     std::vector<std::string> expectedFeatures = {};
     std::vector<std::string> actualFeatures = UpdateSchedulerImpl::readInstalledFeatures();
     EXPECT_EQ(expectedFeatures, actualFeatures);
 }
 
-TEST_F(TestUpdateSchedulerProcessorHelperMethods, waitForTheFirstPolicyDoesNotReorderTasks) // NOLINT
+TEST_F(TestUpdateSchedulerProcessorHelperMethods, waitForTheFirstPolicyDoesNotReorderTasks)
 {
     UpdateScheduler::SchedulerTaskQueue queueTask;
     auto notPolicy = UpdateScheduler::SchedulerTask::TaskType::UpdateNow;
-    queueTask.push(UpdateScheduler::SchedulerTask{notPolicy,"1"});
-    queueTask.push(UpdateScheduler::SchedulerTask{notPolicy,"2"});
-    queueTask.push(UpdateScheduler::SchedulerTask{UpdateScheduler::SchedulerTask::TaskType::Policy,"3", "APPID"});
-    queueTask.push(UpdateScheduler::SchedulerTask{notPolicy,"4"});
-    queueTask.push(UpdateScheduler::SchedulerTask{notPolicy,"5"});
+    queueTask.push(UpdateScheduler::SchedulerTask{ notPolicy, "1" });
+    queueTask.push(UpdateScheduler::SchedulerTask{ notPolicy, "2" });
+    queueTask.push(UpdateScheduler::SchedulerTask{ UpdateScheduler::SchedulerTask::TaskType::Policy, "3", "APPID" });
+    queueTask.push(UpdateScheduler::SchedulerTask{ notPolicy, "4" });
+    queueTask.push(UpdateScheduler::SchedulerTask{ notPolicy, "5" });
     std::string policyContents = UpdateSchedulerImpl::UpdateSchedulerProcessor::waitForPolicy(queueTask, 5, "APPID");
 
     EXPECT_EQ("3", policyContents);
@@ -126,38 +133,41 @@ TEST_F(TestUpdateSchedulerProcessorHelperMethods, waitForTheFirstPolicyDoesNotRe
     EXPECT_FALSE(queueTask.pop(task, 1));
 }
 
-
 // isSuldownloaderRunning tests
 
-TEST_F(TestUpdateSchedulerProcessorHelperMethods, isSuldownloaderRunningReturnsTrueWhenLockFileIsLocked) // NOLINT
+TEST_F(TestUpdateSchedulerProcessorHelperMethods, isSuldownloaderRunningReturnsTrueWhenLockFileIsLocked)
 {
     auto lockPath = Common::ApplicationConfiguration::applicationPathManager().getSulDownloaderLockFilePath();
     auto filesystemMock = new NaggyMock<MockFileSystem>();
     EXPECT_CALL(*filesystemMock, exists(lockPath)).WillRepeatedly(Return(true));
-    Tests::ScopedReplaceFileSystem ScopedReplaceFileSystem{std::unique_ptr<Common::FileSystem::IFileSystem>(filesystemMock)};
+    Tests::ScopedReplaceFileSystem ScopedReplaceFileSystem{ std::unique_ptr<Common::FileSystem::IFileSystem>(
+        filesystemMock) };
 
     auto* mockPidLockFileUtilsPtr = new NaggyMock<MockPidLockFileUtils>();
     EXPECT_CALL(*mockPidLockFileUtilsPtr, flock(_)).WillRepeatedly(Return(-1)); // Cannot take lock
-    std::unique_ptr<MockPidLockFileUtils> mockPidLockFileUtils = std::unique_ptr<MockPidLockFileUtils>(mockPidLockFileUtilsPtr);
+    std::unique_ptr<MockPidLockFileUtils> mockPidLockFileUtils =
+        std::unique_ptr<MockPidLockFileUtils>(mockPidLockFileUtilsPtr);
     Common::FileSystemImpl::replacePidLockUtils(std::move(mockPidLockFileUtils));
 
     bool running = UpdateSchedulerImpl::isSuldownloaderRunning();
     EXPECT_TRUE(running);
 }
 
-TEST_F(TestUpdateSchedulerProcessorHelperMethods, isSuldownloaderRunningReturnsFalseWhenLockFileLockCanBeTaken) // NOLINT
+TEST_F(TestUpdateSchedulerProcessorHelperMethods, isSuldownloaderRunningReturnsFalseWhenLockFileLockCanBeTaken)
 {
     auto lockPath = Common::ApplicationConfiguration::applicationPathManager().getSulDownloaderLockFilePath();
 
     auto filesystemMock = new NaggyMock<MockFileSystem>();
     EXPECT_CALL(*filesystemMock, exists(lockPath)).WillRepeatedly(Return(true));
-    Tests::ScopedReplaceFileSystem ScopedReplaceFileSystem{std::unique_ptr<Common::FileSystem::IFileSystem>(filesystemMock)};
+    Tests::ScopedReplaceFileSystem ScopedReplaceFileSystem{ std::unique_ptr<Common::FileSystem::IFileSystem>(
+        filesystemMock) };
 
     auto* mockPidLockFileUtilsPtr = new NaggyMock<MockPidLockFileUtils>();
     EXPECT_CALL(*mockPidLockFileUtilsPtr, flock(_)).WillRepeatedly(Return(0)); // Took lock.
-    EXPECT_CALL(*mockPidLockFileUtilsPtr, write(_,_,_)).WillRepeatedly(Return(1));
+    EXPECT_CALL(*mockPidLockFileUtilsPtr, write(_, _, _)).WillRepeatedly(Return(1));
     EXPECT_CALL(*mockPidLockFileUtilsPtr, unlink(_));
-    std::unique_ptr<MockPidLockFileUtils> mockPidLockFileUtils = std::unique_ptr<MockPidLockFileUtils>(mockPidLockFileUtilsPtr);
+    std::unique_ptr<MockPidLockFileUtils> mockPidLockFileUtils =
+        std::unique_ptr<MockPidLockFileUtils>(mockPidLockFileUtilsPtr);
     Common::FileSystemImpl::replacePidLockUtils(std::move(mockPidLockFileUtils));
 
     bool running = UpdateSchedulerImpl::isSuldownloaderRunning();
