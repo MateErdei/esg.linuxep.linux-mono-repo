f5793cee92814e6f4628085d0d9ef0eeb1501bfe
Peter.C <peterc@sophos.com>
2023-05-04 10:37:46 +0100

    [base] [LINUXDAR-6678,6677,6676] - Add fields to Telemetry.cpp

--- base/modules/ResponseActions/ResponsePlugin/Telemetry.cpp
+++ base/modules/ResponseActions/ResponsePlugin/Telemetry.cpp
@@ -1,13 +1,16 @@
 // Copyright 2023 Sophos Limited. All rights reserved.
 
 #include "Telemetry.h"
-
+#include "ResponseActions/RACommon/ResponseActionsCommon.h"
 #include "ApplicationPaths.h"
 #include "Logger.h"
 
 #include "Common/TelemetryHelperImpl/TelemetryHelper.h"
 
 #include <Common/UtilityImpl/StringUtils.h>
+
+using namespace ResponseActions::RACommon;
+using namespace Common::Telemetry;
 
 namespace
 {
@@ -32,9 +35,21 @@ namespace ResponsePlugin
 
     void incrementTotalActions(const std::string& type)
     {
-        if (type == "sophos.mgt.action.RunCommands")
+        if (type == RUN_COMMAND_REQUEST_TYPE)
         {
-            Common::Telemetry::TelemetryHelper::getInstance().increment("run-command-actions", 1UL);
+            TelemetryHelper::getInstance().increment("run-command-actions", 1UL);
+        }
+        else if (type == UPLOAD_FILE_REQUEST_TYPE)
+        {
+            TelemetryHelper::getInstance().increment("upload-file-count", 1UL);
+        }
+        else if (type == UPLOAD_FOLDER_REQUEST_TYPE)
+        {
+            TelemetryHelper::getInstance().increment("upload-folder-count", 1UL);
+        }
+        else if (type == DOWNLOAD_FILE_REQUEST_TYPE)
+        {
+            TelemetryHelper::getInstance().increment("download-file-count", 1UL);
         }
     }
 
@@ -42,7 +57,19 @@ namespace ResponsePlugin
     {
         if (type == "sophos.mgt.action.RunCommands")
         {
-            Common::Telemetry::TelemetryHelper::getInstance().increment("run-command-failed", 1UL);
+            TelemetryHelper::getInstance().increment("run-command-failed", 1UL);
+        }
+        else if (type == UPLOAD_FILE_REQUEST_TYPE)
+        {
+            TelemetryHelper::getInstance().increment("upload-file-overall-failures", 1UL);
+        }
+        else if (type == UPLOAD_FOLDER_REQUEST_TYPE)
+        {
+            TelemetryHelper::getInstance().increment("upload-folder-overall-failures", 1UL);
+        }
+        else if (type == DOWNLOAD_FILE_REQUEST_TYPE)
+        {
+            TelemetryHelper::getInstance().increment("download-file-overall-failures", 1UL);
         }
     }
 
@@ -50,7 +77,19 @@ namespace ResponsePlugin
     {
         if (type == "sophos.mgt.action.RunCommands")
         {
-            Common::Telemetry::TelemetryHelper::getInstance().increment("run-command-timeout-actions", 1UL);
+            TelemetryHelper::getInstance().increment("run-command-timeout-actions", 1UL);
+        }
+        else if (type == UPLOAD_FILE_REQUEST_TYPE)
+        {
+            TelemetryHelper::getInstance().increment("upload-file-timeout-failures", 1UL);
+        }
+        else if (type == UPLOAD_FOLDER_REQUEST_TYPE)
+        {
+            TelemetryHelper::getInstance().increment("upload-folder-timeout-failures", 1UL);
+        }
+        else if (type == DOWNLOAD_FILE_REQUEST_TYPE)
+        {
+            TelemetryHelper::getInstance().increment("download-file-timeout-failures", 1UL);
         }
     }
 
@@ -58,7 +97,19 @@ namespace ResponsePlugin
     {
         if (type == "sophos.mgt.action.RunCommands")
         {
-            Common::Telemetry::TelemetryHelper::getInstance().increment("run-command-expired-actions", 1UL);
+            TelemetryHelper::getInstance().increment("run-command-expired-actions", 1UL);
+        }
+        else if (type == UPLOAD_FILE_REQUEST_TYPE)
+        {
+            TelemetryHelper::getInstance().increment("upload-file-expiry-failures", 1UL);
+        }
+        else if (type == UPLOAD_FOLDER_REQUEST_TYPE)
+        {
+            TelemetryHelper::getInstance().increment("upload-folder-expiry-failures", 1UL);
+        }
+        else if (type == DOWNLOAD_FILE_REQUEST_TYPE)
+        {
+            TelemetryHelper::getInstance().increment("download-file-expiry-failures", 1UL);
         }
     }
 
