cdfa487d587e1e97e6f1581e21e10a0ff6d98e26
Mollie Bourke <mollie.bourke@sophos.com>
2023-05-04 14:49:47 +0100

    [spl-tools] [LINUXDAR-6995] Upload fixed file

--- spl-tools/performance/RunPerfTests.py
+++ spl-tools/performance/RunPerfTests.py
@@ -846,12 +846,12 @@ def run_response_actions_download_files_test(region, env, tenant_id):
     log_utils = LogUtils.LogUtils()
     log_utils.mark_ra_action_runner_log()
 
-    file_to_download = "/root/performance/ra_test_file"
+    file_to_download = os.path.join(os.path.dirname(os.path.realpath(__file__)), f"ra_test_file")
     size = os.path.getsize(file_to_download)
     sha256 = hashlib.sha256(open(file_to_download, 'rb').read()).hexdigest()
 
     for i in range(10):
-        filepath = os.path.join(SCRIPT_DIR, f"test_file_{i}")
+        filepath = os.path.join(SCRIPT_DIR, f"downloaded_test_file_{i}")
 
         res = download_response_actions_file(region=region, env=env, tenant_id=tenant_id, endpoint_id=get_endpoint_id(),
                                              file_path=filepath, size=size, sha256=sha256)
@@ -894,7 +894,7 @@ def run_response_actions_upload_files_test(region, env, tenant_id):
 
     start_time = get_current_unix_epoch_in_seconds()
     for i in range(10):
-        file_to_upload = "/root/performance/ra_test_file"
+        file_to_upload = os.path.join(os.path.dirname(os.path.realpath(__file__)), f"ra_test_file")
 
         res = upload_response_actions_file(region=region, env=env, tenant_id=tenant_id, endpoint_id=get_endpoint_id(),
                                            file_path=file_to_upload)
@@ -904,7 +904,7 @@ def run_response_actions_upload_files_test(region, env, tenant_id):
         logging.debug(action_status)
 
         if action_status["endpoints"][0]["result"] != "succeeded":
-            logging.warning(f"Failed to upload response actions file {file_to_upload}")
+            logging.warning(f"Failed to upload response actions file")
             failed_file_uploads += 1
     end_time = get_current_unix_epoch_in_seconds()
 
