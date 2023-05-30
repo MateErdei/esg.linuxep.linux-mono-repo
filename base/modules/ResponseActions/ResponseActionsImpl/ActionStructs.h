d4e75ea769c2571caea3e7a2da58b0e9a74c9f58
Peter.C <peterc@sophos.com>
2023-05-30 14:38:54 +0100

    [base] [LINUXDAR-7051] - Rename UploadType to ActionType and include all actions, map each one to a string to use in log messages.

--- base/modules/ResponseActions/ResponseActionsImpl/ActionStructs.h
+++ base/modules/ResponseActions/ResponseActionsImpl/ActionStructs.h
@@ -1,9 +1,15 @@
 // Copyright 2023 Sophos Limited. All rights reserved.
 
 #pragma once
+#include "ResponseActions/RACommon/ResponseActionsCommon.h"
+
 #include <string>
+#include <vector>
 namespace ResponseActionsImpl
 {
+
+    // Upload action
+
     struct UploadInfo
     {
         std::string targetPath;
@@ -14,12 +20,23 @@ namespace ResponseActionsImpl
         int timeout = 0;
         int maxSize = 0;
     };
-    enum class UploadType
+
+    enum class ActionType
     {
-        FILE,
-        FOLDER
+        UPLOADFILE,
+        UPLOADFOLDER,
+        DOWNLOAD,
+        COMMAND
     };
 
+    static const std::map<ActionType, std::string> actionTypeStrMap {
+        {ActionType::UPLOADFILE, "UploadFile"},
+        {ActionType::UPLOADFOLDER, "UploadFolder"},
+        {ActionType::DOWNLOAD, "Download"},
+        {ActionType::COMMAND, "Run Command"}
+    };
+
+    //Download Action
     struct DownloadInfo
     {
         std::string targetPath;
@@ -31,4 +48,32 @@ namespace ResponseActionsImpl
         u_int64_t expiration = 0;
         int timeout = 0;
     };
-}
+
+    // Run command action
+
+    struct CommandRequest
+    {
+        std::vector<std::string> commands;
+        long timeout;
+        bool ignoreError;
+        u_int64_t expiration = 0;
+    };
+
+    struct SingleCommandResult
+    {
+        std::string stdOut;
+        std::string stdErr;
+        int exitCode = 0;
+        unsigned long duration = 0;
+    };
+
+    struct CommandResponse
+    {
+        std::string type = "sophos.mgt.response.RunCommands";
+        ResponseActions::RACommon::ResponseResult result;
+        std::vector<SingleCommandResult> commandResults;
+        unsigned long startedAt = 0;
+        unsigned long duration = 0;
+    };
+
+} // namespace ResponseActionsImpl
