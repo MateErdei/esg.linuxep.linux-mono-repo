9c4e2e66d81877169e54972aae26cd98bd7b7378
Alex Cooper <alex.cooper@sophos.com>
2023-06-01 14:42:51 +0100

    [base] [LINUXDAR-7053] Limit build threads to handle running on WSL with only 16GB RAM but with laptops with 16 cores, 15 build threads will use a lot of memory and trigger OOM in WSL, causing the build or CLion to be killed by the OS.

--- base/build.sh
+++ base/build.sh
@@ -295,6 +295,10 @@ function build()
 
     [[ -n ${NPROC:-} ]] || { which nproc > /dev/null 2>&1 && NPROC=$((`nproc`)); } || NPROC=2
     (( $NPROC < 1 )) && NPROC=1
+
+    # Some dev laptops have 16 cores but only 32GB RAM (16GB for WSL) and 15 build threads will trigger OOM
+    (( $NPROC > 8 )) && NPROC=7
+
     cmake -G "Unix Makefiles" \
           -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" \
           -DNO_GCOV="true" \
