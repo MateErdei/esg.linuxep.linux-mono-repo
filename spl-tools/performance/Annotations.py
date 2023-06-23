045986dffaf4a0152a943f94bbe229fbcbff8eb0
Mollie Bourke <mollie.bourke@sophos.com>
2023-06-23 09:34:34 +0100

    [spl-tools] [LINUXDAR-5712] TMP - Add test tag to prevent polluting graphs while developing

--- spl-tools/performance/Annotations.py
+++ spl-tools/performance/Annotations.py
@@ -18,7 +18,7 @@ def add_annotation(tag, start_time, text, end_time=None):
     annotation_json = {
         "time": start_time,
         "timeEnd": end_time if end_time else start_time,
-        "tags": [hostname, tag],
+        "tags": [hostname, tag, "test"],
         "text": text
     }
 
