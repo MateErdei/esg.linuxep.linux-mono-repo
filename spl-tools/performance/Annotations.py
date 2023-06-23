c5e780b9a2b2401077e2b6addfae5b074f99a0c3
Mollie Bourke <mollie.bourke@sophos.com>
2023-06-23 10:07:36 +0100

    [spl-tools] [LINUXDAR-5712] Don't exit on first annotation

--- spl-tools/performance/Annotations.py
+++ spl-tools/performance/Annotations.py
@@ -4,6 +4,7 @@ import socket
 import requests
 
 import LogUtils
+
 LOG_UTILS = LogUtils.LogUtils()
 
 
@@ -29,16 +30,17 @@ def add_annotation(tag, start_time, text, end_time=None):
     if r.status_code not in [200, 201]:
         print(f"Failed to store test result: {str(annotation_json)}")
         print(f"Status code: {r.status_code}, text: {r.text}")
-        exit(1)
+        return 1
     else:
         print(f"Annotation Content: {annotation_json}")
-        exit(0)
+        return 0
 
 
 def add_product_update_annotations():
+    annotation_failures = 0
     update_info = LogUtils.get_chunks_from_log(LOG_UTILS.suldownloader_log,
-                                      "Doing product and supplement update",
-                                      "Generating the report file in")
+                                               "Doing product and supplement update",
+                                               "Generating the report file in")
     for update_chunk in update_info:
         # Ignore hourly updates where product is not upgraded
         if "Product installed" not in "".join(update_chunk):
@@ -52,20 +54,33 @@ def add_product_update_annotations():
                     component_version = log_line.split(":")[-1].strip()
                     update_info_text += f"Upgraded {component} to {component_version}\n"
 
-        add_annotation(tag="product-upgrade", start_time=start_time, end_time=end_time, text=update_info_text)
+        annotation_failures += add_annotation(tag="product-upgrade",
+                                              start_time=start_time,
+                                              end_time=end_time,
+                                              text=update_info_text)
+    return annotation_failures
 
 
 def add_scheduled_scan_annotations():
+    annotation_failures = 0
     scan_info = LogUtils.get_chunks_from_log(LOG_UTILS.av_log,
-                                    "Starting scan Sophos Cloud Scheduled Scan",
-                                    "Sending scan complete event to Central")
+                                             "Starting scan Sophos Cloud Scheduled Scan",
+                                             "Sending scan complete event to Central")
 
     for scan_chunk in scan_info:
         start_time = LogUtils.get_epoch_time_from_log_line(scan_chunk[0])
         end_time = LogUtils.get_epoch_time_from_log_line(scan_chunk[-1])
-        add_annotation(tag="scheduled-scan", start_time=start_time, end_time=end_time, text="Scheduled Scan")
+        annotation_failures += add_annotation(tag="scheduled-scan",
+                                              start_time=start_time,
+                                              end_time=end_time,
+                                              text="Scheduled Scan")
+    return annotation_failures
 
 
 def annotate_graphs():
-    add_product_update_annotations()
-    add_scheduled_scan_annotations()
+    annotation_failures = 0
+    annotation_failures += add_product_update_annotations() + \
+                           add_scheduled_scan_annotations()
+
+    if annotation_failures != 0:
+        exit(1)
