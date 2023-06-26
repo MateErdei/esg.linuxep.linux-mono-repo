bc4e36be0fe0213d6cd4fa9dcc866907e9dfc075
Douglas Leeder <douglas.leeder@sophos.com>
2023-06-26 14:40:31 +0100

    [base] [LINUXDAR-7641] Add logger for Policy.

---
+++ base/modules/Common/Policy/Logger.cpp
@@ -0,0 +1,11 @@
+// Copyright 2023 Sophos All rights reserved.
+
+#include "Logger.h"
+
+#include <Common/Logging/LoggerConfig.h>
+
+log4cplus::Logger& getPolicyLogger()
+{
+    static log4cplus::Logger STATIC_LOGGER = Common::Logging::getInstance("Policy");
+    return STATIC_LOGGER;
+}
---
+++ base/modules/Common/Policy/Logger.h
@@ -0,0 +1,15 @@
+// Copyright 2023 Sophos All rights reserved.
+
+#pragma once
+
+#include <Common/Logging/SophosLoggerMacros.h>
+
+log4cplus::Logger& getPolicyLogger();
+
+#define LOGDEBUG(x) LOG4CPLUS_DEBUG(getPolicyLogger(), x) // NOLINT
+#define LOGINFO(x) LOG4CPLUS_INFO(getPolicyLogger(), x) // NOLINT
+#define LOGSUPPORT(x) LOG4CPLUS_SUPPORT(getPolicyLogger(), x) // NOLINT
+#define LOGWARN(x) LOG4CPLUS_WARN(getPolicyLogger(), x) // NOLINT
+#define LOGERROR(x) LOG4CPLUS_ERROR(getPolicyLogger(), x) // NOLINT
+#define LOGFATAL(x) LOG4CPLUS_FATAL(getPolicyLogger(), x) // NOLINT
+
