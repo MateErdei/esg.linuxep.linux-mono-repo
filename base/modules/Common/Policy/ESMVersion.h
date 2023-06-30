1a1c8309b63d63fe1106a4b717363823cba11626
Peter.C <peterc@sophos.com>
2023-06-30 14:23:06 +0100

    [base] [LINUXDAR-7448] - Add a ESMVersion struct so it can do the validation

---
+++ base/modules/Common/Policy/ESMVersion.h
@@ -0,0 +1,23 @@
+// Copyright 2023 Sophos All rights reserved.
+
+#pragma once
+
+#include <string>
+
+struct ESMVersion {
+    ESMVersion(std::string name, std::string token) :
+        name_(std::move(name)),
+        token_(std::move(token))
+    {}
+
+    std::string name_;
+    std::string token_;
+
+    bool isValid()
+    {
+        bool emptyName = name_.empty();
+        bool emptyToken = token_.empty();
+
+        return emptyName == emptyToken;
+    }
+};
