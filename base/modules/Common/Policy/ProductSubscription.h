5d37d70b8af383b4b1895437d9d47c84afe21d28
Peter.C <peterc@sophos.com>
2023-06-30 13:44:42 +0100

    [base] Revert "[LINUXDAR-7448] - ProductSubscription has esmVersionToken field"
    This reverts commit 2c0bb2da9efb7db0d5cf00cbf8223333ae0bc380.

--- base/modules/Common/Policy/ProductSubscription.h
+++ base/modules/Common/Policy/ProductSubscription.h
@@ -16,13 +16,11 @@ namespace Common::Policy
                 std::string rigidName,
                 std::string baseVersion,
                 std::string tag,
-                std::string fixedVersion,
-                std::string esmVersionToken) :
+                std::string fixedVersion) :
                 rigidName_(std::move(rigidName)),
                 baseVersion_(std::move(baseVersion)),
                 tag_(std::move(tag)),
-                fixedVersion_(std::move(fixedVersion)),
-                esmVersionToken_(std::move(esmVersionToken))
+                fixedVersion_(std::move(fixedVersion))
             {}
 
             ProductSubscription() = default;
@@ -30,13 +28,12 @@ namespace Common::Policy
             [[nodiscard]] const std::string& baseVersion() const { return baseVersion_; }
             [[nodiscard]] const std::string& tag() const { return tag_; }
             [[nodiscard]] const std::string& fixedVersion() const { return fixedVersion_; }
-            [[nodiscard]] const std::string& esmVersionToken() const { return esmVersionToken_; }
 
             [[nodiscard]] std::string toString() const
             {
                 std::stringstream s;
                 s << "name = " << rigidName_ << " baseversion = " << baseVersion_ << " tag = " << tag_
-                  << " fixedversion = " << fixedVersion_ << " esmversion = " << esmVersionToken_;
+                  << " fixedversion = " << fixedVersion_;
                 return s.str();
             }
 
@@ -46,8 +43,7 @@ namespace Common::Policy
                     (rigidName_ == rhs.rigidName_) &&
                     (baseVersion_ == rhs.baseVersion_) &&
                     (tag_ == rhs.tag_) &&
-                    (fixedVersion_ == rhs.fixedVersion_) &&
-                    (esmVersionToken_ == rhs.esmVersionToken_));
+                    (fixedVersion_ == rhs.fixedVersion_));
             }
 
             bool operator!=(const ProductSubscription& rhs) const { return !operator==(rhs); }
@@ -57,6 +53,5 @@ namespace Common::Policy
             std::string baseVersion_;
             std::string tag_;
             std::string fixedVersion_;
-            std::string esmVersionToken_;
     };
 }
