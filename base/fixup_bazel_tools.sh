0cdde4a80914bbb08b386bc8d9552a7f0ae1b82e
Krzysztof Oramus <krzysztof.oramus@sophos.com>
2023-07-03 17:13:47 +0100

    [base] [LINUXDAR-6783] Fix the carriage return remover

--- base/fixup_bazel_tools.sh
+++ base/fixup_bazel_tools.sh
@@ -3,5 +3,5 @@
 # This is needed because bazel-tools exported from monorepo are exported on a Windows machine,
 # so some things are not quite as they should be to work on Linux
 
-sed -i 's/\r\n$/\n/' tools/aspects/wrapper.sh
+sed -i 's/\r//' tools/aspects/wrapper.sh
 chmod +x tools/aspects/wrapper.sh
