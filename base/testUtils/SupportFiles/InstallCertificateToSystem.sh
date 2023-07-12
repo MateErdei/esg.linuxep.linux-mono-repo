a850110d61e52b84ece7ab50c127e5237d1b62a1
DarrenASmith <darren.smith@sophos.com>
2023-07-12 11:13:52 +0100

    [base] Retry updating of CA certificates and fix exit when copy fails

--- base/testUtils/SupportFiles/InstallCertificateToSystem.sh
+++ base/testUtils/SupportFiles/InstallCertificateToSystem.sh
@@ -20,22 +20,39 @@ install_cert()
   fi
 }
 
+update_ca_certs_with_retry()
+{
+  local attempt
+  for (( attempt=1; attempt <= 10; ++attempt ))
+  do
+    update-ca-certificates -f
+    if [ $? -eq 0 ]
+    then
+      echo Successfully updated CA certificates
+      return 0
+    fi
+    echo Attempt to update CA certificates $attempt failed - will retry in 1 second
+    sleep 1
+  done
+  return 1
+}
+
 install_cert_rhel()
 {
-	cp $1 /etc/pki/ca-trust/source/anchors/SOPHOS$(basename $1) || ( echo "Failed to copy cert" && exit 1 )
+	cp $1 /etc/pki/ca-trust/source/anchors/SOPHOS$(basename $1) || { echo "Failed to copy cert"; exit 1; }
 	update-ca-trust extract
 }
 
 install_cert_ubuntu()
 {
-	cp $1 /usr/local/share/ca-certificates/SOPHOS$(basename $1) || ( echo "Failed to copy cert" && exit 1 )
-	update-ca-certificates -f
+	cp $1 /usr/local/share/ca-certificates/SOPHOS$(basename $1) || { echo "Failed to copy cert"; exit 1; }
+	update_ca_certs_with_retry
 }
 
 install_cert_sles()
 {
-	cp $1 /usr/share/pki/trust/anchors/SOPHOS$(basename $1) || ( echo "Failed to copy cert" && exit 1 )
-	update-ca-certificates -f
+	cp $1 /usr/share/pki/trust/anchors/SOPHOS$(basename $1) || { echo "Failed to copy cert"; exit 1; }
+  update_ca_certs_with_retry
 }
 
 CERT=$1
