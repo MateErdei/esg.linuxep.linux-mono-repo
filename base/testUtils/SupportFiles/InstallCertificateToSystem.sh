9ad979743419284ac08ea9299187351c6fbb31d0
DarrenASmith <darren.smith@sophos.com>
2023-07-12 12:22:25 +0100

    [base] Replace tabs with spaces to make indentation consistent

--- base/testUtils/SupportFiles/InstallCertificateToSystem.sh
+++ base/testUtils/SupportFiles/InstallCertificateToSystem.sh
@@ -3,14 +3,14 @@ set -x
 
 install_cert()
 {
-	certificate=$1
+  certificate=$1
 
-	if [ -n "$(which apt-get)" ]
-	then
-		install_cert_ubuntu $certificate || ( echo "cert install failed" && exit 1 )
-	elif [ -n "$(which yum)" ]
-	then
-		install_cert_rhel $certificate || ( echo "cert install failed" && exit 1 )
+  if [ -n "$(which apt-get)" ]
+  then
+    install_cert_ubuntu $certificate || ( echo "cert install failed" && exit 1 )
+  elif [ -n "$(which yum)" ]
+  then
+    install_cert_rhel $certificate || ( echo "cert install failed" && exit 1 )
   elif [ -n "$(which zypper)" ]
   then
     install_cert_sles $certificate || ( echo "cert install failed" && exit 1 )
@@ -39,25 +39,26 @@ update_ca_certs_with_retry()
 
 install_cert_rhel()
 {
-	cp $1 /etc/pki/ca-trust/source/anchors/SOPHOS$(basename $1) || { echo "Failed to copy cert"; exit 1; }
-	update-ca-trust extract
+  cp $1 /etc/pki/ca-trust/source/anchors/SOPHOS$(basename $1) || { echo "Failed to copy cert"; exit 1; }
+  update-ca-trust extract
 }
 
 install_cert_ubuntu()
 {
-	cp $1 /usr/local/share/ca-certificates/SOPHOS$(basename $1) || { echo "Failed to copy cert"; exit 1; }
-	update_ca_certs_with_retry
+  cp $1 /usr/local/share/ca-certificates/SOPHOS$(basename $1) || { echo "Failed to copy cert"; exit 1; }
+  update_ca_certs_with_retry
 }
 
 install_cert_sles()
 {
-	cp $1 /usr/share/pki/trust/anchors/SOPHOS$(basename $1) || { echo "Failed to copy cert"; exit 1; }
+  cp $1 /usr/share/pki/trust/anchors/SOPHOS$(basename $1) || { echo "Failed to copy cert"; exit 1; }
   update_ca_certs_with_retry
 }
 
 CERT=$1
 echo args: $@
-if [ -f $CERT ]; then
+if [ -f $CERT ]
+then
     install_cert $1
 else
     echo "usage: InstallCertificateToSystem.sh  <path_to_certificate>" && exit 1
