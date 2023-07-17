86c9e36e7ecbe8cf2a7876c587e22b24b89a63ca
Peter.C <peterc@sophos.com>
2023-07-17 17:56:12 +0100

    [base] [LINUXDAR-7696] - Add more unit tests for ConfigurationDataUtils

--- base/tests/SulDownloader/TestConfigurationDataUtils.cpp
+++ base/tests/SulDownloader/TestConfigurationDataUtils.cpp
@@ -19,6 +19,7 @@ static Common::Policy::UpdateSettings getValidUpdateSettings()
 
 
 //checkIfShouldForceUpdate
+//esm
 TEST(TestConfigurationDataUtils, returnFalseIfESMVersionIsSame)
 {
     auto newSettings = getValidUpdateSettings();
@@ -48,6 +49,114 @@ TEST(TestConfigurationDataUtils, returnTrueIfOneFieldForESMVersionChanges)
     auto newSettings = getValidUpdateSettings();
     newSettings.setEsmVersion(ESMVersion("name", "token"));
     auto previousSettings = getValidUpdateSettings();
-    newSettings.setEsmVersion(ESMVersion("name", "oldtoken"));
+    previousSettings.setEsmVersion(ESMVersion("name", "oldtoken"));
+    EXPECT_TRUE(ConfigurationDataUtil::checkIfShouldForceUpdate(newSettings, previousSettings));
+}
+
+
+//primary subscription
+TEST(TestConfigurationDataUtils, returnsFalseIfPrimarySubDoesntChange)
+{
+    auto newSettings = getValidUpdateSettings();
+    newSettings.setPrimarySubscription({"RIGID", "Base", "tag", "fixed"});
+    auto previousSettings = getValidUpdateSettings();
+    previousSettings.setPrimarySubscription({"RIGID", "Base", "tag", "fixed"});
+    EXPECT_FALSE(ConfigurationDataUtil::checkIfShouldForceUpdate(newSettings, previousSettings));
+}
+
+TEST(TestConfigurationDataUtils, returnFalseIfOnlyBaseChanges)
+{
+    auto newSettings = getValidUpdateSettings();
+    newSettings.setPrimarySubscription({"RIGID", "Base", "tag", "fixed"});
+    auto previousSettings = getValidUpdateSettings();
+    previousSettings.setPrimarySubscription({"RIGID", "BaseOther", "tag", "fixed"});
+    EXPECT_FALSE(ConfigurationDataUtil::checkIfShouldForceUpdate(newSettings, previousSettings));
+}
+
+TEST(TestConfigurationDataUtils, returnTrueIfRigidVersionChanges)
+{
+    auto newSettings = getValidUpdateSettings();
+    newSettings.setPrimarySubscription({"RIGID", "Base", "tag", "fixed"});
+    auto previousSettings = getValidUpdateSettings();
+    previousSettings.setPrimarySubscription({"RIGIDOther", "Base", "tag", "fixed"});
+    EXPECT_TRUE(ConfigurationDataUtil::checkIfShouldForceUpdate(newSettings, previousSettings));
+}
+
+TEST(TestConfigurationDataUtils, returnTrueIfTagChanges)
+{
+    auto newSettings = getValidUpdateSettings();
+    newSettings.setPrimarySubscription({"RIGID", "Base", "tag", "fixed"});
+    auto previousSettings = getValidUpdateSettings();
+    previousSettings.setPrimarySubscription({"RIGID", "Base", "tagother", "fixed"});
+    EXPECT_TRUE(ConfigurationDataUtil::checkIfShouldForceUpdate(newSettings, previousSettings));
+}
+
+TEST(TestConfigurationDataUtils, returnTrueIfFixedChanges)
+{
+    auto newSettings = getValidUpdateSettings();
+    newSettings.setPrimarySubscription({"RIGID", "Base", "tag", "fixed"});
+    auto previousSettings = getValidUpdateSettings();
+    previousSettings.setPrimarySubscription({"RIGID", "Base", "tag", "fixedother"});
+    EXPECT_TRUE(ConfigurationDataUtil::checkIfShouldForceUpdate(newSettings, previousSettings));
+}
+
+
+//product subscription
+TEST(TestConfigurationDataUtils, returnsFalseIfProductSubDoesntChange)
+{
+    ProductSubscription productSubscription("RIGID", "Base", "tag", "fixed");
+
+    auto newSettings = getValidUpdateSettings();
+    newSettings.setProductsSubscription({productSubscription});
+    auto previousSettings = getValidUpdateSettings();
+    previousSettings.setProductsSubscription({productSubscription});
+    EXPECT_FALSE(ConfigurationDataUtil::checkIfShouldForceUpdate(newSettings, previousSettings));
+}
+
+TEST(TestConfigurationDataUtils, returnFalseIfOnlyProductBaseChanges)
+{
+    ProductSubscription newProductSubscription("RIGID", "Base", "tag", "fixed");
+    ProductSubscription previousProductSubscription("RIGID", "BaseOther", "tag", "fixed");
+
+    auto newSettings = getValidUpdateSettings();
+    newSettings.setProductsSubscription({newProductSubscription});
+    auto previousSettings = getValidUpdateSettings();
+    previousSettings.setProductsSubscription({previousProductSubscription});
+    EXPECT_FALSE(ConfigurationDataUtil::checkIfShouldForceUpdate(newSettings, previousSettings));
+}
+
+TEST(TestConfigurationDataUtils, returnFalseIfProductRigidVersionChanges)
+{
+    ProductSubscription newProductSubscription("RIGID", "Base", "tag", "fixed");
+    ProductSubscription previousProductSubscription("RIGIDOther", "Base", "tag", "fixed");
+
+    auto newSettings = getValidUpdateSettings();
+    newSettings.setProductsSubscription({newProductSubscription});
+    auto previousSettings = getValidUpdateSettings();
+    previousSettings.setProductsSubscription({previousProductSubscription});
+    EXPECT_FALSE(ConfigurationDataUtil::checkIfShouldForceUpdate(newSettings, previousSettings));
+}
+
+TEST(TestConfigurationDataUtils, returnTrueIfProductTagAndRigidChanges)
+{
+    ProductSubscription newProductSubscription("RIGID", "Base", "tag", "fixed");
+    ProductSubscription previousProductSubscription("RIGID", "Base", "tagother", "fixed");
+
+    auto newSettings = getValidUpdateSettings();
+    newSettings.setProductsSubscription({newProductSubscription});
+    auto previousSettings = getValidUpdateSettings();
+    previousSettings.setProductsSubscription({previousProductSubscription});
+    EXPECT_TRUE(ConfigurationDataUtil::checkIfShouldForceUpdate(newSettings, previousSettings));
+}
+
+TEST(TestConfigurationDataUtils, returnTrueIfProductFixedChanges)
+{
+    ProductSubscription newProductSubscription("RIGID", "Base", "tag", "fixed");
+    ProductSubscription previousProductSubscription("RIGID", "Base", "tag", "fixedother");
+
+    auto newSettings = getValidUpdateSettings();
+    newSettings.setProductsSubscription({newProductSubscription});
+    auto previousSettings = getValidUpdateSettings();
+    previousSettings.setProductsSubscription({previousProductSubscription});
     EXPECT_TRUE(ConfigurationDataUtil::checkIfShouldForceUpdate(newSettings, previousSettings));
 }
