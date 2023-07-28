68be5f5096e7cd1275c3fab43d9cd1c2074d77c1
ShwarmaCNS <ayush.sharma@sophos.com>
2023-07-28 09:26:11 +0100

    [base] [LINUXDAR-7439] Added common function for try block in NestedExceptions.h

--- base/modules/Common/Exceptions/NestedExceptions.h
+++ base/modules/Common/Exceptions/NestedExceptions.h
@@ -11,24 +11,31 @@ namespace Common
             static std::string expandException(const std::exception& ex, int level);
             static std::string expandException(const IException& ex, int level);
 
-            static std::string expandException(const IException& ex, int level = 0)
+            std::string rethrowIfPossible(const std::exception& ex, int level)
             {
-                std::stringstream exceptionStringStream;
-                exceptionStringStream << std::string(level, ' ')
-                                      << "Nested exception expansion: " << ex.what_with_location() << '\n';
-
                 try
                 {
                     std::rethrow_if_nested(ex);
                 }
                 catch (const IException& nestedEx)
                 {
-                    exceptionStringStream << expandException(nestedEx, level + 1);
+                    return expandException(nestedEx, level + 1);
                 }
                 catch (const std::exception& nestedEx)
                 {
-                    exceptionStringStream << expandException(nestedEx, level + 1);
+                    return expandException(nestedEx, level + 1);
                 }
+                return "";
+            }
+
+            static std::string expandException(const IException& ex, int level = 0)
+            {
+                std::stringstream exceptionStringStream;
+                exceptionStringStream << std::string(level, ' ')
+                                      << "Nested exception expansion: "
+                                      << ex.what_with_location()
+                                      << '\n'
+                                      << rethrowIfPossible(ex, level);
 
                 return exceptionStringStream.str();
             }
@@ -36,20 +43,11 @@ namespace Common
             static std::string expandException(const std::exception& ex, int level = 0)
             {
                 std::stringstream exceptionStringStream;
-                exceptionStringStream << std::string(level, ' ') << "Nested exception expansion: " << ex.what() << '\n';
-
-                try
-                {
-                    std::rethrow_if_nested(ex);
-                }
-                catch (const IException& nestedEx)
-                {
-                    exceptionStringStream << expandException(nestedEx, level + 1);
-                }
-                catch (const std::exception& nestedEx)
-                {
-                    exceptionStringStream << expandException(nestedEx, level + 1);
-                }
+                exceptionStringStream << std::string(level, ' ')
+                                      << "Nested exception expansion: "
+                                      << ex.what()
+                                      << '\n'
+                                      << rethrowIfPossible(ex, level);
 
                 return exceptionStringStream.str();
             }
