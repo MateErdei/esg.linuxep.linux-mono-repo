5feeee3f97b9ad55fd0d2b66f9dd4c08b2e27dca
Douglas Leeder <douglas.leeder@sophos.com>
2023-08-01 16:49:50 +0100

    [av] [LINUXDAR-7502] Fix CORC fuzzer to actually be able to run!

--- av/tests/plugin/CorcPolicyProcessorFuzzer.cpp
+++ av/tests/plugin/CorcPolicyProcessorFuzzer.cpp
@@ -4,6 +4,7 @@
 #include "pluginimpl/PolicyProcessor.h"
 #include "pluginimpl/TaskQueue.h"
 
+#include "Common/ApplicationConfiguration/IApplicationConfiguration.h"
 #include "Common/FileSystem/IFileSystem.h"
 #include "Common/Logging/ConsoleLoggingSetup.h"
 #include "Common/XmlUtilities/AttributesMap.h"
@@ -14,17 +15,31 @@
 #define ERROR(x) std::cerr << x << '\n'
 #define PRINT(x) std::cout << x << '\n'
 
+static void setPLUGIN_INSTALL()
+{
+    // Set PLUGIN_INSTALL so that the code doesn't fail immediately
+    auto& appConfig = Common::ApplicationConfiguration::applicationConfiguration();
+    appConfig.setData("PLUGIN_INSTALL", "/CorcPolicyProcessorFuzzer");
+}
+
+static void testFuzzString(const std::string& fuzzString)
+{
+    setPLUGIN_INSTALL();
+    auto parsedFuzzString = Common::XmlUtilities::parseXml(fuzzString);
+    auto taskQueue = std::make_shared<Plugin::TaskQueue>();
+    Plugin::PolicyProcessor policyProcessor(taskQueue);
+    policyProcessor.processCorcPolicy(parsedFuzzString);
+}
+
 #ifdef USING_LIBFUZZER
 
 static int fuzzCorcPolicy(const uint8_t *Data, size_t Size)
 {
+
     std::string fuzzString(reinterpret_cast<const char*>(Data), Size);
     try
     {
-        auto parsedFuzzString = Common::XmlUtilities::parseXml(fuzzString);
-        auto taskQueue = std::make_shared<Plugin::TaskQueue>();
-        Plugin::PolicyProcessor policyProcessor(taskQueue);
-        policyProcessor.processCorcPolicy(parsedFuzzString);
+        testFuzzString(fuzzString);
     }
     catch (Common::XmlUtilities::XmlUtilitiesException& e)
     {
@@ -52,20 +67,17 @@ int main(int argc, char* argv[])
     if( argc < 2 )
     {
         ERROR("missing arg");
-        return 1;
+        return 2;
     }
 
     if (sophos_filesystem::is_regular_file(argv[1]))
     {
-        std::string fuzzString = Common::FileSystem::fileSystem()->readFile(argv[1]);
-        auto parsedFuzzString = Common::XmlUtilities::parseXml(fuzzString);
-        auto taskQueue = std::make_shared<Plugin::TaskQueue>();
-        Plugin::PolicyProcessor policyProcessor(taskQueue);
-        policyProcessor.processCorcPolicy(parsedFuzzString);
+        auto fuzzString = Common::FileSystem::fileSystem()->readFile(argv[1]);
+        testFuzzString(fuzzString);
     }
     else
     {
-        ERROR("Error: not a file ");
+        ERROR("Error: not a file " << argv[1]);
         return 1;
     }
 
