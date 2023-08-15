5ff035b29c823838a66bde743541978d3a382449
Douglas Leeder <douglas.leeder@sophos.com>
2023-08-15 10:41:33 +0100

    [base] [LINUXDAR-7794] Fix up registerCentral for spaces in install path.

--- base/products/Base/commandline/registerCentral.in.sh
+++ base/products/Base/commandline/registerCentral.in.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
+# Copyright 2018-2023 Sophos Limited. All rights reserved.
 
-
-STARTINGDIR=$(pwd)
-SCRIPTDIR=${0%/*}
+STARTINGDIR="$(pwd)"
+SCRIPTDIR="${0%/*}"
 if [[ "$SCRIPTDIR" == "$0" ]]
 then
-    SCRIPTDIR=${STARTINGDIR}
+    SCRIPTDIR="${STARTINGDIR}"
 fi
 
-ABS_SCRIPTDIR=$(cd $SCRIPTDIR && pwd)
+ABS_SCRIPTDIR=$(cd "$SCRIPTDIR" && pwd)
 
-BASE_DIR=${ABS_SCRIPTDIR%/*}
+BASE_DIR="${ABS_SCRIPTDIR%/*}"
 export BASE_DIR
 
-INST_DIR=${BASE_DIR%/*}
+INST_DIR="${BASE_DIR%/*}"
 export INST_DIR
 
-BIN_DIR=${BASE_DIR}/bin
+BIN_DIR="${BASE_DIR}/bin"
 
-pythonzip=$BASE_DIR/lib64/@PYTHON_ZIP@
-mcsrouterzip=$BASE_DIR/lib64/mcsrouter.zip
-pythonExecutable=@PRODUCT_PYTHON_EXECUTABLE@
+pythonzip="$BASE_DIR/lib64/@PYTHON_ZIP@"
+mcsrouterzip="$BASE_DIR/lib64/mcsrouter.zip"
+pythonExecutable="@PRODUCT_PYTHON_EXECUTABLE@"
 
-export PYTHONPATH=@PYTHONPATH@
-export PYTHONHOME=@PRODUCT_PYTHONHOME@
-export LD_LIBRARY_PATH=@PRODUCT_PYTHON_LD_LIBRARY_PATH@
+export PYTHONPATH="@PYTHONPATH@"
+export PYTHONHOME="@PRODUCT_PYTHONHOME@"
+export LD_LIBRARY_PATH="@PRODUCT_PYTHON_LD_LIBRARY_PATH@"
 
-cd ${BASE_DIR}/../tmp
-exec $pythonExecutable  @PYTHON_ARGS_FOR_PROD@  -m mcsrouter.register_central  "$@"
+cd "${BASE_DIR}/../tmp"
+exec "$pythonExecutable"  @PYTHON_ARGS_FOR_PROD@  -m mcsrouter.register_central  "$@"
