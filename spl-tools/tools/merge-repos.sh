c02a822532f28a055abbcbabf9cf5afe9b1ce1e5
Alex Cooper <alex.cooper@sophos.com>
2023-08-17 15:20:47 +0100

    merge-repos.sh - Use rebase not merge in case it handles future issues better and also make it easier/quicker to re-run.

--- spl-tools/tools/merge-repos.sh
+++ spl-tools/tools/merge-repos.sh
@@ -3,6 +3,8 @@ set -e
 set -x
 
 MERGE_DIR="/home/$USER/repo_merge"
+BRANCH_CLEAN=merge-branch-clean
+BRANCH_MERGE=merge-branch-rebased
 
 function merge_in_repo() {
     mono_repo_remote=$1
@@ -18,6 +20,7 @@ function merge_in_repo() {
     rm -rf "$repo_dir"
     git clone "$remote"
     cd "$repo_dir"
+    # TODO we should check we're on develop.
     git filter-repo --to-subdirectory-filter "$subdir"
     cd "$MERGE_DIR"
 
@@ -29,15 +32,26 @@ function merge_in_repo() {
     cd "$mono_repo_dir"
     # Make sure we have a merge branch
     git fetch
-    git branch -a | grep merge-branch || git checkout -b merge-branch
-    git checkout merge-branch
-    git pull
+    git branch -a | grep $BRANCH_CLEAN || git checkout -b $BRANCH_CLEAN
+    git checkout $BRANCH_CLEAN
 
     # Here we just use subdir as a name for the remote
     git remote add "$subdir" "${MERGE_DIR}/${repo_dir}"
     git fetch "$subdir" --tags
     git merge --no-edit --allow-unrelated-histories "$subdir"/develop # Merge in develop
     git remote remove "$subdir"
+}
+
+function rebase()
+{
+    mono_repo_remote=$1
+    mono_repo_dir=$(python -c "print('$mono_repo_remote'.split('/')[1].split('.git')[0])")
+    cd "$MERGE_DIR/$mono_repo_dir"
+    branch_to_rebase=$2
+    branch_to_rebase_onto=$3
+    echo "Rebase $branch_to_rebase onto $branch_to_rebase_onto"
+    git checkout "$branch_to_rebase"
+    git rebase "$branch_to_rebase_onto"
 }
 
 # When set to 1 we will remove the working merge dir, default this to off in case we have changes to keep.
@@ -80,9 +94,27 @@ fi
 
 # Note - To make this smoother you can cache your ssh key passphrase using ssh-agent and adding your private key
 # Start ssh-agent:
-# eval `ssh-agent -s`
-# Add private key:
-# ssh-add ~/.ssh/id_alexcooper
+if ps -p $SSH_AGENT_PID > /dev/null
+then
+   echo "ssh-agent is already running"
+   # Do something knowing the pid exists, i.e. the process with $PID is running
+else
+    eval `ssh-agent -s`
+
+    if [ -f ./path_to_git_ssh_key ]
+    then
+        #tr -d '\n' < yourfile.txt
+        sshkey=$(cat ./path_to_git_ssh_key)
+    else
+        read -p 'git ssh key path (e.g. "/home/user/.ssh/id_my_key"): ' sshkey
+        echo -n "$sshkey" > ./path_to_git_ssh_key
+    fi
+
+    # Add private key (e.g. ssh-add /home/alex/.ssh/id_alexcooper)
+    [ -f "$sshkey" ] || exit 1
+    ssh-add "$sshkey"
+fi
+
 
 mkdir -p "$MERGE_DIR"
 
@@ -93,5 +125,7 @@ merge_in_repo "$GIT_REMOTE_MONOREPO" "git@github.com:sophos-internal/esg.linuxep
 merge_in_repo "$GIT_REMOTE_MONOREPO" "git@github.com:sophos-internal/esg.linuxep.sspl-plugin-edr-component.git" edr
 merge_in_repo "$GIT_REMOTE_MONOREPO" "git@github.com:sophos-internal/esg.linuxep.sspl-plugin-anti-virus.git" av
 merge_in_repo "$GIT_REMOTE_MONOREPO" "git@github.com:sophos-internal/esg.linuxep.sspl-plugin-event-journaler.git" eventjournaler
+
+rebase "$GIT_REMOTE_MONOREPO" "$BRANCH_MERGE" "$BRANCH_CLEAN"
 
 echo "Merge dir: $MERGE_DIR"
