2b36202edde6106006e3f6951fc895cadb5f9583
Douglas Leeder <douglas.leeder@sophos.com>
2023-08-21 11:08:57 +0100

    [av] [LINUXDAR-7767] Add test for partial reads.

--- av/tests/unixsocket/TestReadBufferAsync.cpp
+++ av/tests/unixsocket/TestReadBufferAsync.cpp
@@ -93,3 +93,40 @@ TEST_F(TestReadBufferAsync, readCompleteReal)
     char* charbuf = reinterpret_cast<char*>(buf.begin());
     EXPECT_STREQ(charbuf, EXPECTED);
 }
+
+
+TEST_F(TestReadBufferAsync, readPartial)
+{
+    auto mockSysCalls = std::make_shared<StrictMock<MockSystemCallWrapper>>();
+    unixsocket::ReadBufferAsync buffer{mockSysCalls, 512};
+
+    buffer.setLength(10);
+    ASSERT_FALSE(buffer.complete());
+
+    static const auto* EXPECTED = "123456789";
+    void* outputbuf;
+    EXPECT_CALL(*mockSysCalls, read(42, _, 10))
+        .WillOnce(DoAll(
+            SaveArg<1>(&outputbuf),
+            Invoke([&outputbuf]() { std::memcpy(outputbuf, EXPECTED, 1 ); }),
+            Return(1)
+                ));
+    EXPECT_CALL(*mockSysCalls, read(42, _, 9))
+        .WillOnce(DoAll(
+            SaveArg<1>(&outputbuf),
+            Invoke([&outputbuf]() { std::memcpy(outputbuf, EXPECTED+1, 9 ); }),
+            Return(9)
+                ));
+
+    auto ret = buffer.read(42);
+    EXPECT_EQ(ret, 1);
+    EXPECT_FALSE(buffer.complete());
+
+    ret = buffer.read(42);
+    EXPECT_EQ(ret, 9);
+    EXPECT_TRUE(buffer.complete());
+
+    auto& buf = buffer.getBuffer();
+    char* charbuf = reinterpret_cast<char*>(buf.begin());
+    EXPECT_STREQ(charbuf, EXPECTED);
+}
