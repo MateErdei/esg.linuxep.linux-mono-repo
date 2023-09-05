e97bf7504d7e8e648a57442e84db9694e9610c63
Douglas Leeder <douglas.leeder@sophos.com>
2023-09-05 13:07:49 +0100

    Remove credentials - use environment instead.

--- aws-runner/DeleteUnusedVolumes.py
+++ aws-runner/DeleteUnusedVolumes.py
@@ -7,10 +7,8 @@ import boto.ec2
 import os
 import sys
 
-
-ec2 = boto.ec2.connect_to_region("eu-west-1",
-        aws_access_key_id="AKIAWR523TF7XZPL2C7H",
-        aws_secret_access_key="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")
+# Use credentials from environment
+ec2 = boto.ec2.connect_to_region("eu-west-1")
 
 volumes = ec2.get_all_volumes(filters={
     "status": "available"
