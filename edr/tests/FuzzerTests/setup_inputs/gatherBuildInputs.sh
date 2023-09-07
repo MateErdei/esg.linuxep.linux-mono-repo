3a2de7643d45a5dee94f125082ba8bc4f061844b
Peter.C <peterc@sophos.com>
2023-09-07 16:00:47 +0100

    edr-fuzzer-fix/gatherbuildinputs now can choose its own mode.

--- edr/tests/FuzzerTests/setup_inputs/gatherBuildInputs.sh
+++ edr/tests/FuzzerTests/setup_inputs/gatherBuildInputs.sh
@@ -7,5 +7,5 @@ source /tmp/venv-for-ci/bin/activate
   #fix for detached head
   git branch temp
   git checkout temp
-  python3 -m build_scripts.artisan_fetch $WORKSPACE/edr/build-files/release-package.xml
+  python3 -m build_scripts.artisan_fetch -m "independent" $WORKSPACE/edr/build-files/release-package.xml
 deactivate
