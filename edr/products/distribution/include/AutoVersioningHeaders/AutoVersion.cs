c8bacb6cff0d0da645ac25afcd6f640652ad48f8
Douglas Leeder <douglas.leeder@sophos.com>
2023-09-07 14:48:47 +0100

    [LINUXDAR-6350] Add default autoversion headers.

---
+++ edr/products/distribution/include/AutoVersioningHeaders/AutoVersion.cs
@@ -0,0 +1,13 @@
+// ********************************************************
+// WARNING: This file gets overwritten by the build system
+// ********************************************************
+
+
+using System.Reflection;
+
+
+[assembly: AssemblyVersion("1.1.10")]
+[assembly: AssemblyFileVersion("1.1.10.9999")]
+[assembly: AssemblyCompany("Sophos Limited")]
+[assembly: AssemblyCopyright("Copyright 1989-2023 Sophos Limited. All rights reserved.")]
+[assembly: AssemblyConfiguration("LocalBuild")]
---
+++ edr/products/distribution/include/AutoVersioningHeaders/AutoVersion.h
@@ -0,0 +1,68 @@
+//{{NO_DEPENDENCIES}}
+/********************************************************
+WARNING: This file gets overwritten by the build system
+********************************************************/
+
+
+#ifndef __AUTOVER__HEADER_H__
+#define __AUTOVER__HEADER_H__
+
+
+/* Company name */
+#define _AUTOVER_COMPANYNAME_                           "Sophos Limited"
+
+/* Copyright statement */
+#define _AUTOVER_COPYRIGHT_                             "Copyright 1989-2023 Sophos Limited. All rights reserved."
+
+/* Changelist number */
+#define _AUTOVER_CHANGELISTNUM_                         LocalBuild
+#define _AUTOVER_CHANGELISTNUM_STR_                     "LocalBuild"
+
+/* Build reference number */
+#define _AUTOVER_BUILDREFNUM_                           20190101000000-LocalBuild
+#define _AUTOVER_BUILDREFNUM_STR_                       "20190101000000-LocalBuild"
+
+/* Short component version */
+#define _AUTOVER_COMPONENTVERSION_                       1.1.10
+#define _AUTOVER_COMPONENTVERSION_STR_                   "1.1.10"
+#define _AUTOVER_COMPONENTVERSION_COMMA_                 1,1,10
+#define _AUTOVER_COMPONENTVERSION_COMMA_STR_             "1,1,10"
+#define _AUTOVER_COMPONENTVERSION_P0                     1
+#define _AUTOVER_COMPONENTVERSION_P0_STR_                "1"
+#define _AUTOVER_COMPONENTVERSION_P1                     1
+#define _AUTOVER_COMPONENTVERSION_P1_STR_                "1"
+#define _AUTOVER_COMPONENTVERSION_P2                     10
+#define _AUTOVER_COMPONENTVERSION_P2_STR_                "10"
+
+/* Build number */
+#define _AUTOVER_COMPONENTBUILDNUM_                      9999
+#define _AUTOVER_COMPONENTBUILDNUM_STR_                  "9999"
+
+/* Long component version */
+#define _AUTOVER_COMPONENTAUTOVERSION_                   1.1.10.9999
+#define _AUTOVER_COMPONENTAUTOVERSION_STR_               "1.1.10.9999"
+#define _AUTOVER_COMPONENTAUTOVERSION_COMMA_             1,1,10,9999
+#define _AUTOVER_COMPONENTAUTOVERSION_COMMA_STR_         "1,1,10,9999"
+#define _AUTOVER_COMPONENTAUTOVERSION_P0                 1
+#define _AUTOVER_COMPONENTAUTOVERSION_P0_STR_            "1"
+#define _AUTOVER_COMPONENTAUTOVERSION_P1                 1
+#define _AUTOVER_COMPONENTAUTOVERSION_P1_STR_            "1"
+#define _AUTOVER_COMPONENTAUTOVERSION_P2                 10
+#define _AUTOVER_COMPONENTAUTOVERSION_P2_STR_            "10"
+#define _AUTOVER_COMPONENTAUTOVERSION_P3                 9999
+#define _AUTOVER_COMPONENTAUTOVERSION_P3_STR_            "9999"
+
+/* Component marketing version */
+#define _AUTOVER_COMPONENTMARKETINGVERSION_               1.1.10
+#define _AUTOVER_COMPONENTMARKETINGVERSION_STR_           "1.1.10"
+#define _AUTOVER_COMPONENTMARKETINGVERSION_COMMA_         1,1,10
+#define _AUTOVER_COMPONENTMARKETINGVERSION_COMMA_STR_     "1,1,10"
+#define _AUTOVER_COMPONENTMARKETINGVERSION_P0             1
+#define _AUTOVER_COMPONENTMARKETINGVERSION_P0_STR_        "1"
+#define _AUTOVER_COMPONENTMARKETINGVERSION_P1             1
+#define _AUTOVER_COMPONENTMARKETINGVERSION_P1_STR_        "1"
+#define _AUTOVER_COMPONENTMARKETINGVERSION_P2             10
+#define _AUTOVER_COMPONENTMARKETINGVERSION_P2_STR_        "10"
+
+
+#endif //__AUTOVER__HEADER_H__
