6ce25026701617e8ae40fcfe30887dfc4f11dc76
MollieSophos <mollie.bourke@sophos.com>
2023-09-20 16:39:28 +0100

    [thininstaller] Bugfix/linuxdar 8031 compatibility tool improvements (#217)
    * [LINUXDAR-8031] Remove checks for CDN via message relays
    
    * [LINUXDAR-8031] Make logging consistent throughout the compatibility checks and add summary message
    
    * [LINUXDAR-8031] Get variable after grep is confirmed to be available on the system
    
    * [LINUXDAR-8031] Only verify connections when cdn url is available
    
    * [LINUXDAR-8031] log fix
    
    * [LINUXDAR-8031] Add network connection checks for configured proxies
    
    * [LINUXDAR-8031] Ascii check wasn't accepting hyphens
    
    * [LINUXDAR-8031] Typos
    
    * [LINUXDAR-8031] Make error logging consistent
    
    * [LINUXDAR-8031] Tidyup network check logging for consistency
    
    * [LINUXDAR-8031] Change unit
    
    * [LINUXDAR-8031] Use TMPDIR for checking temp dir exec status
    
    * [LINUXDAR-8031] tmpdir needed for compatibility tool
    
    * [LINUXDAR-6458] Don't print stdout for exectest
    
    * [LINUXDAR-8031] CDN address wasn't being passed through
    
    * [LINUXDAR-8049] Fix custom installs

--- thininstaller/SPLCompatibilityChecks.in.sh
+++ thininstaller/SPLCompatibilityChecks.in.sh
@@ -3,97 +3,101 @@ PRODUCT_NAME="SPL Compatibility Tool"
 VERSION=1.0.0
 
 EXIT_SUCCESS=0
-EXITCODE_INVALID_ARG=1
-EXITCODE_BAD_INSTALL_PATH=2
-EXITCODE_NOT_LINUX=3
-EXITCODE_NOT_64_BIT=4
-EXITCODE_NO_SYSTEMD=5
-EXIT_FAIL_COULD_NOT_FIND_LIBC_VERSION=6
-EXIT_FAIL_WRONG_LIBC_VERSION=7
-EXITCODE_NOEXEC_TMP=8
-EXITCODE_MISSING_PACKAGE=9
-EXITCODE_NOT_ENOUGH_SPACE=10
-EXITCODE_NOT_ENOUGH_MEM=11
-EXITCODE_INVALID_CA_PATHS=12
+EXIT_FAILURE=1
+EXIT_WARNING=2
+EXITCODE_INVALID_ARG=3
+EXITCODE_BAD_INSTALL_PATH=4
+EXITCODE_NOT_LINUX=5
+EXITCODE_NOT_64_BIT=6
+EXITCODE_NO_SYSTEMD=7
+EXIT_FAIL_COULD_NOT_FIND_LIBC_VERSION=8
+EXIT_FAIL_WRONG_LIBC_VERSION=9
+EXITCODE_NOEXEC_TMP=10
+EXITCODE_MISSING_PACKAGE=11
+EXITCODE_NOT_ENOUGH_SPACE=12
+EXITCODE_NOT_ENOUGH_MEM=13
+EXITCODE_INVALID_CA_PATHS=14
 
 @COMMON_SPL_COMPATIBILITY_FUNCTIONS@
 
 function failure() {
-  local code=$1
-  shift
-  echo "$@" >&2
-  exit "${code}"
+    local code=$1
+    shift
+    echo "$@" >&2
+    exit "${code}"
 }
 
 # Copied from installer_header - decided not to make common since the ThinInstaller has the option to uninstall SAV
 function check_SAV_installed() {
-  local path=$1
-  local sav_instdir=$(readlink ${path} | sed 's/bin\/savscan//g')
-  if [[ "$sav_instdir" == "" ]] || [[ ! -d ${sav_instdir} ]]; then
-    # Sophos Anti-Virus not found at this location
-    return
-  fi
-  echo "WARN: Found an existing installation of Sophos Anti-Virus in $sav_instdir."
-  echo "WARN: This product cannot be run alongside Sophos Anti-Virus. Please uninstall Sophos Anti-Virus or run the SPL installer with --uninstall-sav"
+    local path=$1
+    local sav_instdir=$(readlink ${path} | sed 's/bin\/savscan//g')
+    if [[ "${sav_instdir}" == "" ]] || [[ ! -d ${sav_instdir} ]]; then
+        # Sophos Anti-Virus not found at this location
+        return
+    fi
+    echo "WARN: Found an existing installation of Sophos Anti-Virus in ${sav_instdir}"
+    echo "WARN: This product cannot be run alongside Sophos Anti-Virus. Please uninstall Sophos Anti-Virus or run the SPL installer with --uninstall-sav"
 }
 
 function print_help_text() {
-  echo "${PRODUCT_NAME}, help:"
-  echo "Version: ${VERSION}"
-  echo "Usage: [options]"
-  echo "Valid options are:"
-  echo -e "--help [-h]\t\t\tDisplay this summary"
-  echo -e "--version [-v]\t\t\tDisplay version of this tool"
-  echo -e "--debug [-d]\t\t\tLog debug information"
-  echo -e "--installer-path=<path>\t\t\tPath to SophosSetup.sh"
-  echo -e "--install-dir=<path>\t\t\tPath to desired SPL install location, default is /opt/sophos-spl"
+    echo "${PRODUCT_NAME}, help:"
+    echo "Version: ${VERSION}"
+    echo "Usage: [options]"
+    echo "Valid options are:"
+    echo -e "--help [-h]\t\t\tDisplay this summary"
+    echo -e "--version [-v]\t\t\tDisplay version of this tool"
+    echo -e "--debug [-d]\t\t\tLog debug information"
+    echo -e "--installer-path=<path>\t\t\tPath to SophosSetup.sh"
+    echo -e "--install-dir=<path>\t\t\tPath to desired SPL install location, default is /opt/sophos-spl"
 
-  echo -e "\nExit codes:"
-  echo -e "\t- SUCCESS=0"
-  echo -e "\t- INVALID_ARG=1"
-  echo -e "\t- BAD_INSTALL_PATH=2"
-  echo -e "\t- NOT_LINUX=3"
-  echo -e "\t- NOT_64_BIT=4"
-  echo -e "\t- NO_SYSTEMD=5"
-  echo -e "\t- COULD_NOT_FIND_LIBC_VERSION=6"
-  echo -e "\t- WRONG_LIBC_VERSION=7"
-  echo -e "\t- NOEXEC_TMP=8"
-  echo -e "\t- MISSING_PACKAGE=9"
-  echo -e "\t- NOT_ENOUGH_SPACE=10"
-  echo -e "\t- NOT_ENOUGH_MEM=11"
-  echo -e "\t- INVALID_CA_PATHS=12"
+    echo -e "\nExit codes:"
+    echo -e "\t- EXIT_SUCCESS=0"
+    echo -e "\t- EXIT_FAILURE=1"
+    echo -e "\t- EXIT_WARNING=2"
+    echo -e "\t- EXITCODE_INVALID_ARG=3"
+    echo -e "\t- EXITCODE_BAD_INSTALL_PATH=4"
+    echo -e "\t- EXITCODE_NOT_LINUX=5"
+    echo -e "\t- EXITCODE_NOT_64_BIT=6"
+    echo -e "\t- EXITCODE_NO_SYSTEMD=7"
+    echo -e "\t- EXIT_FAIL_COULD_NOT_FIND_LIBC_VERSION=8"
+    echo -e "\t- EXIT_FAIL_WRONG_LIBC_VERSION=9"
+    echo -e "\t- EXITCODE_NOEXEC_TMP=10"
+    echo -e "\t- EXITCODE_MISSING_PACKAGE=11"
+    echo -e "\t- EXITCODE_NOT_ENOUGH_SPACE=12"
+    echo -e "\t- EXITCODE_NOT_ENOUGH_MEM=13"
+    echo -e "\t- EXITCODE_INVALID_CA_PATHS=14"
 }
 
 echo "This software is governed by the terms and conditions of a licence agreement with Sophos Limited"
 echo "This script performs pre-installation checks for Sophos Protection for Linus (SPL)"
 echo -e "To view the minimum requirements for SPL, please refer to the release notes: https://docs.sophos.com/releasenotes/output/en-us/esg/linux_protection_rn.html\n"
 for arg in "$@"; do
-  case "${arg}" in
-  --help | -h)
-    print_help_text
-    exit ${EXIT_SUCCESS}
-    ;;
-  --version | -v)
-    echo "${PRODUCT_NAME}, version: ${VERSION}"
-    exit ${EXIT_SUCCESS}
-    ;;
-  --debug | -d)
-    DEBUG_THIN_INSTALLER=1
-    ;;
-  --installer-path=*)
-    THIN_INSTALLER_PATH="${arg#*=}"
-    CENTRAL_URL=$(sed -n -e "/^URL=/ s/.*\= *//p" "${THIN_INSTALLER_PATH}")
-    ;;
-  --install-dir=*)
-    SOPHOS_INSTALL="${arg#*=}"
-    verify_install_directory
-    ;;
-  *)
-    echo "Unrecognised argument: ${arg}"
-    print_help_text
-    exit ${EXITCODE_INVALID_ARG}
-    ;;
-  esac
+    case "${arg}" in
+    --help | -h)
+        print_help_text
+        exit ${EXIT_SUCCESS}
+        ;;
+    --version | -v)
+        echo "${PRODUCT_NAME}, version: ${VERSION}"
+        exit ${EXIT_SUCCESS}
+        ;;
+    --debug | -d)
+        DEBUG_THIN_INSTALLER=1
+        ;;
+    --installer-path=*)
+        THIN_INSTALLER_PATH="${arg#*=}"
+        CENTRAL_URL=$(sed -n -e "/^URL=/ s/.*\= *//p" "${THIN_INSTALLER_PATH}")
+        ;;
+    --install-dir=*)
+        SOPHOS_INSTALL="${arg#*=}"
+        verify_install_directory
+        ;;
+    *)
+        echo "Unrecognised argument: ${arg}"
+        print_help_text
+        exit ${EXITCODE_INVALID_ARG}
+        ;;
+    esac
 done
 
 verify_system_requirements
@@ -108,9 +112,9 @@ check_SAV_installed '/usr/bin/sweep'
 
 # Set default central URL when ThinInstaller path is not provided
 if [[ -z "${CENTRAL_URL}" ]]; then
-  CENTRAL_URL="https://mcs2-cloudstation-eu-west-1.prod.hydra.sophos.com/sophos/management/ep"
-  echo "INFO: Failed to get Central URL from the SPL installer, using default: ${CENTRAL_URL}"
-  echo "WARN: This Central URL may be incorrect depending on the region of the server"
+    CENTRAL_URL="https://mcs2-cloudstation-eu-west-1.prod.hydra.sophos.com/sophos/management/ep"
+    echo "INFO: Failed to get Central URL from the SPL installer, using default: ${CENTRAL_URL}"
+    echo "WARN: This Central URL may be incorrect depending on the region of the server"
 fi
 
 # Extract Update Caches from ThinInstaller
@@ -118,19 +122,28 @@ declare -a MESSAGE_RELAYS
 declare -a UPDATE_CACHES
 
 if [[ -n "${THIN_INSTALLER_PATH}" ]]; then
-  IFS=';' read -ra message_relay_array <<<"$(sed -n -e "/^MESSAGE_RELAYS=[^$]/ s/.*\= *//p" "${THIN_INSTALLER_PATH}")"
-  for message_relay in "${message_relay_array[@]}"; do
-    MESSAGE_RELAYS+=("${message_relay%%,*}")
-  done
+    IFS=';' read -ra message_relay_array <<<"$(sed -n -e "/^MESSAGE_RELAYS=[^$]/ s/.*\= *//p" "${THIN_INSTALLER_PATH}")"
+    for message_relay in "${message_relay_array[@]}"; do
+        MESSAGE_RELAYS+=("${message_relay%%,*}")
+    done
 
-  IFS=';' read -ra update_cache_array <<<"$(sed -n -e "/^UPDATE_CACHES=[^$]/ s/.*\= *//p" "${THIN_INSTALLER_PATH}")"
-  for update_cache in "${update_cache_array[@]}"; do
-    UPDATE_CACHES+=("${update_cache%%,*}")
-  done
+    IFS=';' read -ra update_cache_array <<<"$(sed -n -e "/^UPDATE_CACHES=[^$]/ s/.*\= *//p" "${THIN_INSTALLER_PATH}")"
+    for update_cache in "${update_cache_array[@]}"; do
+        UPDATE_CACHES+=("${update_cache%%,*}")
+    done
 else
-  echo "WARN: Path to SophosSetup.sh not provided so network checks will not be done using message relays or update caches"
+    echo "WARN: Path to SophosSetup.sh not provided so network checks will not be done using message relays or update caches"
 fi
 
 verify_network_connections "${MESSAGE_RELAYS[*]}" "${UPDATE_CACHES[*]}"
 
-exit "${EXIT_SUCCESS}"
+if [[ -n "${COMPATIBILITY_ERROR_FOUND}" ]]; then
+    echo "SPL cannot be installed on this system, the pre-installation checks found some critical issues. Please review the logs and address the issues before attempting to reinstall"
+    exit "${EXIT_FAILURE}"
+elif [[ -n "${COMPATIBILITY_WARNING_FOUND}" ]]; then
+    echo "SPL can be installed on this system, however, the pre-installation checks found some non-critical issues. Please review and address these"
+    exit "${EXIT_WARNING}"
+else
+    echo "SPL can be installed on this system based on the pre-installation checks"
+    exit "${EXIT_SUCCESS}"
+fi
