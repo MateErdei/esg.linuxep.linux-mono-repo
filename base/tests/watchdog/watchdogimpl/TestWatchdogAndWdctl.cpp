27d58f8eed6eab73db3637839fa102c2d9d0f152
Douglas Leeder <douglas.leeder@sophos.com>
2023-09-21 11:17:15 +0100

    [LINUXDAR-8108] Fix up unit tests for changes to get username before fork.

--- base/tests/watchdog/watchdogimpl/TestWatchdogAndWdctl.cpp
+++ base/tests/watchdog/watchdogimpl/TestWatchdogAndWdctl.cpp
@@ -98,136 +98,148 @@ std::string templateFakePlugin(const std::string&  installdir)
     } );
 }
 
-
-class TestWatchdogAndWdctl  : public LogInitializedTests
+namespace
 {
-public:
-    static void SetUpTestCase();
-    static void TearDownTestCase();
-    static std::mutex ensureNotInParallel;
-    static std::unique_ptr<Tests::TempDir> tempDir;
-    static std::string installDir()
+    class TestWatchdogAndWdctl : public LogInitializedTests
     {
-        return tempDir->dirPath();
-    }
-    TestWatchdogAndWdctl():
-        stopArgs{ {"wdctl", "stop", "fakeplugin"}},
-        isRunningArgs{ {"wdctl", "isrunning", "fakeplugin"}}
-    {
+    public:
+        static void SetUpTestCase();
 
-    }
-    void waitPluginStarted()
-    {
-        for( int i =0; i<10; i++)
+        static void TearDownTestCase();
+
+        static std::mutex ensureNotInParallel;
+        static std::unique_ptr<Tests::TempDir> tempDir;
+
+        static std::string installDir()
         {
-            if( wdctl.main_afterLogConfigured(isRunningArgs)==0)
-            {
-                break;
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            return tempDir->dirPath();
         }
-    }
-    std::vector<std::string> stopArgs;
-    std::vector<std::string> isRunningArgs;
-    wdctl::wdctlimpl::wdctl_bootstrap wdctl;
-    ScopedFilePermission scopedFilePermission;
-};
-static void *watchdogmain(void * )
-{
-    static int result =0;
-    watchdog::watchdogimpl::Watchdog m;
-    result =  m.initialiseAndRun();
-    return &result;
-}
 
-class WatchdogWdctlRunner
-{
-public:
-    WatchdogWdctlRunner():m_thread_id{}
-    {
-        state = 0;
-    }
-    void start()
-    {
-        state = 1;
-        int s = pthread_create(&m_thread_id, NULL, watchdogmain, NULL);
-        ASSERT_EQ(0, s);
-    }
-
-    int stop()
-    {
-        pthread_kill(m_thread_id, SIGTERM);
-        void* res;
-        pthread_join(m_thread_id, &res);
-        int result = (*static_cast<int*>(res));
-        state = 2;
-        return result;
-    }
-    ~WatchdogWdctlRunner()
-    {
-        try
+        TestWatchdogAndWdctl() :
+                stopArgs{{"wdctl", "stop", "fakeplugin"}},
+                isRunningArgs{{"wdctl", "isrunning", "fakeplugin"}}
         {
-            if ( state == 1)
+
+        }
+
+        void waitPluginStarted()
+        {
+            for (int i = 0; i < 10; i++)
             {
-                stop();
+                if (wdctl.main_afterLogConfigured(isRunningArgs) == 0)
+                {
+                    break;
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
             }
         }
-        catch (std::exception & ex)
+
+        std::vector<std::string> stopArgs;
+        std::vector<std::string> isRunningArgs;
+        wdctl::wdctlimpl::wdctl_bootstrap wdctl;
+        ScopedFilePermission scopedFilePermission;
+    };
+
+    static void* watchdogmain(void*)
+    {
+        static int result = 0;
+        watchdog::watchdogimpl::Watchdog m;
+        result = m.initialiseAndRun();
+        return &result;
+    }
+
+    class WatchdogWdctlRunner
+    {
+    public:
+        WatchdogWdctlRunner() : m_thread_id{}
         {
-            std::cerr << ex.what() << std::endl;
+            state = 0;
         }
 
-    }
+        void start()
+        {
+            state = 1;
+            int s = pthread_create(&m_thread_id, NULL, watchdogmain, NULL);
+            ASSERT_EQ(0, s);
+        }
+
+        int stop()
+        {
+            pthread_kill(m_thread_id, SIGTERM);
+            void* res;
+            pthread_join(m_thread_id, &res);
+            int result = (*static_cast<int*>(res));
+            state = 2;
+            return result;
+        }
+
+        ~WatchdogWdctlRunner()
+        {
+            try
+            {
+                if (state == 1)
+                {
+                    stop();
+                }
+            }
+            catch (std::exception& ex)
+            {
+                std::cerr << ex.what() << std::endl;
+            }
+
+        }
+
     private:
         pthread_t m_thread_id;
         int state;
-};
+    };
 
 
-std::unique_ptr<Tests::TempDir> TestWatchdogAndWdctl::tempDir;
-std::mutex TestWatchdogAndWdctl::ensureNotInParallel;
-void TestWatchdogAndWdctl::SetUpTestCase()
-{
-    tempDir = Tests::TempDir::makeTempDir();
-    Common::ApplicationConfiguration::applicationConfiguration().setData(
-            Common::ApplicationConfiguration::SOPHOS_INSTALL, installDir());
-    Common::ApplicationConfiguration::applicationConfiguration().setData("watchdog.ipc", "ipc://" + installDir()+"/watchdog.ipc");
-    tempDir->createFile("base/etc/logger.conf", "VERBOSITY=DEBUG\n");
-    tempDir->createFile("base/pluginRegistry/fakeplugin.json", templateFakePlugin(installDir()));
-    std::vector<std::string> relativePaths{ {"var/ipc/plugins"}};
-    tempDir->makeDirs(relativePaths);
+    std::unique_ptr<Tests::TempDir> TestWatchdogAndWdctl::tempDir;
+    std::mutex TestWatchdogAndWdctl::ensureNotInParallel;
+
+    void TestWatchdogAndWdctl::SetUpTestCase()
+    {
+        tempDir = Tests::TempDir::makeTempDir();
+        Common::ApplicationConfiguration::applicationConfiguration().setData(
+                Common::ApplicationConfiguration::SOPHOS_INSTALL, installDir());
+        Common::ApplicationConfiguration::applicationConfiguration().setData("watchdog.ipc", "ipc://" + installDir()+"/watchdog.ipc");
+        tempDir->createFile("base/etc/logger.conf", "VERBOSITY=DEBUG\n");
+        tempDir->createFile("base/pluginRegistry/fakeplugin.json", templateFakePlugin(installDir()));
+        std::vector<std::string> relativePaths{ {"var/ipc/plugins"}};
+        tempDir->makeDirs(relativePaths);
+    }
+
+    void TestWatchdogAndWdctl::TearDownTestCase()
+    {
+        tempDir.reset(nullptr);
+    }
+
 }
-
-void TestWatchdogAndWdctl::TearDownTestCase()
-{
-    tempDir.reset(nullptr);
-}
-
 TEST_F(TestWatchdogAndWdctl, WdctlIssuesStopToWatchdog)
 {
+    // Test uses real user on the machine to start process, so need to make sure the correct user and group ids are obtained.
+    // for current user
+    uid_t userId = ::getuid();
+    gid_t  groupId = ::getgid();
+    auto* realFilePermissions = Common::FileSystem::filePermissions();
+    auto username = realFilePermissions->getUserName(userId);
+
+    auto mockFilePermissions = std::make_unique<StrictMock<MockFilePermissions>>();
+    EXPECT_CALL(*mockFilePermissions, getUserId(_)).WillRepeatedly(Return(userId));
+    EXPECT_CALL(*mockFilePermissions, getGroupId(_)).WillRepeatedly(Return(groupId));
+    EXPECT_CALL(*mockFilePermissions, getUserName(userId)).WillRepeatedly(Return(username));
+    std::pair userAndGroup = std::make_pair(userId,groupId);
+    EXPECT_CALL(*mockFilePermissions, getUserAndGroupId(_)).WillRepeatedly(Return(userAndGroup));
+    EXPECT_CALL(*mockFilePermissions, chmod(_, _)).WillRepeatedly(Return());
+    EXPECT_CALL(*mockFilePermissions, getFilePermissions(_)).WillOnce(Return(0600));
+    EXPECT_CALL(*mockFilePermissions, chown(A<const Path&>(), A<const std::string&>(), A<const std::string&>())).WillRepeatedly(Return());
+
+    WatchdogWdctlRunner watchdogRunner;
     {
-        // Test uses real user on the machine to start process, so need to make sure the correct user and group ids are obtained.
-        // for current user
-        uid_t userId = -1;
-        gid_t  groupId = -1;
-        userId = ::getuid();
-        groupId = ::getgid();
-
-        auto mockFilePermissions = new StrictMock<MockFilePermissions>();
-        std::unique_ptr<MockFilePermissions> mockIFilePermissionsPtr =
-            std::unique_ptr<MockFilePermissions>(mockFilePermissions);
-        Tests::replaceFilePermissions(std::move(mockIFilePermissionsPtr));
-        EXPECT_CALL(*mockFilePermissions, getUserId(_)).WillRepeatedly(Return(userId));
-        EXPECT_CALL(*mockFilePermissions, getGroupId(_)).WillRepeatedly(Return(groupId));
-        std::pair userAndGroup = std::make_pair(userId,groupId);
-        EXPECT_CALL(*mockFilePermissions, getUserAndGroupId(_)).WillRepeatedly(Return(userAndGroup));
-        EXPECT_CALL(*mockFilePermissions, chmod(_, _)).WillRepeatedly(Return());
-        EXPECT_CALL(*mockFilePermissions, getFilePermissions(_)).WillOnce(Return(0600));
-        EXPECT_CALL(*mockFilePermissions, chown(A<const Path&>(), A<const std::string&>(), A<const std::string&>())).WillRepeatedly(Return());
-
         std::unique_lock<std::mutex> lock{ensureNotInParallel};
+        Tests::replaceFilePermissions(std::move(mockFilePermissions));
         testing::internal::CaptureStderr();
-        WatchdogWdctlRunner watchdogRunner;
         watchdogRunner.start();
         waitPluginStarted();
         int retValue = wdctl.main_afterLogConfigured(stopArgs, false);
@@ -238,8 +250,7 @@ TEST_F(TestWatchdogAndWdctl, WdctlIssuesStopToWatchdog)
             // this will display the value returned and well as all the
             // log messages on failure.
             EXPECT_EQ(retValue, 0) << logMessage;
-        }
-        else
+        } else
         {
             EXPECT_THAT(logMessage, ::testing::HasSubstr("stop fakeplugin"));
         }
@@ -248,31 +259,30 @@ TEST_F(TestWatchdogAndWdctl, WdctlIssuesStopToWatchdog)
     }
 }
 
-TEST_F(TestWatchdogAndWdctl, WdctlIsRunningDetectCanDetectStatusOfPlugins) // NOLINT
+TEST_F(TestWatchdogAndWdctl, WdctlIsRunningDetectCanDetectStatusOfPlugins)
 {
+    // Test uses real user on the machine to start process, so need to make sure the correct user and group ids are obtained.
+    // for current user
+
+    uid_t userId = ::getuid();
+    gid_t  groupId = ::getgid();
+    auto* realFilePermissions = Common::FileSystem::filePermissions();
+    auto username = realFilePermissions->getUserName(userId);
+
+    auto mockFilePermissions = std::make_unique<StrictMock<MockFilePermissions>>();
+    EXPECT_CALL(*mockFilePermissions, getUserId(_)).WillRepeatedly(Return(userId));
+    EXPECT_CALL(*mockFilePermissions, getGroupId(_)).WillRepeatedly(Return(groupId));
+    EXPECT_CALL(*mockFilePermissions, getUserName(userId)).WillRepeatedly(Return(username));
+    auto userAndGroup = std::make_pair(userId,groupId);
+    EXPECT_CALL(*mockFilePermissions, getUserAndGroupId(_)).WillRepeatedly(Return(userAndGroup));
+    EXPECT_CALL(*mockFilePermissions, chmod(_, _)).WillRepeatedly(Return());
+    EXPECT_CALL(*mockFilePermissions, getFilePermissions(_)).WillOnce(Return(0600));
+    EXPECT_CALL(*mockFilePermissions, chown(A<const Path&>(), A<const std::string&>(), A<const std::string&>())).WillRepeatedly(Return());
+
+    WatchdogWdctlRunner watchdogRunner;
     {
-        // Test uses real user on the machine to start process, so need to make sure the correct user and group ids are obtained.
-        // for current user
-
-        uid_t userId = -1;
-        gid_t  groupId = -1;
-        userId = ::getuid();
-        groupId = ::getgid();
-
-        auto mockFilePermissions = new StrictMock<MockFilePermissions>();
-        std::unique_ptr<MockFilePermissions> mockIFilePermissionsPtr =
-            std::unique_ptr<MockFilePermissions>(mockFilePermissions);
-        Tests::replaceFilePermissions(std::move(mockIFilePermissionsPtr));
-        EXPECT_CALL(*mockFilePermissions, getUserId(_)).WillRepeatedly(Return(userId));
-        EXPECT_CALL(*mockFilePermissions, getGroupId(_)).WillRepeatedly(Return(groupId));
-        std::pair userAndGroup = std::make_pair(userId,groupId);
-        EXPECT_CALL(*mockFilePermissions, getUserAndGroupId(_)).WillRepeatedly(Return(userAndGroup));
-        EXPECT_CALL(*mockFilePermissions, chmod(_, _)).WillRepeatedly(Return());
-        EXPECT_CALL(*mockFilePermissions, getFilePermissions(_)).WillOnce(Return(0600));
-        EXPECT_CALL(*mockFilePermissions, chown(A<const Path&>(), A<const std::string&>(), A<const std::string&>())).WillRepeatedly(Return());
-
         std::unique_lock<std::mutex> lock{ensureNotInParallel};
-        WatchdogWdctlRunner watchdogRunner;
+        Tests::replaceFilePermissions(std::move(mockFilePermissions));
         watchdogRunner.start();
         waitPluginStarted();
 
