0e1fa4eb19e4b0209e65583be3108fb283c0033d
Menaga K <107470038+MenagaSophos@users.noreply.github.com>
2023-10-25 17:27:15 +0100

    [TESTFIX] try making ignoreSignals sig handler closer to product sig … (#223)
    * [TESTFIX] try making ignoreSignals sig handler closer to product sig handler as that does not crash on arm systems
    
    * [TESTFIX] initialise sigaction
    
    * [TESTFIX] formatting

--- base/tests/manualTools/ignoreSignals.cpp
+++ base/tests/manualTools/ignoreSignals.cpp
@@ -5,13 +5,16 @@ void s_signal_handler(int /*s*/)
 {
     return;
 }
+
 int main(void) {
-    struct sigaction action; // NOLINT
+    struct sigaction action {};
     action.sa_handler = s_signal_handler;
+    action.sa_flags = SA_RESTART;
+    sigemptyset(&action.sa_mask);
     sigaction(SIGTERM, &action, nullptr);
 
-    while (true) {
-
+    while (true)
+    {
         sleep(10);
     }
 }
