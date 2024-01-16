d3dc8a1854c57833cf236323a2d9d61cd89962f9
PeteJClark <peter.c@sophos.com>
2024-01-16 14:07:19 +0000

    Fuzzers/adjustments to output check (#502)
    * fuzzer - adjustments to output
    
    * fuzzer - ddint include directory in ls
    
    * fuzzer - test timing
    
    * fuzzerfix - doing is empty check on string instead of is not empty
    
    * fuzzer - ls was wrong
    
    * Revert "fuzzer - test timing"
    
    This reverts commit 5f2964dbceb14db7522afaa4b006adeb660d8f86.
    
    ---------
    
    Co-authored-by: Peter.C <peterc@sophos.com>

--- common/fuzzer/fuzzer_support/check_fuzzer_output.sh
+++ common/fuzzer/fuzzer_support/check_fuzzer_output.sh
@@ -7,9 +7,11 @@ ARTIFACT_OUTPUT="${TEST_DIRECTORY}artifacts/"
 
 #check if any artifacts exist
 [[ -d $ARTIFACT_OUTPUT ]] || exit 1
-[[ $(find "$ARTIFACT_OUTPUT" -type f | wc -l) == 0 ]] || exit 1
+[[ $(find "$ARTIFACT_OUTPUT" -type f | wc -l) == 0 ]] || { (ls "$ARTIFACT_OUTPUT"); exit 1; }
 
 #check if fuzzer ran
-[[ -f $FUZZ_LOG ]] || exit 2
+[[ -f $FUZZ_LOG ]] || { echo "no fuzz log"; exit 2; }
 LINE=$(grep -E "stat::average_exec_per_sec: " $FUZZ_LOG)
-[[ $(echo "$LINE" | grep -E -o  "[0-9]+") != 0 ]] || exit 2
+[[ -n $LINE ]] || { echo "fuzzer exited early"; exit 3;  }
+[[ $(echo "$LINE" | grep -E -o  "[0-9]+") != 0 ]] || { echo "average_exec_per_sec is 0"; exit 4; }
+echo "$LINE"
