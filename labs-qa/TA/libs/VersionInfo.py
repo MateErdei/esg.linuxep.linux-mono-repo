ede6432fdf152911a422f6f0bf72e4047808ecbb
Andras Petri <Andras.Petri@sophos.com>
2024-02-28 11:33:48 +0100

    added querying VDB and engine versions

---
+++ labs-qa/TA/libs/VersionInfo.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2024 Sophos Plc, Oxford, England.
+# All rights reserved.
+
+
+import json
+import re
+from robot.api import logger
+
+
+THREAT_DETECTOR_LOG = "/opt/sophos-spl/plugins/av/chroot/log/sophos_threat_detector.log"
+VERSIONS_LINE_RE = r"SUSI Libraries loaded: (\{.*?\})"
+
+
+def get_vdb_and_engine_versions(logfile = THREAT_DETECTOR_LOG):
+    """
+    Scan the threat detector log for lines that contain version numbers.
+
+    :param logfile: Name of the log file to scan.
+    :return: A dict with keys 'Engine' and 'VDB', and version numbers
+             (or "UNKNOWN") as values.
+    """
+    versions = {'Engine': "UNKNOWN", 'VDB': "UNKNOWN"}
+
+    try:
+        with open(logfile, 'r') as log:
+            for line in log:
+                m = re.search(VERSIONS_LINE_RE, line)
+                if m:
+                    maybe_json = m.group(0)
+                    try:
+                        json_struct = json.loads(maybe_json)
+                    except JSONDecodeError:
+                        continue
+                        
+                    if json_struct:
+                        if 'savi' in json_struct:
+                            versions['Engine'] = json_struct['savi']
+                        if 'vdb' in json_struct:
+                            versions['VDB'] = json_struct['vdb']
+    except Exception:
+        logger.warning("Error: could not read " + logfile)
+        
+    return versions
--- labs-qa/TA/robot/hello_world.robot
+++ labs-qa/TA/robot/hello_world.robot
@@ -1,10 +1,14 @@
 *** Settings ***
 Library   BuiltIn
 Library   ${COMMON_TEST_LIBS}/IniUtils.py
+Library   ../VersionInfo.py
 
 
 *** Test Cases ***
 Example Test
     Log To Console  Hello, world!
     ${basever}=  Get Version Number From INI File   /opt/sophos-spl/base/VERSION.ini
-    Log To Console  Base version: ${basever}
+    Log To Console  Product base version: ${basever}
+    &{versions}=  Get VDB and Engine Versions
+    Log To Console  VDB version: ${versions}[VDB]
+    Log To Console  Engine version: ${versions}[Engine]
