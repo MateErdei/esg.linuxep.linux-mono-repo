11ad8325375b6834a61b9dc5e91de9645836005d
Andras Petri <Andras.Petri@sophos.com>
2024-02-28 13:28:10 +0100

    reshaped version queying

--- labs-qa/TA/libs/VersionInfo.py
+++ labs-qa/TA/libs/VersionInfo.py
@@ -13,18 +13,40 @@ THREAT_DETECTOR_LOG = "/opt/sophos-spl/plugins/av/chroot/log/sophos_threat_detec
 VERSIONS_LINE_RE = r"SUSI Libraries loaded: (\{.*?\})$"
 
 
-def get_vdb_and_engine_versions(logfile = THREAT_DETECTOR_LOG):
+def get_susi_configuration(versions_to_return = None):
     """
     Scan the threat detector log for lines that contain version numbers.
 
-    :param logfile: Name of the log file to scan.
-    :return: A dict with keys 'Engine' and 'VDB', and version numbers
-             (or "UNKNOWN") as values.
+    :param :obj:`list` versions_to_return: A list of version data types. The
+                       function will return only the data types specified in
+                       this list. Unnown data types are ignored.
+                       if the list is empty, *all* known data types will be
+                       returned.
+    :return: A dict with version data types as keys and the corresponding
+             version numbers (or "UNKNOWN") as values.
     """
-    versions = {'Engine': "UNKNOWN", 'VDB': "UNKNOWN"}
+    # Keys: known data types
+    # Values: corresponding attributes in the "SUSI Libraries loaded:" JSON
+    #         data structure, or None    
+    attribute_map = (
+        'SFS_Data': "vdb",
+        'SFS_Engine': "savi",
+        'ML_Data': "mldata",
+        # 'ML_Telemetry_Data': None,
+        'Localrep_Data': "lrdata"
+    }
+    versions = {}
+
+    for data_type in versions_to_return:
+        if data_type in attribute_map:
+            versions[data_type] = "UNKNOWN"
+
+    if not versions:
+        for key in attribute_map.keys():
+            versions[key] = "UNKNOWN"
 
     try:
-        with open(logfile, 'r') as log:
+        with open(THREAT_DETECTOR_LOG, 'r') as log:
             for line in log:
                 m = re.search(VERSIONS_LINE_RE, line)
                 if m:
@@ -32,13 +54,14 @@ def get_vdb_and_engine_versions(logfile = THREAT_DETECTOR_LOG):
                     try:
                         json_struct = json.loads(maybe_json)
                     except json.decoder.JSONDecodeError:
+                        json_struct = None
                         continue
                         
                     if json_struct:
-                        if 'savi' in json_struct:
-                            versions['Engine'] = json_struct['savi']
-                        if 'vdb' in json_struct:
-                            versions['VDB'] = json_struct['vdb']
+                        for data_type in versions:
+                            json_attr = attribute_map[data_type]
+                            if json_attr in json_struct:
+                                versions[data_type] = json_struct[json_attr]
     except Exception:
         logger.warn("Error: could not read " + logfile)
         
--- labs-qa/TA/robot/hello_world.robot
+++ labs-qa/TA/robot/hello_world.robot
@@ -6,9 +6,12 @@ Library   ../libs/VersionInfo.py
 
 *** Test Cases ***
 Example Test
-    Log To Console  Hello, world!
-    ${basever}=  Get Version Number From INI File   /opt/sophos-spl/base/VERSION.ini
-    Log To Console  Product base version: ${basever}
-    &{versions}=  Get VDB and Engine Versions
-    Log To Console  VDB version: ${versions}[VDB]
-    Log To Console  Engine version: ${versions}[Engine]
+    Log To Console    Hello, world!
+    ${basever}=    Get Version Number From INI File    /opt/sophos-spl/base/VERSION.ini
+    Log To Console    Product base version: ${basever}
+    ${versions_to_present} =    Create List    SFS_Data    SFS_Engine    ML_Data
+    &{versions}=    Get SUSI Configuration    ${versions_to_present}
+    Log To Console    Product Configuration:
+    Log To Console    VDB version: ${versions}[SFS_Data]
+    Log To Console    Engine version: ${versions}[SFS_Engine]
+    Log To Console    ML data version: ${versions}[ML_Data]
